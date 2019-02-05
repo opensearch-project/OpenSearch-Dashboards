9c5d2c22ce67c76338449cc6f0215382f9587bbc
Marco Vettorello <vettorello.marco@gmail.com>
2019-02-05 18:43:11 +0100

    docs: add few different stories of barcharts with various linear values

--- packages/osd-charts/src/stories/bar_chart.tsx
+++ packages/osd-charts/src/stories/bar_chart.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Axis, BarSeries, Chart, getAxisId, getSpecId, Position, ScaleType, Settings } from '..';
 import * as TestDatasets from '../lib/series/utils/test_dataset';
 import { niceTimeFormatter } from '../utils/data/formatters';
+import { DataGenerator } from '../utils/data_generators/data_generator';
 
 storiesOf('Bar Chart', module)
   .add('basic', () => {
@@ -506,6 +507,146 @@ storiesOf('Bar Chart', module)
           yAccessors={['y1', 'y2']}
           splitSeriesAccessors={['g1', 'g2']}
           data={TestDatasets.BARCHART_2Y2G}
+          yScaleToDataExtent={false}
+        />
+      </Chart>
+    );
+  })
+  .add('with high data volume', () => {
+    const dg = new DataGenerator();
+    const data = dg.generateSimpleSeries(200);
+    return (
+      <Chart renderer="canvas" className={'story-chart'}>
+        <Axis id={getAxisId('bottom')} position={Position.Bottom} title={'Bottom axis'} />
+        <Axis
+          id={getAxisId('left2')}
+          title={'Left axis'}
+          position={Position.Left}
+          tickFormat={(d) => Number(d).toFixed(2)}
+        />
+
+        <BarSeries
+          id={getSpecId('bars')}
+          xScaleType={ScaleType.Linear}
+          yScaleType={ScaleType.Linear}
+          xAccessor="x"
+          yAccessors={['y']}
+          data={data}
+          yScaleToDataExtent={false}
+        />
+      </Chart>
+    );
+  })
+  .add('single data chart', () => {
+    return (
+      <Chart renderer="canvas" className={'story-chart'}>
+        <Axis id={getAxisId('bottom')} position={Position.Bottom} title={'Bottom axis'} />
+        <Axis
+          id={getAxisId('left2')}
+          title={'Left axis'}
+          position={Position.Left}
+          tickFormat={(d) => Number(d).toFixed(2)}
+        />
+
+        <BarSeries
+          id={getSpecId('bars')}
+          xScaleType={ScaleType.Linear}
+          yScaleType={ScaleType.Linear}
+          xAccessor="x"
+          yAccessors={['y']}
+          data={[{ x: 1, y: 10 }]}
+          yScaleToDataExtent={false}
+        />
+      </Chart>
+    );
+  })
+  .add('single data clusterd chart', () => {
+    return (
+      <Chart renderer="canvas" className={'story-chart'}>
+        <Axis id={getAxisId('bottom')} position={Position.Bottom} title={'Bottom axis'} />
+        <Axis
+          id={getAxisId('left2')}
+          title={'Left axis'}
+          position={Position.Left}
+          tickFormat={(d) => Number(d).toFixed(2)}
+        />
+
+        <BarSeries
+          id={getSpecId('bars')}
+          xScaleType={ScaleType.Linear}
+          yScaleType={ScaleType.Linear}
+          xAccessor="x"
+          yAccessors={['y']}
+          splitSeriesAccessors={['g']}
+          data={[
+            { x: 1, y: 10, g: 'a' },
+            { x: 1, y: 5, g: 'b' },
+            { x: 1, y: 3, g: 'c' },
+            { x: 1, y: 10, g: 'd' },
+          ]}
+          yScaleToDataExtent={false}
+        />
+      </Chart>
+    );
+  })
+  .add('single data stacked chart', () => {
+    return (
+      <Chart renderer="canvas" className={'story-chart'}>
+        <Axis id={getAxisId('bottom')} position={Position.Bottom} title={'Bottom axis'} />
+        <Axis
+          id={getAxisId('left2')}
+          title={'Left axis'}
+          position={Position.Left}
+          tickFormat={(d) => Number(d).toFixed(2)}
+        />
+
+        <BarSeries
+          id={getSpecId('bars')}
+          xScaleType={ScaleType.Linear}
+          yScaleType={ScaleType.Linear}
+          xAccessor="x"
+          yAccessors={['y']}
+          splitSeriesAccessors={['g']}
+          stackAccessors={['x']}
+          data={[
+            { x: 1, y: 10, g: 'a' },
+            { x: 1, y: 5, g: 'b' },
+            { x: 1, y: 3, g: 'c' },
+            { x: 1, y: 10, g: 'd' },
+          ]}
+          yScaleToDataExtent={false}
+        />
+      </Chart>
+    );
+  })
+  .add('negative and positive x values', () => {
+    return (
+      <Chart renderer="canvas" className={'story-chart'}>
+        <Axis id={getAxisId('bottom')} position={Position.Bottom} title={'Bottom axis'} />
+        <Axis
+          id={getAxisId('left2')}
+          title={'Left axis'}
+          position={Position.Left}
+          tickFormat={(d) => Number(d).toFixed(2)}
+        />
+
+        <BarSeries
+          id={getSpecId('bars')}
+          xScaleType={ScaleType.Linear}
+          yScaleType={ScaleType.Linear}
+          xAccessor="x"
+          yAccessors={['y']}
+          splitSeriesAccessors={['g']}
+          stackAccessors={['x']}
+          data={[
+            { x: -3, y: 1 },
+            { x: 0, y: 4 },
+            { x: -2, y: 2 },
+            { x: 1, y: 3 },
+            { x: 2, y: 2 },
+            { x: -1, y: 3 },
+            { x: 3, y: 1 },
+          ]}
           yScaleToDataExtent={false}
         />
       </Chart>
