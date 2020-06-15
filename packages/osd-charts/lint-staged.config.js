263ad92f1e983c4edc8fb2675170d5f2f160b540
Nick Partridge <nick.ryan.partridge@gmail.com>
2020-06-15 09:07:00 -0500

    ci: lint only staged files (#708)

--- packages/osd-charts/lint-staged.config.js
+++ packages/osd-charts/lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   // TODO: make this `lint:fix` if ever added to `pre-commit`
   // using in `commit-msg` doesn't save fixed changes so
   // in the meantime should error on bad linting when committing
-  '*.{js,ts,tsx}': ['yarn lint'],
+  '*.{js,ts,tsx}': 'cross-env NODE_ENV=production eslint --quiet',
 };
