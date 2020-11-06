283e54a71db7c86e6cd718cb9d5cb834bccdebda
Nick Partridge <nick.ryan.partridge@gmail.com>
2020-11-06 09:48:44 -0600

    docs: add randomness toggle on fit stacked series story (#892)

--- packages/osd-charts/stories/mixed/6_fitting_stacked.tsx
+++ packages/osd-charts/stories/mixed/6_fitting_stacked.tsx
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import { select, number } from '@storybook/addon-knobs';
+import { select, number, boolean } from '@storybook/addon-knobs';
 import numeral from 'numeral';
 import React from 'react';
 
@@ -26,6 +26,7 @@ import { getRandomNumberGenerator } from '../../src/mocks/utils';
 import { SB_KNOBS_PANEL } from '../utils/storybook';
 
 export const Example = () => {
+  const randomizeBoundingData = process.env.RNG_SEED !== null ? false : boolean('randomize bounding data', false);
   const dataTypes: Record<string, Array<{ x: number | string; y: number | null }>> = {
     isolated: [
       { x: 0, y: 3 },
@@ -158,7 +159,8 @@ export const Example = () => {
   const parsedEndValue: number | 'nearest' = Number.isNaN(Number(endValue)) ? 'nearest' : Number(endValue);
   const value = number('Explicit value (using Fit.Explicit)', 5);
   const xScaleType = dataKey === 'ordinal' ? ScaleType.Ordinal : ScaleType.Linear;
-  const rng = getRandomNumberGenerator();
+  const rngSeed = randomizeBoundingData ? undefined : process.env.RNG_SEED ?? '__seed__';
+  const rng = getRandomNumberGenerator(rngSeed);
   const tickFormat = stackMode === 'percentage' ? (d: any) => numeral(d).format('0[.]00%') : undefined;
   return (
     <Chart className="story-chart">
