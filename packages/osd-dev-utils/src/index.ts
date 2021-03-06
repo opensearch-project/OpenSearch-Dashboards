ebb36e5959d50473815225df013fae64bdbd9ad2
Kawika Avilla <kavilla414@gmail.com>
2021-03-05 19:15:39 -0800

    [Rename] kbn-dev-utils to osd-dev-utils (#74)
    * [Rename] kbn-dev-utils to osd-dev-utils
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    
    * Handle PR comments
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

---
+++ packages/osd-dev-utils/src/index.ts
@@ -0,0 +1,46 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+export * from '@osd/utils';
+export { withProcRunner, ProcRunner } from './proc_runner';
+export * from './tooling_log';
+export * from './serializers';
+export {
+  CA_CERT_PATH,
+  OPENSEARCH_KEY_PATH,
+  OPENSEARCH_CERT_PATH,
+  OPENSEARCH_P12_PATH,
+  OPENSEARCH_P12_PASSWORD,
+  OPENSEARCH_EMPTYPASSWORD_P12_PATH,
+  OPENSEARCH_NOPASSWORD_P12_PATH,
+  OPENSEARCH_DASHBOARDS_KEY_PATH,
+  OPENSEARCH_DASHBOARDS_CERT_PATH,
+  OPENSEARCH_DASHBOARDS_P12_PATH,
+  OPENSEARCH_DASHBOARDS_P12_PASSWORD,
+} from './certs';
+export * from './osd_client';
+export * from './run';
+export * from './axios';
+export * from './stdio';
+export * from './ci_stats_reporter';
+export * from './plugin_list';
+export * from './simple_opensearch_dashboards_platform_plugin_discovery';
+export * from './streams';
+export * from './babel';
+export * from './parse_opensearch_dashboards_platform_plugin';
