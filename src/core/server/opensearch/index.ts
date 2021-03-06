1cd50a48f80c562eb29465e3502365ab8b5e0370
Mihir Soni <mihirsoni.123@gmail.com>
2021-03-06 02:16:01 +0000

    [Rename] incorporate review comments
    Signed-off-by: Mihir Soni <mihirsoni.123@gmail.com>

--- src/core/server/opensearch/index.ts
+++ src/core/server/opensearch/index.ts
@@ -1,8 +1,8 @@
 /*
- * Licensed to mihson. under one or more contributor
+ * Licensed to Elasticsearch B.V.. under one or more contributor
  * license agreements. See the NOTICE file distributed with
  * this work for additional information regarding copyright
- * ownership. mihson. licenses this file to you under
+ * ownership. Elasticsearch B.V.. licenses this file to you under
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,7 +19,7 @@
 
 export { OpenSearchService } from './opensearch_service';
 export { config, configSchema, OpenSearchConfig } from './opensearch_config';
-export { NodesVersionCompatibility } from './version_check/ensure_es_version';
+export { NodesVersionCompatibility } from './version_check/ensure_opensearch_version';
 export {
   OpenSearchServiceSetup,
   OpenSearchServiceStart,
