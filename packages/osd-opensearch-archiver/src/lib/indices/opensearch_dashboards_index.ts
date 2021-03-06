6e1606fa828f03f52501d2cea54e7f79d972a93e
Bishoy Boktor <65934617+boktorbb-amzn@users.noreply.github.com>
2021-03-05 16:46:14 -0800

    Rename kbn-es-archiver to osd-opensearch-archiver (#37) (#42)
    * Renames kbn-es-archiver to osd-opensearch-archiver (#37)
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>
    
    * Renames kbn-es-archiver to osd-opensearch-archiver (#37)
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>
    
    * Renames kbn-eslint-import-resolver-kibana to osd-eslint-import-resolver-opensearch-dashboards (#37)
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>
    
    * Revert "Renames kbn-eslint-import-resolver-kibana to osd-eslint-import-resolver-opensearch-dashboards (#37)"
    
    This reverts commit 2ecd0713fc94a1e174c52e2601de6b5a777b4de3.
    
    * Address PR comments
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>
    
    * Revert changes to README.md in root packages folder
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>
    
    * Missed an es url
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>

---
+++ packages/osd-opensearch-archiver/src/lib/indices/opensearch_dashboards_index.ts
@@ -0,0 +1,173 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Client, CreateDocumentParams } from 'elasticsearch';
+import { ToolingLog, OsdClient } from '@osd/dev-utils';
+import { Stats } from '../stats';
+import { deleteIndex } from './delete_index';
+
+/**
+ * Deletes all indices that start with `.opensearch-dashboards`
+ */
+export async function deleteOpenSearchDashboardsIndices({
+  client,
+  stats,
+  log,
+}: {
+  client: Client;
+  stats: Stats;
+  log: ToolingLog;
+}) {
+  const indexNames = await fetchOpenSearchDashboardsIndices(client);
+  if (!indexNames.length) {
+    return;
+  }
+
+  await client.indices.putSettings({
+    index: indexNames,
+    body: { index: { blocks: { read_only: false } } },
+  });
+
+  await deleteIndex({
+    client,
+    stats,
+    index: indexNames,
+    log,
+  });
+
+  return indexNames;
+}
+
+/**
+ * Given an opensearch client, and a logger, migrates the `.opensearch-dashboards` index. This
+ * builds up an object that implements just enough of the osdMigrations interface
+ * as is required by migrations.
+ */
+export async function migrateOpenSearchDashboardsIndex({
+  client,
+  osdClient,
+}: {
+  client: Client;
+  osdClient: OsdClient;
+}) {
+  // we allow dynamic mappings on the index, as some interceptors are accessing documents before
+  // the migration is actually performed. The migrator will put the value back to `strict` after migration.
+  await client.indices.putMapping({
+    index: '.opensearch-dashboards',
+    body: {
+      dynamic: true,
+    },
+  } as any);
+
+  await osdClient.savedObjects.migrate();
+}
+
+/**
+ * Migrations mean that the OpenSearch Dashboards index will look something like:
+ * .opensearch-dashboards, .opensearch-dashboards_1, .opensearch-dashboards_323, etc. This finds all indices starting
+ * with .opensearch-dashboards, then filters out any that aren't actually OpenSearch Dashboards's core
+ * index (e.g. we don't want to remove .opensearch-dashboards_task_manager or the like).
+ */
+async function fetchOpenSearchDashboardsIndices(client: Client) {
+  const opensearchDashboardsIndices = await client.cat.indices({
+    index: '.opensearch-dashboards*',
+    format: 'json',
+  });
+  const isOpenSearchDashboardsIndex = (index: string) =>
+    /^\.opensearch-dashboards(:?_\d*)?$/.test(index);
+  return opensearchDashboardsIndices
+    .map((x: { index: string }) => x.index)
+    .filter(isOpenSearchDashboardsIndex);
+}
+
+export async function cleanOpenSearchDashboardsIndices({
+  client,
+  stats,
+  log,
+  opensearchDashboardsPluginIds,
+}: {
+  client: Client;
+  stats: Stats;
+  log: ToolingLog;
+  opensearchDashboardsPluginIds: string[];
+}) {
+  if (!opensearchDashboardsPluginIds.includes('spaces')) {
+    return await deleteOpenSearchDashboardsIndices({
+      client,
+      stats,
+      log,
+    });
+  }
+
+  while (true) {
+    const resp = await client.deleteByQuery({
+      index: `.opensearch-dashboards`,
+      body: {
+        query: {
+          bool: {
+            must_not: {
+              ids: {
+                type: '_doc',
+                values: ['space:default'],
+              },
+            },
+          },
+        },
+      },
+      ignore: [409],
+    });
+
+    if (resp.total !== resp.deleted) {
+      log.warning(
+        'delete by query deleted %d of %d total documents, trying again',
+        resp.deleted,
+        resp.total
+      );
+      continue;
+    }
+
+    break;
+  }
+
+  log.warning(
+    `since spaces are enabled, all objects other than the default space were deleted from ` +
+      `.opensearch-dashboards rather than deleting the whole index`
+  );
+
+  stats.deletedIndex('.opensearch-dashboards');
+}
+
+export async function createDefaultSpace({ index, client }: { index: string; client: Client }) {
+  await client.create({
+    index,
+    type: '_doc',
+    id: 'space:default',
+    ignore: 409,
+    body: {
+      type: 'space',
+      updated_at: new Date().toISOString(),
+      space: {
+        name: 'Default Space',
+        description: 'This is the default space',
+        disabledFeatures: [],
+        _reserved: true,
+      },
+    },
+  } as CreateDocumentParams);
+}
