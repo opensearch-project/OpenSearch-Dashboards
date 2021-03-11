cfcf11ebb4e7d7340f4a832c323238f510caa05f
Mihir Soni <mihson@amazon.com>
2021-03-10 19:53:08 -0800

    [Rename] Refactored src/plugins/vis_type_markdown directory (#95)
    Signed-off-by: Mihir Soni <mihirsoni.123@gmail.com>

--- src/plugins/vis_type_markdown/public/settings_options.tsx
+++ src/plugins/vis_type_markdown/public/settings_options.tsx
@@ -19,7 +19,7 @@
 
 import React from 'react';
 import { EuiPanel } from '@elastic/eui';
-import { i18n } from '@kbn/i18n';
+import { i18n } from '@osd/i18n';
 
 import { VisOptionsProps } from 'src/plugins/vis_default_editor/public';
 import { RangeOption, SwitchOption } from '../../charts/public';
