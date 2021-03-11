85714f768b25902bcc1db29b29bd822e0ce82891
Mihir Soni <mihson@amazon.com>
2021-03-10 19:14:59 -0800

    Src/fixtures,setup node env,test utils (#141)
    * [Rename] src/fixtures directory
    
    * [Rename] src/setup_node_env directory
    
    * [Rename] src/test_utils directory
    
    * [Rename] src/apm.js
    
    * [Rename] addressed review feedbacks
    
    Signed-off-by: Mihir Soni <mihirsoni.123@gmail.com>

--- src/fixtures/logstash_fields.js
+++ src/fixtures/logstash_fields.js
@@ -17,13 +17,13 @@
  * under the License.
  */
 
-import { shouldReadFieldFromDocValues, castEsToKbnFieldTypeName } from '../plugins/data/server';
+import { shouldReadFieldFromDocValues, castOpenSearchToOsdFieldTypeName } from '../plugins/data/server';
 
 function stubbedLogstashFields() {
   return [
     //                                  |aggregatable
     //                                  |      |searchable
-    // name               esType        |      |      |metadata       | subType
+    // name               opensearchType        |      |      |metadata       | subType
     ['bytes', 'long', true, true, { count: 10 }],
     ['ssl', 'boolean', true, true, { count: 20 }],
     ['@timestamp', 'date', true, true, { count: 30 }],
@@ -53,7 +53,7 @@ function stubbedLogstashFields() {
     ['script date', 'date', true, false, { script: '1234', lang: 'painless' }],
     ['script murmur3', 'murmur3', true, false, { script: '1234' }],
   ].map(function (row) {
-    const [name, esType, aggregatable, searchable, metadata = {}, subType = undefined] = row;
+    const [name, opensearchType, aggregatable, searchable, metadata = {}, subType = undefined] = row;
 
     const {
       count = 0,
@@ -62,15 +62,15 @@ function stubbedLogstashFields() {
       scripted = !!script,
     } = metadata;
 
-    // the conflict type is actually a kbnFieldType, we
+    // the conflict type is actually a osdFieldType, we
     // don't have any other way to represent it here
-    const type = esType === 'conflict' ? esType : castEsToKbnFieldTypeName(esType);
+    const type = opensearchType === 'conflict' ? opensearchType : castOpenSearchToOsdFieldTypeName(opensearchType);
 
     return {
       name,
       type,
-      esTypes: [esType],
-      readFromDocValues: shouldReadFieldFromDocValues(aggregatable, esType),
+      opensearchTypes: [opensearchType],
+      readFromDocValues: shouldReadFieldFromDocValues(aggregatable, opensearchType),
       aggregatable,
       searchable,
       count,
