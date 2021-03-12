d67737338b3ebbefc77a4971512ae5271054dc8a
Mihir Soni <mihson@amazon.com>
2021-03-11 21:42:05 -0800

    [Rename] kbn-pm in packages directory to osd-pm (#65)
    * [Rename] kbn-pm in packages directory to osd-pm
    
    Signed-off-by: Mihir Soni <mihirsoni.123@gmail.com>
    
    * [Rename] accomodate PR review feedback
    
    Signed-off-by: Mihir Soni <mihirsoni.123@gmail.com>
    
    * [Rename] addressed review feedbacks
    
    Signed-off-by: Mihir Soni <mihirsoni.123@gmail.com>

---
+++ packages/osd-pm/src/commands/watch.ts
@@ -0,0 +1,89 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { CliError } from '../utils/errors';
+import { log } from '../utils/log';
+import { parallelizeBatches } from '../utils/parallelize';
+import { ProjectMap, topologicallyBatchProjects } from '../utils/projects';
+import { waitUntilWatchIsReady } from '../utils/watch';
+import { ICommand } from './';
+
+/**
+ * Name of the script in the package/project package.json file to run during `osd watch`.
+ */
+const watchScriptName = 'osd:watch';
+
+/**
+ * Name of the OpenSearch Dashboards project.
+ */
+const opensearchDashboardsProjectName = 'opensearch-dashboards';
+
+/**
+ * Command that traverses through list of available projects/packages that have `osd:watch` script in their
+ * package.json files, groups them into topology aware batches and then processes theses batches one by one
+ * running `osd:watch` scripts in parallel within the same batch.
+ *
+ * Command internally relies on the fact that most of the build systems that are triggered by `osd:watch`
+ * will emit special "marker" once build/watch process is ready that we can use as completion condition for
+ * the `osd:watch` script and eventually for the entire batch. Currently we support completion "markers" for
+ * `webpack` and `tsc` only, for the rest we rely on predefined timeouts.
+ */
+export const WatchCommand: ICommand = {
+  description: 'Runs `osd:watch` script for every project.',
+  name: 'watch',
+
+  async run(projects, projectGraph) {
+    const projectsToWatch: ProjectMap = new Map();
+    for (const project of projects.values()) {
+      // We can't watch project that doesn't have `osd:watch` script.
+      if (project.hasScript(watchScriptName)) {
+        projectsToWatch.set(project.name, project);
+      }
+    }
+
+    if (projectsToWatch.size === 0) {
+      throw new CliError(
+        `There are no projects to watch found. Make sure that projects define 'osd:watch' script in 'package.json'.`
+      );
+    }
+
+    const projectNames = Array.from(projectsToWatch.keys());
+    log.info(`Running ${watchScriptName} scripts for [${projectNames.join(', ')}].`);
+
+    // OpenSearch Dashboards should always be run the last, so we don't rely on automatic
+    // topological batching and push it to the last one-entry batch manually.
+    const shouldWatchOpenSearchDashboardsProject = projectsToWatch.delete(opensearchDashboardsProjectName);
+
+    const batchedProjects = topologicallyBatchProjects(projectsToWatch, projectGraph);
+
+    if (shouldWatchOpenSearchDashboardsProject) {
+      batchedProjects.push([projects.get(opensearchDashboardsProjectName)!]);
+    }
+
+    await parallelizeBatches(batchedProjects, async (pkg) => {
+      const completionHint = await waitUntilWatchIsReady(
+        pkg.runScriptStreaming(watchScriptName, {
+          debug: false,
+        }).stdout
+      );
+
+      log.success(`[${pkg.name}] Initial build completed (${completionHint}).`);
+    });
+  },
+};
---
+++ packages/osd-pm/src/utils/opensearch_dashboards.ts
@@ -0,0 +1,151 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Path from 'path';
+
+import multimatch from 'multimatch';
+import isPathInside from 'is-path-inside';
+
+import { resolveDepsForProject, YarnLock } from './yarn_lock';
+import { Log } from './log';
+import { ProjectMap, getProjects, includeTransitiveProjects } from './projects';
+import { Project } from './project';
+import { getProjectPaths } from '../config';
+
+/**
+ * Helper class for dealing with a set of projects as children of
+ * the OpenSearch Dashboards project. The osd/pm is currently implemented to be
+ * more generic, where everything is an operation of generic projects,
+ * but that leads to exceptions where we need the OpenSearch Dashboards project and
+ * do things like `project.get('opensearch-dashboards')!`.
+ *
+ * Using this helper we can restructre the generic list of projects
+ * as a OpenSearch Dashboards object which encapulates all the projects in the
+ * workspace and knows about the root OpenSearch Dashboards project.
+ */
+export class OpenSearchDashboards {
+  static async loadFrom(rootPath: string) {
+    return new OpenSearchDashboards(await getProjects(rootPath, getProjectPaths({ rootPath })));
+  }
+
+  private readonly opensearchDashboardsProject: Project;
+
+  constructor(private readonly allWorkspaceProjects: ProjectMap) {
+    const opensearchDashboardsProject = allWorkspaceProjects.get('opensearch-dashboards');
+
+    if (!opensearchDashboardsProject) {
+      throw new TypeError(
+        'Unable to create OpenSearch Dashboards object without all projects, including the OpenSearch Dashboards project.'
+      );
+    }
+
+    this.opensearchDashboardsProject = opensearchDashboardsProject;
+  }
+
+  /** make an absolute path by resolving subPath relative to the opensearch-dashboards repo */
+  getAbsolute(...subPath: string[]) {
+    return Path.resolve(this.opensearchDashboardsProject.path, ...subPath);
+  }
+
+  /** convert an absolute path to a relative path, relative to the opensearch-dashboards repo */
+  getRelative(absolute: string) {
+    return Path.relative(this.opensearchDashboardsProject.path, absolute);
+  }
+
+  /** get a copy of the map of all projects in the opensearch-dashboards workspace */
+  getAllProjects() {
+    return new Map(this.allWorkspaceProjects);
+  }
+
+  /** determine if a project with the given name exists */
+  hasProject(name: string) {
+    return this.allWorkspaceProjects.has(name);
+  }
+
+  /** get a specific project, throws if the name is not known (use hasProject() first) */
+  getProject(name: string) {
+    const project = this.allWorkspaceProjects.get(name);
+
+    if (!project) {
+      throw new Error(`No package with name "${name}" in the workspace`);
+    }
+
+    return project;
+  }
+
+  /** get a project and all of the projects it depends on in a ProjectMap */
+  getProjectAndDeps(name: string) {
+    const project = this.getProject(name);
+    return includeTransitiveProjects([project], this.allWorkspaceProjects);
+  }
+
+  /** filter the projects to just those matching certain paths/include/exclude tags */
+  getFilteredProjects(options: {
+    skipOpenSearchDashboardsPlugins: boolean;
+    ossOnly: boolean;
+    exclude: string[];
+    include: string[];
+  }) {
+    const allProjects = this.getAllProjects();
+    const filteredProjects: ProjectMap = new Map();
+
+    const pkgJsonPaths = Array.from(allProjects.values()).map((p) => p.packageJsonLocation);
+    const filteredPkgJsonGlobs = getProjectPaths({
+      ...options,
+      rootPath: this.opensearchDashboardsProject.path,
+    }).map((g) => Path.resolve(g, 'package.json'));
+    const matchingPkgJsonPaths = multimatch(pkgJsonPaths, filteredPkgJsonGlobs);
+
+    for (const project of allProjects.values()) {
+      const pathMatches = matchingPkgJsonPaths.includes(project.packageJsonLocation);
+      const notExcluded = !options.exclude.includes(project.name);
+      const isIncluded = !options.include.length || options.include.includes(project.name);
+
+      if (pathMatches && notExcluded && isIncluded) {
+        filteredProjects.set(project.name, project);
+      }
+    }
+
+    return filteredProjects;
+  }
+
+  isPartOfRepo(project: Project) {
+    return (
+      project.path === this.opensearchDashboardsProject.path ||
+      isPathInside(project.path, this.opensearchDashboardsProject.path)
+    );
+  }
+
+  isOutsideRepo(project: Project) {
+    return !this.isPartOfRepo(project);
+  }
+
+  resolveAllProductionDependencies(yarnLock: YarnLock, log: Log) {
+    const opensearchDashboardsDeps = resolveDepsForProject({
+      project: this.opensearchDashboardsProject,
+      yarnLock,
+      osd: this,
+      includeDependentProject: true,
+      productionDepsOnly: true,
+      log,
+    })!;
+
+    return new Map([...opensearchDashboardsDeps.entries()]);
+  }
+}
