01b46fe394d576add337568287e622483b20bca7
Mihir Soni <mihirsoni.123@gmail.com>
2021-03-22 09:42:12 -0700

    [PURIFY] Removes unused Jenkis shell scripts (#211)
    Signed-off-by: Mihir Soni <mihirsoni.123@gmail.com>

--- test/scripts/jenkins_accessibility.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source test/scripts/jenkins_test_setup_oss.sh
-
-checks-reporter-with-killswitch "OpenSearch Dashboards accessibility tests" \
-  node scripts/functional_tests \
-    --debug --bail \
-    --opensearch-dashboards-install-dir "$OPENSEARCH_DASHBOARDS_INSTALL_DIR" \
-    --config test/accessibility/config.ts;
--- test/scripts/jenkins_unit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source test/scripts/jenkins_test_setup.sh
-
-"$(FORCE_COLOR=0 yarn bin)/grunt" jenkins:unit --dev;
