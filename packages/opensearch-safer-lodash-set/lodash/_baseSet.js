f5cc0396a8e1e0ebce792ef2e8f0f9b9a0f447d0
Bishoy Boktor <65934617+boktorbb-amzn@users.noreply.github.com>
2021-04-08 10:22:41 -0700

    [Rename] elastic-safer-lodash-set (#252)
    Renames the elastic-safer-lodash-set to opensearch-safer-lodash-set and
    updated precommit hook rules and eslintignore since this package file
    names need to match upstream lodash
    
    Signed-off-by: Bishoy Boktor <boktorbb@amazon.com>

---
+++ packages/opensearch-safer-lodash-set/lodash/_baseSet.js
@@ -0,0 +1,73 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/* eslint-disable */
+
+var assignValue = require('lodash/_assignValue'),
+    castPath = require('lodash/_castPath'),
+    isFunction = require('lodash/isFunction'),
+    isIndex = require('lodash/_isIndex'),
+    isObject = require('lodash/isObject'),
+    toKey = require('lodash/_toKey');
+
+/**
+ * The base implementation of `_.set`.
+ *
+ * @private
+ * @param {Object} object The object to modify.
+ * @param {Array|string} path The path of the property to set.
+ * @param {*} value The value to set.
+ * @param {Function} [customizer] The function to customize path creation.
+ * @returns {Object} Returns `object`.
+ */
+function baseSet(object, path, value, customizer) {
+  if (!isObject(object)) {
+    return object;
+  }
+  path = castPath(path, object);
+
+  var index = -1,
+      length = path.length,
+      lastIndex = length - 1,
+      nested = object;
+
+  while (nested != null && ++index < length) {
+    var key = toKey(path[index]),
+        newValue = value;
+
+    if (key == 'prototype' && isFunction(nested)) {
+      throw new Error('Illegal access of function prototype')
+    }
+
+    if (index != lastIndex) {
+      var objValue = hasOwnProperty.call(nested, key) ? nested[key] : undefined
+      newValue = customizer ? customizer(objValue, key, nested) : undefined;
+      if (newValue === undefined) {
+        newValue = isObject(objValue)
+          ? objValue
+          : (isIndex(path[index + 1]) ? [] : {});
+      }
+    }
+    assignValue(nested, key, newValue);
+    nested = nested[key];
+  }
+  return object;
+}
+
+module.exports = baseSet;
---
+++ packages/opensearch-safer-lodash-set/test/fp.ts
@@ -0,0 +1,99 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { expectType } from 'tsd';
+import { set, setWith, assoc, assocPath } from '../fp';
+
+const someObj: object = {};
+const anyValue: any = 'any value';
+
+function customizer(value: any, key: string, obj: object) {
+  expectType<any>(value);
+  expectType<string>(key);
+  expectType<object>(obj);
+}
+
+expectType<object>(set('a.b.c', anyValue, someObj));
+expectType<object>(set('a.b.c')(anyValue, someObj));
+expectType<object>(set('a.b.c')(anyValue)(someObj));
+expectType<object>(set('a.b.c', anyValue)(someObj));
+
+expectType<object>(set(['a.b.c'], anyValue, someObj));
+expectType<object>(set(['a.b.c'])(anyValue, someObj));
+expectType<object>(set(['a.b.c'])(anyValue)(someObj));
+expectType<object>(set(['a.b.c'], anyValue)(someObj));
+
+expectType<object>(set(['a.b.c', 2, Symbol('hep')], anyValue, someObj));
+expectType<object>(set(['a.b.c', 2, Symbol('hep')])(anyValue, someObj));
+expectType<object>(set(['a.b.c', 2, Symbol('hep')])(anyValue)(someObj));
+expectType<object>(set(['a.b.c', 2, Symbol('hep')], anyValue)(someObj));
+
+expectType<object>(assoc('a.b.c', anyValue, someObj));
+expectType<object>(assoc('a.b.c')(anyValue, someObj));
+expectType<object>(assoc('a.b.c')(anyValue)(someObj));
+expectType<object>(assoc('a.b.c', anyValue)(someObj));
+
+expectType<object>(assoc(['a.b.c'], anyValue, someObj));
+expectType<object>(assoc(['a.b.c'])(anyValue, someObj));
+expectType<object>(assoc(['a.b.c'])(anyValue)(someObj));
+expectType<object>(assoc(['a.b.c'], anyValue)(someObj));
+
+expectType<object>(assoc(['a.b.c', 2, Symbol('hep')], anyValue, someObj));
+expectType<object>(assoc(['a.b.c', 2, Symbol('hep')])(anyValue, someObj));
+expectType<object>(assoc(['a.b.c', 2, Symbol('hep')])(anyValue)(someObj));
+expectType<object>(assoc(['a.b.c', 2, Symbol('hep')], anyValue)(someObj));
+
+expectType<object>(assocPath('a.b.c', anyValue, someObj));
+expectType<object>(assocPath('a.b.c')(anyValue, someObj));
+expectType<object>(assocPath('a.b.c')(anyValue)(someObj));
+expectType<object>(assocPath('a.b.c', anyValue)(someObj));
+
+expectType<object>(assocPath(['a.b.c'], anyValue, someObj));
+expectType<object>(assocPath(['a.b.c'])(anyValue, someObj));
+expectType<object>(assocPath(['a.b.c'])(anyValue)(someObj));
+expectType<object>(assocPath(['a.b.c'], anyValue)(someObj));
+
+expectType<object>(assocPath(['a.b.c', 2, Symbol('hep')], anyValue, someObj));
+expectType<object>(assocPath(['a.b.c', 2, Symbol('hep')])(anyValue, someObj));
+expectType<object>(assocPath(['a.b.c', 2, Symbol('hep')])(anyValue)(someObj));
+expectType<object>(assocPath(['a.b.c', 2, Symbol('hep')], anyValue)(someObj));
+
+expectType<object>(setWith(customizer, 'a.b.c', anyValue, someObj));
+expectType<object>(setWith(customizer)('a.b.c', anyValue, someObj));
+expectType<object>(setWith(customizer)('a.b.c')(anyValue, someObj));
+expectType<object>(setWith(customizer)('a.b.c')(anyValue)(someObj));
+expectType<object>(setWith(customizer, 'a.b.c')(anyValue)(someObj));
+expectType<object>(setWith(customizer, 'a.b.c', anyValue)(someObj));
+expectType<object>(setWith(customizer, 'a.b.c')(anyValue, someObj));
+
+expectType<object>(setWith(customizer, ['a.b.c'], anyValue, someObj));
+expectType<object>(setWith(customizer)(['a.b.c'])(anyValue, someObj));
+expectType<object>(setWith(customizer)(['a.b.c'], anyValue, someObj));
+expectType<object>(setWith(customizer)(['a.b.c'])(anyValue)(someObj));
+expectType<object>(setWith(customizer, ['a.b.c'])(anyValue)(someObj));
+expectType<object>(setWith(customizer, ['a.b.c'], anyValue)(someObj));
+expectType<object>(setWith(customizer, ['a.b.c'])(anyValue, someObj));
+
+expectType<object>(setWith(customizer, ['a.b.c', 2, Symbol('hep')], anyValue, someObj));
+expectType<object>(setWith(customizer)(['a.b.c', 2, Symbol('hep')])(anyValue, someObj));
+expectType<object>(setWith(customizer)(['a.b.c', 2, Symbol('hep')], anyValue, someObj));
+expectType<object>(setWith(customizer)(['a.b.c', 2, Symbol('hep')])(anyValue)(someObj));
+expectType<object>(setWith(customizer, ['a.b.c', 2, Symbol('hep')])(anyValue)(someObj));
+expectType<object>(setWith(customizer, ['a.b.c', 2, Symbol('hep')], anyValue)(someObj));
+expectType<object>(setWith(customizer, ['a.b.c', 2, Symbol('hep')])(anyValue, someObj));
