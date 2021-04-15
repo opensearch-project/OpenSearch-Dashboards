6dc7246c4adca502009edcfd07c28d414503da83
Marco Vettorello <vettorello.marco@gmail.com>
2021-04-15 20:21:12 +0200

    chore: temporarily disable codecov

--- packages/osd-charts/.ci/unit_tests.sh
+++ packages/osd-charts/.ci/unit_tests.sh
@@ -8,8 +8,9 @@ source .ci/global_setup.sh
 ###
 ### install codecov dependencies
 ###
-echo " -- installing codecov dependencies"
-yarn add codecov --prefer-offline --frozen-lockfile
+# temporarily disabled
+# echo " -- installing codecov dependencies"
+# yarn add codecov --prefer-offline --frozen-lockfile
 
 ###
 ### timezone specific testing
@@ -21,10 +22,12 @@ yarn test:tz --ci
 ### testing
 ###
 echo " -- testing"
-yarn test --coverage --ci
+# yarn test --coverage --ci
+yarn test --ci
 
 ###
 ### upload code coverage
 ###
-echo " -- upload code coverage"
-./node_modules/.bin/codecov
+# temporarily disabled
+# echo " -- upload code coverage"
+# ./node_modules/.bin/codecov
