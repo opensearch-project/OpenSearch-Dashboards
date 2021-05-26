3512553760608a9133cc43e9065037a02f9711a1
Tommy Markley <tommymarkley@protonmail.com>
2021-05-26 15:32:22 -0500

    Update favicon icons with new OpenSearch branding (#369)
    Signed-off-by: Tommy Markley <markleyt@amazon.com>

--- src/core/server/rendering/views/template.tsx
+++ src/core/server/rendering/views/template.tsx
@@ -89,35 +89,32 @@ export const Template: FunctionComponent<Props> = ({
         <meta name="viewport" content="width=device-width" />
         <title>OpenSearch</title>
         <Fonts url={uiPublicUrl} />
-        {/* Favicons (generated from http://realfavicongenerator.net/) */}
+        {/* Favicons (generated from https://realfavicongenerator.net/) */}
         <link
-          rel="apple-touch-icon-heatmap"
+          rel="apple-touch-icon"
           sizes="180x180"
-          href={`${uiPublicUrl}/favicons/apple-touch-icon-heatmap.png`}
+          href={`${uiPublicUrl}/favicons/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={`${uiPublicUrl}/favicons/favicon-heatmap-32x32.png`}
+          href={`${uiPublicUrl}/favicons/favicon-32x32.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={`${uiPublicUrl}/favicons/favicon-heatmap-16x16.png`}
+          href={`${uiPublicUrl}/favicons/favicon-16x16.png`}
         />
         <link rel="manifest" href={`${uiPublicUrl}/favicons/manifest.json`} />
         <link
           rel="mask-icon"
           color="#e8488b"
-          href={`${uiPublicUrl}/favicons/safari-pinned-tab-heatmap.svg`}
+          href={`${uiPublicUrl}/favicons/safari-pinned-tab.svg`}
         />
-        <link rel="shortcut icon" href={`${uiPublicUrl}/favicons/favicon-heatmap.ico`} />
-        <meta
-          name="msapplication-config"
-          content={`${uiPublicUrl}/favicons/browserconfig-heatmap.xml`}
-        />
+        <link rel="shortcut icon" href={`${uiPublicUrl}/favicons/favicon.ico`} />
+        <meta name="msapplication-config" content={`${uiPublicUrl}/favicons/browserconfig.xml`} />
         <meta name="theme-color" content="#ffffff" />
         <Styles darkMode={darkMode} />
 
