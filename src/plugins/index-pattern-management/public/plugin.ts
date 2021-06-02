1fb0e4a10c41a3373bbab8b0fec521db7df3bd4b
Vacha Shah <vachshah@amazon.com>
2021-06-02 00:57:03 -0700

    Renaming index_pattern_management plugin folder to kebab-case
    Signed-off-by: Vacha Shah <vachshah@amazon.com>

---
+++ src/plugins/index-pattern-management/public/plugin.ts
@@ -0,0 +1,122 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+import { i18n } from '@osd/i18n';
+import { PluginInitializerContext, CoreSetup, CoreStart, Plugin } from 'src/core/public';
+import { DataPublicPluginStart } from 'src/plugins/data/public';
+import { UrlForwardingSetup } from '../../url_forwarding/public';
+import {
+  IndexPatternManagementService,
+  IndexPatternManagementServiceSetup,
+  IndexPatternManagementServiceStart,
+} from './service';
+
+import { ManagementSetup } from '../../management/public';
+
+export interface IndexPatternManagementSetupDependencies {
+  management: ManagementSetup;
+  urlForwarding: UrlForwardingSetup;
+}
+
+export interface IndexPatternManagementStartDependencies {
+  data: DataPublicPluginStart;
+}
+
+export type IndexPatternManagementSetup = IndexPatternManagementServiceSetup;
+
+export type IndexPatternManagementStart = IndexPatternManagementServiceStart;
+
+const sectionsHeader = i18n.translate('indexPatternManagement.indexPattern.sectionsHeader', {
+  defaultMessage: 'Index Patterns',
+});
+
+const IPM_APP_ID = 'indexPatterns';
+
+export class IndexPatternManagementPlugin
+  implements
+    Plugin<
+      IndexPatternManagementSetup,
+      IndexPatternManagementStart,
+      IndexPatternManagementSetupDependencies,
+      IndexPatternManagementStartDependencies
+    > {
+  private readonly indexPatternManagementService = new IndexPatternManagementService();
+
+  constructor(initializerContext: PluginInitializerContext) {}
+
+  public setup(
+    core: CoreSetup<IndexPatternManagementStartDependencies, IndexPatternManagementStart>,
+    { management, urlForwarding }: IndexPatternManagementSetupDependencies
+  ) {
+    const opensearchDashboardsSection = management.sections.section.opensearchDashboards;
+
+    if (!opensearchDashboardsSection) {
+      throw new Error('`opensearchDashboards` management section not found.');
+    }
+
+    const newAppPath = `management/opensearch-dashboards/${IPM_APP_ID}`;
+    const legacyPatternsPath = 'management/opensearch-dashboards/index_patterns';
+
+    urlForwarding.forwardApp(
+      'management/opensearch-dashboards/index_pattern',
+      newAppPath,
+      (path) => '/create'
+    );
+    urlForwarding.forwardApp(legacyPatternsPath, newAppPath, (path) => {
+      const pathInApp = path.substr(legacyPatternsPath.length + 1);
+      return pathInApp && `/patterns${pathInApp}`;
+    });
+
+    opensearchDashboardsSection.registerApp({
+      id: IPM_APP_ID,
+      title: sectionsHeader,
+      order: 0,
+      mount: async (params) => {
+        const { mountManagementSection } = await import('./management-app');
+
+        return mountManagementSection(core.getStartServices, params, () =>
+          this.indexPatternManagementService.environmentService.getEnvironment().ml()
+        );
+      },
+    });
+
+    return this.indexPatternManagementService.setup({ httpClient: core.http });
+  }
+
+  public start(core: CoreStart, plugins: IndexPatternManagementStartDependencies) {
+    return this.indexPatternManagementService.start();
+  }
+
+  public stop() {
+    this.indexPatternManagementService.stop();
+  }
+}
