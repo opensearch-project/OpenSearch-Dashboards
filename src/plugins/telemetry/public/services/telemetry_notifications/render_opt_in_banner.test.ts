1c1d334f4e2e04f0227db008326f1c29f3534248
Anan <79961084+ananzh@users.noreply.github.com>
2021-06-23 22:58:21 -0700

    [Test] Enable unit test suite: render_opt_in_banner.test.ts (#503) (#505)
    All the unit tests related to unused telemetry are temporarily
    skipped after the fork. Unit tests of the disabled telemetry
    functions should also be modified correspondingly. To build
    a clean unit test, we decide to modify and enable all the
    working unit tests. This PR modifies and enables
    render_opt_in_banner.test.ts.
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/plugins/telemetry/public/services/telemetry_notifications/render_opt_in_banner.test.ts
+++ src/plugins/telemetry/public/services/telemetry_notifications/render_opt_in_banner.test.ts
@@ -34,7 +34,7 @@ import { renderOptInBanner } from './render_opt_in_banner';
 // eslint-disable-next-line @osd/eslint/no-restricted-paths
 import { overlayServiceMock } from '../../../../../core/public/overlays/overlay_service.mock';
 
-describe.skip('renderOptInBanner', () => {
+describe('renderOptInBanner', () => {
   it('adds a banner to banners with priority of 10000', () => {
     const bannerID = 'brucer-wayne';
     const overlays = overlayServiceMock.createStartContract();
