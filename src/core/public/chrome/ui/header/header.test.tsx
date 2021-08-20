43dd663d64e5f28e462492337e8c71b1c13941e1
Abby Hu <abigailhu2000@gmail.com>
2021-08-20 06:55:11 +0000

    Make top left logo on the main screen configurable
    Add a new config opensearchDashboards.branding.logoUrl in yaml file
    for making top left corner logo on the main screen configurable. If
    URL is invalid, the default OpenSearch logo will be shown.
    
    Signed-off-by: Abby Hu <abigailhu2000@gmail.com>

--- src/core/public/chrome/ui/header/header.test.tsx
+++ src/core/public/chrome/ui/header/header.test.tsx
@@ -69,6 +69,7 @@ function mockProps() {
     isLocked$: new BehaviorSubject(false),
     loadingCount$: new BehaviorSubject(0),
     onIsLockedUpdate: () => {},
+    branding: { logoUrl: '/' },
   };
 }
 
