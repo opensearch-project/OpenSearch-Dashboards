kavilla/OpenSearch-Dashboards-1

Branding config structure change and renaming
Change the branding related config to a map structure in the yml file.
Also rename the configs according to the official branding guidelines.
The full logo on the main page header will be called logo; the small
logo icon will be called mark.

Signed-off-by: Abby Hu <abigailhu2000@gmail.com>

src/core/public/chrome/ui/header/header.test.tsx
@@ -69,7 +69,11 @@ function mockProps() {
     isLocked$: new BehaviorSubject(false),
     loadingCount$: new BehaviorSubject(0),
     onIsLockedUpdate: () => {},
-    branding: { fullLogoUrl: '/', logoUrl: '/', title: 'OpenSearch Dashboards' },
+    branding: {
+      logo: { defaultUrl: '/' },
+      mark: { defaultUrl: '/' },
+      title: 'OpenSearch Dashboards',
+    },
   };
 }
 

src/core/public/chrome/ui/header/header.tsx
@@ -87,7 +87,17 @@ export interface HeaderProps {
   isLocked$: Observable<boolean>;
   loadingCount$: ReturnType<HttpStart['getLoadingCount$']>;
   onIsLockedUpdate: OnIsLockedUpdate;
-  branding: { fullLogoUrl?: string; logoUrl?: string; title: string };
+  branding: {
+    logo: {
+      defaultUrl?: string;
+      darkModeUrl?: string;
+    };
+    mark: {
+      defaultUrl?: string;
+      darkModeUrl?: string;
+    };
+    title?: string;
+  };
 }
 
 export function Header({

src/core/server/opensearch_dashboards_config.ts
@@ -53,16 +53,36 @@ export const config = {
     autocompleteTerminateAfter: schema.duration({ defaultValue: 100000 }),
     autocompleteTimeout: schema.duration({ defaultValue: 1000 }),
     branding: schema.object({
-      fullLogoUrl: schema.string({
+      logo: schema.object({
+        defaultUrl: schema.string({
+          defaultValue: '/',
+        }),
+        darkModeUrl: schema.string({
+          defaultValue: '/',
+        }),
+      }),
+      mark: schema.object({
+        defaultUrl: schema.string({
+          defaultValue: '/',
+        }),
+        darkModeUrl: schema.string({
+          defaultValue: '/',
+        }),
+      }),
+      loadingLogo: schema.object({
+        defaultUrl: schema.string({
+          defaultValue: '/',
+        }),
+        darkModeUrl: schema.string({
+          defaultValue: '/',
+        }),
+      }),
+      favicon: schema.string({
         defaultValue: '/',
       }),
-      logoUrl: schema.string({
-        defaultValue: '/',
+      title: schema.string({
+        defaultValue: '',
       }),
-      loadingLogoUrl: schema.string({
-        defaultValue: '/',
-      }),
-      title: schema.string({ defaultValue: '' }),
     }),
   }),
   deprecations,

opensearch-project/OpenSearch-Dashboards

src/core/public/chrome/ui/header/header.test.tsx
@@ -69,7 +69,11 @@ function mockProps() {
     isLocked$: new BehaviorSubject(false),
     loadingCount$: new BehaviorSubject(0),
     onIsLockedUpdate: () => {},
-    branding: { fullLogoUrl: '/', logoUrl: '/', title: 'OpenSearch Dashboards' },
+    branding: {
+      logo: { defaultUrl: '/' },
+      mark: { defaultUrl: '/' },
+      applicationTitle: 'OpenSearch Dashboards',
+    },
   };
 }
 

src/core/public/chrome/ui/header/header.tsx
@@ -87,7 +87,17 @@ export interface HeaderProps {
   isLocked$: Observable<boolean>;
   loadingCount$: ReturnType<HttpStart['getLoadingCount$']>;
   onIsLockedUpdate: OnIsLockedUpdate;
-  branding: { fullLogoUrl?: string; logoUrl?: string; title: string };
+  branding: {
+    logo: {
+      defaultUrl?: string;
+      darkModeUrl?: string;
+    };
+    mark: {
+      defaultUrl?: string;
+      darkModeUrl?: string;
+    };
+    applicationTitle?: string;
+  };
 }
 
 export function Header({

src/core/server/opensearch_dashboards_config.ts
@@ -53,16 +53,36 @@ export const config = {
     autocompleteTerminateAfter: schema.duration({ defaultValue: 100000 }),
     autocompleteTimeout: schema.duration({ defaultValue: 1000 }),
     branding: schema.object({
-      fullLogoUrl: schema.string({
+      logo: schema.object({
+        defaultUrl: schema.string({
+          defaultValue: '/',
+        }),
+        darkModeUrl: schema.string({
+          defaultValue: '/',
+        }),
+      }),
+      mark: schema.object({
+        defaultUrl: schema.string({
+          defaultValue: '/',
+        }),
+        darkModeUrl: schema.string({
+          defaultValue: '/',
+        }),
+      }),
+      loadingLogo: schema.object({
+        defaultUrl: schema.string({
+          defaultValue: '/',
+        }),
+        darkModeUrl: schema.string({
+          defaultValue: '/',
+        }),
+      }),
+      favicon: schema.string({
         defaultValue: '/',
       }),
-      logoUrl: schema.string({
-        defaultValue: '/',
+      applicationTitle: schema.string({
+        defaultValue: '',
       }),
-      loadingLogoUrl: schema.string({
-        defaultValue: '/',
-      }),
-      title: schema.string({ defaultValue: '' }),
     }),
   }),
   deprecations,