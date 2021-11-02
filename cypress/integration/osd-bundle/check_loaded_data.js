dd67fabfbb40a9c382038122f07ce0e95f263cb9
Anan Zhuang <ananzh@amazon.com>
2021-11-02 06:20:05 +0000

    Add more bwc tests for osd with bundles
    This PR adds the following bwc tests:
    1)verify sample data work properly for bwc
    2)verify timeline visualization work properly for bwc
    
    add more commands check in check_timeline and rename sample data check
    minimize the login time and make the tests more robust
    change query content to make bwc test more robust
    update license header
    solve timestamp issue
    fix comments and eslint
    
    Partically Resolved:
    
    https://github.com/opensearch-project/opensearch-build/issues/705
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

---
+++ cypress/integration/osd-bundle/check_loaded_data.js
@@ -0,0 +1,90 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/* eslint-disable */
+import { MiscUtils, CommonUI, LoginPage } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
+
+const miscUtils = new MiscUtils(cy);
+const commonUI = new CommonUI(cy);
+const loginPage = new LoginPage(cy);
+
+describe('check previously loaded data', () => {
+    beforeEach(() => {
+        miscUtils.visitPage('app/dashboards#');
+        loginPage.enterUserName('admin');
+        loginPage.enterPassword('admin');
+        loginPage.submit();
+    });
+
+    afterEach(() => {
+        cy.clearCookies();
+    });
+
+    it('previous loaded data should exist in dashboards', () => {
+        let items = cy.get('[data-test-subj="itemsInMemTable"]');
+        
+        items.get('[data-test-subj="dashboardListingTitleLink-[Flights]-Global-Flight-Dashboard"]')
+             .should('have.text', '[Flights] Global Flight Dashboard');
+            
+        items.get('[data-test-subj="dashboardListingTitleLink-[Logs]-Web-Traffic"]')
+             .should('have.text', '[Logs] Web Traffic');  
+
+        items.get('[data-test-subj="dashboardListingTitleLink-[eCommerce]-Revenue-Dashboard"]')
+             .should('have.text', '[eCommerce] Revenue Dashboard');
+    });
+        
+    describe('Check Global Flight Dashboard', () => {
+        beforeEach(() => {
+            cy.get('[data-test-subj="itemsInMemTable"]')
+              .get('[data-test-subj="dashboardListingTitleLink-[Flights]-Global-Flight-Dashboard"]')
+              .click();            
+            commonUI.removeAllFilters();
+            commonUI.setDateRange('Dec 1, 2021 @ 00:00:00.000', 'Nov 1, 2021 @ 00:00:00.000')
+        });
+
+        it('Global Flight Dashboard is loaded and funtions correctly', () => {
+            cy.get('[data-test-subj="breadcrumb last"]').should('have.text', '[Flights] Global Flight Dashboard');
+            cy.get('[data-title="[Flights] Total Flights"]').should('exist');
+            cy.get('[data-title="[Flights] Average Ticket Price"]').should('exist');
+
+            commonUI.addFilterRetrySelection('FlightDelayType', 'is not', 'No Delay');
+            let types = cy.get('[data-title="[Flights] Delay Type"]')
+            types.find('[data-label="Weather Delay"]').should('exist');
+            types.find('[data-label="No Delay"]').should('not.exist'); ;
+            commonUI.removeFilter('FlightDelayType');
+
+            commonUI.addFilterRetrySelection('Carrier', 'is', 'Logstash Airways');
+            cy.get('[data-title="[Flights] Airline Carrier"]')
+              .find('[class="label-text"]')
+              .should('have.text', 'Logstash Airways (100%)');
+        });
+    });
+
+    describe('Check eCommerce Revenue Dashboard', () => {
+        beforeEach(() => {
+            cy.get('[data-test-subj="itemsInMemTable"]')
+              .get('[data-test-subj="dashboardListingTitleLink-[eCommerce]-Revenue-Dashboard"]')
+              .click();          
+            commonUI.removeAllFilters();
+            commonUI.setDateRange('Nov 1, 2021 @ 00:00:00.000', 'Nov 1, 2016 @ 00:00:00.000')
+        });
+
+        it('eCommerce Revenue Dashboard is loaded and functions correctly', () => {
+            cy.get('[data-test-subj="breadcrumb last"]').should('have.text', '[eCommerce] Revenue Dashboard');
+            cy.get('[data-title="[eCommerce] Average Sales Price"]').should('exist');
+            cy.get('[data-title="[eCommerce] Average Sold Quantity"]').should('exist');
+
+            commonUI.addFilterRetrySelection('customer_gender', 'is', 'FEMALE');
+            cy.get('[data-title="[eCommerce] Sales by Gender"]')
+              .find('[class="label-text"]')
+              .should('have.text', 'FEMALE (100%)');
+            
+            commonUI.addFilterRetrySelection('category', 'is not', "Women's Clothing");
+            let category = cy.get('[data-title="[eCommerce] Sales by Category"]')
+            category.find('[data-label="Men\'s Clothing"]').should('exist');
+            category.find('[data-label="Women\'s Clothing"]').should('not.exist');    
+        });
+    });
+});
