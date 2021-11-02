3d1e2b6c9ddb4465de6e6f5896609b828a3f4be9
Anan Zhuang <ananzh@amazon.com>
2021-11-02 05:56:38 +0000

    Add more bwc tests for osd without bundles
    This PR adds the following bwc tests:
    1) verify sample data work properly for bwc
    2) verify timeline visualization work properly for bwc
    This PR also simplifies check_filter_and_query bwc test.
    It first removes Unique Visitors check because even fix
    the time interval, the number of unique visitors number
    is random. Then it simplifies this bwc test.
    
    add more tests in check_timeline and modify test names
    change one query content to make bwc tests more robust
    update license header
    add missing test and solve timestamp issue
    fix eslint and comments
    
    Partially Resolved:
    https://github.com/opensearch-project/opensearch-build/issues/705
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- cypress/integration/osd/check_default_page.js
+++ cypress/integration/osd/check_default_page.js
@@ -1,32 +1,8 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
  */
 
-/*
- * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
-/*
- * Modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
- */
-
-/* es-lint-disable for missing definitions */
 /* eslint-disable */
 import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
   
@@ -37,8 +13,7 @@ describe('verify default landing page work for bwc', () => {
     miscUtils.visitPage('');
   });
   
-  it('the overview page is set as the default landing pag', () => {
+  it('the overview page is set as the default landing page', () => {
     cy.url().should('include', '/app/opensearch_dashboards_overview#/');
-    cy.contains('Display a different page on log in');
   });
 });
