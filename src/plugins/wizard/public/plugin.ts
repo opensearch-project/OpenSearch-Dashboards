9628160b6423eefd809a0d47d849b0b272d7e885
Ashwin Pc <ashwinpc@amazon.com>
2021-12-11 01:40:09 +0000

    Moves Drag and Drop to create visualization menu
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>

--- src/plugins/wizard/public/plugin.ts
+++ src/plugins/wizard/public/plugin.ts
@@ -3,42 +3,53 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { i18n } from '@osd/i18n';
 import {
   AppMountParameters,
+  AppNavLinkStatus,
   CoreSetup,
   CoreStart,
-  DEFAULT_APP_CATEGORIES,
   Plugin,
+  PluginInitializerContext,
 } from '../../../core/public';
-import { WizardPluginStartDependencies, WizardServices } from './types';
+import {
+  WizardPluginSetupDependencies,
+  WizardPluginStartDependencies,
+  WizardServices,
+} from './types';
 import { PLUGIN_NAME } from '../common';
 
-export class WizardPlugin implements Plugin<void, void, object, WizardPluginStartDependencies> {
-  public setup(core: CoreSetup<WizardPluginStartDependencies>) {
-    // Register an application into the side navigation menu
+export class WizardPlugin
+  implements Plugin<void, void, WizardPluginSetupDependencies, WizardPluginStartDependencies> {
+  constructor(public initializerContext: PluginInitializerContext) {}
+
+  public setup(
+    core: CoreSetup<WizardPluginStartDependencies>,
+    { visualizations }: WizardPluginSetupDependencies
+  ) {
+    // Register the plugin to core
     core.application.register({
       id: 'wizard',
       title: PLUGIN_NAME,
-      euiIconType: 'inputOutput',
-      defaultPath: '#/',
-      category: DEFAULT_APP_CATEGORIES.opensearchDashboards,
+      navLinkStatus: AppNavLinkStatus.hidden,
       async mount(params: AppMountParameters) {
         // Load application bundle
         const { renderApp } = await import('./application');
         // Get start services as specified in opensearch_dashboards.json
         const [coreStart, pluginsStart] = await core.getStartServices();
+        const { data, savedObjects, navigation } = pluginsStart;
 
         const services: WizardServices = {
           ...coreStart,
           toastNotifications: coreStart.notifications.toasts,
-          data: pluginsStart.data,
-          savedObjectsPublic: pluginsStart.savedObjects,
-          navigation: pluginsStart.navigation,
+          data,
+          savedObjectsPublic: savedObjects,
+          navigation,
           setHeaderActionMenu: params.setHeaderActionMenu,
         };
 
         // make sure the index pattern list is up to date
-        pluginsStart.data.indexPatterns.clearCache();
+        data.indexPatterns.clearCache();
         // make sure a default index pattern exists
         // if not, the page will be redirected to management and visualize won't be rendered
         await pluginsStart.data.indexPatterns.ensureDefaultIndexPattern();
@@ -46,6 +57,20 @@ export class WizardPlugin implements Plugin<void, void, object, WizardPluginStar
         // Render the application
         return renderApp(params, services);
       },
+    });
+
+    // Register the plugin as an alias to create visualization
+    visualizations.registerAlias({
+      name: 'wizard',
+      title: 'Wizard',
+      description: i18n.translate('wizard.vizPicker.description', {
+        defaultMessage: 'TODO...',
+      }),
+      // TODO: Replace with actual icon once available
+      icon: 'vector',
+      stage: 'beta',
+      aliasApp: 'wizard',
+      aliasPath: '#/',
     });
   }
 
