86d13f85e54a1f323cf7bde0b7f9c5c763be0d72
Tommy Markley <markleyt@amazon.com>
2022-01-12 10:16:58 -0600

    Fixes incorrect license headers (#1131)
    Resolves #1130
    
    Signed-off-by: Tommy Markley <markleyt@amazon.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -81,14 +81,6 @@ const APACHE_2_0_LICENSE_HEADER = `
  */
 `;
 
-const ELASTIC_LICENSE_HEADER = `
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
- */
-`;
-
 const SAFER_LODASH_SET_HEADER = `
 /*
  * Elasticsearch B.V licenses this file to you under the MIT License.
@@ -171,7 +163,6 @@ module.exports = {
           'error',
           {
             licenses: [
-              ELASTIC_LICENSE_HEADER,
               SAFER_LODASH_SET_HEADER,
               SAFER_LODASH_SET_LODASH_HEADER,
               SAFER_LODASH_SET_DEFINITELYTYPED_HEADER,
@@ -207,7 +198,6 @@ module.exports = {
           'error',
           {
             licenses: [
-              ELASTIC_LICENSE_HEADER,
               APACHE_2_0_LICENSE_HEADER,
               SAFER_LODASH_SET_HEADER,
               SAFER_LODASH_SET_DEFINITELYTYPED_HEADER,
@@ -230,7 +220,6 @@ module.exports = {
           {
             licenses: [
               OSS_HEADER,
-              ELASTIC_LICENSE_HEADER,
               APACHE_2_0_LICENSE_HEADER,
               SAFER_LODASH_SET_LODASH_HEADER,
               SAFER_LODASH_SET_DEFINITELYTYPED_HEADER,
@@ -254,7 +243,6 @@ module.exports = {
           {
             licenses: [
               OSS_HEADER,
-              ELASTIC_LICENSE_HEADER,
               APACHE_2_0_LICENSE_HEADER,
               SAFER_LODASH_SET_HEADER,
               SAFER_LODASH_SET_LODASH_HEADER,
