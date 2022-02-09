6db875f0c396ec83915ffbe22e63a47d7f4bae7d
Tommy Markley <markleyt@amazon.com>
2022-02-09 14:11:55 -0600

    Removes storybook package and related code (#1172)
    In order to address potential licensing issues as well as resolve related CVEs, 
    all storybook code is removed. The storybook features have been broken
    since the fork and the work to fix everything was greater than removing it. 
    Alternatives will be considered in the future.
    
    Resolves #1130 
    Resolves #1171
    
    Signed-off-by: Tommy Markley <markleyt@amazon.com>

--- src/dev/precommit_hook/casing_check_config.js
+++ src/dev/precommit_hook/casing_check_config.js
@@ -61,10 +61,6 @@ export const IGNORE_FILE_GLOBS = [
   // filename must match language code which requires capital letters
   '**/translations/*.json',
 
-  // Storybook has predetermined filesnames
-  '**/preview-body.html',
-  '**/preview-head.html',
-
   // filename required by api-extractor
   'api-documenter.json',
 
