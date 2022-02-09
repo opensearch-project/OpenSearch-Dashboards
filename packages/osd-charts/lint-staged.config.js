520fb94b0ace5e398661ea3d2cd4e73015019830
Miki <miki@amazon.com>
2022-02-09 13:01:05 -0800

    Removes unnecessary config and ci files from Charts pre-fold
    Signed-off-by: Miki <miki@amazon.com>

--- packages/osd-charts/lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // TODO: make this `lint:fix` if ever added to `pre-commit`
-  // using in `commit-msg` doesn't save fixed changes so
-  // in the meantime should error on bad linting when committing
-  '*.{js,ts,tsx}': 'cross-env NODE_ENV=production eslint --quiet',
-};
