a1e1e2e1e9179d1074782bf11926774008fcc75b
Miki <miki@amazon.com>
2022-02-10 12:44:35 -0800

    Bootstraps `packages/osd-eui`
    * Define `osd:bootstrap`
    * Extend OSD's `tsconfig.base.json`
    * Add `remark-parse` to OSD's `nohoist` list
    * Catch promise rejections in dtsgenerator and exit
    
    Signed-off-by: Miki <miki@amazon.com>

--- packages/osd-eui/.babelrc.js
+++ packages/osd-eui/.babelrc.js
@@ -7,7 +7,7 @@ module.exports = {
   "presets": [
     ["@babel/env", {
       // `targets` property set via `.browserslistrc`
-      "useBuiltIns": process.env.NO_COREJS_POLYFILL ? false : "usage",
+      "useBuiltIns": "entry",
       "corejs": 3,
       "modules": process.env.BABEL_MODULES ? process.env.BABEL_MODULES === 'false' ? false : process.env.BABEL_MODULES : "commonjs" // babel's default is commonjs
     }],
