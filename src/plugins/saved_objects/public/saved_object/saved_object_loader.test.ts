0641af37ca23f318cc3024469efc480b080bd742
sitbubu <royi.sitbon@logz.io>
2022-06-09 16:53:22 +0300

    Add unit-tests for updated_at as null undefined or unknown
    Signed-off-by: sitbubu <royi.sitbon@logz.io>

---
+++ src/plugins/saved_objects/public/saved_object/saved_object_loader.test.ts
@@ -0,0 +1,106 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Any modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { SavedObjectsClientContract } from 'opensearch-dashboards/public';
+import { SavedObjectLoader } from './saved_object_loader';
+
+describe('SimpleSavedObjectLoader', () => {
+  const createLoader = (updatedAt?: any) => {
+    const id = 'logstash-*';
+    const type = 'index-pattern';
+
+    const savedObject = {
+      attributes: {},
+      id,
+      type,
+      updated_at: updatedAt as any,
+    };
+
+    client = {
+      ...client,
+      find: jest.fn(() =>
+        Promise.resolve({
+          total: 1,
+          savedObjects: [savedObject],
+        })
+      ),
+    } as any;
+
+    return new SavedObjectLoader(savedObject, client);
+  };
+
+  let client: SavedObjectsClientContract;
+  beforeEach(() => {
+    client = {
+      update: jest.fn(),
+      create: jest.fn(),
+      delete: jest.fn(),
+    } as any;
+  });
+
+  it('persists updated_at if undefined', async () => {
+    const loader = createLoader(undefined);
+    const savedObjects = await loader.findAll();
+
+    expect(savedObjects.hits).toEqual([
+      {
+        id: 'logstash-*',
+        updated_at: undefined,
+        url: '#/index-pattern/logstash-*',
+      },
+    ]);
+  });
+
+  it("persists updated_at as undefined if doesn't exist", async () => {
+    const loader = createLoader();
+    const savedObjects = await loader.findAll();
+
+    expect(savedObjects.hits).toEqual([
+      {
+        id: 'logstash-*',
+        updated_at: undefined,
+        url: '#/index-pattern/logstash-*',
+      },
+    ]);
+  });
+
+  it('set updated_at as undefined if null', async () => {
+    const loader = createLoader(null);
+    const savedObjects = await loader.findAll();
+
+    expect(savedObjects.hits).toEqual([
+      {
+        id: 'logstash-*',
+        updated_at: undefined,
+        url: '#/index-pattern/logstash-*',
+      },
+    ]);
+  });
+});
