46bc9ff05e1b601e3107e72922e871cdfe33bd60
Brooke Green <cptn@amazon.com>
2022-06-16 18:10:46 -0700

    Adds the data source selector and a useIndexPatterns hook
    Signed-off-by: Brooke Green <cptn@amazon.com>

--- src/plugins/wizard/public/application/utils/use/use_index_pattern.tsx
+++ src/plugins/wizard/public/application/utils/use/use_index_pattern.tsx
@@ -28,3 +28,47 @@ export const useIndexPattern = (): IndexPattern | undefined => {
 
   return indexPattern;
 };
+
+export const useIndexPatterns = () => {
+  const { indexPattern: indexId = '' } = useTypedSelector((state) => state.visualization);
+  const [indexPatterns, setIndexPatterns] = useState<IndexPattern[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | undefined>(undefined);
+  const {
+    services: { data },
+  } = useOpenSearchDashboards<WizardServices>();
+
+  let foundSelected;
+  if (!loading && !error) {
+    foundSelected = indexPatterns.filter((p) => p.id === indexId)[0];
+    if (foundSelected === undefined) {
+      setError(
+        new Error("Attempted to select an index pattern that wasn't in the index pattern list")
+      );
+    }
+  }
+
+  useEffect(() => {
+    const handleUpdate = async () => {
+      try {
+        const ids = await data.indexPatterns.getIds(indexId);
+        const patterns = await Promise.all(ids.map((id) => data.indexPatterns.get(id)));
+        setIndexPatterns(patterns);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    handleUpdate();
+    // we want to run this hook exactly once, which you do by an empty dep array
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    indexPatterns,
+    error,
+    loading,
+    selected: foundSelected!,
+  };
+};
