71933173bae82b3288d52e2094a370aa0c61f160
Brooke <97559014+CPTNB@users.noreply.github.com>
2022-06-22 17:55:31 -0700

    Adds the data source selector and a useIndexPatterns hook (#1763)
    Signed-off-by: Brooke Green <cptn@amazon.com>

--- src/plugins/wizard/public/application/components/side_nav.tsx
+++ src/plugins/wizard/public/application/components/side_nav.tsx
@@ -3,35 +3,22 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
-import { i18n } from '@osd/i18n';
-import { EuiFormLabel, EuiTabbedContent, EuiTabbedContentTab } from '@elastic/eui';
-import { useOpenSearchDashboards } from '../../../../opensearch_dashboards_react/public';
-import { WizardServices } from '../../types';
+import React, { ReactElement } from 'react';
+import { EuiTabbedContent, EuiTabbedContentTab } from '@elastic/eui';
 import './side_nav.scss';
-import { useTypedDispatch, useTypedSelector } from '../utils/state_management';
-import { setIndexPattern } from '../utils/state_management/visualization_slice';
 import { useVisualizationType } from '../utils/use';
+import { DataSourceSelect } from './data_source_select';
 import { DataTab } from '../contributions';
 import { StyleTabConfig } from '../../services/type_service';
 
 export const SideNav = () => {
-  const {
-    services: {
-      data,
-      savedObjects: { client: savedObjectsClient },
-    },
-  } = useOpenSearchDashboards<WizardServices>();
-  const { IndexPatternSelect } = data.ui;
-  const { indexPattern: indexPatternId } = useTypedSelector((state) => state.visualization);
-  const dispatch = useTypedDispatch();
   const {
     ui: { containerConfig },
   } = useVisualizationType();
 
   const tabs: EuiTabbedContentTab[] = Object.entries(containerConfig).map(
     ([containerName, config]) => {
-      let content = null;
+      let content: null | ReactElement = null;
       switch (containerName) {
         case 'data':
           content = <DataTab key="containerName" />;
@@ -52,27 +39,8 @@ export const SideNav = () => {
 
   return (
     <section className="wizSidenav">
-      <div className="wizDatasourceSelector">
-        <EuiFormLabel>
-          {i18n.translate('wizard.nav.dataSource.selector.title', {
-            defaultMessage: 'Index Pattern',
-          })}
-        </EuiFormLabel>
-        <IndexPatternSelect
-          savedObjectsClient={savedObjectsClient}
-          placeholder={i18n.translate('wizard.nav.dataSource.selector.placeholder', {
-            defaultMessage: 'Select index pattern',
-          })}
-          indexPatternId={indexPatternId || ''}
-          onChange={async (newIndexPatternId: any) => {
-            const newIndexPattern = await data.indexPatterns.get(newIndexPatternId);
-
-            if (newIndexPattern) {
-              dispatch(setIndexPattern(newIndexPatternId));
-            }
-          }}
-          isClearable={false}
-        />
+      <div className="wizDatasourceSelect">
+        <DataSourceSelect />
       </div>
       <EuiTabbedContent tabs={tabs} className="wizSidenavTabs" />
     </section>
