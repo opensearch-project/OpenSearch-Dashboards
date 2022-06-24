79cf07d4f0508e710db9a872760020c5cb3b5855
Brooke Green <cptn@amazon.com>
2022-06-24 12:00:58 -0700

    Adding breadcrumbs for drag and drop creation
    Signed-off-by: Brooke Green <cptn@amazon.com>

--- src/plugins/wizard/public/application/components/top_nav.tsx
+++ src/plugins/wizard/public/application/components/top_nav.tsx
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useMemo } from 'react';
-import { PLUGIN_ID } from '../../../common';
+import { i18n } from '@osd/i18n';
+import React, { useMemo, useEffect } from 'react';
+import { PLUGIN_ID, VISUALIZE_ID } from '../../../common';
 import { useOpenSearchDashboards } from '../../../../opensearch_dashboards_react/public';
 import { getTopNavconfig } from '../utils/get_top_nav_config';
 import { WizardServices } from '../../types';
@@ -16,6 +17,7 @@ export const TopNav = () => {
   const { services } = useOpenSearchDashboards<WizardServices>();
   const {
     setHeaderActionMenu,
+    chrome,
     navigation: {
       ui: { TopNavMenu },
     },
@@ -23,6 +25,25 @@ export const TopNav = () => {
 
   const config = useMemo(() => getTopNavconfig(services), [services]);
   const indexPattern = useIndexPattern();
+
+  useEffect(() => {
+    const visualizeHref = window.location.href.split(`${PLUGIN_ID}#/`)[0] + `${VISUALIZE_ID}#/`;
+    chrome.setBreadcrumbs([
+      {
+        text: i18n.translate('visualize.listing.breadcrumb', {
+          defaultMessage: 'Visualize',
+        }),
+        href: visualizeHref,
+      },
+      {
+        text: i18n.translate('wizard.nav.breadcrumb.create', {
+          defaultMessage: 'Create',
+        }),
+      },
+    ]);
+    // we want to run this hook exactly once, which you do by an empty dep array
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="wizTopNav">
