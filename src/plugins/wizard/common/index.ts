e9447bb7588aba8d61fd2a5f8a718e4c6769f9f5
Brooke <97559014+CPTNB@users.noreply.github.com>
2022-07-06 13:28:45 -0700

    Adding breadcrumbs for drag and drop creation (#1797)
    Signed-off-by: Brooke Green <cptn@amazon.com>

--- src/plugins/wizard/common/index.ts
+++ src/plugins/wizard/common/index.ts
@@ -5,5 +5,6 @@
 
 export const PLUGIN_ID = 'wizard';
 export const PLUGIN_NAME = 'Wizard';
+export const VISUALIZE_ID = 'visualize';
 
 export { WizardSavedObjectAttributes, WIZARD_SAVED_OBJECT } from './wizard_saved_object_attributes';
