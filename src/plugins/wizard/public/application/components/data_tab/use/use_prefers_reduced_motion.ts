1b174349206a4667ead711c38c178cb9b205c1ad
Ashwin Pc <ashwinpc@amazon.com>
2022-07-08 10:05:22 +0000

    simpler usePrefersReducedMotion
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>

--- src/plugins/wizard/public/application/components/data_tab/use/use_prefers_reduced_motion.ts
+++ src/plugins/wizard/public/application/components/data_tab/use/use_prefers_reduced_motion.ts
@@ -6,24 +6,18 @@
 import { useEffect, useState } from 'react';
 
 const QUERY = '(prefers-reduced-motion: no-preference)';
-const isRenderingOnServer = typeof window === 'undefined';
-
-const getInitialState = () => {
-  // For our initial server render, we won't know if the user
-  // prefers reduced motion, but it doesn't matter. This value
-  // will be overwritten on the client, before any animations
-  // occur.
-  return isRenderingOnServer ? true : !window.matchMedia(QUERY).matches;
-};
 
 export function usePrefersReducedMotion() {
-  const [prefersReducedMotion, setPrefersReducedMotion] = useState(getInitialState);
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(
+    !window.matchMedia(QUERY).matches
+  );
 
   useEffect(() => {
     const mediaQueryList = window.matchMedia(QUERY);
     const listener = (event) => {
       setPrefersReducedMotion(!event.matches);
     };
+
     if (mediaQueryList.addEventListener) {
       mediaQueryList.addEventListener('change', listener);
     }
