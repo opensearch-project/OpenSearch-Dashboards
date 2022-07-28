9171a5d44fdf2a6425b7d17b3dd2c29c1eced52b
Louis Chu <lingzhichu.clz@gmail.com>
2022-07-28 08:44:45 -0700

    Instantiate credential management plugin code structure (#1996)
    Signed-off-by: Kristen Tian <tyarong@amazon.com>

---
+++ src/plugins/credential_management/public/plugin.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { i18n } from '@osd/i18n';
+import { AppMountParameters, CoreSetup, CoreStart, Plugin } from '../../../core/public';
+import {
+  CredentialManagementPluginSetup,
+  CredentialManagementPluginStart,
+  AppPluginStartDependencies,
+} from './types';
+import { PLUGIN_NAME } from '../common';
+
+export class CredentialManagementPlugin
+  implements Plugin<CredentialManagementPluginSetup, CredentialManagementPluginStart> {
+  public setup(core: CoreSetup): CredentialManagementPluginSetup {
+    // Register an application into the side navigation menu
+    core.application.register({
+      id: 'credentialManagement',
+      title: PLUGIN_NAME,
+      async mount(params: AppMountParameters) {
+        // Load application bundle
+        const { renderApp } = await import('./application');
+        // Get start services as specified in opensearch_dashboards.json
+        const [coreStart, depsStart] = await core.getStartServices();
+        // Render the application
+        return renderApp(coreStart, depsStart as AppPluginStartDependencies, params);
+      },
+    });
+
+    // Return methods that should be available to other plugins
+    return {
+      getGreeting() {
+        return i18n.translate('credentialManagement.greetingText', {
+          defaultMessage: 'Hello from {name}!',
+          values: {
+            name: PLUGIN_NAME,
+          },
+        });
+      },
+    };
+  }
+
+  public start(core: CoreStart): CredentialManagementPluginStart {
+    return {};
+  }
+
+  public stop() {}
+}
