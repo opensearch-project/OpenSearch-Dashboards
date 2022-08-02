086936e8becab2828d453993f515d7c5f2ed94ea
Kristen Tian <tyarong@amazon.com>
2022-08-02 15:49:46 -0700

    Register Data source to savedObject & Update license header
    Signed-off-by: Kristen Tian <tyarong@amazon.com>

---
+++ src/plugins/data_source/common/data_sources/types.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { SavedObjectAttributes } from 'src/core/types';
+
+export interface DataSourceAttributes extends SavedObjectAttributes {
+  title: string;
+  endpoint: string;
+}
---
+++ src/plugins/data_source/server/saved_objects/data_source.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { SavedObjectsType } from 'opensearch-dashboards/server';
+
+export const dataSource: SavedObjectsType = {
+  name: 'data-source',
+  namespaceType: 'agnostic',
+  hidden: false,
+  management: {
+    icon: 'apps', // todo: pending ux #2034
+    defaultSearchField: 'title',
+    importableAndExportable: true,
+    getTitle(obj) {
+      return obj.attributes.title;
+    },
+    // todo: update getEditUrl & getInAppUrl after #2021
+  },
+  mappings: {
+    dynamic: false,
+    properties: {
+      title: {
+        type: 'text',
+      },
+      endpoint: {
+        type: 'keyword',
+        index: false,
+        doc_values: false,
+      },
+      version: {
+        type: 'integer',
+      },
+    },
+  },
+};
