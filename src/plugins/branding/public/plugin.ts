4c05efd2aef6c26bb26975c9173915dbc75a840e
abbyhu2000 <abigailhu2000@gmail.com>
2022-08-09 18:32:32 +0000

    branding assignment service

--- src/plugins/branding/public/plugin.ts
+++ src/plugins/branding/public/plugin.ts
@@ -1,9 +1,16 @@
 import { i18n } from '@osd/i18n';
+import { PluginInitializerContext } from 'opensearch-dashboards/server';
 import { AppMountParameters, CoreSetup, CoreStart, Plugin } from '../../../core/public';
 import { BrandingPluginSetup, BrandingPluginStart, AppPluginStartDependencies } from './types';
 import { PLUGIN_NAME } from '../common';
+import { ConfigSchema } from '../config';
+import { BrandingAssignment } from './services';
 
 export class BrandingPlugin implements Plugin<BrandingPluginSetup, BrandingPluginStart> {
+  private readonly brandingAssignment = new BrandingAssignment();
+
+  constructor(private readonly initializerContext: PluginInitializerContext<ConfigSchema>) {}
+
   public setup(core: CoreSetup): BrandingPluginSetup {
     // Register an application into the side navigation menu
     core.application.register({
