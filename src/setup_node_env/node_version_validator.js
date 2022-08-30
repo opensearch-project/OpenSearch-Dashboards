c53b54a34dee6294e430393de15469f0021ae67d
AbhishekReddy1127 <nallamsa@amazon.com>
2022-08-30 05:15:00 +0000

    Updated text
    Signed-off-by: AbhishekReddy1127 <nallamsa@amazon.com>

--- src/setup_node_env/node_version_validator.js
+++ src/setup_node_env/node_version_validator.js
@@ -53,7 +53,7 @@ if (!isVersionValid) {
   } else {
     errorMessage +=
       '\nOpenSearch Dashboards is running as OSD_NODE_HOME environment variable is set, ' +
-      'Ignoring any incpatibilities in node version.';
+      'So ignoring any incpatibilities in node version.';
     console.warn(errorMessage);
   }
 }
