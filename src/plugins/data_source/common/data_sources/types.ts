847e4c6fac6ae1e182e7ba5e296b6f0b4839990a
Louis Chu <lingzhichu.clz@gmail.com>
2022-09-01 15:49:22 -0500

    [MD] Credentials security redesign (#2253)
    1. Data model changes for data source saved object
    2. Server side changes for data source saved object
      a. Implement data_source_saved_objects_client_wrapper to integrate with CryptographyClient for password
      encryption / decryption.
      b. Change data_source_service to fetch credentials directly from data source (still decrypt via CryptographyClient)
      c. Fix unit tests accordingly
    
    Signed-off-by: Louis Chu <clingzhi@amazon.com>
    Signed-off-by: Kristen Tian <tyarong@amazon.com>

--- src/plugins/data_source/common/data_sources/types.ts
+++ src/plugins/data_source/common/data_sources/types.ts
@@ -7,6 +7,20 @@ import { SavedObjectAttributes } from 'src/core/types';
 
 export interface DataSourceAttributes extends SavedObjectAttributes {
   title: string;
+  description?: string;
   endpoint: string;
-  noAuth: boolean;
+  auth: {
+    type: AuthType;
+    credentials: UsernamePasswordTypedContent | undefined;
+  };
+}
+
+export interface UsernamePasswordTypedContent extends SavedObjectAttributes {
+  username: string;
+  password: string;
+}
+
+export enum AuthType {
+  NoAuth = 'no_auth',
+  UsernamePasswordType = 'username_password',
 }
--- src/plugins/data_source/common/index.ts
+++ src/plugins/data_source/common/index.ts
@@ -6,11 +6,3 @@
 export const PLUGIN_ID = 'dataSource';
 export const PLUGIN_NAME = 'data_source';
 export const DATA_SOURCE_SAVED_OBJECT_TYPE = 'data-source';
-export const CREDENTIAL_SAVED_OBJECT_TYPE = 'credential';
-
-export {
-  CredentialMaterialsType,
-  CredentialSavedObjectAttributes,
-  CredentialMaterials,
-  UsernamePasswordTypedContent,
-} from './credentials';
