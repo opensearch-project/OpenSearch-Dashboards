77af7f9a3331d48a25631010b9e31c4c472b3266
Ashwin P Chandran <ashwinpc@amazon.com>
2022-09-07 18:32:37 -0700

    [D&D] Adds Bar line and Area charts to Wizard (#2266)
    * Working histogram
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * Adds histogram options
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * Adds line chart
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * Adds area chart
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * updates nav sizes
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * adds resizeable container to right nav
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * Misc fixes
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * Adds chart switcher test
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * Github comment feedback
    
    Co-authored-by: Josh Romero <rmerqg@amazon.com>
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * Updates copy
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    Co-authored-by: Josh Romero <rmerqg@amazon.com>

---
+++ src/plugins/wizard/public/visualizations/vislib/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { createHistogramConfig } from './histogram';
+export { createLineConfig } from './line';
+export { createAreaConfig } from './area';
