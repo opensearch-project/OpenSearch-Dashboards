93f6872b76fece38be8b91e114b18eb621acacca
Ashwin P Chandran <ashwinpc@amazon.com>
2022-09-12 14:24:59 -0700

    [D&D] Fixes time series for new chart types (#2309)
    * fixes time series for new chart types
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    * moves translate to source string
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>
    
    Signed-off-by: Ashwin Pc <ashwinpc@amazon.com>

--- src/plugins/wizard/public/application/utils/use/use_can_save.ts
+++ src/plugins/wizard/public/application/utils/use/use_can_save.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { i18n } from '@osd/i18n';
 import { useTypedSelector } from '../state_management';
 
 export const useCanSave = () => {
@@ -20,12 +21,20 @@ export const useCanSave = () => {
 
 // TODO: Need to finalize the error messages
 const getErrorMsg = (isEmpty, hasNoChange, hasDraftAgg) => {
+  const i18nTranslate = (key: string, defaultMessage: string) =>
+    i18n.translate(`wizard.saveVisualizationTooltip.${key}`, {
+      defaultMessage,
+    });
+
   if (isEmpty) {
-    return 'The canvas is empty. Add some aggregations before saving.';
+    return i18nTranslate('empty', 'The canvas is empty. Add some aggregations before saving.');
   } else if (hasNoChange) {
-    return 'Add some changes before saving.';
+    return i18nTranslate('noChange', 'Add some changes before saving.');
   } else if (hasDraftAgg) {
-    return 'Has unapplied aggregations changes, update them before saving.';
+    return i18nTranslate(
+      'hasDraftAgg',
+      'Has unapplied aggregations changes, update them before saving.'
+    );
   } else {
     return undefined;
   }
