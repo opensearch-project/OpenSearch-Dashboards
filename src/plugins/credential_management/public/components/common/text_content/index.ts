680fe55807a032aeba1e30723f76134f26ebde39
Kristen Tian <tyarong@amazon.com>
2022-09-13 21:00:08 -0700

    Delete credential management
    Signed-off-by: Kristen Tian <tyarong@amazon.com>

--- src/plugins/credential_management/public/components/common/text_content/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * as LocalizedContent from '../text_content/text_content';
--- src/plugins/credential_management/public/components/index.tsx
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export { CredentialsTableWithRouter } from './credential_table';
-export { CreateCredentialWizardWithRouter } from './create_credential_wizard';
-export { EditCredentialPageWithRouter } from './edit_credential_wizard';
