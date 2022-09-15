442111d7720eca29ade9d315a315b53013ac8bb9
Manideep Pabba <109986843+mpabba3003@users.noreply.github.com>
2022-09-14 21:16:12 -0500

    [MD] Datasource Management - Create data source - Unit tests (#2341)
    * Unit test cases for data source management - create
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    * adding tests to utils.ts & changing it to test
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    Signed-off-by: Kristen Tian <tyarong@amazon.com>

--- src/plugins/data_source_management/public/components/utils.ts
+++ src/plugins/data_source_management/public/components/utils.ts
@@ -7,16 +7,16 @@ import { SavedObjectsClientContract } from 'src/core/public';
 import { DataSourceTableItem, DataSourceAttributes } from '../types';
 
 export async function getDataSources(savedObjectsClient: SavedObjectsClientContract) {
-  return (
-    savedObjectsClient
-      .find({
-        type: 'data-source',
-        fields: ['id', 'description', 'title'],
-        perPage: 10000,
-      })
-      .then((response) =>
-        response.savedObjects
-          .map((source) => {
+  return savedObjectsClient
+    .find({
+      type: 'data-source',
+      fields: ['id', 'description', 'title'],
+      perPage: 10000,
+    })
+    .then(
+      (response) =>
+        response?.savedObjects
+          ?.map((source) => {
             const id = source.id;
             const title = source.get('title');
             const description = source.get('description');
@@ -36,27 +36,24 @@ export async function getDataSources(savedObjectsClient: SavedObjectsClientContr
             } else {
               return 0;
             }
-          })
-      ) || []
-  );
+          }) || []
+    );
 }
 
 export async function getDataSourceById(
   id: string,
   savedObjectsClient: SavedObjectsClientContract
 ) {
-  return (
-    savedObjectsClient.get('data-source', id).then((response) => {
-      const attributes: any = response?.attributes || {};
-      return {
-        id: response.id,
-        title: attributes.title,
-        endpoint: attributes.endpoint,
-        description: attributes.description || '',
-        auth: attributes.auth,
-      };
-    }) || null
-  );
+  return savedObjectsClient.get('data-source', id).then((response) => {
+    const attributes: any = response?.attributes || {};
+    return {
+      id: response.id,
+      title: attributes.title,
+      endpoint: attributes.endpoint,
+      description: attributes.description || '',
+      auth: attributes.auth,
+    };
+  });
 }
 
 export async function createSingleDataSource(
