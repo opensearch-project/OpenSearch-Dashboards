e5b0088698bd5d16e64c6cc3232acab44ec6437e
Anan Zhuang <ananzh@amazon.com>
2022-09-28 04:15:49 +0000

    [Table Visualization] make table vis column resizable
    * add resizable state to column
    
    Partially resolve:
    https://github.com/opensearch-project/OpenSearch-Dashboards/issues/2305
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/plugins/vis_type_table/public/components/table_vis_component.tsx
+++ src/plugins/vis_type_table/public/components/table_vis_component.tsx
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
+import { isEqual } from 'lodash';
 import { EuiDataGridProps, EuiDataGrid } from '@elastic/eui';
 
 import { IInterpreterRenderHandlers } from 'src/plugins/expressions';
 import { Table } from '../table_vis_response_handler';
-import { TableVisConfig } from '../types';
+import { TableVisConfig, ColumnWidth } from '../types';
 import { getDataGridColumns } from './table_vis_grid_columns';
 
 interface TableVisComponentProps {
@@ -45,7 +46,56 @@ export const TableVisComponent = ({ table, visConfig, handlers }: TableVisCompon
     }) as EuiDataGridProps['renderCellValue'];
   }, [rows, pagination.pageIndex, pagination.pageSize]);
 
-  const dataGridColumns = getDataGridColumns(table, visConfig, handlers);
+  // resize column
+  const [columnsWidth, setColumnsWidth] = useState<ColumnWidth[]>(
+    handlers.uiState.get('vis.columnsWidth') || []
+  );
+  const curColumnsWidth = useRef<{
+    columnsWidth: ColumnWidth[];
+  }>({
+    columnsWidth: handlers.uiState?.get('vis.columnsWidth'),
+  });
+
+  const onColumnResize: EuiDataGridProps['onColumnResize'] = useCallback(
+    ({ columnId, width }) => {
+      setColumnsWidth((prevState) => {
+        const nextColIndex = columns.findIndex((c) => c.id === columnId);
+        const prevColIndex = prevState.findIndex((c) => c.colIndex === nextColIndex);
+        const nextState = [...prevState];
+        const updatedColWidth = { colIndex: nextColIndex, width };
+
+        // if updated column index is not found, then add it to nextState
+        // else reset it in nextState
+        if (prevColIndex < 0) nextState.push(updatedColWidth);
+        else nextState[prevColIndex] = updatedColWidth;
+
+        // update uiState
+        handlers.uiState?.set('vis.columnsWidth', nextState);
+        return nextState;
+      });
+    },
+    [columns, setColumnsWidth, handlers.uiState]
+  );
+
+  useEffect(() => {
+    const updateTable = () => {
+      const updatedVisState = handlers.uiState?.getChanges()?.vis;
+      if (!isEqual(updatedVisState?.columnsWidth, curColumnsWidth.current.columnsWidth)) {
+        curColumnsWidth.current.columnsWidth = updatedVisState?.columnsWidth;
+        setColumnsWidth(updatedVisState?.columnsWidth || []);
+      }
+    };
+
+    if (handlers.uiState) {
+      handlers.uiState.on('change', updateTable);
+    }
+
+    return () => {
+      handlers.uiState?.off('change', updateTable);
+    };
+  }, [handlers.uiState]);
+
+  const dataGridColumns = getDataGridColumns(table, visConfig, handlers, columnsWidth);
   return (
     <EuiDataGrid
       aria-label="tableVis"
@@ -61,6 +111,7 @@ export const TableVisComponent = ({ table, visConfig, handlers }: TableVisCompon
         onChangeItemsPerPage,
         onChangePage,
       }}
+      onColumnResize={onColumnResize}
     />
   );
 };
