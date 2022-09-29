41fb4691a6ac2523137707b6a061169e29fd1030
Anan Zhuang <ananzh@amazon.com>
2022-09-29 23:12:54 +0000

    update config comments to be more general
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/dev/jest/config.js
+++ src/dev/jest/config.js
@@ -91,7 +91,7 @@ export default {
     '^.+\\.html?$': 'jest-raw-loader',
   },
   transformIgnorePatterns: [
-    // ignore all node_modules except monaco-editor which requires babel transforms to handle dynamic import()
+    // ignore all node_modules except those which require babel transforms to handle dynamic import()
     // since ESM modules are not natively supported in Jest yet (https://github.com/facebook/jest/issues/4842)
     '[/\\\\]node_modules(?![\\/\\\\](monaco-editor|weak-lru-cache|ordered-binary|d3-color))[/\\\\].+\\.js$',
     'packages/osd-pm/dist/index.js',
