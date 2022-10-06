a668b8bd5a635ab2a8a5d650793c377bd480cec1
Kristen Tian <105667444+kristenTian@users.noreply.github.com>
2022-10-06 11:25:39 -0700

    [MD] Address UX comments for index pattern pages (#2505)
    Signed-off-by: Kristen Tian <tyarong@amazon.com>
    
    Signed-off-by: Kristen Tian <tyarong@amazon.com>
    (cherry picked from commit 4095e123d77182bed213fedf41be00ccaffd34a7)

--- src/plugins/index_pattern_management/public/components/create_index_pattern_wizard/components/step_data_source/components/header/header.tsx
+++ src/plugins/index_pattern_management/public/components/create_index_pattern_wizard/components/step_data_source/components/header/header.tsx
@@ -3,38 +3,39 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import './header.scss';
 
 import {
-  EuiTitle,
-  EuiSpacer,
-  EuiText,
-  EuiFlexItem,
   EuiButton,
   EuiFlexGroup,
+  EuiFlexItem,
   EuiRadio,
+  EuiSelectable,
+  EuiSpacer,
+  EuiText,
+  EuiTitle,
 } from '@elastic/eui';
 
-import { FormattedMessage } from '@osd/i18n/react';
 import { i18n } from '@osd/i18n';
+import { FormattedMessage } from '@osd/i18n/react';
+import { useEffectOnce } from 'react-use';
 import {
   DataSourceRef,
   IndexPatternManagmentContext,
 } from 'src/plugins/index_pattern_management/public/types';
-import { SavedObjectFinderUi } from '../../../../../../../../../plugins/saved_objects/public';
 import { useOpenSearchDashboards } from '../../../../../../../../../plugins/opensearch_dashboards_react/public';
-import { StepInfo } from '../../../../types';
+import { getDataSources } from '../../../../../../components/utils';
+import { DataSourceTableItem, StepInfo } from '../../../../types';
+import { LoadingState } from '../../../loading_state';
 
 interface HeaderProps {
-  onDataSourceSelected: (id: string, type: string) => void;
+  onDataSourceSelected: (id: string, type: string, title: string) => void;
   dataSourceRef: DataSourceRef;
   goToNextStep: (dataSourceRef: DataSourceRef) => void;
   isNextStepDisabled: boolean;
   stepInfo: StepInfo;
 }
-
-const DATA_SOURCE_PAGE_SIZE = 5;
 
 export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
   const { dataSourceRef, onDataSourceSelected, goToNextStep, isNextStepDisabled, stepInfo } = props;
@@ -42,10 +43,48 @@ export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
 
   const [defaultChecked, setDefaultChecked] = useState(true);
   const [dataSourceChecked, setDataSourceChecked] = useState(false);
+  const [dataSources, setDataSources] = useState<DataSourceTableItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const { savedObjects, uiSettings } = useOpenSearchDashboards<
-    IndexPatternManagmentContext
-  >().services;
+  const {
+    savedObjects,
+    notifications: { toasts },
+  } = useOpenSearchDashboards<IndexPatternManagmentContext>().services;
+
+  useEffectOnce(() => {
+    fetchDataSources();
+  });
+
+  const fetchDataSources = () => {
+    setIsLoading(true);
+    getDataSources(savedObjects.client)
+      .then((fetchedDataSources: DataSourceTableItem[]) => {
+        setIsLoading(false);
+        if (fetchedDataSources?.length) {
+          setDataSources(fetchedDataSources);
+        }
+      })
+      .catch(() => {
+        toasts.addWarning(
+          i18n.translate(
+            'indexPatternManagement.createIndexPattern.stepDataSource.fetchDataSourceError',
+            {
+              defaultMessage: 'Unable to find existing data sources',
+            }
+          )
+        );
+      });
+  };
+
+  const onSelectedDataSource = (options: DataSourceTableItem[]) => {
+    const selectedDataSource = options.find(({ checked }) => checked);
+    setDataSources(options);
+    onDataSourceSelected(
+      selectedDataSource!.id,
+      selectedDataSource!.type,
+      selectedDataSource!.title
+    );
+  };
 
   const onChangeDefaultChecked = (e) => {
     setDefaultChecked(e.target.checked);
@@ -58,111 +97,119 @@ export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
   };
 
   return (
-    <div>
-      <EuiTitle size="s">
-        <h2>
+    <>
+      <div>
+        <EuiTitle size="s">
+          <h2>
+            <FormattedMessage
+              id="indexPatternManagement.createIndexPattern.stepDataSourceHeader"
+              defaultMessage="Step {currentStepNumber} of {totalStepNumber}: Configure data source"
+              values={{ currentStepNumber, totalStepNumber }}
+            />
+          </h2>
+        </EuiTitle>
+        <EuiText>
           <FormattedMessage
-            id="indexPatternManagement.createIndexPattern.stepDataSourceHeader"
-            defaultMessage="Step {currentStepNumber} of {totalStepNumber}: Configure data source"
-            values={{ currentStepNumber, totalStepNumber }}
+            id="indexPatternManagement.createIndexPattern.stepDataSourceLabel"
+            defaultMessage="Please pick the data source -- within which to configure index patterns."
           />
-        </h2>
-      </EuiTitle>
-      <EuiText>
-        <FormattedMessage
-          id="indexPatternManagement.createIndexPattern.stepDataSourceLabel"
-          defaultMessage="Please pick the data source -- within which to configure index patterns."
+        </EuiText>
+        <EuiSpacer size="m" />
+        <EuiRadio
+          data-test-subj="createIndexPatternStepDataSourceUseDefaultRadio"
+          id={'useDefault'}
+          label={
+            <FormattedMessage
+              id="indexPatternManagement.createIndexPattern.stepDataSource.useDefaultLabel"
+              defaultMessage="Use default"
+            />
+          }
+          checked={defaultChecked}
+          onChange={(e) => onChangeDefaultChecked(e)}
+          compressed
         />
-      </EuiText>
-      <EuiSpacer size="m" />
-      <EuiRadio
-        data-test-subj="createIndexPatternStepDataSourceUseDefaultRadio"
-        id={'useDefault'}
-        label={
+        <EuiText size="s" className="dataSourceRadioHelperText">
           <FormattedMessage
-            id="indexPatternManagement.createIndexPattern.stepDataSource.useDefaultLabel"
-            defaultMessage="Use default"
+            id="indexPatternManagement.createIndexPattern.stepDataSource.useDefault.helperText"
+            defaultMessage="Uses the default data source configured by the OpenSearch Dashboards administrator."
           />
-        }
-        checked={defaultChecked}
-        onChange={(e) => onChangeDefaultChecked(e)}
-        compressed
-      />
-      <EuiText size="s" className="dataSourceRadioHelperText">
-        <FormattedMessage
-          id="indexPatternManagement.createIndexPattern.stepDataSource.useDefault.helperText"
-          defaultMessage="Uses the default data source configured by the OpenSearch Dashboards administrator."
+        </EuiText>
+        <EuiSpacer size="l" />
+        <EuiRadio
+          data-test-subj="createIndexPatternStepDataSourceUseDataSourceRadio"
+          id={'useDataSource'}
+          label={
+            <FormattedMessage
+              id="indexPatternManagement.createIndexPattern.stepDataSource.useDataSourceLabel"
+              defaultMessage="Choose data source"
+            />
+          }
+          checked={dataSourceChecked}
+          onChange={(e) => onChangeDataSourceChecked(e)}
+          compressed
         />
-      </EuiText>
-      <EuiSpacer size="l" />
-      <EuiRadio
-        data-test-subj="createIndexPatternStepDataSourceUseDataSourceRadio"
-        id={'useDataSource'}
-        label={
+        <EuiText size="s" className="dataSourceRadioHelperText">
           <FormattedMessage
-            id="indexPatternManagement.createIndexPattern.stepDataSource.useDataSourceLabel"
-            defaultMessage="Choose data source"
+            id="indexPatternManagement.createIndexPattern.stepDataSource.useDataSource.helperText"
+            defaultMessage="Connect to an existing external data source."
           />
-        }
-        checked={dataSourceChecked}
-        onChange={(e) => onChangeDataSourceChecked(e)}
-        compressed
-      />
-      <EuiText size="s" className="dataSourceRadioHelperText">
-        <FormattedMessage
-          id="indexPatternManagement.createIndexPattern.stepDataSource.useDataSource.helperText"
-          defaultMessage="Connect to an existing external data source."
-        />
-      </EuiText>
-      {dataSourceChecked && (
-        <EuiFlexItem grow={false}>
-          <EuiSpacer size="m" />
-          <SavedObjectFinderUi
-            key="searchSavedObjectFinder"
-            onChoose={onDataSourceSelected}
-            showFilter={false}
-            noItemsMessage={i18n.translate(
-              'indexPatternManagement.createIndexPattern.searchSelection.notFoundLabel',
-              {
-                defaultMessage: 'No data sources have been configured yet.',
+        </EuiText>
+        {dataSourceChecked && (
+          <EuiFlexItem grow={false}>
+            <EuiSpacer size="m" />
+            <EuiSelectable
+              aria-label={
+                <FormattedMessage
+                  id="indexPatternManagement.createIndexPattern.stepDataSource.searchLabel"
+                  defaultMessage="Search data sources"
+                />
               }
-            )}
-            savedObjectMetaData={[
-              {
-                type: 'data-source',
-                getIconForSavedObject: () => 'apps', // todo: #2034
-                name: i18n.translate(
-                  'indexPatternManagement.createIndexPattern.searchSelection.savedObjectType.dataSource',
+              searchable
+              searchProps={{
+                'data-test-subj': 'selectDataSources',
+                placeholder: i18n.translate(
+                  'indexPatternManagement.createIndexPattern.stepDataSource.searchPlaceHolder',
                   {
-                    defaultMessage: 'Data Source',
+                    defaultMessage: 'Search data sources',
                   }
                 ),
-              },
-            ]}
-            fixedPageSize={DATA_SOURCE_PAGE_SIZE}
-            uiSettings={uiSettings}
-            savedObjects={savedObjects}
-          />
-        </EuiFlexItem>
-      )}
-      <EuiSpacer size="m" />
-      <EuiFlexGroup justifyContent="flexEnd">
-        <EuiFlexItem grow={false}>
-          <EuiButton
-            data-test-subj="createIndexPatternStepDataSourceNextStepButton"
-            fill
-            iconSide="right"
-            iconType="arrowRight"
-            onClick={() => goToNextStep(dataSourceRef)}
-            isDisabled={isNextStepDisabled && !defaultChecked}
-          >
-            <FormattedMessage
-              id="indexPatternManagement.createIndexPattern.step.nextStepButton"
-              defaultMessage="Next step"
-            />
-          </EuiButton>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    </div>
+                isInvalid: !!dataSources,
+              }}
+              singleSelection={'always'}
+              options={dataSources}
+              onChange={(newOptions) => onSelectedDataSource(newOptions)}
+            >
+              {(list, search) => (
+                <Fragment>
+                  {search}
+                  <EuiSpacer size="s" />
+                  {list}
+                </Fragment>
+              )}
+            </EuiSelectable>
+          </EuiFlexItem>
+        )}
+        <EuiSpacer size="m" />
+        <EuiFlexGroup justifyContent="flexEnd">
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              data-test-subj="createIndexPatternStepDataSourceNextStepButton"
+              fill
+              iconSide="right"
+              iconType="arrowRight"
+              onClick={() => goToNextStep(dataSourceRef)}
+              isDisabled={isNextStepDisabled && !defaultChecked}
+            >
+              <FormattedMessage
+                id="indexPatternManagement.createIndexPattern.step.nextStepButton"
+                defaultMessage="Next step"
+              />
+            </EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </div>
+
+      {isLoading ? <LoadingState /> : null}
+    </>
   );
 };
