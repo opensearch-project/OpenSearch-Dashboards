224650e68cb9bd565b3f132829f6632509627835
Zhongnan Su <szhongna@amazon.com>
2022-10-06 14:02:33 -0700

    [MD] Display error toast for create index pattern with data source (#2506)
    * add error toast for create index pattern with data source
    
    Signed-off-by: Su <szhongna@amazon.com>
    
    * update change log
    
    Signed-off-by: Su <szhongna@amazon.com>
    
    Signed-off-by: Su <szhongna@amazon.com>
    Signed-off-by: Sergey V. Osipov <sipopo@yandex.ru>

--- src/plugins/index_pattern_management/public/components/create_index_pattern_wizard/create_index_pattern_wizard.tsx
+++ src/plugins/index_pattern_management/public/components/create_index_pattern_wizard/create_index_pattern_wizard.tsx
@@ -134,6 +134,7 @@ export class CreateIndexPatternWizard extends Component<
             id: errorMsg.props.id,
             color: 'warning',
             iconType: 'alert',
+            text: errors.body.message,
           },
         ]),
       }));
