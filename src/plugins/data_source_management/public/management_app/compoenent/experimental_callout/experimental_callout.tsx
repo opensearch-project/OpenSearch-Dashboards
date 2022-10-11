9b56ff50327154cd531fcd4479ab2fc3798cf86b
Yibo Wang <109543558+yibow98@users.noreply.github.com>
2022-10-11 13:44:42 -0700

    [MD] UX refactor for datasource create page (#2501)
    * UX refactor for datasource create page
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>
    
    * address comments&add CHANGLOG
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>
    
    * Apply callout in all ds pages & update endpoint field placeholder text
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>
    
    * add documentation links
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>
    
    * update snapshot test and snapshots
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>
    
    * Update CHANGELOG.md&.lycheeexclude
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>
    
    * Update UT
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>
    
    Signed-off-by: Yibo Wang <yibow@amazon.com>

---
+++ src/plugins/data_source_management/public/management_app/compoenent/experimental_callout/experimental_callout.tsx
@@ -0,0 +1,39 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { EuiCallOut, EuiLink, EuiSpacer } from '@elastic/eui';
+import { DocLinksStart } from 'opensearch-dashboards/public';
+import {
+  EXPERIMENTAL_FEATURE,
+  EXPERIMENTAL_FEATURE_CALL_OUT_DESCRIPTION,
+  DATA_SOURCE_LEAVE_FEEDBACK_TEXT,
+  DATA_SOURCE_DOCUMENTATION_TEXT,
+  DATA_SOURCE_OPEN_FORUM_TEXT,
+} from '../../../components/text_content';
+
+export const ExperimentalCallout = ({ docLinks }: { docLinks: DocLinksStart }) => {
+  return (
+    <>
+      <EuiCallOut
+        title={EXPERIMENTAL_FEATURE}
+        iconType="iInCircle"
+        data-test-subj="data-source-experimental-call"
+      >
+        <p>
+          {EXPERIMENTAL_FEATURE_CALL_OUT_DESCRIPTION}
+          <EuiLink href={docLinks.links.noDocumentation.indexPatterns.introduction} target="_blank">
+            {DATA_SOURCE_DOCUMENTATION_TEXT}
+          </EuiLink>{' '}
+          {DATA_SOURCE_LEAVE_FEEDBACK_TEXT}
+          <EuiLink href={docLinks.links.noDocumentation.openSearchForum} target="_blank">
+            {DATA_SOURCE_OPEN_FORUM_TEXT}
+          </EuiLink>
+        </p>
+      </EuiCallOut>
+      <EuiSpacer size="m" />
+    </>
+  );
+};
