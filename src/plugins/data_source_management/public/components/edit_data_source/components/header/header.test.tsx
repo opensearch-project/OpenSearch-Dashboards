115105a3c945dae4c2218caaeacaa854b2789afa
Manideep Pabba <109986843+mpabba3003@users.noreply.github.com>
2022-10-13 21:25:42 -0500

    [MD] Adding unit test coverage for Update data source management (#2567)
    * adding unit test cases for edit data source page
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    * lint mock file
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    * updating snapshot
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    * updating CHANGELOG.MD
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    * updating CHANGELOG.MD
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    * updating CHANGELOG.MD
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>
    
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>

---
+++ src/plugins/data_source_management/public/components/edit_data_source/components/header/header.test.tsx
@@ -0,0 +1,98 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { mount, ReactWrapper } from 'enzyme';
+import React from 'react';
+import { Header } from './header';
+import { wrapWithIntl } from 'test_utils/enzyme_helpers';
+import { mockManagementPlugin } from '../../../../mocks';
+import { OpenSearchDashboardsContextProvider } from '../../../../../../opensearch_dashboards_react/public';
+import { act } from 'react-dom/test-utils';
+
+const headerTitleIdentifier = '[data-test-subj="editDataSourceTitle"]';
+const deleteIconIdentifier = '[data-test-subj="editDatasourceDeleteIcon"]';
+const confirmModalIdentifier = '[data-test-subj="editDatasourceDeleteConfirmModal"]';
+
+describe('Datasource Management: Edit Datasource Header', () => {
+  const mockedContext = mockManagementPlugin.createDataSourceManagementContext();
+  let component: ReactWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+  const mockFn = jest.fn();
+  const dataSourceName = 'testTest20';
+
+  describe('show delete icon', () => {
+    beforeEach(() => {
+      component = mount(
+        wrapWithIntl(
+          <Header
+            showDeleteIcon={true}
+            onClickDeleteIcon={mockFn}
+            dataSourceName={dataSourceName}
+          />
+        ),
+        {
+          wrappingComponent: OpenSearchDashboardsContextProvider,
+          wrappingComponentProps: {
+            services: mockedContext,
+          },
+        }
+      );
+    });
+
+    test('should render normally', () => {
+      expect(component).toMatchSnapshot();
+      expect(component.find(headerTitleIdentifier).last().text()).toBe(dataSourceName);
+    });
+    test('should show confirm delete modal pop up on trash icon click and cancel button work normally', () => {
+      component.find(deleteIconIdentifier).first().simulate('click');
+      // test if modal pop up when click the delete button
+      expect(component.find(confirmModalIdentifier).exists()).toBe(true);
+
+      act(() => {
+        // @ts-ignore
+        component.find(confirmModalIdentifier).first().props().onCancel();
+      });
+
+      component.update();
+      expect(component.find(confirmModalIdentifier).exists()).toBe(false);
+    });
+    test('should show confirm delete modal pop up on trash icon click and confirm button should delete datasource', () => {
+      component.find(deleteIconIdentifier).first().simulate('click');
+      // test if modal pop up when click the delete button
+      expect(component.find(confirmModalIdentifier).exists()).toBe(true);
+
+      act(() => {
+        // @ts-ignore
+        component.find(confirmModalIdentifier).first().props().onConfirm();
+      });
+
+      component.update();
+      expect(component.find(confirmModalIdentifier).exists()).toBe(false);
+    });
+  });
+  describe('do not show delete icon', () => {
+    beforeEach(() => {
+      component = mount(
+        wrapWithIntl(
+          <Header
+            showDeleteIcon={false}
+            onClickDeleteIcon={mockFn}
+            dataSourceName={dataSourceName}
+          />
+        ),
+        {
+          wrappingComponent: OpenSearchDashboardsContextProvider,
+          wrappingComponentProps: {
+            services: mockedContext,
+          },
+        }
+      );
+    });
+    test('should render normally', () => {
+      expect(component).toMatchSnapshot();
+      expect(component.find(headerTitleIdentifier).last().text()).toBe(dataSourceName);
+      expect(component.find(deleteIconIdentifier).exists()).toBe(false);
+    });
+  });
+});
