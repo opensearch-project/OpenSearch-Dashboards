b269deef0dfcd7aa8519170351c5b0b651275d6e
Ashwin P Chandran <ashwinpc@amazon.com>
2022-10-19 15:26:50 -0700

    [Vis Builder] Update vislib params and misc fixes (#2610)
    * Updates bar, line, area visbuilder config
    
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    
    * Misc fixes from functional testing
    
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    
    * Adds Changelog entry
    
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    Signed-off-by: vimal k <vimalinfo10@gmail.com>

--- src/plugins/vis_builder/public/visualizations/vislib/histogram/histogram_vis_type.ts
+++ src/plugins/vis_builder/public/visualizations/vislib/histogram/histogram_vis_type.ts
@@ -46,6 +46,28 @@ export const createHistogramConfig = (): VisualizationTypeOptions<HistogramOptio
             min: 0,
             max: 1,
             aggFilter: ['!geohash_grid', '!geotile_grid', '!filter', '!filters'],
+            defaults: { aggTypes: ['date_histogram', 'terms'] },
+          },
+          {
+            group: AggGroupNames.Buckets,
+            name: 'group',
+            title: i18n.translate('visTypeVislib.histogram.groupTitle', {
+              defaultMessage: 'Split series',
+            }),
+            min: 0,
+            max: 3,
+            aggFilter: ['!geohash_grid', '!geotile_grid', '!filter'],
+            defaults: { aggTypes: ['terms'] },
+          },
+          {
+            group: AggGroupNames.Buckets,
+            name: 'split',
+            title: i18n.translate('visTypeVislib.histogram.splitTitle', {
+              defaultMessage: 'Split chart',
+            }),
+            min: 0,
+            max: 1,
+            aggFilter: ['!geohash_grid', '!geotile_grid', '!filter'],
             defaults: { aggTypes: ['terms'] },
           },
         ]),
