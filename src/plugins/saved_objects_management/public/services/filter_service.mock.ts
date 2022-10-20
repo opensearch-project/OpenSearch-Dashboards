ee15b77f9bd2c66e021007b7e8cbd328771e08ff
Craig Perkins <cwperx@amazon.com>
2022-10-20 16:47:54 -0400

    Update saved_objects_table snapshot
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

---
+++ src/plugins/saved_objects_management/public/services/filter_service.mock.ts
@@ -0,0 +1,49 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Any modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+import {
+  SavedObjectsManagementFilterService,
+  SavedObjectsManagementFilterServiceSetup,
+  SavedObjectsManagementFilterServiceStart,
+} from './filter_service';
+
+const createSetupMock = (): jest.Mocked<SavedObjectsManagementFilterServiceSetup> => {
+  const mock = {
+    register: jest.fn(),
+  };
+  return mock;
+};
+
+const createStartMock = (): jest.Mocked<SavedObjectsManagementFilterServiceStart> => {
+  const mock = {
+    has: jest.fn(),
+    getAll: jest.fn(),
+  };
+
+  mock.has.mockReturnValue(true);
+  mock.getAll.mockReturnValue([]);
+
+  return mock;
+};
+
+const createServiceMock = (): jest.Mocked<PublicMethodsOf<SavedObjectsManagementFilterService>> => {
+  const mock = {
+    setup: jest.fn().mockReturnValue(createSetupMock()),
+    start: jest.fn().mockReturnValue(createStartMock()),
+  };
+  return mock;
+};
+
+export const filterServiceMock = {
+  create: createServiceMock,
+  createSetup: createSetupMock,
+  createStart: createStartMock,
+};
