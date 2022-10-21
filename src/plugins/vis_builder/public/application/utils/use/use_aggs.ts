b9eb5d27f71242d3a9d2e71ad024d9143f53785d
Ashwin P Chandran <ashwinpc@amazon.com>
2022-10-21 03:52:07 +0000

    Correctly sets timerange
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>

---
+++ src/plugins/vis_builder/public/application/utils/use/use_aggs.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { cloneDeep } from 'lodash';
+import { useLayoutEffect, useMemo, useState } from 'react';
+import { useOpenSearchDashboards } from '../../../../../opensearch_dashboards_react/public';
+import { WizardServices } from '../../../types';
+import { useTypedSelector, useTypedDispatch } from '../state_management';
+import { useIndexPatterns } from './use_index_pattern';
+
+export const useAggs = () => {
+  const {
+    services: {
+      data: {
+        search: { aggs: aggService },
+        query: {
+          timefilter: { timefilter },
+        },
+      },
+    },
+  } = useOpenSearchDashboards<WizardServices>();
+  const indexPattern = useIndexPatterns().selected;
+  const [timeRange, setTimeRange] = useState(timefilter.getTime());
+  const aggConfigParams = useTypedSelector(
+    (state) => state.visualization.activeVisualization?.aggConfigParams
+  );
+  const dispatch = useTypedDispatch();
+
+  const aggConfigs = useMemo(() => {
+    const configs =
+      indexPattern && aggService.createAggConfigs(indexPattern, cloneDeep(aggConfigParams));
+    return configs;
+  }, [aggConfigParams, aggService, indexPattern]);
+
+  useLayoutEffect(() => {
+    const subscription = timefilter.getTimeUpdate$().subscribe(() => {
+      setTimeRange(timefilter.getTime());
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [dispatch, timefilter]);
+
+  return {
+    indexPattern,
+    aggConfigs,
+    aggs: aggConfigs?.aggs ?? [],
+    timeRange,
+  };
+};
--- src/plugins/vis_builder/public/visualizations/vislib/common/create_vis.ts
+++ src/plugins/vis_builder/public/visualizations/vislib/common/create_vis.ts
@@ -3,11 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { AggConfigs, IndexPattern, search } from '../../../../../data/public';
+import { AggConfigs, IndexPattern } from '../../../../../data/public';
 import { Vis } from '../../../../../visualizations/public';
 import { getSearchService, getTimeFilter } from '../../../plugin_services';
-
-const { isDateHistogramBucketAggConfig } = search.aggs;
 
 export const createVis = async (
   type: string,
@@ -21,12 +19,8 @@ export const createVis = async (
 
   const responseAggs = vis.data.aggs.getResponseAggs().filter((agg) => agg.enabled);
   responseAggs.forEach((agg) => {
-    if (isDateHistogramBucketAggConfig(agg)) {
-      const timeFilter = getTimeFilter();
-
-      agg.params.timeRange = timeFilter.getTime();
-    }
+    const timeFilter = getTimeFilter();
+    agg.params.timeRange = timeFilter.getTime();
   });
-
   return vis;
 };
