dcd2ee87cc4cd79d11a2785bc4505671b9f93cf6
Zhongnan Su <szhongna@amazon.com>
2022-10-26 22:41:29 -0700

    [MD] Refactor data source server side error handling (#2661)
    - Rename DataSourceConfigError to DataSourceError
    - Add helper createDataSourceError() to create data source error from， and replace usage of new DataSourceError(e) with createDataSourceError(e)
    - import createDataSourceError()  from data source plugin setup interface,
    refactor to move data source error response logic from router.ts to resolve_index.ts.
    
    Signed-off-by: Su <szhongna@amazon.com>
    Signed-off-by: Sergey V. Osipov <sipopo@yandex.ru>

--- src/plugins/data/server/search/opensearch_search/opensearch_search_strategy.ts
+++ src/plugins/data/server/search/opensearch_search/opensearch_search_strategy.ts
@@ -33,6 +33,7 @@ import { SharedGlobalConfig, Logger } from 'opensearch-dashboards/server';
 import { SearchResponse } from 'elasticsearch';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '@opensearch-project/opensearch';
+import { DataSourcePluginSetup } from 'src/plugins/data_source/server';
 import { SearchUsage } from '../collectors/usage';
 import { toSnakeCase } from './to_snake_case';
 import {
@@ -47,7 +48,8 @@ import { decideClient } from './decide_client';
 export const opensearchSearchStrategyProvider = (
   config$: Observable<SharedGlobalConfig>,
   logger: Logger,
-  usage?: SearchUsage
+  usage?: SearchUsage,
+  dataSource?: DataSourcePluginSetup
 ): ISearchStrategy => {
   return {
     search: async (context, request, options) => {
@@ -88,6 +90,10 @@ export const opensearchSearchStrategyProvider = (
         };
       } catch (e) {
         if (usage) usage.trackError();
+
+        if (dataSource && request.dataSourceId) {
+          throw dataSource.createDataSourceError(e);
+        }
         throw e;
       }
     },
