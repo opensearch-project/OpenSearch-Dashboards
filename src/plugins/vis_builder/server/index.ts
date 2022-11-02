dfde15afbdb7c88f9085caaa00f7a0e1e37ccaf8
Ashwin P Chandran <ashwinpc@amazon.com>
2022-11-02 09:58:50 -0700

    Enable visbuilder by default (#2725)
    * Enable visbuilder by default
    
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    
    * Adds changelog entry
    
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    Signed-off-by: Sergey V. Osipov <sipopo@yandex.ru>

--- src/plugins/vis_builder/server/index.ts
+++ src/plugins/vis_builder/server/index.ts
@@ -17,8 +17,5 @@ export function plugin(initializerContext: PluginInitializerContext) {
 export { VisBuilderPluginSetup, VisBuilderPluginStart } from './types';
 
 export const config: PluginConfigDescriptor<ConfigSchema> = {
-  exposeToBrowser: {
-    enabled: true,
-  },
   schema: configSchema,
 };
