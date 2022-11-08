7792efa667a3690affb07c4b9903766c3c48c372
Lin Wang <wonglam@amazon.com>
2022-11-08 12:12:51 +0800

    update test/plugin_functional/test_suites/application_links/redirect_app_links.ts
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- test/plugin_functional/test_suites/application_links/redirect_app_links.ts
+++ test/plugin_functional/test_suites/application_links/redirect_app_links.ts
@@ -28,7 +28,7 @@
  * under the License.
  */
 
-import url from 'url';
+import { URL } from 'url';
 import expect from '@osd/expect';
 import { PluginFunctionalProviderContext } from '../../services';
 import '../../plugins/core_app_status/public/types';
@@ -40,8 +40,8 @@ declare global {
 }
 
 const getPathWithHash = (absoluteUrl: string) => {
-  const parsed = url.parse(absoluteUrl);
-  return `${parsed.path}${parsed.hash ?? ''}`;
+  const parsed = new URL(absoluteUrl);
+  return parsed.toString().substring(parsed.origin.length);
 };
 
 export default function ({ getService, getPageObjects }: PluginFunctionalProviderContext) {
