7dd01d6538a04fae5d11b6108c2dd476862f747d
Tommy Markley <5437176+tmarkley@users.noreply.github.com>
2022-12-07 19:32:16 +0000

    Fixes misleading embeddable plugin error message
    Resolves #1479
    Resolves #3083
    
    Signed-off-by: Tommy Markley <5437176+tmarkley@users.noreply.github.com>

--- src/plugins/embeddable/public/lib/errors.ts
+++ src/plugins/embeddable/public/lib/errors.ts
@@ -49,7 +49,7 @@ export class EmbeddableFactoryNotFoundError extends Error {
   constructor(type: string) {
     super(
       i18n.translate('embeddableApi.errors.embeddableFactoryNotFound', {
-        defaultMessage: `{type} can't be loaded. Please upgrade to the default distribution of OpenSearch and OpenSearch Dashboards with the appropriate license.`,
+        defaultMessage: `OpenSearch Dashboards can't load "{type}" visualizations. Check for a missing plugin or an incompatible visualization type.`,
         values: {
           type,
         },
