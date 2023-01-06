289e33c52a37bf7bef31f5a8312275bf3a08023b
Josh Romero <rmerqg@amazon.com>
2023-01-06 22:56:48 +0000

    [Chore] Fix webpack config filter
    - filter on `vega-lite` instead of aliased `vega-lite-next`
    - Keep new babel proposal plugin version in sync with other babel plugins
    
    Signed-off-by: Josh Romero <rmerqg@amazon.com>

--- packages/osd-optimizer/src/worker/webpack.config.ts
+++ packages/osd-optimizer/src/worker/webpack.config.ts
@@ -207,7 +207,7 @@ export function getWebpackConfig(bundle: Bundle, bundleRefs: BundleRefs, worker:
           test: /\.(js|tsx?)$/,
           // vega-lite and some of its dependencies don't have es5 builds
           // so we need to build from source and transpiled for webpack v4
-          exclude: /[\/\\]node_modules[\/\\](?!vega-(lite-next|label|functions)[\/\\])/,
+          exclude: /[\/\\]node_modules[\/\\](?!vega-(lite|label|functions)[\/\\])/,
           use: {
             loader: 'babel-loader',
             options: {
