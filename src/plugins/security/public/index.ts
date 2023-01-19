481b4097e91c4f9e9de1a07491556636a812eb71
Chang Liu <lc12251109@gmail.com>
2023-01-18 16:16:14 -0800

    Create security plugin
    Signed-off-by: Chang Liu <lc12251109@gmail.com>

---
+++ src/plugins/security/public/index.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ *   Copyright OpenSearch Contributors
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+import { SecurityPlugin } from './plugin';
+import { PluginInitializerContext } from '../../../core/public';
+
+// This exports static code and TypeScript types,
+// as well as, OpenSearchDashboards Platform `plugin()` initializer.
+export function plugin(initializerContext: PluginInitializerContext) {
+  return new SecurityPlugin(initializerContext);
+}
+export { SecurityPluginSetup, SecurityPluginStart } from './types';
---
+++ src/plugins/security/public/types.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*
+ *   Copyright OpenSearch Contributors
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+import { NavigationPublicPluginStart } from '../../../plugins/navigation/public';
+import {
+  SavedObjectsManagementPluginSetup,
+  SavedObjectsManagementPluginStart,
+} from '../../../plugins/saved_objects_management/public';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SecurityPluginSetup {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SecurityPluginStart {}
+
+export interface SecurityPluginSetupDependencies {
+  savedObjectsManagement: SavedObjectsManagementPluginSetup;
+}
+
+export interface SecurityPluginStartDependencies {
+  navigation: NavigationPublicPluginStart;
+  savedObjectsManagement: SavedObjectsManagementPluginStart;
+}
