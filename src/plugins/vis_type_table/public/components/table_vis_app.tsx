3a7fbe74de043d3f2d392812c6210cb1c5e68869
Anan Zhuang <ananzh@amazon.com>
2023-02-07 18:27:05 +0000

    [2.x Backport][Table Vis] move format table, consolidate types and add unit tests
    Currently, table data is formatted by a until function convertToFormattedData
    in TableVisComponent. In this PR, we moved the formatting data process
    to table_vis_response_handler.ts to combine with other data process
    logics. In this way, component render and data handling logics are
    completely isolated. This PR also solidate some types.
    
    Backport PR:
    https://github.com/opensearch-project/OpenSearch-Dashboards/pull/3397
    
    Issue Resolved:
    https://github.com/opensearch-project/OpenSearch-Dashboards/issues/3395
    https://github.com/opensearch-project/OpenSearch-Dashboards/issues/2856
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/plugins/vis_type_table/public/components/table_vis_app.tsx
+++ src/plugins/vis_type_table/public/components/table_vis_app.tsx
@@ -4,20 +4,22 @@
  */
 
 import './table_vis_app.scss';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames';
 import { CoreStart } from 'opensearch-dashboards/public';
 import { I18nProvider } from '@osd/i18n/react';
 import { IInterpreterRenderHandlers } from 'src/plugins/expressions';
+import { PersistedState } from '../../../visualizations/public';
 import { OpenSearchDashboardsContextProvider } from '../../../opensearch_dashboards_react/public';
-import { TableContext } from '../table_vis_response_handler';
-import { TableVisConfig, ColumnSort, ColumnWidth, TableUiState } from '../types';
+import { TableVisData } from '../table_vis_response_handler';
+import { TableVisConfig } from '../types';
 import { TableVisComponent } from './table_vis_component';
 import { TableVisComponentGroup } from './table_vis_component_group';
+import { getTableUIState, TableUiState } from '../utils';
 
 interface TableVisAppProps {
   services: CoreStart;
-  visData: TableContext;
+  visData: TableVisData;
   visConfig: TableVisConfig;
   handlers: IInterpreterRenderHandlers;
 }
@@ -38,12 +40,7 @@ export const TableVisApp = ({
     visTable__groupInColumns: direction === 'column',
   });
 
-  // TODO: remove duplicate sort and width state
-  // Issue: https://github.com/opensearch-project/OpenSearch-Dashboards/issues/2704#issuecomment-1299380818
-  const [sort, setSort] = useState<ColumnSort>({ colIndex: null, direction: null });
-  const [width, setWidth] = useState<ColumnWidth[]>([]);
-
-  const tableUiState: TableUiState = { sort, setSort, width, setWidth };
+  const tableUiState: TableUiState = getTableUIState(handlers.uiState as PersistedState);
 
   return (
     <I18nProvider>
