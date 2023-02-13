59622d8e407989aa76d554f2596d179943e4da47
Vijayan Balasubramanian <balasvij@amazon.com>
2023-02-13 09:16:29 -0800

    Add new ui setting for size (#3399)
    Introduce new ui setting for custom vector map's size
    parameter. The default value is 1000.
    Users can increase this limit by updating this value
    in Advanced Settings.
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>
    Signed-off-by: Arpit Bandejiya <abandeji@amazon.com>

--- src/plugins/region_map/common/constants/shared.ts
+++ src/plugins/region_map/common/constants/shared.ts
@@ -5,3 +5,4 @@
 
 export const DEFAULT_MAP_CHOICE = 'default';
 export const CUSTOM_MAP_CHOICE = 'custom';
+export const CUSTOM_VECTOR_MAP_MAX_SIZE_SETTING = 'visualization:regionmap:customVectorMapMaxSize';
