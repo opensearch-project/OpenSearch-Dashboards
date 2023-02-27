90ef3d64350e2f25267640f23a1135bdfba0173f
Miki <miki@amazon.com>
2023-02-27 09:29:41 -0800

    Allow selecting the Node.js binary OSD uses
    New reusable use_node scripts take care of selecting the correct Node.js binary based OSD_NODE_HOME, OSD_HOME, and whatever is in the PATH. It also takes care of finding the binary on FreeBSD.
    * The startup scripts check `OSD_NODE_HOME` and `NODE_HOME` before falling back to use the bundled or system-wide Node.js binary.
    * Update `package.json` to replace `node` with `use_node`.
    * Update file templates and shell scripts to internally use `use_node`.
    * Update the build scripts to copy `use_node` into releases
    * Make tests that execute node internally, honor the binary being used.
    
    Signed-off-by: Miki <miki@amazon.com>

--- src/dev/ci_setup/setup_percy.sh
+++ src/dev/ci_setup/setup_percy.sh
@@ -23,7 +23,7 @@ export PUPPETEER_EXECUTABLE_PATH
 ###
 ### Set Percy parallel build support environment vars
 ###
-eval "$(node ./src/dev/ci_setup/get_percy_env)"
+eval "$(./scripts/use_node ./src/dev/ci_setup/get_percy_env)"
 echo " -- PERCY_PARALLEL_NONCE='$PERCY_PARALLEL_NONCE'"
 echo " -- PERCY_PARALLEL_TOTAL='$PERCY_PARALLEL_TOTAL'"
 echo " -- PERCY_BRANCH='$PERCY_BRANCH'"
