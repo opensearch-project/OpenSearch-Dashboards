53a10634291a9667f585e65c2fad4bc75a4169a5
Aigerim Suleimenova <aigerim.suleimenova27@gmail.com>
2023-04-12 13:05:37 +0200

    Update src/setup_node_env/root/index.js
    Co-authored-by: Josh Romero <rmerqg@amazon.com>

--- src/setup_node_env/root/index.js
+++ src/setup_node_env/root/index.js
@@ -31,6 +31,7 @@
 var force = require('./force')(process.argv);
 var uid = process.getuid && process.getuid();
 var isRoot = require('./is_root')(uid);
+
 if (isRoot && !force) {
   console.error('OpenSearch Dashboards should not be run as root.  Use --allow-root to continue.');
   process.exit(1);
