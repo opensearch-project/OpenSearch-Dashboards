41daddda3dc4337f0fd804876c80153cc208ef08
David Sinclair <david@sinclair.tech>
2023-04-23 21:24:38 -0700

    revert border and prevent destroy options
    Signed-off-by: David Sinclair <david@sinclair.tech>

--- src/plugins/embeddable/public/lib/panel/embeddable_panel.tsx
+++ src/plugins/embeddable/public/lib/panel/embeddable_panel.tsx
@@ -83,12 +83,6 @@ interface Props {
   SavedObjectFinder: React.ComponentType<any>;
   stateTransfer?: EmbeddableStateTransfer;
   hideHeader?: boolean;
-  // By default, embeddable.destroy() is called when this component unmounts.
-  // To prevent this default behavior, set this prop to true.
-  isDestroyPrevented?: boolean;
-  // Toggle off the border and shadow applied around the embeddable.
-  // By default, the embeddable will have a shadow and border around it.
-  isBorderless?: boolean;
 }
 
 interface State {
@@ -206,10 +200,6 @@ export class EmbeddablePanel extends React.Component<Props, State> {
     if (this.state.errorEmbeddable) {
       this.state.errorEmbeddable.destroy();
     }
-
-    if (!this.props.isDestroyPrevented) {
-      this.props.embeddable.destroy();
-    }
   }
 
   public onFocus = (focusedPanelIndex: string) => {
@@ -243,8 +233,6 @@ export class EmbeddablePanel extends React.Component<Props, State> {
         paddingSize="none"
         role="figure"
         aria-labelledby={headerId}
-        hasBorder={!this.props.isBorderless}
-        hasShadow={!this.props.isBorderless}
       >
         {!this.props.hideHeader && (
           <PanelHeader
