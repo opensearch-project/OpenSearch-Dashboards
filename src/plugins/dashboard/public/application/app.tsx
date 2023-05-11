b6a783de0c9d949d0267545f3b69688d5a8f0ed9
abbyhu2000 <abigailhu2000@gmail.com>
2023-05-11 22:02:14 +0000

    Replace angular modules with react components
    Use React to start up the dashboard app, and use react routing to configure basic
    routing for dashboard plugin.
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

---
+++ src/plugins/dashboard/public/application/app.tsx
@@ -0,0 +1,37 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Any modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+import { AppMountParameters } from 'opensearch-dashboards/public';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { DashboardConstants, createDashboardEditUrl } from '../dashboard_constants';
+import { DashboardEditor, DashboardListing, DashboardNoMatch } from './components';
+
+export interface DashboardAppProps {
+  onAppLeave: AppMountParameters['onAppLeave'];
+}
+
+export const DashboardApp = ({ onAppLeave }: DashboardAppProps) => {
+  return (
+    <Switch>
+      <Route exact path={['/', DashboardConstants.LANDING_PAGE_PATH]}>
+        <DashboardListing />
+      </Route>
+      <Route
+        exact
+        path={[DashboardConstants.CREATE_NEW_DASHBOARD_URL, createDashboardEditUrl(':id')]}
+      >
+        <DashboardEditor />
+      </Route>
+      <DashboardNoMatch />
+    </Switch>
+  );
+};
