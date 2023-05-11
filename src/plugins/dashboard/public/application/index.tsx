675826264147661066dbbf2844ebcf78e8366e1e
abbyhu2000 <abigailhu2000@gmail.com>
2023-05-11 22:02:14 +0000

    Replace angular modules with react components
    Use React to start up the dashboard app, and use react routing to configure basic
    routing for dashboard plugin.
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

---
+++ src/plugins/dashboard/public/application/index.tsx
@@ -0,0 +1,42 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Any modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { AppMountParameters } from 'opensearch-dashboards/public';
+import { OpenSearchDashboardsContextProvider } from '../../../opensearch_dashboards_react/public';
+import { addHelpMenuToAppChrome } from './help_menu/help_menu_util';
+import { DashboardApp } from './app';
+import { DashboardServices } from '../types';
+export * from './embeddable';
+export * from './actions';
+
+export const renderApp = (
+  { element, appBasePath, onAppLeave }: AppMountParameters,
+  services: DashboardServices
+) => {
+  addHelpMenuToAppChrome(services.chrome, services.docLinks);
+
+  const app = (
+    <Router history={services.history}>
+      <OpenSearchDashboardsContextProvider services={services}>
+        <services.i18n.Context>
+          <DashboardApp onAppLeave={onAppLeave} />
+        </services.i18n.Context>
+      </OpenSearchDashboardsContextProvider>
+    </Router>
+  );
+
+  ReactDOM.render(app, element);
+
+  return () => ReactDOM.unmountComponentAtNode(element);
+};
