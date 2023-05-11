eb2c0f2f931f617642397762c0fe9a01a4710470
abbyhu2000 <abigailhu2000@gmail.com>
2023-05-11 22:02:14 +0000

    Replace angular modules with react components
    Use React to start up the dashboard app, and use react routing to configure basic
    routing for dashboard plugin.
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

---
+++ src/plugins/dashboard/public/application/components/dashboard_editor.tsx
@@ -0,0 +1,10 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+
+export const DashboardEditor = () => {
+  return <div>Dashboard Editor</div>;
+};
