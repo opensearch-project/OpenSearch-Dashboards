2f94a6dd87d53c61a83775d181e7d3e1df6971e0
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-05-23 18:51:49 -0700

    showing default tooltip on mark (#4120)
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- src/plugins/vis_augmenter/public/constants.ts
+++ src/plugins/vis_augmenter/public/constants.ts
@@ -11,3 +11,4 @@ export const EVENT_MARK_SIZE = 100;
 export const EVENT_MARK_SIZE_ENLARGED = 140;
 export const EVENT_MARK_SHAPE = 'triangle-up';
 export const EVENT_TIMELINE_HEIGHT = 25;
+export const EVENT_TOOLTIP_CENTER_ON_MARK = 5;
