0536917cf864664b9e38059889018b1b75e5d4ce
zhichao-aws <zhichaog@amazon.com>
2023-06-16 14:16:53 +0800

    workspace dropdown list (#9)
    Add workspace dropdown list 
    
    ---------
    
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>
    Signed-off-by: SuZhoue-Joe <suzhou@amazon.com>
    Signed-off-by: suzhou <suzhou@amazon.com>
    Co-authored-by: SuZhoue-Joe <suzhou@amazon.com>

---
+++ src/plugins/workspace/public/containers/workspace_dropdown_list/workspace_dropdown_list.tsx
@@ -0,0 +1,83 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
+
+import { EuiButton, EuiComboBox, EuiComboBoxOptionOption } from '@elastic/eui';
+import useObservable from 'react-use/lib/useObservable';
+import { CoreStart, WorkspaceAttribute } from '../../../../../core/public';
+
+type WorkspaceOption = EuiComboBoxOptionOption<WorkspaceAttribute>;
+
+interface WorkspaceDropdownListProps {
+  coreStart: CoreStart;
+  onCreateWorkspace: () => void;
+  onSwitchWorkspace: (workspaceId: string) => Promise<void>;
+}
+
+function workspaceToOption(workspace: WorkspaceAttribute): WorkspaceOption {
+  return { label: workspace.name, key: workspace.id, value: workspace };
+}
+
+export function WorkspaceDropdownList(props: WorkspaceDropdownListProps) {
+  const { coreStart, onCreateWorkspace, onSwitchWorkspace } = props;
+  const workspaceList = useObservable(coreStart.workspaces.client.workspaceList$, []);
+  const currentWorkspaceId = useObservable(coreStart.workspaces.client.currentWorkspaceId$, '');
+
+  const [loading, setLoading] = useState(false);
+  const [workspaceOptions, setWorkspaceOptions] = useState([] as WorkspaceOption[]);
+
+  const currentWorkspaceOption = useMemo(() => {
+    const workspace = workspaceList.find((item) => item.id === currentWorkspaceId);
+    if (!workspace) {
+      coreStart.notifications.toasts.addDanger(
+        `can not get current workspace of id [${currentWorkspaceId}]`
+      );
+      return [workspaceToOption({ id: currentWorkspaceId, name: '' })];
+    }
+    return [workspaceToOption(workspace)];
+  }, [workspaceList, currentWorkspaceId, coreStart]);
+  const allWorkspaceOptions = useMemo(() => {
+    return workspaceList.map(workspaceToOption);
+  }, [workspaceList]);
+
+  const onSearchChange = useCallback(
+    (searchValue: string) => {
+      setWorkspaceOptions(allWorkspaceOptions.filter((item) => item.label.includes(searchValue)));
+    },
+    [allWorkspaceOptions]
+  );
+
+  const onChange = (workspaceOption: WorkspaceOption[]) => {
+    /** switch the workspace */
+    setLoading(true);
+    onSwitchWorkspace(workspaceOption[0].key!)
+      .catch((err) =>
+        coreStart.notifications.toasts.addDanger('some error happens in workspace service')
+      )
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    onSearchChange('');
+  }, [onSearchChange]);
+
+  return (
+    <>
+      <EuiComboBox
+        async
+        options={workspaceOptions}
+        isLoading={loading}
+        onChange={onChange}
+        selectedOptions={currentWorkspaceOption}
+        singleSelection={{ asPlainText: true }}
+        onSearchChange={onSearchChange}
+        append={<EuiButton onClick={onCreateWorkspace}>Create workspace</EuiButton>}
+      />
+    </>
+  );
+}
