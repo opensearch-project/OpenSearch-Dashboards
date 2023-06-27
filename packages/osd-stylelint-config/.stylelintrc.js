f575699f39e8fefe627819de690cf371a45af463
Matt Provost <provomat@amazon.com>
2023-06-27 21:23:19 +0000

    Add no_restricted_values linter rule
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- packages/osd-stylelint-config/.stylelintrc.js
+++ packages/osd-stylelint-config/.stylelintrc.js
@@ -15,14 +15,6 @@ module.exports = {
   ],
 
   rules: {
-    '@osd/stylelint/no_restricted_properties': [
-      {
-        config: "./../../../osd-stylelint-config/config/restricted_properties.json"
-      },
-      {
-        severity: "error"
-      }
-    ],
     '@osd/stylelint/no_modifying_global_selectors': [
       {
         config: "./../../../osd-stylelint-config/config/global_selectors.json"
@@ -35,6 +27,22 @@ module.exports = {
       {
         config: './../../../osd-stylelint-config/config/colors.json'
       },
-    ]
+    ],
+    '@osd/stylelint/no_restricted_properties': [
+      {
+        config: "./../../../osd-stylelint-config/config/restricted_properties.json"
+      },
+      {
+        severity: "error"
+      }
+    ],
+    '@osd/stylelint/no_restricted_values': [
+      {
+        config: "./../../../osd-stylelint-config/config/restricted_values.json"
+      },
+      {
+        severity: "error"
+      }
+    ],
   }
 }
---
+++ packages/osd-stylelint-plugin-stylelint/src/rules/no_restricted_values/index.ts
@@ -0,0 +1,87 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import stylelint from 'stylelint';
+import { NAMESPACE } from '../..';
+import {
+  getNotCompliantMessage,
+  getRuleFromConfig,
+  getRulesFromConfig,
+  isValidOptions,
+  FileBasedConfig,
+} from '../../utils';
+
+const { ruleMessages, report } = stylelint.utils;
+
+const ruleName = 'no_restricted_values';
+const messages = ruleMessages(ruleName, {
+  expected: (message) => `${message}`,
+});
+
+const ruleFunction: stylelint.Rule = (
+  primaryOption: Record<string, any>,
+  secondaryOptionObject: Record<string, any>,
+  context
+) => {
+  return (postcssRoot, postcssResult) => {
+    const validOptions = isValidOptions(postcssResult, ruleName, primaryOption);
+    if (!validOptions) {
+      return;
+    }
+
+    const rules: FileBasedConfig = getRulesFromConfig(primaryOption.config);
+
+    const isAutoFixing = Boolean(context.fix);
+
+    postcssRoot.walkDecls((decl) => {
+      const valueRule = getRuleFromConfig(rules, decl.value);
+      if (!valueRule) {
+        return;
+      }
+
+      let shouldReport = false;
+
+      const file = postcssRoot.source?.input.file;
+      if (!file) {
+        return;
+      }
+
+      const approvedFiles = valueRule.approved;
+
+      const reportInfo = {
+        ruleName: `${NAMESPACE}/${ruleName}`,
+        result: postcssResult,
+        node: decl,
+        message: '',
+      };
+
+      if (approvedFiles) {
+        shouldReport = !approvedFiles.some((inspectedFile) => {
+          return file.includes(inspectedFile);
+        });
+      }
+
+      if (shouldReport && isAutoFixing) {
+        decl.remove();
+        return;
+      }
+
+      if (!shouldReport) {
+        return;
+      }
+
+      reportInfo.message = messages.expected(
+        getNotCompliantMessage(`Usage of value "${decl.value}" is not allowed.`)
+      );
+      report(reportInfo);
+    });
+  };
+};
+
+ruleFunction.ruleName = ruleName;
+ruleFunction.messages = messages;
+
+// eslint-disable-next-line import/no-default-export
+export default ruleFunction;
