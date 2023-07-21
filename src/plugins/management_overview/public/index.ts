1021018e36db57f0a7d15c23a69b1cb11497b2e6
Hailong Cui <ihailong@amazon.com>
2023-07-21 15:47:51 +0800

    refactor for management overview page
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- src/plugins/management_overview/public/index.ts
+++ src/plugins/management_overview/public/index.ts
@@ -6,6 +6,6 @@
 import { ManagementOverViewPlugin } from './plugin';
 
 export { OverviewApp } from './overview_app';
-export { ManagementOverViewPluginSetup, ManagementOverViewPluginStart } from './plugin';
+export { ManagementOverViewPluginSetup } from './plugin';
 
 export const plugin = () => new ManagementOverViewPlugin();
