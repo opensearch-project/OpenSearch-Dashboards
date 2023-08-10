013bcb09a26fab275104986d984dd6405609ed48
Arpit Jaswal <iamarpitjaswal@gmail.com>
2023-08-11 02:04:54 +0530

    Simplify React Component and Fix Imports

--- src/plugins/vis_type_table/public/components/table_vis_component_group.tsx
+++ src/plugins/vis_type_table/public/components/table_vis_component_group.tsx
@@ -1,11 +1,5 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React, { memo } from 'react';
 import { IInterpreterRenderHandlers } from 'src/plugins/expressions';
-import { EuiFlexItem } from '@elastic/eui';
 import { TableGroup } from '../table_vis_response_handler';
 import { TableVisConfig } from '../types';
 import { TableVisComponent } from './table_vis_component';
@@ -22,8 +16,8 @@ export const TableVisComponentGroup = memo(
   ({ tableGroups, visConfig, event, uiState }: TableVisGroupComponentProps) => {
     return (
       <>
-        {tableGroups.map(({ table, title }) => (
-          <EuiFlexItem key={title} className="visTable__group">
+        {tableGroups.map(({ table, title }, index) => (
+          <div key={title} className="visTable__group" data-test-subj={`visTableGroup${index}`}>
             <TableVisComponent
               title={title}
               table={table}
@@ -31,7 +25,7 @@ export const TableVisComponentGroup = memo(
               event={event}
               uiState={uiState}
             />
-          </EuiFlexItem>
+          </div>
         ))}
       </>
     );
