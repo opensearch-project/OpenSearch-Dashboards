1657bdcc9e54a6adcbb5016edc374ece6b8c97c2
abbyhu2000 <abigailhu2000@gmail.com>
2023-08-15 22:01:38 +0000

    fix snapshots
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

--- src/plugins/dashboard/public/application/components/dashboard_listing/dashboard_listing.test.tsx
+++ src/plugins/dashboard/public/application/components/dashboard_listing/dashboard_listing.test.tsx
@@ -44,10 +44,17 @@ import { DashboardListing } from './dashboard_listing';
 import { createDashboardServicesMock } from '../../utils/mocks';
 import { OpenSearchDashboardsContextProvider } from 'src/plugins/opensearch_dashboards_react/public';
 import { I18nProvider } from '@osd/i18n/react';
+import { IOsdUrlStateStorage } from 'src/plugins/opensearch_dashboards_utils/public';
 
 function wrapDashboardListingInContext(mockServices: any) {
+  const osdUrlStateStorage = ({
+    set: jest.fn(),
+    get: jest.fn(() => ({ linked: false })),
+    flush: jest.fn(),
+  } as unknown) as IOsdUrlStateStorage;
   const services = {
     ...mockServices,
+    osdUrlStateStorage,
     dashboardProviders: () => {
       return {
         dashboard: {
