99587992db51433b8c763642e76e3cc8e03dbe70
Anan Zhuang <ananzh@amazon.com>
2023-08-31 15:42:36 -0700

    [Data Explorer][Discover 2.0] Fix issues when change index pattern (#4875)
    * allow side nav show only selected index pattern fields when switch
    * allow reset column state when switch index pattern
    
    Issue Resolve:
    https://github.com/opensearch-project/OpenSearch-Dashboards/issues/4840
    https://github.com/opensearch-project/OpenSearch-Dashboards/issues/4846
    
    Signed-off-by: ananzh <ananzh@amazon.com>

--- src/plugins/discover/public/application/view_components/canvas/index.tsx
+++ src/plugins/discover/public/application/view_components/canvas/index.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiCallOut, EuiLink } from '@elastic/eui';
 import { TopNav } from './top_nav';
 import { ViewProps } from '../../../../../data_explorer/public';
@@ -14,10 +14,26 @@ import { ResultStatus, SearchData } from '../utils/use_search';
 import { DiscoverNoResults } from '../../components/no_results/no_results';
 import { DiscoverUninitialized } from '../../components/uninitialized/uninitialized';
 import { LoadingSpinner } from '../../components/loading_spinner/loading_spinner';
+import { setColumns, useDispatch, useSelector } from '../../utils/state_management';
+import { DiscoverViewServices } from '../../../build_services';
+import { useOpenSearchDashboards } from '../../../../../opensearch_dashboards_react/public';
+import { filterColumns } from '../utils/filter_columns';
+import { DEFAULT_COLUMNS_SETTING } from '../../../../common';
 
 // eslint-disable-next-line import/no-default-export
 export default function DiscoverCanvas({ setHeaderActionMenu, history }: ViewProps) {
   const { data$, refetch$, indexPattern } = useDiscoverContext();
+  const {
+    services: { uiSettings },
+  } = useOpenSearchDashboards<DiscoverViewServices>();
+  const { columns } = useSelector((state) => state.discover);
+  const filteredColumns = filterColumns(
+    columns,
+    indexPattern,
+    uiSettings.get(DEFAULT_COLUMNS_SETTING)
+  );
+  const dispatch = useDispatch();
+  const prevIndexPattern = useRef(indexPattern);
 
   const [fetchState, setFetchState] = useState<SearchData>({
     status: data$.getValue().status,
@@ -68,6 +84,13 @@ export default function DiscoverCanvas({ setHeaderActionMenu, history }: ViewPro
       subscription.unsubscribe();
     };
   }, [data$, fetchState]);
+
+  useEffect(() => {
+    if (indexPattern !== prevIndexPattern.current) {
+      dispatch(setColumns({ columns: filteredColumns }));
+      prevIndexPattern.current = indexPattern;
+    }
+  }, [dispatch, filteredColumns, indexPattern]);
 
   const timeField = indexPattern?.timeFieldName ? indexPattern.timeFieldName : undefined;
 
