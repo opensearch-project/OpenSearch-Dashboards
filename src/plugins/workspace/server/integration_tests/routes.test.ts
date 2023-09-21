6e1c65604969494772385a312bc3cf218cf0fb4a
SuZhou-Joe <suzhou@amazon.com>
2023-09-21 13:20:29 +0800

    feat: add integration test
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

---
+++ src/plugins/workspace/server/integration_tests/routes.test.ts
@@ -0,0 +1,141 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { WorkspaceAttribute } from 'src/core/types';
+import { omit } from 'lodash';
+import * as osdTestServer from '../../../../core/test_helpers/osd_server';
+
+const testWorkspace: WorkspaceAttribute = {
+  id: 'fake_id',
+  name: 'test_workspace',
+  description: 'test_workspace_description',
+};
+
+describe('workspace service', () => {
+  let root: ReturnType<typeof osdTestServer.createRoot>;
+  let opensearchServer: osdTestServer.TestOpenSearchUtils;
+  beforeAll(async () => {
+    const { startOpenSearch } = osdTestServer.createTestServers({
+      adjustTimeout: (t: number) => jest.setTimeout(t),
+    });
+    opensearchServer = await startOpenSearch();
+    root = osdTestServer.createRootWithCorePlugins();
+
+    await root.setup();
+    await root.start();
+  }, 30000);
+  afterAll(async () => {
+    await root.shutdown();
+    await opensearchServer.stop();
+  });
+  describe('Workspace CRUD apis', () => {
+    afterEach(async () => {
+      const listResult = await osdTestServer.request
+        .post(root, `/api/workspaces/_list`)
+        .send({
+          page: 1,
+        })
+        .expect(200);
+      await Promise.all(
+        listResult.body.result.workspaces.map((item: WorkspaceAttribute) =>
+          osdTestServer.request.delete(root, `/api/workspaces/${item.id}`).expect(200)
+        )
+      );
+    });
+    it('create', async () => {
+      await osdTestServer.request
+        .post(root, `/api/workspaces`)
+        .send({
+          attributes: testWorkspace,
+        })
+        .expect(400);
+
+      const result: any = await osdTestServer.request
+        .post(root, `/api/workspaces`)
+        .send({
+          attributes: omit(testWorkspace, 'id'),
+        })
+        .expect(200);
+
+      expect(result.body.success).toEqual(true);
+      expect(typeof result.body.result.id).toBe('string');
+    });
+    it('get', async () => {
+      const result = await osdTestServer.request
+        .post(root, `/api/workspaces`)
+        .send({
+          attributes: omit(testWorkspace, 'id'),
+        })
+        .expect(200);
+
+      const getResult = await osdTestServer.request.get(
+        root,
+        `/api/workspaces/${result.body.result.id}`
+      );
+      expect(getResult.body.result.name).toEqual(testWorkspace.name);
+    });
+    it('update', async () => {
+      const result: any = await osdTestServer.request
+        .post(root, `/api/workspaces`)
+        .send({
+          attributes: omit(testWorkspace, 'id'),
+        })
+        .expect(200);
+
+      await osdTestServer.request
+        .put(root, `/api/workspaces/${result.body.result.id}`)
+        .send({
+          attributes: {
+            ...omit(testWorkspace, 'id'),
+            name: 'updated',
+          },
+        })
+        .expect(200);
+
+      const getResult = await osdTestServer.request.get(
+        root,
+        `/api/workspaces/${result.body.result.id}`
+      );
+
+      expect(getResult.body.success).toEqual(true);
+      expect(getResult.body.result.name).toEqual('updated');
+    });
+    it('delete', async () => {
+      const result: any = await osdTestServer.request
+        .post(root, `/api/workspaces`)
+        .send({
+          attributes: omit(testWorkspace, 'id'),
+        })
+        .expect(200);
+
+      await osdTestServer.request
+        .delete(root, `/api/workspaces/${result.body.result.id}`)
+        .expect(200);
+
+      const getResult = await osdTestServer.request.get(
+        root,
+        `/api/workspaces/${result.body.result.id}`
+      );
+
+      expect(getResult.body.success).toEqual(false);
+    });
+    it('list', async () => {
+      await osdTestServer.request
+        .post(root, `/api/workspaces`)
+        .send({
+          attributes: omit(testWorkspace, 'id'),
+        })
+        .expect(200);
+
+      const listResult = await osdTestServer.request
+        .post(root, `/api/workspaces/_list`)
+        .send({
+          page: 1,
+        })
+        .expect(200);
+      expect(listResult.body.result.total).toEqual(1);
+    });
+  });
+});
