090e2fec45213b387c14b3bee44ca48f1ceada50
SuZhou-Joe <suzhou@amazon.com>
2023-09-21 13:43:01 +0800

    feat: add configuration
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/server/index.ts
+++ src/plugins/workspace/server/index.ts
@@ -2,8 +2,9 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-import { PluginInitializerContext } from '../../../core/server';
+import { PluginConfigDescriptor, PluginInitializerContext } from '../../../core/server';
 import { WorkspacePlugin } from './plugin';
+import { configSchema } from '../config';
 
 // This exports static code and TypeScript types,
 // as well as, OpenSearch Dashboards Platform `plugin()` initializer.
@@ -11,3 +12,9 @@ import { WorkspacePlugin } from './plugin';
 export function plugin(initializerContext: PluginInitializerContext) {
   return new WorkspacePlugin(initializerContext);
 }
+
+export const config: PluginConfigDescriptor = {
+  schema: configSchema,
+};
+
+export { WorkspaceFindOptions } from './types';
