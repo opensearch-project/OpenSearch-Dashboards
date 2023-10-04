422da0b426a0667fb57166299b0eaec006399687
Eric <menwe@amazon.com>
2023-10-04 03:15:42 +0000

    add default datasource tests
    Signed-off-by: Eric <menwe@amazon.com>

---
+++ src/plugins/discover/public/datasources/default_datasource.test.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { DefaultDslDataSource } from './default_datasource';
+
+describe('DefaultDslDataSource', () => {
+  let indexPatternsMock;
+
+  beforeEach(() => {
+    indexPatternsMock = {
+      ensureDefaultIndexPattern: jest.fn(),
+      getCache: jest.fn(),
+    };
+  });
+
+  it('should ensure default index pattern and get cache', async () => {
+    const dataSource = new DefaultDslDataSource({
+      name: 'testName',
+      type: 'testType',
+      metadata: {},
+      indexPatterns: indexPatternsMock,
+    });
+
+    await dataSource.getDataSet();
+
+    expect(indexPatternsMock.ensureDefaultIndexPattern).toHaveBeenCalledTimes(1);
+    expect(indexPatternsMock.getCache).toHaveBeenCalledTimes(1);
+  });
+
+  it('should throw an error', async () => {
+    const dataSource = new DefaultDslDataSource({
+      name: 'testName',
+      type: 'testType',
+      metadata: {},
+      indexPatterns: indexPatternsMock,
+    });
+
+    await expect(dataSource.testConnection()).resolves.toBe(true);
+  });
+
+  it('should return null', async () => {
+    const dataSource = new DefaultDslDataSource({
+      name: 'testName',
+      type: 'testType',
+      metadata: {},
+      indexPatterns: indexPatternsMock,
+    });
+
+    const result = await dataSource.runQuery({});
+    expect(result).toBeUndefined();
+  });
+});
