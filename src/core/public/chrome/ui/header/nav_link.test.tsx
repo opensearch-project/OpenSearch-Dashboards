aa623d62ffc741c18380f5cee69b52d5f74e1b6a
Willie Hung <willie880201044@gmail.com>
2023-11-11 00:34:57 -0600

    Remove redundant tests and add comment for future reference
    Signed-off-by: Willie Hung <willie880201044@gmail.com>

--- src/core/public/chrome/ui/header/nav_link.test.tsx
+++ src/core/public/chrome/ui/header/nav_link.test.tsx
@@ -1,82 +1,11 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- *
- * Any modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
- */
-
-/*
- * Licensed to Elasticsearch B.V. under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch B.V. licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
  */
 
 import { isActiveNavLink, createEuiListItem } from './nav_link';
-import { isModifiedOrPrevented } from './nav_link';
 import { ChromeNavLink } from '../../..';
 import { httpServiceMock } from '../../../http/http_service.mock';
-import React from 'react';
-
-describe('isModifiedOrPrevented', () => {
-  test('returns true if metaKey is pressed', () => {
-    const mockEvent: Partial<React.MouseEvent<HTMLButtonElement, MouseEvent>> = {
-      metaKey: true,
-      altKey: false,
-      ctrlKey: false,
-      shiftKey: false,
-      defaultPrevented: false,
-    };
-
-    expect(
-      isModifiedOrPrevented(mockEvent as React.MouseEvent<HTMLButtonElement, MouseEvent>)
-    ).toBe(true);
-  });
-
-  test('returns true if defaultPrevented is true', () => {
-    const mockEvent: Partial<React.MouseEvent<HTMLButtonElement, MouseEvent>> = {
-      metaKey: false,
-      altKey: false,
-      ctrlKey: false,
-      shiftKey: false,
-      defaultPrevented: true,
-    };
-
-    expect(
-      isModifiedOrPrevented(mockEvent as React.MouseEvent<HTMLButtonElement, MouseEvent>)
-    ).toBe(true);
-  });
-
-  test('returns false if no modifier keys are pressed and default is not prevented', () => {
-    const mockEvent: Partial<React.MouseEvent<HTMLButtonElement, MouseEvent>> = {
-      metaKey: false,
-      altKey: false,
-      ctrlKey: false,
-      shiftKey: false,
-      defaultPrevented: false,
-    };
-
-    expect(
-      isModifiedOrPrevented(mockEvent as React.MouseEvent<HTMLButtonElement, MouseEvent>)
-    ).toBe(false);
-  });
-});
 
 describe('isActiveNavLink', () => {
   it('should return true if the currentId is "discover" and targetId is "discover"', () => {
