6dae6d1692595f154a6e410b92c900e187a6394b
Willie Hung <willie880201044@gmail.com>
2023-11-13 18:24:35 -0600

    Update src/core/public/chrome/ui/header/nav_link.tsx
    Co-authored-by: Josh Romero <rmerqg@amazon.com>
    Signed-off-by: Willie Hung <willie880201044@gmail.com>

--- src/core/public/chrome/ui/header/nav_link.tsx
+++ src/core/public/chrome/ui/header/nav_link.tsx
@@ -39,7 +39,7 @@ import { relativeToAbsolute } from '../../nav_links/to_nav_link';
 export const isModifiedOrPrevented = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
   event.metaKey || event.altKey || event.ctrlKey || event.shiftKey || event.defaultPrevented;
 
-// Future: should make this function more generic
+// TODO: make this function more generic
 export const isActiveNavLink = (currentId: string | undefined, targetId: string): boolean => {
   if (currentId === 'discover' || currentId === 'data-explorer') {
     return ['discover', 'data-explorer'].includes(targetId);
