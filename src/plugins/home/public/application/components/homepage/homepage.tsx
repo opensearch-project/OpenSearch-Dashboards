35ffb802123b66347e60b5f04e136e0f4eb4e605
Matt Provost <provomat@amazon.com>
2023-12-15 20:52:22 +0000

    Add page layout
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- src/plugins/home/public/application/components/homepage/homepage.tsx
+++ src/plugins/home/public/application/components/homepage/homepage.tsx
@@ -4,13 +4,16 @@
  */
 
 import React, { useState, useEffect } from 'react';
+import { i18n } from '@osd/i18n';
+import { EuiPageTemplate, EuiButtonEmpty } from '@elastic/eui';
 import { Homepage as HomepageType } from '../../../services/section_type/section_type';
 import { getServices } from '../../opensearch_dashboards_services';
 import { HeroSection } from './hero_section';
 import { Section } from './section';
 
 export const Homepage = () => {
-  const { sectionTypes } = getServices();
+  const { sectionTypes, application } = getServices();
+  const getUrl = application.getUrlForApp;
 
   // TODO: ideally, this should be some sort of observable so changes can be made without having to explicitly hit a save button
   const [homepage, setHomepage] = useState<HomepageType>();
@@ -33,6 +36,18 @@ export const Homepage = () => {
     return <span>Error loading homepage</span>;
   }
 
+  const sideItems: React.ReactNode[] = [
+    <EuiButtonEmpty iconType="indexOpen" href={getUrl('home', { path: '#/tutorial_directory' })}>
+      Add data
+    </EuiButtonEmpty>,
+    <EuiButtonEmpty iconType="gear" href={getUrl('management')}>
+      Manage
+    </EuiButtonEmpty>,
+    <EuiButtonEmpty iconType="wrench" href={getUrl('dev_tools', { path: '#/console' })}>
+      Dev tools
+    </EuiButtonEmpty>,
+  ].reverse();
+
   function renderSections() {
     return homepage!.sections.map(({ render, title, description, links }, i) => (
       <Section key={i} title={title} description={description} links={links} render={render} />
@@ -42,9 +57,25 @@ export const Homepage = () => {
   const hero = homepage!.heroes[0];
 
   return (
-    <div>
+    <EuiPageTemplate
+      restrictWidth={1400}
+      pageHeader={{
+        pageTitle: i18n.translate('home.title', { defaultMessage: 'Home' }),
+        rightSideItems: sideItems,
+        alignItems: 'center',
+      }}
+      pageContentProps={{
+        color: 'transparent',
+        hasBorder: false,
+        hasShadow: false,
+      }}
+      pageContentBodyProps={{
+        paddingSize: 'none',
+        className: 'home-homepage-pageBody',
+      }}
+    >
       {hero && <HeroSection render={hero.render} />}
       {renderSections()}
-    </div>
+    </EuiPageTemplate>
   );
 };
