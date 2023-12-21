34994f816c79854efb26c91df045a79915d89383
Qiwen Li <qiwen_li@brown.edu>
2023-12-21 14:06:21 -0800

    [osci23] implmented advance setting in Dicover: MODIFY_COLUMN_ON_SWITCH (#5508)
    * Revert "[Data Explorer][Discover 2.0] Fix issues when change index pattern (#4875) (#4885)"
    
    This reverts commit 0102a32dd007f90a85a140b80be8d1fb312f30cb.
    try revert:
    
    * [osci23] implmented advance setting in Dicover: MODIFY_COLUMN_ON_SWITCH
    
    Signed-off-by: qiwen li <qiwen_li@brown.edu>
    
    * fix deps
    
    Signed-off-by: qiwen li <qiwen_li@brown.edu>
    
    * Update CHANGELOG.md
    
    Signed-off-by: Qiwen Li <qiwen_li@brown.edu>
    
    * modify to match discover legacy behavior, columns from previous column are only shown in canvas area
    
    Signed-off-by: qiwen li <qiwen_li@brown.edu>
    
    * Update CHANGELOG.md
    
    Co-authored-by: Anan Zhuang <ananzh@amazon.com>
    Signed-off-by: Qiwen Li <qiwen_li@brown.edu>
    
    * removed unused variables, added comments and @param
    
    Signed-off-by: Qiwen Li <qiwen_li@brown.edu>
    
    ---------
    
    Signed-off-by: qiwen li <qiwen_li@brown.edu>
    Signed-off-by: Qiwen Li <qiwen_li@brown.edu>
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    Signed-off-by: Ashwin P Chandran <ashwinpc@amazon.com>
    Co-authored-by: Anan Zhuang <ananzh@amazon.com>
    Co-authored-by: Ashwin P Chandran <ashwinpc@amazon.com>

--- src/plugins/discover/public/application/view_components/utils/filter_columns.test.ts
+++ src/plugins/discover/public/application/view_components/utils/filter_columns.test.ts
@@ -13,21 +13,32 @@ describe('filterColumns', () => {
     },
   } as IndexPattern;
 
-  it('should return columns that exist in the index pattern fields', () => {
+  it('should return columns that exist in the index pattern fields when MODIFY_COLUMN_ON_SWITCH is true', () => {
     const columns = ['a', 'b'];
-    const result = filterColumns(columns, indexPatternMock, ['a']);
+    const result = filterColumns(columns, indexPatternMock, ['a'], true);
     expect(result).toEqual(['a']);
   });
 
-  it('should return defaultColumns if no columns exist in the index pattern fields', () => {
+  it('should return all of the columns when MODIFY_COLUMN_ON_SWITCH is false', () => {
+    const columns = ['a', 'b'];
+    const result = filterColumns(columns, indexPatternMock, ['a'], false);
+    expect(result).toEqual(['a', 'b']);
+  });
+
+  it('should return defualt columns if columns are empty', () => {
+    const result = filterColumns([], indexPatternMock, ['a'], false);
+    expect(result).toEqual(['_source']);
+  });
+
+  it('should return defaultColumns if no columns exist in the index pattern fields when MODIFY_COLUMN_ON_SWITCH is true', () => {
     const columns = ['b', 'e'];
-    const result = filterColumns(columns, indexPatternMock, ['e']);
+    const result = filterColumns(columns, indexPatternMock, ['e'], true);
     expect(result).toEqual(['_source']);
   });
 
   it('should return defaultColumns if no columns and indexPattern is undefined', () => {
     const columns = ['b', 'e'];
-    const result = filterColumns(columns, undefined, ['a']);
+    const result = filterColumns(columns, undefined, ['a'], true);
     expect(result).toEqual(['_source']);
   });
 });
