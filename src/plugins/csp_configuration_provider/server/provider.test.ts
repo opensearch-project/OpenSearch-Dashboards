a6c0f6e4bd109a55679f0c00a81f739169adb4b2
Tianle Huang <tianleh@amazon.com>
2023-12-27 01:58:29 +0000

    add unit tests for the provider class
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

---
+++ src/plugins/csp_configuration_provider/server/provider.test.ts
@@ -0,0 +1,134 @@
+import { SearchResponse } from '@opensearch-project/opensearch/api/types';
+import {
+  OpenSearchClientMock,
+  opensearchClientMock,
+} from '../../../core/server/opensearch/client/mocks';
+import { OpenSearchCspClient } from './provider';
+
+const INDEX_NAME = 'test_index';
+const INDEX_DOCUMENT_NAME = 'csp_doc';
+const ERROR_MESSAGE = 'Service unavailable';
+
+describe('Provider', () => {
+  let opensearchClient: OpenSearchClientMock;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+    opensearchClient = opensearchClientMock.createOpenSearchClient();
+  });
+
+  afterEach(() => {});
+
+  describe('exists', () => {
+    it('returns true if the target index does exists', async () => {
+      const indexExists = true;
+      opensearchClient.indices.exists.mockImplementation(() => {
+        return opensearchClientMock.createSuccessTransportRequestPromise(indexExists);
+      });
+
+      const client = new OpenSearchCspClient(opensearchClient);
+      const value = await client.exists(INDEX_NAME);
+
+      expect(value).toBeTruthy();
+      expect(opensearchClient.indices.exists).toBeCalledWith({ index: INDEX_NAME });
+    });
+
+    it('returns false if the target index does not exists', async () => {
+      const indexExists = false;
+      opensearchClient.indices.exists.mockImplementation(() => {
+        return opensearchClientMock.createSuccessTransportRequestPromise(indexExists);
+      });
+
+      const client = new OpenSearchCspClient(opensearchClient);
+      const existsValue = await client.exists(INDEX_NAME);
+
+      expect(existsValue).toBeFalsy();
+      expect(opensearchClient.indices.exists).toBeCalledWith({ index: INDEX_NAME });
+    });
+
+    it('returns false if opensearch errors happen', async () => {
+      opensearchClient.indices.exists.mockImplementation(() => {
+        return opensearchClientMock.createErrorTransportRequestPromise(new Error(ERROR_MESSAGE));
+      });
+
+      const client = new OpenSearchCspClient(opensearchClient);
+      const existsValue = await client.exists(INDEX_NAME);
+
+      expect(existsValue).toBeFalsy();
+      expect(opensearchClient.indices.exists).toBeCalledWith({ index: INDEX_NAME });
+    });
+  });
+
+  describe('get', () => {
+    it('returns the csp rules from the index', async () => {
+      const cspRules = "frame-ancestors 'self'";
+
+      opensearchClient.search.mockImplementation(() => {
+        return opensearchClientMock.createSuccessTransportRequestPromise({
+          hits: {
+            hits: [
+              {
+                _source: {
+                  value: cspRules,
+                },
+              },
+            ],
+          },
+        } as SearchResponse<any>);
+      });
+
+      const client = new OpenSearchCspClient(opensearchClient);
+      const getValue = await client.get(INDEX_NAME, INDEX_DOCUMENT_NAME);
+
+      expect(getValue).toBe(cspRules);
+      expect(opensearchClient.search).toBeCalledWith(
+        getSearchInput(INDEX_NAME, INDEX_DOCUMENT_NAME)
+      );
+    });
+
+    it('returns empty when opensearch response is malformed', async () => {
+      opensearchClient.search.mockImplementation(() => {
+        return opensearchClientMock.createSuccessTransportRequestPromise({
+          hits: {},
+        } as SearchResponse<any>);
+      });
+
+      const client = new OpenSearchCspClient(opensearchClient);
+      const getValue = await client.get(INDEX_NAME, INDEX_DOCUMENT_NAME);
+
+      expect(getValue).toBe('');
+      expect(opensearchClient.search).toBeCalledWith(
+        getSearchInput(INDEX_NAME, INDEX_DOCUMENT_NAME)
+      );
+    });
+
+    it('returns empty string when opensearch errors happen', async () => {
+      opensearchClient.search.mockImplementation(() => {
+        return opensearchClientMock.createErrorTransportRequestPromise(new Error(ERROR_MESSAGE));
+      });
+
+      const client = new OpenSearchCspClient(opensearchClient);
+      const cspRules = await client.get(INDEX_NAME, INDEX_DOCUMENT_NAME);
+
+      expect(cspRules).toBe('');
+      expect(opensearchClient.search).toBeCalledWith(
+        getSearchInput(INDEX_NAME, INDEX_DOCUMENT_NAME)
+      );
+    });
+  });
+});
+
+function getSearchInput(indexName: string, documentName: string) {
+  return {
+    index: indexName,
+    body: {
+      query: {
+        match: {
+          _id: {
+            query: documentName,
+          },
+        },
+      },
+    },
+  };
+}
--- src/plugins/csp_configuration_provider/server/provider.ts
+++ src/plugins/csp_configuration_provider/server/provider.ts
@@ -3,18 +3,29 @@ import { OpenSearchClient } from '../../../../src/core/server';
 import { CspClient } from './types';
 
 export class OpenSearchCspClient implements CspClient {
-  client: OpenSearchClient;
+  private client: OpenSearchClient;
 
   constructor(inputOpenSearchClient: OpenSearchClient) {
     this.client = inputOpenSearchClient;
   }
 
   async exists(configurationName: string): Promise<boolean> {
-    const exists = await this.client.indices.exists({
-      index: configurationName,
-    });
+    let value = false;
 
-    return exists.body;
+    try {
+      const exists = await this.client.indices.exists({
+        index: configurationName,
+      });
+
+      value = exists.body;
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Failed to call exists with configurationName ${configurationName} due to error ${e}`
+      );
+    }
+
+    return value;
   }
 
   async get(configurationName: string, cspRulesName: string): Promise<string> {
@@ -28,12 +39,22 @@ export class OpenSearchCspClient implements CspClient {
       },
     };
 
-    const data = await this.client.search({
-      index: configurationName,
-      _source: true,
-      body: query,
-    });
+    let value = '';
 
-    return data.body.hits.hits[0]?._source.value;
+    try {
+      const data = await this.client.search({
+        index: configurationName,
+        body: query,
+      });
+
+      value = data?.body?.hits?.hits[0]?._source?.value;
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Failed to call get with configurationName ${configurationName} and cspRulesName ${cspRulesName} due to error ${e}`
+      );
+    }
+
+    return value;
   }
 }
