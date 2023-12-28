4e03159b900513b926ef650fe0985a1a7442588a
Tianle Huang <tianleh@amazon.com>
2023-12-28 04:01:14 +0000

    add content to read me
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- src/plugins/csp_configuration_provider/server/routes/index.ts
+++ src/plugins/csp_configuration_provider/server/routes/index.ts
@@ -5,18 +5,4 @@
 
 import { IRouter } from '../../../../core/server';
 
-export function defineRoutes(router: IRouter) {
-  router.get(
-    {
-      path: '/api/csp_configuration_provider/example',
-      validate: false,
-    },
-    async (context, request, response) => {
-      return response.ok({
-        body: {
-          time: new Date().toISOString(),
-        },
-      });
-    }
-  );
-}
+export function defineRoutes(router: IRouter) {}
