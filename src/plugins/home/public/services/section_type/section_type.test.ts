decff052cb443424a219e08b0d2593d771402a34
Matt Provost <provomat@amazon.com>
2024-01-08 23:13:38 +0000

    Add a few more tests
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- src/plugins/home/public/services/section_type/section_type.test.ts
+++ src/plugins/home/public/services/section_type/section_type.test.ts
@@ -67,4 +67,56 @@ describe('SectionTypeService', () => {
       }).toThrowErrorMatchingInlineSnapshot(`"Section with id 'foo' already exists."`);
     });
   });
+
+  describe('getHeroSectionTypes', () => {
+    test('returns empty array if no hero sections registered', () => {
+      const sectionTypeService = new SectionTypeService();
+      expect(sectionTypeService.getHeroSectionTypes()).toEqual([]);
+    });
+
+    test('returns registered hero sections', () => {
+      const sectionTypeService = new SectionTypeService();
+
+      sectionTypeService.setup().registerHeroSection({
+        id: 'foo',
+        render: () => () => {},
+      });
+      sectionTypeService.setup().registerHeroSection({
+        id: 'bar',
+        render: () => () => {},
+      });
+
+      expect(sectionTypeService.getHeroSectionTypes()).toEqual([
+        { id: 'foo', render: expect.any(Function) },
+        { id: 'bar', render: expect.any(Function) },
+      ]);
+    });
+  });
+
+  describe('getSectionTypes', () => {
+    test('returns empty array if no sections registered', () => {
+      const sectionTypeService = new SectionTypeService();
+      expect(sectionTypeService.getSectionTypes()).toEqual([]);
+    });
+
+    test('returns registered sections', () => {
+      const sectionTypeService = new SectionTypeService();
+
+      sectionTypeService.setup().registerSection({
+        id: 'foo',
+        title: 'Foo',
+        render: () => () => {},
+      });
+      sectionTypeService.setup().registerSection({
+        id: 'bar',
+        title: 'Bar',
+        render: () => () => {},
+      });
+
+      expect(sectionTypeService.getSectionTypes()).toEqual([
+        { id: 'foo', title: 'Foo', render: expect.any(Function) },
+        { id: 'bar', title: 'Bar', render: expect.any(Function) },
+      ]);
+    });
+  });
 });
