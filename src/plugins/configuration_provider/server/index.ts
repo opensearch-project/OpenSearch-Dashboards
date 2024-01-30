e4efc0ecdce1e8de3cf7ef87ae718265fddc2b29
Tianle Huang <tianleh@amazon.com>
2024-01-30 01:52:04 +0000

    make the new plugin disabled by default
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- src/plugins/configuration_provider/server/index.ts
+++ src/plugins/configuration_provider/server/index.ts
@@ -3,11 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { PluginInitializerContext } from '../../../core/server';
+import { PluginConfigDescriptor, PluginInitializerContext } from '../../../core/server';
+import { ConfigurationProviderConfigSchema, configSchema } from '../config';
 import { ConfigurationProviderPlugin } from './plugin';
 
 // This exports static code and TypeScript types,
 // as well as, OpenSearch Dashboards Platform `plugin()` initializer.
+
+export const config: PluginConfigDescriptor<ConfigurationProviderConfigSchema> = {
+  schema: configSchema,
+};
 
 export function plugin(initializerContext: PluginInitializerContext) {
   return new ConfigurationProviderPlugin(initializerContext);
