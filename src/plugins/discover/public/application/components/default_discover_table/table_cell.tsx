b008ad23907e073ee877fc4ca8f7427c4dd9d601
Miki <miki@amazon.com>
2024-01-31 07:25:59 -0800

    [Discover] Prevent wrapping of date fields
    Signed-off-by: Miki <miki@amazon.com>

--- src/plugins/discover/public/application/components/default_discover_table/table_cell.tsx
+++ src/plugins/discover/public/application/components/default_discover_table/table_cell.tsx
@@ -18,6 +18,7 @@ import { DocViewFilterFn } from '../../doc_views/doc_views_types';
 
 export interface TableCellProps {
   columnId: string;
+  columnType?: string;
   onFilter: DocViewFilterFn;
   filterable?: boolean;
   fieldMapping?: any;
@@ -26,16 +27,18 @@ export interface TableCellProps {
 
 export const TableCell = ({
   columnId,
+  columnType,
   onFilter,
   fieldMapping,
   sanitizedCellValue,
 }: TableCellProps) => {
+  const tdClassNames =
+    columnType === 'date'
+      ? 'osdDocTableCell eui-textNoWrap'
+      : 'osdDocTableCell eui-textBreakAll eui-textBreakWord';
   return (
-    // eslint-disable-next-line react/no-danger
-    <td
-      data-test-subj="docTableField"
-      className="osdDocTableCell eui-textBreakAll eui-textBreakWord"
-    >
+    <td data-test-subj="docTableField" className={tdClassNames}>
+      {/* eslint-disable-next-line react/no-danger */}
       <span dangerouslySetInnerHTML={{ __html: sanitizedCellValue }} />
       <span className="osdDocTableCell__filter">
         <EuiToolTip
