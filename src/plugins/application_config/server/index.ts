4dd1f7288152fb6ada10f6f705324d95421bef46
Tianle Huang <tianleh@amazon.com>
2024-02-24 03:09:05 +0000

    add enabled to this plugin
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- src/plugins/application_config/server/index.ts
+++ src/plugins/application_config/server/index.ts
@@ -3,11 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { PluginInitializerContext } from '../../../core/server';
+import { PluginConfigDescriptor, PluginInitializerContext } from '../../../core/server';
+import { ApplicationConfigSchema, configSchema } from '../config';
 import { ApplicationConfigPlugin } from './plugin';
 
 // This exports static code and TypeScript types,
 // as well as, OpenSearch Dashboards Platform `plugin()` initializer.
+
+export const config: PluginConfigDescriptor<ApplicationConfigSchema> = {
+  schema: configSchema,
+};
 
 export function plugin(initializerContext: PluginInitializerContext) {
   return new ApplicationConfigPlugin(initializerContext);
