f4213f346700ff4bdf38838735c5f619b6505260
SuZhou-Joe <suzhou@amazon.com>
2024-02-26 09:12:53 +0000

    feat: remove useless config in test
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/server/saved_objects/integration_tests/saved_objects_wrapper_for_check_workspace_conflict.test.ts
+++ src/plugins/workspace/server/saved_objects/integration_tests/saved_objects_wrapper_for_check_workspace_conflict.test.ts
@@ -35,9 +35,6 @@ describe('saved_objects_wrapper_for_check_workspace_conflict integration test', 
         osd: {
           workspace: {
             enabled: true,
-            permission: {
-              enabled: true,
-            },
           },
         },
       },
