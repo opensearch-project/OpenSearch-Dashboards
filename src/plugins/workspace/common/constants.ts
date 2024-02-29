3ba70da0bcaa68c3d517977b0b77650f345ffcfd
SuZhou-Joe <suzhou@amazon.com>
2024-02-29 11:11:01 +0800

    feat: remove useless code
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/common/constants.ts
+++ src/plugins/workspace/common/constants.ts
@@ -3,16 +3,5 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const WORKSPACE_APP_ID = 'workspace';
-export const WORKSPACE_APP_NAME = 'Workspace';
-
-export const PATHS = {
-  create: '/create',
-  overview: '/overview',
-  update: '/update',
-};
-export const WORKSPACE_OP_TYPE_CREATE = 'create';
-export const WORKSPACE_OP_TYPE_UPDATE = 'update';
-
 export const WORKSPACE_ID_QUERYSTRING_NAME = '_workspace_id_';
 export const WORKSPACE_SAVED_OBJECTS_CLIENT_WRAPPER_ID = 'workspace';
