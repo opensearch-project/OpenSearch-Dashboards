4dbcc211cf24d4b82379f26aed5a1dd0c44a6d1e
Tianle Huang <tianleh@amazon.com>
2024-02-29 21:44:13 +0000

    clean up code to prepare for application config
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- src/plugins/configuration_provider/server/plugin.ts
+++ src/plugins/configuration_provider/server/plugin.ts
@@ -3,82 +3,39 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Observable } from 'rxjs';
-import { first } from 'rxjs/operators';
 import {
   CoreSetup,
   CoreStart,
-  IScopedClusterClient,
   Logger,
   Plugin,
   PluginInitializerContext,
-  SharedGlobalConfig,
 } from '../../../core/server';
 
 import { createCspRulesPreResponseHandler } from './csp_handlers';
-import { OpenSearchConfigurationClient } from './provider';
-import { defineRoutes } from './routes';
-import {
-  ConfigurationClient,
-  ConfigurationProviderPluginSetup,
-  ConfigurationProviderPluginStart,
-} from './types';
+import { ConfigurationProviderPluginSetup, ConfigurationProviderPluginStart } from './types';
 
 export class ConfigurationProviderPlugin
   implements Plugin<ConfigurationProviderPluginSetup, ConfigurationProviderPluginStart> {
   private readonly logger: Logger;
-  private readonly config$: Observable<SharedGlobalConfig>;
-  private configurationClient: ConfigurationClient;
-  private configurationIndexName: string;
 
   constructor(initializerContext: PluginInitializerContext) {
     this.logger = initializerContext.logger.get();
-    this.config$ = initializerContext.config.legacy.globalConfig$;
-    this.configurationIndexName = '';
-  }
-
-  private setConfigurationClient(inputConfigurationClient: ConfigurationClient) {
-    this.configurationClient = inputConfigurationClient;
-  }
-
-  private getConfigurationClient(inputOpenSearchClient: IScopedClusterClient) {
-    if (this.configurationClient) {
-      return this.configurationClient;
-    }
-
-    const openSearchConfigurationClient = new OpenSearchConfigurationClient(
-      inputOpenSearchClient,
-      this.configurationIndexName,
-      this.logger
-    );
-
-    this.setConfigurationClient(openSearchConfigurationClient);
-    return this.configurationClient;
   }
 
   public async setup(core: CoreSetup) {
-    this.logger.debug('ConfigurationProvider: Setup');
-    const router = core.http.createRouter();
-
-    // Register server side APIs
-    defineRoutes(router, this.getConfigurationClient.bind(this), this.logger);
-
-    const config = await this.config$.pipe(first()).toPromise();
-
-    this.configurationIndexName = config.opensearchDashboards.config_index;
+    // TODO: replace this with real
+    const getConfigurationClient = {};
 
     core.http.registerOnPreResponse(
       createCspRulesPreResponseHandler(
         core,
         core.http.csp.header,
-        this.getConfigurationClient.bind(this),
+        getConfigurationClient,
         this.logger
       )
     );
 
-    return {
-      setConfigurationClient: this.setConfigurationClient.bind(this),
-    };
+    return {};
   }
 
   public start(core: CoreStart) {
--- src/plugins/configuration_provider/server/types.ts
+++ src/plugins/configuration_provider/server/types.ts
@@ -7,13 +7,3 @@
 export interface ConfigurationProviderPluginSetup {}
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ConfigurationProviderPluginStart {}
-
-export interface ConfigurationClient {
-  existsCspRules(): Promise<boolean>;
-
-  getCspRules(): Promise<string>;
-
-  updateCspRules(cspRules: string): Promise<string>;
-
-  deleteCspRules(): Promise<string>;
-}
