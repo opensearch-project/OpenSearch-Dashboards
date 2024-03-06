f52eeaa7a883d6bd0d3c726a36698e74a3fca7cf
SuZhou-Joe <suzhou@amazon.com>
2024-03-06 13:31:33 +0800

    feat: support Discover edge case
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/public/plugin.ts
+++ src/plugins/workspace/public/plugin.ts
@@ -5,6 +5,7 @@
 
 import { BehaviorSubject, Subscription } from 'rxjs';
 import { debounce } from 'lodash';
+import { UnregisterCallback } from 'history';
 import { CoreSetup, Plugin } from '../../../core/public';
 import { getStateFromOsdUrl } from '../../opensearch_dashboards_utils/public';
 import { formatUrlWithWorkspaceId } from './utils';
@@ -13,17 +14,39 @@ import { WORKSPACE_ID_STATE_KEY } from '../common/constants';
 export class WorkspacePlugin implements Plugin<{}, {}, {}> {
   private core?: CoreSetup;
   private URLChange$ = new BehaviorSubject('');
-  private applicationSubscription = new Subscription();
+  private currentWorkspaceIdSubscription = new Subscription();
   private urlChangeSubscription = new Subscription();
+  private historyUnregisterCallback?: UnregisterCallback;
   private getworkspaceIdFromURL(): string | null {
     return getStateFromOsdUrl(WORKSPACE_ID_STATE_KEY);
   }
+  /**
+   * Current workspace id may come from multiple source: 1. url hash 2. memory: core.workspaces 3. localstorage
+   * this methods will detect if there is any workspace id present and return that.
+   * @returns string
+   */
   private getworkspaceId(): string {
-    if (this.getworkspaceIdFromURL()) {
-      return this.getworkspaceIdFromURL() || '';
+    // workspace id in url has highest priority
+    const workspaceIdFromUrl = this.getworkspaceIdFromURL();
+    if (workspaceIdFromUrl) {
+      return workspaceIdFromUrl;
     }
 
-    return this.core?.workspaces.currentWorkspaceId$.getValue() || '';
+    // workspace id in memory has second priority and mainly used for reserve workspace id when navigating through SPA
+    const workspaceIdFromMemory = this.core?.workspaces.currentWorkspaceId$.getValue();
+
+    if (workspaceIdFromMemory) {
+      return workspaceIdFromMemory;
+    }
+
+    // workspace in localStorage is used in cases like Discover that will open a new tab in the browser, the localStorage will be used for cross-tab communication.
+    const workspaceIdFromLocalStorage = localStorage.getItem(WORKSPACE_ID_STATE_KEY);
+
+    if (workspaceIdFromLocalStorage) {
+      return workspaceIdFromLocalStorage;
+    }
+
+    return '';
   }
   private getPatchedUrl = (url: string, workspaceId: string) => {
     return formatUrlWithWorkspaceId(url, workspaceId);
@@ -63,7 +86,7 @@ export class WorkspacePlugin implements Plugin<{}, {}, {}> {
   private async listenToHistoryChange(): Promise<void> {
     const startService = await this.core?.getStartServices();
     if (startService) {
-      startService[0].application.history.listen(() => {
+      this.historyUnregisterCallback = startService[0].application.history.listen(() => {
         if (this.shouldPatchUrl()) {
           const currentWorkspaceId = this.getworkspaceId();
           this.URLChange$.next(this.getPatchedUrl(window.location.href, currentWorkspaceId));
@@ -71,19 +94,27 @@ export class WorkspacePlugin implements Plugin<{}, {}, {}> {
       });
     }
   }
+  private listenToCurrentWorkspaceChange() {
+    this.currentWorkspaceIdSubscription = (this
+      .core as CoreSetup).workspaces.currentWorkspaceId$.subscribe((currentWorkspaceId) => {
+      localStorage.setItem(WORKSPACE_ID_STATE_KEY, currentWorkspaceId);
+      if (this.shouldPatchUrl()) {
+        this.URLChange$.next(this.getPatchedUrl(window.location.href, currentWorkspaceId));
+      }
+    });
+  }
   public async setup(core: CoreSetup) {
     this.core = core;
+
     /**
      * Retrive workspace id from url
      */
-    const workspaceId = this.getworkspaceIdFromURL();
+    const workspaceId = this.getworkspaceId();
 
-    if (workspaceId) {
-      /**
-       * Enter a workspace
-       */
-      this.core.workspaces.currentWorkspaceId$.next(workspaceId);
-    }
+    /**
+     * listen to current workspace change and patch workspace id in hash
+     */
+    this.listenToCurrentWorkspaceChange();
 
     /**
      * listen to history change and patch workspace id in hash
@@ -104,6 +135,13 @@ export class WorkspacePlugin implements Plugin<{}, {}, {}> {
       }, 500)
     );
 
+    if (workspaceId) {
+      /**
+       * Enter a workspace
+       */
+      this.core.workspaces.currentWorkspaceId$.next(workspaceId);
+    }
+
     return {};
   }
 
@@ -113,7 +151,14 @@ export class WorkspacePlugin implements Plugin<{}, {}, {}> {
 
   public stop() {
     this.urlChangeSubscription.unsubscribe();
-    this.applicationSubscription.unsubscribe();
+    this.currentWorkspaceIdSubscription.unsubscribe();
+    this.historyUnregisterCallback?.();
     window.removeEventListener('hashchange', this.hashChangeHandler);
+
+    /**
+     * Remove the localStorage record here so that when user copy a url without workspace id
+     * the record in local storage won't pollute getWorkspaceId
+     */
+    localStorage.removeItem(WORKSPACE_ID_STATE_KEY);
   }
 }
