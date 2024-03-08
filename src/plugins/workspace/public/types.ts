2d710b8520e933c1b1b563fa9e8539002b2803db
SuZhou-Joe <suzhou@amazon.com>
2024-03-08 11:39:01 +0800

    feat: remove useless code
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/workspace/public/types.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { CoreStart } from '../../../core/public';
-import { WorkspaceClient } from './workspace_client';
-
-export type Services = CoreStart & { workspaceClient: WorkspaceClient };
