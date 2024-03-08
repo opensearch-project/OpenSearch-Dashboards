be6538408fd926851a0f1a3538cd7b3579df5abd
Huy Nguyen <73027756+huyaboo@users.noreply.github.com>
2024-03-08 00:50:54 +0000

    Increased test coverage of search() method
    Signed-off-by: Huy Nguyen <73027756+huyaboo@users.noreply.github.com>

--- src/plugins/vis_type_vega/public/data_model/search_api.test.ts
+++ src/plugins/vis_type_vega/public/data_model/search_api.test.ts
@@ -5,8 +5,91 @@
 
 import { SavedObjectsClientContract, SavedObjectsFindOptions } from 'opensearch-dashboards/public';
 import { SearchAPI, SearchAPIDependencies } from './search_api';
+import { ISearchStart } from 'src/plugins/data/public';
+import { IUiSettingsClient } from 'opensearch-dashboards/public';
 
-describe('SearchAPI.findDataSourceId', () => {
+jest.mock('rxjs', () => ({
+  combineLatest: jest.fn().mockImplementation((obj) => obj),
+}));
+
+jest.mock('../../../data/public', () => ({
+  getSearchParamsFromRequest: jest.fn().mockImplementation((obj, _) => obj),
+}));
+
+interface MockSearch {
+  params?: Record<string, unknown>;
+  dataSourceId?: string;
+  pipe: () => {};
+}
+
+describe('SearchAPI.search', () => {
+  // This will only test that searchApiParams were correctly set. As such, every other function can be mocked
+  const getSearchAPI = (dataSourceEnabled: boolean) => {
+    const savedObjectsClient = {} as SavedObjectsClientContract;
+
+    const searchStartMock = {} as ISearchStart;
+    searchStartMock.search = jest.fn().mockImplementation((obj, _) => {
+      const mockedSearchResults = {} as MockSearch;
+      mockedSearchResults.params = obj;
+      mockedSearchResults.pipe = jest.fn().mockReturnValue(mockedSearchResults.params);
+      return mockedSearchResults;
+    });
+
+    const uiSettings = {} as IUiSettingsClient;
+    uiSettings.get = jest.fn().mockReturnValue(0);
+    uiSettings.get.bind = jest.fn().mockReturnValue(0);
+
+    const dependencies = {
+      savedObjectsClient,
+      dataSourceEnabled,
+      search: searchStartMock,
+      uiSettings,
+    } as SearchAPIDependencies;
+    const searchAPI = new SearchAPI(dependencies);
+    searchAPI.findDataSourceIdbyName = jest.fn().mockImplementation((name) => {
+      if (!dataSourceEnabled) {
+        throw new Error();
+      }
+      if (name === 'exampleName') {
+        return Promise.resolve('some-id');
+      }
+    });
+
+    return searchAPI;
+  };
+
+  test('If MDS is disabled and there is no datasource, return params without datasource id', async () => {
+    const searchAPI = getSearchAPI(false);
+    const requests = [{ name: 'example-id' }];
+    const fetchParams = ((await searchAPI.search(requests)) as unknown) as MockSearch[];
+    expect(fetchParams[0].params).toBe(requests[0]);
+    expect(fetchParams[0].hasOwnProperty('dataSourceId')).toBe(false);
+  });
+
+  test('If MDS is disabled and there is a datasource, it should throw an errorr', () => {
+    const searchAPI = getSearchAPI(false);
+    const requests = [{ name: 'example-id', data_source_name: 'non-existent-datasource' }];
+    expect(searchAPI.search(requests)).rejects.toThrowError();
+  });
+
+  test('If MDS is enabled and there is no datasource, return params without datasource id', async () => {
+    const searchAPI = getSearchAPI(true);
+    const requests = [{ name: 'example-id' }];
+    const fetchParams = ((await searchAPI.search(requests)) as unknown) as MockSearch[];
+    expect(fetchParams[0].params).toBe(requests[0]);
+    expect(fetchParams[0].hasOwnProperty('dataSourceId')).toBe(false);
+  });
+
+  test('If MDS is enabled and there is a datasource, return params with datasource id', async () => {
+    const searchAPI = getSearchAPI(true);
+    const requests = [{ name: 'example-id', data_source_name: 'exampleName' }];
+    const fetchParams = ((await searchAPI.search(requests)) as unknown) as MockSearch[];
+    expect(fetchParams[0].hasOwnProperty('params')).toBe(true);
+    expect(fetchParams[0].dataSourceId).toBe('some-id');
+  });
+});
+
+describe('SearchAPI.findDataSourceIdbyName', () => {
   const savedObjectsClient = {} as SavedObjectsClientContract;
   savedObjectsClient.find = jest.fn().mockImplementation((query: SavedObjectsFindOptions) => {
     if (query.search === `"uniqueDataSource"`) {
