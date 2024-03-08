89fcf63ca0790858143a97c3bfb47460214afc7e
Manasvini B Suryanarayana <manasvis@amazon.com>
2024-03-07 18:09:29 -0800

    Add functional test cypress workflow improvements and enable the workflow for in-house tests (#6061)
    Add cigroup matrix for parallel runs
    
    Signed-off-by: Manasvini B Suryanarayana <manasvis@amazon.com>

---
+++ cypress/utils/commands.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// --- Typed commands --
+
+Cypress.Commands.add('getElementByTestId', (testId, options = {}) => {
+  return cy.get(`[data-test-subj="${testId}"]`, options);
+});
+
+Cypress.Commands.add('getElementsByTestIds', (testIds, options = {}) => {
+  const selectors = [testIds].flat(Infinity).map((testId) => `[data-test-subj="${testId}"]`);
+  return cy.get(selectors.join(','), options);
+});
