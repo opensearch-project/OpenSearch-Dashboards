e58cea32e1038f43425e09c466affbb32b1dd393
Lin Wang <wonglam@amazon.com>
2024-03-22 01:18:05 +0800

    Update annotation for default selected features ids
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- src/plugins/workspace/common/constants.ts
+++ src/plugins/workspace/common/constants.ts
@@ -8,7 +8,10 @@ export const WORKSPACE_CREATE_APP_ID = 'workspace_create';
 export const WORKSPACE_LIST_APP_ID = 'workspace_list';
 export const WORKSPACE_UPDATE_APP_ID = 'workspace_update';
 export const WORKSPACE_OVERVIEW_APP_ID = 'workspace_overview';
-// These features will be checked and disabled in checkbox on default.
+/**
+ * Since every workspace always have overview and update page, these features will be selected by default
+ * and can't be changed in the workspace form feature selector
+ */
 export const DEFAULT_SELECTED_FEATURES_IDS = [WORKSPACE_UPDATE_APP_ID, WORKSPACE_OVERVIEW_APP_ID];
 export const WORKSPACE_SAVED_OBJECTS_CLIENT_WRAPPER_ID = 'workspace';
 export const WORKSPACE_CONFLICT_CONTROL_SAVED_OBJECTS_CLIENT_WRAPPER_ID =
