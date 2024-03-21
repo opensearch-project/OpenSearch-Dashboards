a2ed39f76c75bec843243ff85113485984102dbc
Yulong Ruan <ruanyl@amazon.com>
2024-03-21 09:55:37 +0800

    [Workspace] Register a workspace dropdown menu at the top of left nav bar (#6150)
    When workspace is enabled, the workspace plugin will register a
    workspace dropdown menu via `chrome.registerCollapsibleNavHeader`
    which displays a list of workspaces, links to create workspace page
    and workspace list page.
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- src/plugins/workspace/public/plugin.ts
+++ src/plugins/workspace/public/plugin.ts
@@ -4,6 +4,7 @@
  */
 
 import type { Subscription } from 'rxjs';
+import React from 'react';
 import {
   Plugin,
   CoreStart,
@@ -15,6 +16,7 @@ import { WORKSPACE_FATAL_ERROR_APP_ID, WORKSPACE_OVERVIEW_APP_ID } from '../comm
 import { getWorkspaceIdFromUrl } from '../../../core/public/utils';
 import { Services } from './types';
 import { WorkspaceClient } from './workspace_client';
+import { WorkspaceMenu } from './components/workspace_menu/workspace_menu';
 
 type WorkspaceAppType = (params: AppMountParameters, services: Services) => () => void;
 
@@ -30,9 +32,11 @@ export class WorkspacePlugin implements Plugin<{}, {}, {}> {
       });
     }
   }
+
   private getWorkspaceIdFromURL(basePath?: string): string | null {
     return getWorkspaceIdFromUrl(window.location.href, basePath);
   }
+
   public async setup(core: CoreSetup) {
     const workspaceClient = new WorkspaceClient(core.http, core.workspaces);
     await workspaceClient.init();
@@ -95,6 +99,16 @@ export class WorkspacePlugin implements Plugin<{}, {}, {}> {
         const { renderFatalErrorApp } = await import('./application');
         return mountWorkspaceApp(params, renderFatalErrorApp);
       },
+    });
+
+    /**
+     * Register workspace dropdown selector on the top of left navigation menu
+     */
+    core.chrome.registerCollapsibleNavHeader(() => {
+      if (!this.coreStart) {
+        return null;
+      }
+      return React.createElement(WorkspaceMenu, { coreStart: this.coreStart });
     });
 
     return {};
