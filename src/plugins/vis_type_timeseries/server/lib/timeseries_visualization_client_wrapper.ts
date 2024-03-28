ebb1bbd66f69ecdd64bff48f3568b9c5818b4a89
Huy Nguyen <73027756+huyaboo@users.noreply.github.com>
2024-03-28 23:09:57 +0000

    Add datasource references to the saved object
    Signed-off-by: Huy Nguyen <73027756+huyaboo@users.noreply.github.com>

---
+++ src/plugins/vis_type_timeseries/server/lib/timeseries_visualization_client_wrapper.ts
@@ -0,0 +1,82 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  SavedObjectsClientContract,
+  SavedObjectsClientWrapperFactory,
+  SavedObjectsClientWrapperOptions,
+  SavedObjectsCreateOptions,
+} from '../../../../core/server';
+import { getDataSourceEnabled } from './services';
+
+export const TIMESERIES_VISUALIZATION_CLIENT_WRAPPER_ID = 'timeseries-visualization-client-wrapper';
+
+export const timeSeriesVisualizationClientWrapper: SavedObjectsClientWrapperFactory = (
+  wrapperOptions: SavedObjectsClientWrapperOptions
+) => {
+  const createForTimeSeries = async <T = unknown>(
+    type: string,
+    attributes: T,
+    options?: SavedObjectsCreateOptions
+  ) => {
+    if (type !== 'visualization' || !getDataSourceEnabled().enabled) {
+      return await wrapperOptions.client.create(type, attributes, options);
+    }
+
+    // @ts-expect-error
+    const visState = JSON.parse(attributes.visState);
+
+    if (visState.type !== 'metrics' || !visState.params) {
+      return await wrapperOptions.client.create(type, attributes, options);
+    }
+
+    const newReferences = options?.references?.filter(
+      (reference) => reference.type !== 'data-source'
+    );
+
+    if (visState.params.data_source_id) {
+      if (await checkIfDataSourceExists(visState.params.data_source_id, wrapperOptions.client)) {
+        newReferences?.push({
+          id: visState.params.data_source_id,
+          name: 'dataSource',
+          type: 'data-source',
+        });
+      } else {
+        delete visState.params.data_source_id;
+      }
+    }
+
+    // @ts-expect-error
+    attributes.visState = JSON.stringify(visState);
+
+    return await wrapperOptions.client.create(type, attributes, {
+      ...options,
+      references: newReferences,
+    });
+  };
+
+  return {
+    ...wrapperOptions.client,
+    create: createForTimeSeries,
+    bulkCreate: wrapperOptions.client.bulkCreate,
+    checkConflicts: wrapperOptions.client.checkConflicts,
+    delete: wrapperOptions.client.delete,
+    find: wrapperOptions.client.find,
+    bulkGet: wrapperOptions.client.bulkGet,
+    get: wrapperOptions.client.get,
+    update: wrapperOptions.client.update,
+    bulkUpdate: wrapperOptions.client.bulkUpdate,
+    errors: wrapperOptions.client.errors,
+    addToNamespaces: wrapperOptions.client.addToNamespaces,
+    deleteFromNamespaces: wrapperOptions.client.deleteFromNamespaces,
+  };
+};
+
+const checkIfDataSourceExists = async (
+  id: string,
+  client: SavedObjectsClientContract
+): Promise<boolean> => {
+  return client.get('data-source', id).then((response) => !!response.attributes);
+};
