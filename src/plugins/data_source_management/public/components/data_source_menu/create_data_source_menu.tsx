726fb0e34704dbd54c71f9e716654323d67a302e
Yuanqi(Ella) Zhu <53279298+zhyuanqi@users.noreply.github.com>
2024-04-04 11:06:39 -0700

    Add default icon for selectable component and make sure the default datasource shows automatically (#6327)
    Signed-off-by: Yuanqi(Ella) Zhu <zhyuanqi@amazon.com>

--- src/plugins/data_source_management/public/components/data_source_menu/create_data_source_menu.tsx
+++ src/plugins/data_source_management/public/components/data_source_menu/create_data_source_menu.tsx
@@ -5,17 +5,18 @@
 
 import React from 'react';
 import { EuiHeaderLinks } from '@elastic/eui';
+import { IUiSettingsClient } from 'src/core/public';
 import { DataSourceMenu } from './data_source_menu';
 import { DataSourceMenuProps } from './types';
 import { MountPointPortal } from '../../../../opensearch_dashboards_react/public';
 
-export function createDataSourceMenu<T>() {
+export function createDataSourceMenu<T>(uiSettings: IUiSettingsClient) {
   return (props: DataSourceMenuProps<T>) => {
     if (props.setMenuMountPoint) {
       return (
         <MountPointPortal setMountPoint={props.setMenuMountPoint}>
           <EuiHeaderLinks data-test-subj="top-nav" gutterSize="xs">
-            <DataSourceMenu {...props} />
+            <DataSourceMenu {...props} uiSettings={uiSettings} />
           </EuiHeaderLinks>
         </MountPointPortal>
       );
