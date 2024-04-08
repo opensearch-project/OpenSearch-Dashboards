83532936b9420fea53816950eb7507b757a0bd4e
Anan Zhuang <ananzh@amazon.com>
2024-04-08 20:34:30 +0000

    clean out
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/plugins/vis_builder/server/capabilities_provider.ts
+++ src/plugins/vis_builder/server/capabilities_provider.ts
@@ -4,7 +4,7 @@
  */
 
 export const capabilitiesProvider = () => ({
-  'visualization-visbuilder-new': {
+  'visualization-visbuilder': {
     // TODO: investigate which capabilities we need to provide
     // createNew: true,
     // createShortUrl: true,
