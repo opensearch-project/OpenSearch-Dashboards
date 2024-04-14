70944f843633dd9589432223f43473779bf64c71
yujin-emma <yujin.emma.work@gmail.com>
2024-04-14 16:50:56 +0000

    add no-data-source component in examples
    Signed-off-by: yujin-emma <yujin.emma.work@gmail.com>

--- examples/multiple_data_source_examples/public/components/no_data_source_example.tsx
+++ examples/multiple_data_source_examples/public/components/no_data_source_example.tsx
@@ -23,22 +23,37 @@ import {
   // OuiWarningTextColor
 } from '@elastic/eui';
 import { DataSourceManagementPluginSetup } from 'src/plugins/data_source_management/public';
+import { NoDataSourceConfig } from 'src/plugins/data_source_management/public/components/data_source_menu';
+import { CoreStart, MountPoint } from 'opensearch-dashboards/public';
+
 
 interface NoDataSourceExampleProps {
   dataSourceEnabled: boolean;
   dataSourceManagement: DataSourceManagementPluginSetup;
+  setActionMenu?: (menuMount: MountPoint | undefined) => void;
+
 }
 
 export const NoDataSourceExample = ({
   dataSourceEnabled,
-  dataSourceManagement
+  dataSourceManagement,
+  setActionMenu
 }: NoDataSourceExampleProps) => {
-  // const DataSourceMenu = dataSourceManagement.ui.getDataSourceMenu<{}>();
+  const DataSourceMenu = dataSourceManagement.ui.getDataSourceMenu<NoDataSourceConfig>();
 const label = "    No data sources";
   return (
     <EuiPageBody component="main">
       <EuiPageHeader>
-        {dataSourceEnabled && (
+      {dataSourceEnabled && (
+          <DataSourceMenu
+          setMenuMountPoint={setActionMenu}
+            componentType={'NoDataSource'}
+            componentConfig={{
+              fullWidth: false,
+            }}
+          />
+        )}
+        {/* {dataSourceEnabled && (
 
  <>
             <EuiButtonEmpty
@@ -52,7 +67,7 @@ const label = "    No data sources";
               {label}
             </EuiButtonEmpty></>
 
-        )}
+        )} */}
         <EuiPageHeaderSection>
           <EuiTitle size="l">
             <h1>No data source example</h1>
@@ -62,17 +77,13 @@ const label = "    No data sources";
       <EuiPageContent>
         <EuiPageContentBody>
           <EuiText>
-            The data source view component is introduced in 2.14 which uses OuiContextMenu and
-            OuiPopOver as the base components. When multi data source feature is enabled, this
+            The no data source component is introduced in 2.14 which uses OuiButton as the base
+            component. When multi data source feature is enabled, this
             component can be consumed by adding dataSourceManagement as option plugin, and then
             mounted to the navigation bar by passing setHeaderActionMenu from AppMountParameters to
             the getDataSourceMenu function exposed from the plugin. This component can be used to
-            show specified connected data sources in the page. Find the mounted example in the
+            show no connected data sources in the page. Find the mounted example in the
             navigation bar
-          </EuiText>
-          <EuiSpacer />
-          <EuiText>
-            The component exposes a few properties via the DataSourceMenu component:
           </EuiText>
         </EuiPageContentBody>
       </EuiPageContent>
