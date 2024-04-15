opensearch-project/OpenSearch-Dashboards

commit cb9b26cedc65b874b9acce8afb0b47dbdaa037d7

[Workspace] Filter left nav menu items according to the current workspace (#6234)
* Filter left nav menu items according to the current workspace

An "Application Not Found" page will be displayed if accessing app which
is not configured by the workspace

---------

Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
Co-authored-by: SuZhou-Joe <suzhou@amazon.com>

src/plugins/workspace/public/plugin.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import type { Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import React from 'react';
 import { i18n } from '@osd/i18n';
 import {
@@ -12,6 +12,8 @@ import {
   CoreSetup,
   AppMountParameters,
   AppNavLinkStatus,
+  AppUpdater,
+  AppStatus,
 } from '../../../core/public';
 import {
   WORKSPACE_FATAL_ERROR_APP_ID,
@@ -26,6 +28,7 @@ import { WorkspaceClient } from './workspace_client';
 import { SavedObjectsManagementPluginSetup } from '../../../plugins/saved_objects_management/public';
 import { WorkspaceMenu } from './components/workspace_menu/workspace_menu';
 import { getWorkspaceColumn } from './components/workspace_column';
+import { isAppAccessibleInWorkspace } from './utils';
 
 type WorkspaceAppType = (params: AppMountParameters, services: Services) => () => void;
 
@@ -36,6 +39,8 @@ interface WorkspacePluginSetupDeps {
 export class WorkspacePlugin implements Plugin<{}, {}, WorkspacePluginSetupDeps> {
   private coreStart?: CoreStart;
   private currentWorkspaceSubscription?: Subscription;
+  private currentWorkspaceIdSubscription?: Subscription;
+  private appUpdater$ = new BehaviorSubject<AppUpdater>(() => undefined);
   private _changeSavedObjectCurrentWorkspace() {
     if (this.coreStart) {
       return this.coreStart.workspaces.currentWorkspaceId$.subscribe((currentWorkspaceId) => {
@@ -46,9 +51,34 @@ export class WorkspacePlugin implements Plugin<{}, {}, WorkspacePluginSetupDeps>
     }
   }
 
+  /**
+   * Filter nav links by the current workspace, once the current workspace change, the nav links(left nav bar)
+   * should also be updated according to the configured features of the current workspace
+   */
+  private filterNavLinks(core: CoreStart) {
+    const currentWorkspace$ = core.workspaces.currentWorkspace$;
+    this.currentWorkspaceSubscription?.unsubscribe();
+
+    this.currentWorkspaceSubscription = currentWorkspace$.subscribe((currentWorkspace) => {
+      if (currentWorkspace) {
+        this.appUpdater$.next((app) => {
+          if (isAppAccessibleInWorkspace(app, currentWorkspace)) {
+            return;
+          }
+          /**
+           * Change the app to `inaccessible` if it is not configured in the workspace
+           * If trying to access such app, an "Application Not Found" page will be displayed
+           */
+          return { status: AppStatus.inaccessible };
+        });
+      }
+    });
+  }
+
   public async setup(core: CoreSetup, { savedObjectsManagement }: WorkspacePluginSetupDeps) {
     const workspaceClient = new WorkspaceClient(core.http, core.workspaces);
     await workspaceClient.init();
+    core.application.registerAppUpdater(this.appUpdater$);
 
     /**
      * Retrieve workspace id from url
@@ -171,11 +201,16 @@ export class WorkspacePlugin implements Plugin<{}, {}, WorkspacePluginSetupDeps>
   public start(core: CoreStart) {
     this.coreStart = core;
 
-    this.currentWorkspaceSubscription = this._changeSavedObjectCurrentWorkspace();
+    this.currentWorkspaceIdSubscription = this._changeSavedObjectCurrentWorkspace();
+
+    // When starts, filter the nav links based on the current workspace
+    this.filterNavLinks(core);
+
     return {};
   }
 
   public stop() {
     this.currentWorkspaceSubscription?.unsubscribe();
+    this.currentWorkspaceIdSubscription?.unsubscribe();
   }
 }

src/plugins/workspace/public/utils.test.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { featureMatchesConfig } from './utils';
+import { AppNavLinkStatus } from '../../../core/public';
+import { featureMatchesConfig, isAppAccessibleInWorkspace } from './utils';
 
 describe('workspace utils: featureMatchesConfig', () => {
   it('feature configured with `*` should match any features', () => {
@@ -89,5 +90,62 @@ describe('workspace utils: featureMatchesConfig', () => {
     expect(match({ id: 'integrations', category: { id: 'management', label: 'Management' } })).toBe(
       true
     );
+  });
+});
+
+describe('workspace utils: isAppAccessibleInWorkspace', () => {
+  it('any app is accessible when workspace has no features configured', () => {
+    expect(
+      isAppAccessibleInWorkspace(
+        { id: 'any_app', title: 'Any app', mount: jest.fn() },
+        { id: 'workspace_id', name: 'workspace name' }
+      )
+    ).toBe(true);
+  });
+
+  it('An app is accessible when the workspace has the app configured', () => {
+    expect(
+      isAppAccessibleInWorkspace(
+        { id: 'dev_tools', title: 'Any app', mount: jest.fn() },
+        { id: 'workspace_id', name: 'workspace name', features: ['dev_tools'] }
+      )
+    ).toBe(true);
+  });
+
+  it('An app is not accessible when the workspace does not have the app configured', () => {
+    expect(
+      isAppAccessibleInWorkspace(
+        { id: 'dev_tools', title: 'Any app', mount: jest.fn() },
+        { id: 'workspace_id', name: 'workspace name', features: [] }
+      )
+    ).toBe(false);
+  });
+
+  it('An app is accessible if the nav link is hidden', () => {
+    expect(
+      isAppAccessibleInWorkspace(
+        {
+          id: 'dev_tools',
+          title: 'Any app',
+          mount: jest.fn(),
+          navLinkStatus: AppNavLinkStatus.hidden,
+        },
+        { id: 'workspace_id', name: 'workspace name', features: [] }
+      )
+    ).toBe(true);
+  });
+
+  it('An app is accessible if it is chromeless', () => {
+    expect(
+      isAppAccessibleInWorkspace(
+        {
+          id: 'dev_tools',
+          title: 'Any app',
+          mount: jest.fn(),
+          chromeless: true,
+        },
+        { id: 'workspace_id', name: 'workspace name', features: [] }
+      )
+    ).toBe(true);
   });
 });

src/plugins/workspace/public/utils.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { AppCategory } from '../../../core/public';
+import { App, AppCategory, AppNavLinkStatus, WorkspaceObject } from '../../../core/public';
 
 /**
  * Checks if a given feature matches the provided feature configuration.
@@ -25,6 +25,11 @@ export const featureMatchesConfig = (featureConfigs: string[]) => ({
 }) => {
   let matched = false;
 
+  /**
+   * Iterate through each feature configuration to determine if the given feature matches any of them.
+   * Note: The loop will not break prematurely because the order of featureConfigs array matters.
+   * Later configurations may override previous ones, so each configuration must be evaluated in sequence.
+   */
   for (const featureConfig of featureConfigs) {
     // '*' matches any feature
     if (featureConfig === '*') {
@@ -55,3 +60,42 @@ export const featureMatchesConfig = (featureConfigs: string[]) => ({
 
   return matched;
 };
+
+/**
+ * Check if an app is accessible in a workspace based on the workspace configured features
+ */
+export function isAppAccessibleInWorkspace(app: App, workspace: WorkspaceObject) {
+  /**
+   * When workspace has no features configured, all apps are considered to be accessible
+   */
+  if (!workspace.features) {
+    return true;
+  }
+
+  /**
+   * The app is configured into a workspace, it is accessible after entering the workspace
+   */
+  const featureMatcher = featureMatchesConfig(workspace.features);
+  if (featureMatcher({ id: app.id, category: app.category })) {
+    return true;
+  }
+
+  /*
+   * An app with hidden nav link is not configurable by workspace, which means user won't be
+   * able to select/unselect it when configuring workspace features. Such apps are by default
+   * accessible when in a workspace.
+   */
+  if (app.navLinkStatus === AppNavLinkStatus.hidden) {
+    return true;
+  }
+
+  /**
+   * A chromeless app is not configurable by workspace, which means user won't be
+   * able to select/unselect it when configuring workspace features. Such apps are by default
+   * accessible when in a workspace.
+   */
+  if (app.chromeless) {
+    return true;
+  }
+  return false;
+}

commit 8e5930a03c9698bd3b2f5b40b520060fa287d3cc

fix(workspace): apps are missing when updating a workspace
This is caused by #6234 which marked apps as inaccessible when the apps
are not configured into the current workspace. However, inaccessible
apps can not be configured into a workspace.

Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

src/plugins/workspace/public/application.tsx
@@ -11,6 +11,7 @@ import { WorkspaceFatalError } from './components/workspace_fatal_error';
 import { WorkspaceCreatorApp } from './components/workspace_creator_app';
 import { WorkspaceUpdaterApp } from './components/workspace_updater_app';
 import { WorkspaceListApp } from './components/workspace_list_app';
+import { WorkspaceUpdaterProps } from './components/workspace_updater';
 import { Services } from './types';
 
 export const renderCreatorApp = ({ element }: AppMountParameters, services: Services) => {
@@ -26,10 +27,14 @@ export const renderCreatorApp = ({ element }: AppMountParameters, services: Serv
   };
 };
 
-export const renderUpdaterApp = ({ element }: AppMountParameters, services: Services) => {
+export const renderUpdaterApp = (
+  { element }: AppMountParameters,
+  services: Services,
+  props: WorkspaceUpdaterProps
+) => {
   ReactDOM.render(
     <OpenSearchDashboardsContextProvider services={services}>
-      <WorkspaceUpdaterApp />
+      <WorkspaceUpdaterApp {...props} />
     </OpenSearchDashboardsContextProvider>,
     element
   );

src/plugins/workspace/public/components/workspace_form/types.ts
@@ -35,4 +35,5 @@ export interface WorkspaceFormProps {
   onSubmit?: (formData: WorkspaceFormSubmitData) => void;
   defaultValues?: WorkspaceFormData;
   operationType?: WorkspaceOperationType;
+  restrictedApps?: Set<string>;
 }

src/plugins/workspace/public/components/workspace_form/utils.ts
@@ -44,18 +44,28 @@ export const getNumberOfErrors = (formErrors: WorkspaceFormErrors) => {
 export const convertApplicationsToFeaturesOrGroups = (
   applications: Array<
     Pick<PublicAppInfo, 'id' | 'title' | 'category' | 'navLinkStatus' | 'chromeless'>
-  >
+  >,
+  restrictedApps?: Set<string>
 ) => {
   const UNDEFINED = 'undefined';
 
   // Filter out all hidden applications and management applications and default selected features
-  const visibleApplications = applications.filter(
-    ({ navLinkStatus, chromeless, category, id }) =>
+  const visibleApplications = applications.filter(({ navLinkStatus, chromeless, category, id }) => {
+    /**
+     * Restrict apps are apps that can be configured into a workspace, but restrict to access
+     * because the current workspace didn't have the apps configured, such apps should NOT filter out
+     */
+    if (restrictedApps && restrictedApps.has(id)) {
+      return true;
+    }
+
+    return (
       navLinkStatus !== AppNavLinkStatus.hidden &&
       !chromeless &&
       !DEFAULT_SELECTED_FEATURES_IDS.includes(id) &&
       category?.id !== DEFAULT_APP_CATEGORIES.management.id
-  );
+    );
+  });
 
   /**
    *

src/plugins/workspace/public/components/workspace_form/workspace_feature_selector.tsx
@@ -24,16 +24,19 @@ export interface WorkspaceFeatureSelectorProps {
   >;
   selectedFeatures: string[];
   onChange: (newFeatures: string[]) => void;
+  restrictedApps?: Set<string>;
 }
 
 export const WorkspaceFeatureSelector = ({
   applications,
   selectedFeatures,
   onChange,
+  restrictedApps,
 }: WorkspaceFeatureSelectorProps) => {
-  const featuresOrGroups = useMemo(() => convertApplicationsToFeaturesOrGroups(applications), [
-    applications,
-  ]);
+  const featuresOrGroups = useMemo(
+    () => convertApplicationsToFeaturesOrGroups(applications, restrictedApps),
+    [applications, restrictedApps]
+  );
 
   const handleFeatureChange = useCallback<EuiCheckboxGroupProps['onChange']>(
     (featureId) => {

src/plugins/workspace/public/components/workspace_form/workspace_form.tsx
@@ -136,6 +136,7 @@ export const WorkspaceForm = (props: WorkspaceFormProps) => {
             applications={applications}
             selectedFeatures={formData.features}
             onChange={handleFeaturesChange}
+            restrictedApps={props.restrictedApps}
           />
         </EuiPanel>
       )}

src/plugins/workspace/public/components/workspace_updater/index.tsx
@@ -3,4 +3,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export { WorkspaceUpdater } from './workspace_updater';
+export { WorkspaceUpdater, WorkspaceUpdaterProps } from './workspace_updater';

src/plugins/workspace/public/components/workspace_updater/workspace_updater.tsx
@@ -8,7 +8,7 @@ import { EuiPage, EuiPageBody, EuiPageHeader, EuiPageContent } from '@elastic/eu
 import { i18n } from '@osd/i18n';
 import { WorkspaceAttribute } from 'opensearch-dashboards/public';
 import { useObservable } from 'react-use';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { useOpenSearchDashboards } from '../../../../opensearch_dashboards_react/public';
 import { WorkspaceForm, WorkspaceFormSubmitData, WorkspaceOperationType } from '../workspace_form';
 import { WORKSPACE_OVERVIEW_APP_ID } from '../../../common/constants';
@@ -16,18 +16,23 @@ import { formatUrlWithWorkspaceId } from '../../../../../core/public/utils';
 import { WorkspaceClient } from '../../workspace_client';
 import { WorkspaceFormData } from '../workspace_form/types';
 
+export interface WorkspaceUpdaterProps {
+  restrictedApps$?: BehaviorSubject<Set<string>>;
+}
+
 function getFormDataFromWorkspace(
   currentWorkspace: WorkspaceAttribute | null | undefined
 ): WorkspaceFormData {
   return (currentWorkspace || {}) as WorkspaceFormData;
 }
 
-export const WorkspaceUpdater = () => {
+export const WorkspaceUpdater = (props: WorkspaceUpdaterProps) => {
   const {
     services: { application, workspaces, notifications, http, workspaceClient },
   } = useOpenSearchDashboards<{ workspaceClient: WorkspaceClient }>();
 
   const currentWorkspace = useObservable(workspaces ? workspaces.currentWorkspace$ : of(null));
+  const restrictedApps = useObservable(props.restrictedApps$ ?? of(undefined));
   const [currentWorkspaceFormData, setCurrentWorkspaceFormData] = useState<WorkspaceFormData>(
     getFormDataFromWorkspace(currentWorkspace)
   );
@@ -108,6 +113,7 @@ export const WorkspaceUpdater = () => {
               defaultValues={currentWorkspaceFormData}
               onSubmit={handleWorkspaceFormSubmit}
               operationType={WorkspaceOperationType.Update}
+              restrictedApps={restrictedApps}
             />
           )}
         </EuiPageContent>

src/plugins/workspace/public/components/workspace_updater_app.tsx
@@ -7,9 +7,9 @@ import React, { useEffect } from 'react';
 import { I18nProvider } from '@osd/i18n/react';
 import { i18n } from '@osd/i18n';
 import { useOpenSearchDashboards } from '../../../opensearch_dashboards_react/public';
-import { WorkspaceUpdater } from './workspace_updater';
+import { WorkspaceUpdater, WorkspaceUpdaterProps } from './workspace_updater';
 
-export const WorkspaceUpdaterApp = () => {
+export const WorkspaceUpdaterApp = (props: WorkspaceUpdaterProps) => {
   const {
     services: { chrome },
   } = useOpenSearchDashboards();
@@ -29,7 +29,7 @@ export const WorkspaceUpdaterApp = () => {
 
   return (
     <I18nProvider>
-      <WorkspaceUpdater />
+      <WorkspaceUpdater {...props} />
     </I18nProvider>
   );
 };

src/plugins/workspace/public/plugin.ts
@@ -14,6 +14,7 @@ import {
   AppNavLinkStatus,
   AppUpdater,
   AppStatus,
+  DEFAULT_APP_CATEGORIES,
 } from '../../../core/public';
 import {
   WORKSPACE_FATAL_ERROR_APP_ID,
@@ -30,7 +31,11 @@ import { WorkspaceMenu } from './components/workspace_menu/workspace_menu';
 import { getWorkspaceColumn } from './components/workspace_column';
 import { isAppAccessibleInWorkspace } from './utils';
 
-type WorkspaceAppType = (params: AppMountParameters, services: Services) => () => void;
+type WorkspaceAppType = (
+  params: AppMountParameters,
+  services: Services,
+  props: Record<string, any>
+) => () => void;
 
 interface WorkspacePluginSetupDeps {
   savedObjectsManagement?: SavedObjectsManagementPluginSetup;
@@ -41,6 +46,7 @@ export class WorkspacePlugin implements Plugin<{}, {}, WorkspacePluginSetupDeps>
   private currentWorkspaceSubscription?: Subscription;
   private currentWorkspaceIdSubscription?: Subscription;
   private appUpdater$ = new BehaviorSubject<AppUpdater>(() => undefined);
+  private restrictedApps$ = new BehaviorSubject(new Set<string>());
   private _changeSavedObjectCurrentWorkspace() {
     if (this.coreStart) {
       return this.coreStart.workspaces.currentWorkspaceId$.subscribe((currentWorkspaceId) => {
@@ -65,6 +71,20 @@ export class WorkspacePlugin implements Plugin<{}, {}, WorkspacePluginSetupDeps>
           if (isAppAccessibleInWorkspace(app, currentWorkspace)) {
             return;
           }
+
+          if (app.status === AppStatus.inaccessible) {
+            return;
+          }
+
+          /**
+           * Restricted apps can be configured into a workspace, but they are not configured by the
+           * current workspace. Apps of management category can NOT configured into a workspace, so
+           * needs to be excluded.
+           */
+          if (app.category?.id !== DEFAULT_APP_CATEGORIES.management.id) {
+            this.restrictedApps$.next(this.restrictedApps$.value.add(app.id));
+          }
+
           /**
            * Change the app to `inaccessible` if it is not configured in the workspace
            * If trying to access such app, an "Application Not Found" page will be displayed
@@ -129,7 +149,7 @@ export class WorkspacePlugin implements Plugin<{}, {}, WorkspacePluginSetupDeps>
         workspaceClient,
       };
 
-      return renderApp(params, services);
+      return renderApp(params, services, { restrictedApps$: this.restrictedApps$ });
     };
 
     // create

src/plugins/workspace/public/components/workspace_form/utils.test.ts
@@ -7,6 +7,43 @@ import { AppNavLinkStatus, DEFAULT_APP_CATEGORIES } from '../../../../../core/pu
 import { convertApplicationsToFeaturesOrGroups } from './utils';
 
 describe('convertApplicationsToFeaturesOrGroups', () => {
+  it('should not filter out restrict Apps', () => {
+    expect(
+      convertApplicationsToFeaturesOrGroups(
+        [
+          { id: 'foo1', title: 'Foo 1', navLinkStatus: AppNavLinkStatus.hidden },
+          { id: 'foo2', title: 'Foo 2', navLinkStatus: AppNavLinkStatus.visible, chromeless: true },
+          {
+            id: 'foo3',
+            title: 'Foo 3',
+            navLinkStatus: AppNavLinkStatus.visible,
+            category: DEFAULT_APP_CATEGORIES.management,
+          },
+          {
+            id: 'workspace_overview',
+            title: 'Workspace Overview',
+            navLinkStatus: AppNavLinkStatus.visible,
+          },
+          {
+            id: 'bar',
+            title: 'Bar',
+            navLinkStatus: AppNavLinkStatus.visible,
+          },
+        ],
+        new Set(['foo1'])
+      )
+    ).toEqual([
+      {
+        id: 'foo1',
+        name: 'Foo 1',
+      },
+      {
+        id: 'bar',
+        name: 'Bar',
+      },
+    ]);
+  });
+
   it('should filter out invisible features', () => {
     expect(
       convertApplicationsToFeaturesOrGroups([