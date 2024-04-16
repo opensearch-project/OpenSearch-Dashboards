eadc779af376df2985a10efa997e0d4aa0b2a45a
Yu Jin <112784385+yujin-emma@users.noreply.github.com>
2024-04-16 13:37:22 -0700

    Update data_source_menu.tsx

--- src/plugins/data_source_management/public/components/data_source_menu/data_source_menu.tsx
+++ src/plugins/data_source_management/public/components/data_source_menu/data_source_menu.tsx
@@ -19,7 +19,7 @@ import {
 import { DataSourceSelectable } from '../data_source_selectable';
 
 export function DataSourceMenu<T>(props: DataSourceMenuProps<T>): ReactElement | null {
-  const { componentType, componentConfig, uiSettings, hideLocalCluster } = props;
+  const { componentType, componentConfig, uiSettings, hideLocalCluster, application } = props;
 
   function renderDataSourceView(config: DataSourceViewConfig): ReactElement | null {
     const {
@@ -81,6 +81,7 @@ export function DataSourceMenu<T>(props: DataSourceMenuProps<T>): ReactElement |
         hideLocalCluster={hideLocalCluster || false}
         fullWidth={fullWidth}
         uiSettings={uiSettings}
+        application={application}
       />
     );
   }
