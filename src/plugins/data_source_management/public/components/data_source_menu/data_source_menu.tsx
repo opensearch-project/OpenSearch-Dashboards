b6978f235fc6004c679420e6eb3b801db1d602a1
Yuanqi(Ella) Zhu <53279298+zhyuanqi@users.noreply.github.com>
2024-04-17 13:58:04 -0700

    UI change for datasource view picker to enable selectable (#6497)
    * UI change for data source view to enable selectable
    
    Signed-off-by: Yuanqi(Ella) Zhu <zhyuanqi@amazon.com>
    
    * Fix typo and rebase
    
    Signed-off-by: Yuanqi(Ella) Zhu <zhyuanqi@amazon.com>
    
    ---------
    
    Signed-off-by: Yuanqi(Ella) Zhu <zhyuanqi@amazon.com>

--- src/plugins/data_source_management/public/components/data_source_menu/data_source_menu.tsx
+++ src/plugins/data_source_management/public/components/data_source_menu/data_source_menu.tsx
@@ -40,6 +40,7 @@ export function DataSourceMenu<T>(props: DataSourceMenuProps<T>): ReactElement |
         dataSourceFilter={dataSourceFilter}
         onSelectedDataSources={onSelectedDataSources}
         uiSettings={uiSettings}
+        application={application}
       />
     );
   }
