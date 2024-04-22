8976752132299f60ee500eac2f09a44a5572f190
Kawika Avilla <kavilla414@gmail.com>
2024-04-22 20:59:34 +0000

    Cleanup unused helper function
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- src/plugins/data/common/opensearch_query/opensearch_query/index.ts
+++ src/plugins/data/common/opensearch_query/opensearch_query/index.ts
@@ -31,6 +31,5 @@
 export { buildOpenSearchQuery, OpenSearchQueryConfig } from './build_opensearch_query';
 export { buildQueryFromFilters } from './from_filters';
 export { luceneStringToDsl } from './lucene_string_to_dsl';
-export { getIndexPatternFromSql, sqlStringToDsl } from './sql_string_to_dsl';
 export { decorateQuery } from './decorate_query';
 export { getOpenSearchQueryConfig } from './get_opensearch_query_config';
