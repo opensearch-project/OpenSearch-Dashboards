8db96881c4a4e710e522e4459fa6a3a656c70699
yuboluo <15242088755@163.com>
2024-04-22 14:07:42 +0800

    [Workspace] Make dashboards management available (#6575)
    * [Workspace] Make dashboards management available
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    
    * [Workspace] optimize the code
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    
    * [Workspace] optimize the code
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    
    * [Workspace] modify requiredPlugins
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    
    * Changeset file for PR #6575 created/updated
    
    * [Workspace] add more comments
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    
    * [Workspace] delete useless code
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    
    * [Workspace] delete useless code
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    
    ---------
    
    Signed-off-by: yubonluo <yubonluo@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/workspace/public/utils.ts
+++ src/plugins/workspace/public/utils.ts
@@ -108,14 +108,19 @@ export function isAppAccessibleInWorkspace(app: App, workspace: WorkspaceObject)
   return false;
 }
 
+// Get all apps that should be displayed in workspace when create/update a workspace.
 export const filterWorkspaceConfigurableApps = (applications: PublicAppInfo[]) => {
   const visibleApplications = applications.filter(({ navLinkStatus, chromeless, category, id }) => {
-    return (
+    const filterCondition =
       navLinkStatus !== AppNavLinkStatus.hidden &&
       !chromeless &&
-      !DEFAULT_SELECTED_FEATURES_IDS.includes(id) &&
-      category?.id !== DEFAULT_APP_CATEGORIES.management.id
-    );
+      !DEFAULT_SELECTED_FEATURES_IDS.includes(id);
+    // If the category is management, only retain Dashboards Management which contains saved objets and index patterns.
+    // Saved objets can show all saved objects in the current workspace and index patterns is at workspace level.
+    if (category?.id === DEFAULT_APP_CATEGORIES.management.id) {
+      return filterCondition && id === 'management';
+    }
+    return filterCondition;
   });
 
   return visibleApplications;
