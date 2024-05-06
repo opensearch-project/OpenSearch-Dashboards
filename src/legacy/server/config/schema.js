ab8e6c06dd27e533e153708ef96d9dfebd0e8940
tygao <tygao@amazon.com>
2024-05-06 09:26:14 +0800

    update joi default value
    Signed-off-by: tygao <tygao@amazon.com>

--- src/legacy/server/config/schema.js
+++ src/legacy/server/config/schema.js
@@ -251,7 +251,7 @@ export default () =>
       survey: Joi.object({
         url: Joi.any().default('/'),
       }),
-      topRightNavigation: Joi.boolean().default(true),
+      topRightNavigation: Joi.boolean().default(false),
     }).default(),
 
     savedObjects: HANDLED_IN_NEW_PLATFORM,
