1a182034c84752e47aacf3e0e8994b704d463829
Flyingliuhub <33105471+flyingliuhub@users.noreply.github.com>
2024-05-07 05:03:24 +0000

    prevent crash when disk full
    Signed-off-by: Flyingliuhub <33105471+flyingliuhub@users.noreply.github.com>

--- src/legacy/server/logging/configuration.js
+++ src/legacy/server/logging/configuration.js
@@ -64,6 +64,7 @@ export default function loggingConfiguration(config) {
       json: config.get('logging.json'),
       dest: config.get('logging.dest'),
       timezone: config.get('logging.timezone'),
+      streamErrorHandling: config.get('logging.streamErrorHandling'),
 
       // I'm adding the default here because if you add another filter
       // using the commandline it will remove authorization. I want users
