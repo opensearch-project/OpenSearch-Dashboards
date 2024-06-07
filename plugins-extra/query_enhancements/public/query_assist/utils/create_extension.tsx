daa485b2cf148d15910df2c4cc6e6f63eae586c5
Joshua Li <joshuali925@gmail.com>
2024-06-07 19:06:49 +0000

    support MDS for query assist
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- plugins-extra/query_enhancements/public/query_assist/utils/create_extension.tsx
+++ plugins-extra/query_enhancements/public/query_assist/utils/create_extension.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
 import { HttpSetup } from 'opensearch-dashboards/public';
-import { QueryAssistBar } from '../components';
+import React from 'react';
+import { getMdsDataSourceId } from '.';
 import { SearchBarExtensionConfig } from '../../../../../src/plugins/data/public/ui/search_bar_extensions';
+import { getData } from '../../services';
+import { QueryAssistBar } from '../components';
 
 export const createQueryAssistExtension = (
   http: HttpSetup,
@@ -11,19 +13,27 @@ export const createQueryAssistExtension = (
     id: 'query-assist',
     order: 1000,
     isEnabled: (() => {
-      let agentConfigured: boolean;
+      const agentConfiguredMap: Map<string | undefined, boolean> = new Map();
       return async (dependencies) => {
         // currently query assist tool relies on opensearch API to get index
         // mappings, other data sources are not supported
         if (dependencies.dataSource && dependencies.dataSource?.getType() !== 'default')
           return false;
-        if (agentConfigured === undefined) {
-          agentConfigured = await http
-            .get<{ configured: boolean }>(`/api/ql/query_assist/configured/${language}`)
-            .then((response) => response.configured)
-            .catch(() => false);
-        }
-        return agentConfigured;
+
+        const dataSourceId = await getMdsDataSourceId(
+          getData().indexPatterns,
+          dependencies.indexPatterns?.at(0)
+        );
+        const cached = agentConfiguredMap.get(dataSourceId);
+        if (cached !== undefined) return cached;
+        const configured = await http
+          .get<{ configured: boolean }>(`/api/ql/query_assist/configured/${language}`, {
+            query: { dataSourceId },
+          })
+          .then((response) => response.configured)
+          .catch(() => false);
+        agentConfiguredMap.set(dataSourceId, configured);
+        return configured;
       };
     })(),
     getComponent: (dependencies) => (
--- plugins-extra/query_enhancements/server/types.ts
+++ plugins-extra/query_enhancements/server/types.ts
@@ -4,6 +4,9 @@
  */
 
 import { DataPluginSetup } from 'src/plugins/data/server/plugin';
+import { HomeServerPluginSetup } from 'src/plugins/home/server/plugin';
+import { Logger } from '../../../src/core/server';
+import { DataSourcePluginStart } from '../../../src/plugins/data_source/server';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface QueryEnhancementsPluginSetup {}
@@ -11,6 +14,7 @@ export interface QueryEnhancementsPluginSetup {}
 export interface QueryEnhancementsPluginStart {}
 export interface QueryEnhancementsPluginSetupDependencies {
   data: DataPluginSetup;
+  dataSource?: DataSourcePluginStart;
 }
 
 export interface ISchema {
@@ -30,4 +34,13 @@ export interface IPPLEventsDataSource {
   schema: ISchema[];
   datarows: any[];
   jsonData?: any[];
+}
+
+declare module '../../../src/core/server' {
+  interface RequestHandlerContext {
+    query_assist: {
+      logger: Logger;
+      dataSourceEnabled: boolean;
+    };
+  }
 }
