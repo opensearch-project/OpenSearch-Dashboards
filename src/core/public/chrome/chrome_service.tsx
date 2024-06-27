afe21ba186b411b4ab82efc36842696eb956bb2f
SuZhou-Joe <suzhou@amazon.com>
2024-06-27 10:20:42 +0800

    feat: comply recent items with workspace
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/core/public/chrome/chrome_service.tsx
+++ src/core/public/chrome/chrome_service.tsx
@@ -48,7 +48,7 @@ import { ChromeNavLinks, NavLinksService, ChromeNavLink } from './nav_links';
 import { ChromeRecentlyAccessed, RecentlyAccessedService } from './recently_accessed';
 import { Header } from './ui';
 import { ChromeHelpExtensionMenuLink } from './ui/header/header_help_menu';
-import { Branding } from '../';
+import { Branding, WorkspacesStart } from '../';
 import { getLogos } from '../../common';
 import type { Logos } from '../../common/types';
 import { OverlayStart } from '../overlays';
@@ -98,6 +98,7 @@ export interface StartDeps {
   notifications: NotificationsStart;
   uiSettings: IUiSettingsClient;
   overlays: OverlayStart;
+  workspaces: WorkspacesStart;
 }
 
 type CollapsibleNavHeaderRender = () => JSX.Element | null;
@@ -169,6 +170,7 @@ export class ChromeService {
     notifications,
     uiSettings,
     overlays,
+    workspaces,
   }: StartDeps): Promise<InternalChromeStart> {
     this.initVisibility(application);
 
@@ -184,7 +186,7 @@ export class ChromeService {
 
     const navControls = this.navControls.start();
     const navLinks = this.navLinks.start({ application, http });
-    const recentlyAccessed = await this.recentlyAccessed.start({ http });
+    const recentlyAccessed = await this.recentlyAccessed.start({ http, workspaces });
     const docTitle = this.docTitle.start({ document: window.document });
 
     // erase chrome fields from a previous app while switching to a next app
