70ed2c738c646665e84dd8f6cdccb0a236ee20e0
Sean Li <lnse@amazon.com>
2024-07-11 04:35:13 -0700

    initial implementation for dataset dropdown
    Signed-off-by: Sean Li <lnse@amazon.com>

---
+++ src/plugins/data/public/ui/dataset_navigator/index.tsx
@@ -0,0 +1,7 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { DataSetNavigator } from './dataset_navigator';
+export { IndexPatternSelectable } from './index_patterns_selectable';
