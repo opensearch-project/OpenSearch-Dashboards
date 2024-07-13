dab4789e6f725ee59eba0de2b3c15f23f1a5f811
SuZhou-Joe <suzhou@amazon.com>
2024-07-13 13:26:36 +0800

    feat: add all use case
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/core/public/chrome/nav_group/nav_group_service.ts
+++ src/core/public/chrome/nav_group/nav_group_service.ts
@@ -29,6 +29,11 @@ export interface ChromeRegistrationNavLink {
    * link with parentNavLinkId field will be displayed as nested items in navigation.
    */
   parentNavLinkId?: string;
+
+  /**
+   * If the nav link should be shown in 'all' nav group
+   */
+  showInAllNavGroup?: boolean;
 }
 
 export type NavGroupItemInMap = ChromeNavGroup & {
