c9da1130a93147ba49867592266b8c30ba90983c
Eric <menwe@amazon.com>
2024-07-17 22:50:07 +0000

    move ppl out of scope
    Signed-off-by: Eric <menwe@amazon.com>

--- src/plugins/data/public/antlr/opensearch_ppl/query_manager/ast/builder/timestamp_builder.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
--- src/plugins/data/public/antlr/opensearch_ppl/query_manager/query_builder/index.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export { PPLQueryBuilder } from './ppl_query_builder';
