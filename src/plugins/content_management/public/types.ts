53ec827974e72bd5d49d9723d8b2186430c55d73
Yulong Ruan <ruanyl@amazon.com>
2024-07-20 06:37:10 +0800

    mark registerContentProvider API as experimental
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- src/plugins/content_management/public/types.ts
+++ src/plugins/content_management/public/types.ts
@@ -13,6 +13,9 @@ export interface ContentManagementPluginSetup {
   registerPage: ContentManagementService['registerPage'];
 }
 export interface ContentManagementPluginStart {
+  /**
+   * @experimental this API is experimental and might change in future releases
+   */
   registerContentProvider: (provider: ContentProvider) => void;
   renderPage: (id: string) => React.ReactNode;
 }
