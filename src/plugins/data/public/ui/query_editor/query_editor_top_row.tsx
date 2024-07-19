a004be677a04ba75737de905de0ed3a4fd70970b
Sean Li <lnse@amazon.com>
2024-07-19 11:38:11 -0700

    fixing readonly source
    Signed-off-by: Sean Li <lnse@amazon.com>

--- src/plugins/data/public/ui/query_editor/query_editor_top_row.tsx
+++ src/plugins/data/public/ui/query_editor/query_editor_top_row.tsx
@@ -191,16 +191,7 @@ export default function QueryEditorTopRow(props: QueryEditorTopRowProps) {
   }
 
   function isValidQuery(query: Query | undefined) {
-    if (!query || !query.query) return false;
-    return (
-      !Array.isArray(props.indexPatterns!) ||
-      compact(props.indexPatterns!).length === 0 ||
-      fromUser(query!.query).includes(
-        typeof props.indexPatterns[0] === 'string'
-          ? props.indexPatterns[0]
-          : props.indexPatterns[0].title
-      )
-    );
+    return query && query.query;
   }
 
   function getQueryStringInitialValue(language: string) {
