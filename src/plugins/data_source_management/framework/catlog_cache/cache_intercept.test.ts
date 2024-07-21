15cc993f2a2eb844435e75b18c3b080c5bfb715d
Ryan Liang <jiallian@amazon.com>
2024-07-20 17:38:25 -0700

    unit test 8 - Add tests for caching components
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

---
+++ src/plugins/data_source_management/framework/catlog_cache/cache_intercept.test.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { SECURITY_DASHBOARDS_LOGOUT_URL } from '../../framework/constants';
+import { ASYNC_QUERY_ACCELERATIONS_CACHE, ASYNC_QUERY_DATASOURCE_CACHE } from '../utils/shared';
+import { catalogRequestIntercept } from './cache_intercept';
+
+interface LooseObject {
+  [key: string]: any;
+}
+
+// Mock sessionStorage
+const sessionStorageMock = (() => {
+  let store = {} as LooseObject;
+  return {
+    getItem(key: string) {
+      return store[key] || null;
+    },
+    setItem(key: string, value: string) {
+      store[key] = value.toString();
+    },
+    removeItem(key: string) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, 'sessionStorage', { value: sessionStorageMock });
+
+describe('Intercept logout handler', () => {
+  beforeEach(() => {
+    jest.spyOn(window.sessionStorage, 'removeItem');
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  const logoutPath = {
+    path: SECURITY_DASHBOARDS_LOGOUT_URL,
+  };
+
+  it('Intercept logout handler should clear the cache session', () => {
+    const logoutInterceptFn = catalogRequestIntercept();
+    logoutInterceptFn(logoutPath, null);
+    expect(sessionStorage.removeItem).toBeCalledWith(ASYNC_QUERY_DATASOURCE_CACHE);
+    expect(sessionStorage.removeItem).toBeCalledWith(ASYNC_QUERY_ACCELERATIONS_CACHE);
+  });
+});
