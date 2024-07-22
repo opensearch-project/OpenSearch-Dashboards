d4fd27626c7032f8811e421e6900a1746e728f22
Ryan Liang <jiallian@amazon.com>
2024-07-22 12:54:09 -0700

    Resolve comments - 2
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- src/plugins/data_source_management/framework/catalog_cache/cache_intercept.ts
+++ src/plugins/data_source_management/framework/catalog_cache/cache_intercept.ts
@@ -3,15 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { HttpFetchOptionsWithPath, IHttpInterceptController } from 'opensearch-dashboards/public';
+import { HttpFetchOptionsWithPath } from 'opensearch-dashboards/public';
 import { SECURITY_DASHBOARDS_LOGOUT_URL } from '../constants';
 import { CatalogCacheManager } from './cache_manager';
 
-export function catalogRequestIntercept(): any {
-  return (
-    fetchOptions: Readonly<HttpFetchOptionsWithPath>,
-    _controller: IHttpInterceptController
-  ) => {
+export function catalogRequestIntercept(): (
+  fetchOptions: Readonly<HttpFetchOptionsWithPath>
+) => void {
+  return (fetchOptions: Readonly<HttpFetchOptionsWithPath>) => {
     if (fetchOptions.path.includes(SECURITY_DASHBOARDS_LOGOUT_URL)) {
       // Clears all user catalog cache details
       CatalogCacheManager.clearDataSourceCache();
