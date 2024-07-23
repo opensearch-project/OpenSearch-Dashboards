4722476dee08b450aeb0039e74c38fd3539a7e3d
SuZhou-Joe <suzhou@amazon.com>
2024-07-23 23:17:38 +0800

    feat: revert the home related change
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/core/public/chrome/ui/header/collapsible_nav_group_enabled.tsx
+++ src/core/public/chrome/ui/header/collapsible_nav_group_enabled.tsx
@@ -177,7 +177,7 @@ export function CollapsibleNavGroupEnabled({
   basePath,
   id,
   isLocked,
-  isNavOpen: isNavOpenProps,
+  isNavOpen,
   storage = window.localStorage,
   onIsLockedUpdate,
   closeNav,
@@ -262,18 +262,6 @@ export function CollapsibleNavGroupEnabled({
 
     return fulfillRegistrationLinksToChromeNavLinks(navLinksForAll, navLinks);
   }, [navLinks, navGroupsMap, currentNavGroup]);
-
-  const isNavOpen = useMemo(() => {
-    // For now, only home page need to always collapse left navigation
-    // when workspace is enabled.
-    // If there are more pages need to collapse left navigation in the future
-    // need to come up with a mechanism to register.
-    if (capabilities.workspaces.enabled && appId === 'home') {
-      return false;
-    }
-
-    return isNavOpenProps;
-  }, [isNavOpenProps, capabilities.workspaces.enabled, appId]);
 
   const width = useMemo(() => {
     if (!isNavOpen) {
