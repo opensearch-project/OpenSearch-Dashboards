754330518b8b044c8c67b56527288988611fa48d
Eric <menwe@amazon.com>
2024-07-25 02:04:11 +0000

    update unit tests
    Signed-off-by: Eric <menwe@amazon.com>

--- src/plugins/data/public/antlr/shared/utils.test.ts
+++ src/plugins/data/public/antlr/shared/utils.test.ts
@@ -4,42 +4,74 @@
  */
 
 import { of } from 'rxjs';
-import { getRawSuggestionData$, fetchData, fetchTableSchemas, fetchColumnValues } from './utils';
+import { fetchData } from './utils';
+import { DataSetManager } from '../../query';
 
 describe('fetchData', () => {
   it('should fetch data using the dataSourceRequestHandler', async () => {
     const mockTables = ['table1', 'table2'];
-    const mockQueryFormatter = jest.fn((table) => ({ query: `formatted ${table}` }));
+    const mockQueryFormatter = jest.fn((table, dataSourceId, title) => ({
+      query: { qs: `formatted ${table}`, format: 'jdbc' },
+      df: {
+        meta: {
+          queryConfig: {
+            dataSourceId,
+            title,
+          },
+        },
+      },
+    }));
     const mockApi = {
       http: {
         fetch: jest.fn().mockResolvedValue('fetchedData'),
       },
     };
-    const mockConnectionService = {
-      getSelectedConnection$: jest
+    const mockDataSetManager: Partial<DataSetManager> = {
+      getUpdates$: jest
         .fn()
-        .mockReturnValue(of({ id: 'testId', attributes: { title: 'testTitle' } })),
+        .mockReturnValue(of({ dataSourceRef: { id: 'testId', name: 'testTitle' } })),
     };
 
-    const result = await fetchData(mockTables, mockQueryFormatter, mockApi, mockConnectionService);
+    const result = await fetchData(
+      mockTables,
+      mockQueryFormatter,
+      mockApi,
+      mockDataSetManager as DataSetManager
+    );
     expect(result).toEqual(['fetchedData', 'fetchedData']);
     expect(mockApi.http.fetch).toHaveBeenCalledTimes(2);
+    expect(mockQueryFormatter).toHaveBeenCalledWith('table1', 'testId', 'testTitle');
+    expect(mockQueryFormatter).toHaveBeenCalledWith('table2', 'testId', 'testTitle');
   });
 
   it('should fetch data using the defaultRequestHandler', async () => {
     const mockTables = ['table1', 'table2'];
-    const mockQueryFormatter = jest.fn((table) => ({ query: `formatted ${table}` }));
+    const mockQueryFormatter = jest.fn((table) => ({
+      query: { qs: `formatted ${table}`, format: 'jdbc' },
+      df: {
+        meta: {
+          queryConfig: {},
+        },
+      },
+    }));
     const mockApi = {
       http: {
         fetch: jest.fn().mockResolvedValue('fetchedData'),
       },
     };
-    const mockConnectionService = {
-      getSelectedConnection$: jest.fn().mockReturnValue(of(undefined)),
+    const mockDataSetManager: Partial<DataSetManager> = {
+      getUpdates$: jest.fn().mockReturnValue(of(undefined)),
     };
 
-    const result = await fetchData(mockTables, mockQueryFormatter, mockApi, mockConnectionService);
+    const result = await fetchData(
+      mockTables,
+      mockQueryFormatter,
+      mockApi,
+      mockDataSetManager as DataSetManager
+    );
     expect(result).toEqual(['fetchedData', 'fetchedData']);
     expect(mockApi.http.fetch).toHaveBeenCalledTimes(2);
+    expect(mockQueryFormatter).toHaveBeenCalledWith('table1');
+    expect(mockQueryFormatter).toHaveBeenCalledWith('table2');
   });
 });
