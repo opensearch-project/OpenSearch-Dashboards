75e1c697a6ebd9f8dac5c82c4f42de2da2e2a755
Dan Dong <danieldong51@gmail.com>
2024-08-02 13:12:00 -0700

    Updated Testing for get_no_items_message
    Signed-off-by: Dan Dong <danieldong51@gmail.com>

--- src/plugins/dashboard/public/application/utils/get_no_items_message.test.tsx
+++ src/plugins/dashboard/public/application/utils/get_no_items_message.test.tsx
@@ -5,7 +5,10 @@
 
 import { getNoItemsMessage } from './get_no_items_message';
 import { mountWithIntl } from 'test_utils/enzyme_helpers';
-import { ApplicationStart } from 'opensearch-dashboards/public';
+import { ApplicationStart, Capabilities, PublicAppInfo } from 'opensearch-dashboards/public';
+import { EuiLink } from '@elastic/eui';
+import { RecursiveReadonly } from '@osd/utility-types';
+import { Observable } from 'rxjs';
 
 describe('dashboard listing table with no item', () => {
   test('and no write controls', () => {
@@ -15,8 +18,19 @@ describe('dashboard listing table with no item', () => {
   });
 
   test('and with write controls', () => {
-    const component = mountWithIntl(getNoItemsMessage(false, jest.fn(), {} as ApplicationStart));
+    const application = {
+      capabilities: {} as RecursiveReadonly<Capabilities>,
+      applications$: {} as Observable<ReadonlyMap<string, PublicAppInfo>>,
+      navigateToApp: jest.fn(),
+      navigateToUrl: jest.fn(),
+      getUrlForApp: jest.fn(),
+      registerMountContext: jest.fn(),
+      currentAppId$: {} as Observable<string | undefined>,
+    };
+    const component = mountWithIntl(getNoItemsMessage(false, jest.fn(), application));
 
     expect(component).toMatchSnapshot();
+    component.find(EuiLink).simulate('click');
+    expect(application.navigateToApp).toHaveBeenCalledTimes(1);
   });
 });
