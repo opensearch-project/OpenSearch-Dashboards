cc11962cda524908ff43745869a798a7f4297858
Yulong Ruan <ruanyl@amazon.com>
2024-08-06 14:08:02 +0800

    fix license header
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- src/plugins/content_management/public/components/card_container/types.ts
+++ src/plugins/content_management/public/components/card_container/types.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { ContainerInput } from '../../../../embeddable/public';
 
 export interface CardExplicitInput {
