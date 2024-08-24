653d1d2d3233f34091d999c3a212202beaeaca99
Kawika Avilla <kavilla414@gmail.com>
2024-08-24 02:16:12 +0000

    fix index pattern
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- src/plugins/data/public/query/query_string/dataset_manager/lib/index_pattern_handler.ts
+++ src/plugins/data/public/query/query_string/dataset_manager/lib/index_pattern_handler.ts
@@ -61,7 +61,7 @@ export const indexPatternHandlerConfig: DatasetHandlerConfig = {
       ...dataStructure,
       columnHeader: 'Index patterns',
       children: indexPatterns,
-      isLeaf: false,
+      isLeaf: true,
     };
   },
 
--- src/plugins/data/public/ui/dataset_selector/dataset_explorer.tsx
+++ src/plugins/data/public/ui/dataset_selector/dataset_explorer.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   EuiButton,
   EuiButtonEmpty,
