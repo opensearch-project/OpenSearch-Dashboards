c8141e30dd89654b1aecff60459c84756aac0331
yubonluo <yubonluo@amazon.com>
2024-08-26 15:00:56 +0800

    delete useless code
    Signed-off-by: yubonluo <yubonluo@amazon.com>

--- src/plugins/workspace/common/types.ts
+++ src/plugins/workspace/common/types.ts
@@ -4,7 +4,6 @@
  */
 
 import { DataSourceAttributes } from 'src/plugins/data_source/common/data_sources';
-import { DirectQueryDatasourceDetails } from 'src/plugins/data_source_management/public';
 
 export type DataSource = Pick<
   DataSourceAttributes,
@@ -12,14 +11,6 @@ export type DataSource = Pick<
 > & {
   // Id defined in SavedObjectAttribute could be single or array, here only should be single string.
   id: string;
-};
-
-export type DirectQueryConnection = Pick<
-  DirectQueryDatasourceDetails,
-  'name' | 'connector' | 'status' | 'description'
-> & {
-  // DQC parent data source id
-  parentId: string;
 };
 
 export enum DataSourceConnectionType {
