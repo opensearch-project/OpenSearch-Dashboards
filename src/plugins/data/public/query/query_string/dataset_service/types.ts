1f4cbbb30964ae8fc63137ca443c2b122def9681
Kawika Avilla <kavilla414@gmail.com>
2024-08-29 15:31:56 -0700

    [discover] S3 data connection (#7917)
    Update the S3 type config.
    
    This commit does:
        Browsing connections, databases, tables
        Creating a dataset from a table and setting the string correctly.
    
    This commit does NOT:
        Cache data structures
        Use session ID for querying
        Re-add async polling to search interceptor
    
    
    ---------
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/data/public/query/query_string/dataset_service/types.ts
+++ src/plugins/data/public/query/query_string/dataset_service/types.ts
@@ -2,9 +2,9 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-import { SavedObjectsClientContract } from 'opensearch-dashboards/public';
 import { EuiIconProps } from '@elastic/eui';
 import { Dataset, DatasetField, DataStructure } from '../../../../common';
+import { IDataPluginServices } from '../../../types';
 
 /**
  * Configuration for handling dataset operations.
@@ -29,11 +29,11 @@ export interface DatasetTypeConfig {
   toDataset: (path: DataStructure[]) => Dataset;
   /**
    * Fetches child options for a given DataStructure.
-   * @param {SavedObjectsClientContract} client - The saved objects client.
+   * @param {IDataPluginServices} services - The data plugin services.
    * @param {DataStructure} dataStructure - The parent DataStructure.
    * @returns {Promise<DatasetHandlerFetchResponse>} A promise that resolves to a DatasetHandlerFetchResponse.
    */
-  fetch: (client: SavedObjectsClientContract, path: DataStructure[]) => Promise<DataStructure>;
+  fetch: (services: IDataPluginServices, path: DataStructure[]) => Promise<DataStructure>;
   /**
    * Fetches fields for the dataset.
    * @returns {Promise<DatasetField[]>} A promise that resolves to an array of DatasetFields.
