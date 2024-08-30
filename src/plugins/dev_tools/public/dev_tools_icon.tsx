89800d1a92d8e3817c03eed310c134f313873a96
SuZhou-Joe <suzhou@amazon.com>
2024-08-30 15:13:26 +0800

    feat: render the content inside a modal
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/dev_tools/public/dev_tools_icon.tsx
+++ src/plugins/dev_tools/public/dev_tools_icon.tsx
@@ -3,23 +3,95 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
-import { EuiButtonIcon } from '@elastic/eui';
+import React, { useRef, useCallback, useState } from 'react';
+import {
+  EuiButtonIcon,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiOverlayMask,
+  EuiPanel,
+  EuiSpacer,
+  EuiText,
+} from '@elastic/eui';
 import { CoreStart } from 'opensearch-dashboards/public';
-import { DEFAULT_NAV_GROUPS } from '../../../core/public';
+import { MainApp } from './application';
+import { DevToolApp } from './dev_tool';
+import { DevToolsSetupDependencies } from './plugin';
+import './dev_tools_icon.scss';
 
-export function DevToolsIcon({ core, appId }: { core: CoreStart; appId: string }) {
+export function DevToolsIcon({
+  core,
+  appId,
+  devTools,
+  deps,
+  title,
+}: {
+  core: CoreStart;
+  appId: string;
+  devTools: readonly DevToolApp[];
+  deps: DevToolsSetupDependencies;
+  title: string;
+}) {
+  const [modalVisibile, setModalVisible] = useState(false);
+  const elementRef = useRef<HTMLDivElement | null>(null);
+  const setMountPoint = useCallback((renderFn) => {
+    renderFn(elementRef.current);
+    return () => {};
+  }, []);
+
   return (
-    <EuiButtonIcon
-      aria-label="go-to-dev-tools"
-      iconType="consoleApp"
-      onClick={() => {
+    <>
+      <EuiButtonIcon
+        aria-label="go-to-dev-tools"
+        iconType="consoleApp"
+        onClick={() => {
+          setModalVisible(true);
+        }}
+      />
+      {modalVisibile ? (
         /**
-         * This is a workaround in 2.16, once devTools being refactor to a drawer, we can remove the setCurrentNavGroup line.
+         * We can not use OuiModal component here because OuiModal uses OuiOverlayMask as its parent node
+         * but overlay mask has a default padding bottom that prevent the modal from covering the whole page.
          */
-        core.chrome.navGroup.setCurrentNavGroup(DEFAULT_NAV_GROUPS.dataAdministration.id);
-        core.application.navigateToApp(appId);
-      }}
-    />
+        <EuiOverlayMask className="devToolsOverlayMask" headerZindexLocation="below">
+          <div style={{ width: '100vw', height: '100vh', maxWidth: '100vw' }}>
+            <EuiButtonIcon
+              iconType="cross"
+              onClick={() => setModalVisible(false)}
+              className="euiModal__closeIcon"
+              color="text"
+            />
+            <EuiPanel className="eui-fullHeight">
+              <EuiFlexGroup direction="column" className="eui-fullHeight">
+                <EuiFlexItem grow={false}>
+                  <EuiSpacer />
+                  <EuiFlexGroup justifyContent="spaceBetween">
+                    <EuiFlexItem grow={1}>
+                      <EuiText size="s">
+                        <h2>{title}</h2>
+                      </EuiText>
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <div ref={(element) => (elementRef.current = element)} />
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+                <EuiFlexItem className="devAppWrapper">
+                  <MainApp
+                    devTools={devTools}
+                    savedObjects={core.savedObjects}
+                    notifications={core.notifications}
+                    dataSourceEnabled={!!deps.dataSource}
+                    dataSourceManagement={deps.dataSourceManagement}
+                    useUpdatedUX
+                    setMenuMountPoint={setMountPoint}
+                  />
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiPanel>
+          </div>
+        </EuiOverlayMask>
+      ) : null}
+    </>
   );
 }
