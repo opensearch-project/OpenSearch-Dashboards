7640ef6ed57b390c9d1421cb2e1bc43cc365c0a1
yubonluo <yubonluo@amazon.com>
2024-09-04 13:31:44 +0800

    update css defination class name
    Signed-off-by: yubonluo <yubonluo@amazon.com>

--- src/plugins/workspace/public/components/workspace_detail/data_source_connection_table.tsx
+++ src/plugins/workspace/public/components/workspace_detail/data_source_connection_table.tsx
@@ -123,9 +123,9 @@ export const DataSourceConnectionTable = ({
           items={item?.relatedConnections ?? []}
           itemId="id"
           columns={baseColumns}
-          className="customized-table"
+          className="direct-query-expanded-table"
           rowProps={{
-            className: 'customized-row',
+            className: 'direct-query-expanded-row',
           }}
         />
       );
@@ -295,6 +295,7 @@ export const DataSourceConnectionTable = ({
         isSelectable={true}
         itemIdToExpandedRowMap={itemIdToExpandedRowMap}
         isExpandable={true}
+        className="direct-query-table"
         pagination={{
           initialPageSize: 10,
           pageSizeOptions: [10, 20, 30],
