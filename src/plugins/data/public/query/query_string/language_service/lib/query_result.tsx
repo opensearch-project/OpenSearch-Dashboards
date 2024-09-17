f9d478c67d068bb35f75058350b9c55ba3891913
abbyhu2000 <abigailhu2000@gmail.com>
2024-09-17 21:59:23 +0000

    add loading indicator and counter to query result
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

--- src/plugins/data/public/query/query_string/language_service/lib/query_result.tsx
+++ src/plugins/data/public/query/query_string/language_service/lib/query_result.tsx
@@ -8,7 +8,7 @@ import { i18n } from '@osd/i18n';
 import './_recent_query.scss';
 import { EuiButtonEmpty, EuiPopover, EuiText, EuiPopoverTitle } from '@elastic/eui';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export enum ResultStatus {
   UNINITIALIZED = 'uninitialized',
@@ -28,21 +28,60 @@ export interface QueryStatus {
     statusCode?: number;
   };
   elapsedMs?: number;
+  startTime?: number;
 }
+
+// This is the time in milliseconds that the query will wait before showing the loading spinner
+const BUFFER_TIME = 3000;
 
 export function QueryResult(props: { queryStatus: QueryStatus }) {
   const [isPopoverOpen, setPopover] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
   const onButtonClick = () => {
     setPopover(!isPopoverOpen);
   };
 
+  const getTime = () => {
+    const time = new Date().getTime() - props.queryStatus.startTime!;
+    if (time > BUFFER_TIME) {
+      setElapsedTime(time);
+    } else {
+      setElapsedTime(0);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(getTime, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  if (props.queryStatus.status === ResultStatus.LOADING) {
+    if (elapsedTime < BUFFER_TIME) {
+      return null;
+    }
+    const time = Math.floor(elapsedTime / 1000);
+    return (
+      <EuiButtonEmpty color="text" size="xs" onClick={() => {}} isLoading>
+        {`Loading ${time} s`}
+      </EuiButtonEmpty>
+    );
+  }
+
   if (props.queryStatus.status === ResultStatus.READY) {
+    let message;
+    if (!props.queryStatus.elapsedMs) {
+      message = 'Completed';
+    } else if (props.queryStatus.elapsedMs < 1000) {
+      message = `Completed in ${props.queryStatus.elapsedMs} ms`;
+    } else {
+      message = `Completed in ${Math.floor(props.queryStatus.elapsedMs / 1000)} s`;
+    }
+
     return (
       <EuiButtonEmpty iconSide="left" iconType={'checkInCircleEmpty'} size="xs" onClick={() => {}}>
         <EuiText size="xs" color="subdued">
-          {props.queryStatus.elapsedMs
-            ? `Completed in ${props.queryStatus.elapsedMs} ms`
-            : 'Completed'}
+          {message}
         </EuiText>
       </EuiButtonEmpty>
     );
