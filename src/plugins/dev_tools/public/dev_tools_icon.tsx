dc5ef675e423f2115b26ebe98dbb03eabd290509
Kapian1234 <wanjinch@amazon.com>
2024-09-18 11:14:39 +0800

    Add tooltips for sidebar icons
    Signed-off-by: Kapian1234 <wanjinch@amazon.com>

--- src/plugins/dev_tools/public/dev_tools_icon.tsx
+++ src/plugins/dev_tools/public/dev_tools_icon.tsx
@@ -4,22 +4,29 @@
  */
 
 import React from 'react';
-import { EuiButtonIcon } from '@elastic/eui';
+import { EuiButtonIcon, EuiToolTip } from '@elastic/eui';
+import { i18n } from '@osd/i18n';
 import { CoreStart } from 'opensearch-dashboards/public';
 import { DEFAULT_NAV_GROUPS } from '../../../core/public';
 
 export function DevToolsIcon({ core, appId }: { core: CoreStart; appId: string }) {
   return (
-    <EuiButtonIcon
-      aria-label="go-to-dev-tools"
-      iconType="consoleApp"
-      onClick={() => {
-        /**
-         * This is a workaround in 2.16, once devTools being refactor to a drawer, we can remove the setCurrentNavGroup line.
-         */
-        core.chrome.navGroup.setCurrentNavGroup(DEFAULT_NAV_GROUPS.dataAdministration.id);
-        core.application.navigateToApp(appId);
-      }}
-    />
+    <EuiToolTip
+      content={i18n.translate('devTools.icon.nav.title', {
+        defaultMessage: 'Developer tools',
+      })}
+    >
+      <EuiButtonIcon
+        aria-label="go-to-dev-tools"
+        iconType="consoleApp"
+        onClick={() => {
+          /**
+           * This is a workaround in 2.16, once devTools being refactor to a drawer, we can remove the setCurrentNavGroup line.
+           */
+          core.chrome.navGroup.setCurrentNavGroup(DEFAULT_NAV_GROUPS.dataAdministration.id);
+          core.application.navigateToApp(appId);
+        }}
+      />
+    </EuiToolTip>
   );
 }
