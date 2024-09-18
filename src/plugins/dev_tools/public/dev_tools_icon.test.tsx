f38f45706d5bdbf37574e857f637bfaf7f03150c
Kapian1234 <wanjinch@amazon.com>
2024-09-18 14:39:43 +0800

    modify UTs
    Signed-off-by: Kapian1234 <wanjinch@amazon.com>

--- src/plugins/dev_tools/public/dev_tools_icon.test.tsx
+++ src/plugins/dev_tools/public/dev_tools_icon.test.tsx
@@ -13,7 +13,7 @@ describe('<DevToolsIcon />', () => {
   it('should call chrome.navGroup.setCurrentNavGroup and application.navigateToApp methods from core service when click', () => {
     const coreStartMock = coreMock.createStart();
     const { container } = render(<DevToolsIcon core={coreStartMock} appId="foo" />);
-    const component = container.children[0];
+    const component = container.children[0].children[0];
     fireEvent.click(component);
     expect(coreStartMock.chrome.navGroup.setCurrentNavGroup).toBeCalledWith(
       DEFAULT_NAV_GROUPS.dataAdministration.id
