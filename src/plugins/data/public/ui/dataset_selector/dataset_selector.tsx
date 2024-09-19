87d599db892ccbe501322e231da81c67bd6a5fa0
Joshua Li <joshuali925@gmail.com>
2024-09-19 22:07:40 +0000

    avoid overlap of dataset title and language selector
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- src/plugins/data/public/ui/dataset_selector/dataset_selector.tsx
+++ src/plugins/data/public/ui/dataset_selector/dataset_selector.tsx
@@ -148,6 +148,7 @@ export const DatasetSelector = ({
           </EuiButtonEmpty>
         </EuiToolTip>
       }
+      className="datasetSelector__popover"
       isOpen={isOpen}
       closePopover={closePopover}
       anchorPosition="downLeft"
