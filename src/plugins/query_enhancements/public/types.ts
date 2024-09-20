d54f4562e0d2d25fe9877746dcc64101bcd4f7a7
Kawika Avilla <kavilla414@gmail.com>
2024-09-20 16:21:27 -0700

    [discover] Clean up enhanced search API (#8226)
    * Clean up enhanced search API
    
    * Deletes the connections service.
    * Removes dataframe object being passed into request
    * Deletes unused APIs
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    
    * Changeset file for PR #8226 created/updated
    
    ---------
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/query_enhancements/public/types.ts
+++ src/plugins/query_enhancements/public/types.ts
@@ -33,8 +33,3 @@ export interface Connection {
     auth?: any;
   };
 }
-
-export interface ConnectionsServiceDeps {
-  http: CoreSetup['http'];
-  startServices: Promise<[CoreStart, QueryEnhancementsPluginStartDependencies, unknown]>;
-}
