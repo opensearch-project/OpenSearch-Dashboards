e7abbbb9c3ee2bc648cf17394c451c49674b45c3
SuZhou-Joe <suzhou@amazon.com>
2024-09-29 13:03:56 +0800

    feat: update test
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/dev_tools/public/dev_tools_icon.test.tsx
+++ src/plugins/dev_tools/public/dev_tools_icon.test.tsx
@@ -4,16 +4,38 @@
  */
 
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { DevToolsIcon } from './dev_tools_icon';
 import { coreMock } from '../../../core/public/mocks';
+import { urlForwardingPluginMock } from 'src/plugins/url_forwarding/public/mocks';
+
+const createDepsMock = () => {
+  return {
+    urlForwarding: urlForwardingPluginMock.createSetupContract(),
+  };
+};
 
 describe('<DevToolsIcon />', () => {
   it('should call chrome.navGroup.setCurrentNavGroup and application.navigateToApp methods from core service when click', () => {
     const coreStartMock = coreMock.createStart();
-    const { container } = render(<DevToolsIcon core={coreStartMock} appId="foo" />);
-    const component = container.children[0];
-    fireEvent.click(component);
-    expect(coreStartMock.application.navigateToApp).toBeCalledWith('foo');
+    const { container } = render(
+      <DevToolsIcon core={coreStartMock} devTools={[]} deps={createDepsMock()} title="Dev tools" />
+    );
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <button
+          aria-label="go-to-dev-tools"
+          class="euiButtonIcon euiButtonIcon--primary euiButtonIcon--empty euiButtonIcon--xSmall"
+          type="button"
+        >
+          <span
+            aria-hidden="true"
+            class="euiButtonIcon__icon"
+            color="inherit"
+            data-euiicon-type="consoleApp"
+          />
+        </button>
+      </div>
+    `);
   });
 });
