dc7802685dcd83d5fe42bd6fc7dd0842d813d75b
Lin Wang <wonglam@amazon.com>
2024-10-02 18:53:59 +0800

    Fix use case hidden features not accessible
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- src/plugins/workspace/public/components/workspace_form/workspace_use_case_flyout.tsx
+++ src/plugins/workspace/public/components/workspace_form/workspace_use_case_flyout.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { i18n } from '@osd/i18n';
 import {
   EuiFlyout,
@@ -17,9 +17,78 @@ import {
   EuiFlexItem,
   EuiIcon,
 } from '@elastic/eui';
-import { ALL_USE_CASE_ID } from '../../../../../core/public';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import {
+  ALL_USE_CASE_ID,
+  ChromeNavLink,
+  ChromeRegistrationNavLink,
+  fulfillRegistrationLinksToChromeNavLinks,
+} from '../../../../../core/public';
+import { useOpenSearchDashboards } from '../../../../opensearch_dashboards_react/public';
 
 import { AvailableUseCaseItem } from './types';
+
+interface DisplayedFeature {
+  id: string;
+  title?: string;
+  details?: string[];
+}
+
+interface UseCaseWithDisplayedFeatures extends AvailableUseCaseItem {
+  displayedFeatures?: DisplayedFeature[];
+}
+
+export const fulfillUseCaseWithDisplayedFeatures = ({
+  allNavLinks,
+  navGroupNavLinks,
+  useCase,
+}: {
+  allNavLinks: ChromeNavLink[];
+  navGroupNavLinks: ChromeRegistrationNavLink[];
+  useCase: AvailableUseCaseItem;
+}) => {
+  const visibleNavLinks = allNavLinks.filter((link) => !link.hidden);
+  const visibleNavLinksWithinNavGroup = fulfillRegistrationLinksToChromeNavLinks(
+    navGroupNavLinks,
+    visibleNavLinks
+  );
+  const displayedFeatures: DisplayedFeature[] = [];
+  const category2NavLinks: {
+    [key: string]: DisplayedFeature & { details: string[] };
+  } = {};
+  for (const { id: featureId, title: featureTitle, category } of visibleNavLinksWithinNavGroup) {
+    const lowerFeatureId = featureId.toLowerCase();
+    // Filter out overview and getting started links
+    if (lowerFeatureId.endsWith('overview') || lowerFeatureId.endsWith('started')) {
+      continue;
+    }
+    if (!category) {
+      displayedFeatures.push({ id: featureId, title: featureTitle });
+      continue;
+    }
+    // Filter out custom features
+    if (category.id === 'custom') {
+      continue;
+    }
+    if (!category2NavLinks[category.id]) {
+      category2NavLinks[category.id] = {
+        id: category.id,
+        title: category.label,
+        details: [],
+      };
+    }
+    if (featureTitle) {
+      category2NavLinks[category.id].details.push(featureTitle);
+    }
+  }
+  displayedFeatures.push(...Object.values(category2NavLinks));
+  return {
+    ...useCase,
+    displayedFeatures,
+  };
+};
 
 const WORKSPACE_USE_CASE_FLYOUT_TITLE_ID = 'workspaceUseCaseFlyoutTitle';
 
@@ -34,6 +103,45 @@ export const WorkspaceUseCaseFlyout = ({
   availableUseCases,
   defaultExpandUseCase,
 }: WorkspaceUseCaseFlyoutProps) => {
+  const [useCaseWithDisplayedFeatures, setUseCaseWithDisplayedFeatures] = useState<
+    UseCaseWithDisplayedFeatures[]
+  >([]);
+  const {
+    services: { chrome },
+  } = useOpenSearchDashboards();
+
+  useEffect(() => {
+    if (!chrome) {
+      setUseCaseWithDisplayedFeatures(availableUseCases);
+      return;
+    }
+    const subscription = combineLatest([
+      chrome.navGroup.getNavGroupsMap$(),
+      chrome.navLinks.getNavLinks$(),
+    ])
+      .pipe(
+        map(([navGroupsMap, allNavLinks]) => {
+          return availableUseCases.flatMap((availableUseCase) => {
+            const navGroup = navGroupsMap[availableUseCase.id];
+            if (!navGroup) {
+              return availableUseCase;
+            }
+            return fulfillUseCaseWithDisplayedFeatures({
+              allNavLinks,
+              navGroupNavLinks: navGroup.navLinks,
+              useCase: availableUseCase,
+            });
+          });
+        })
+      )
+      .subscribe((fulfilledUseCases) => {
+        setUseCaseWithDisplayedFeatures(fulfilledUseCases);
+      });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [chrome, availableUseCases]);
+
   return (
     <EuiFlyout
       style={{ maxWidth: 431 }}
@@ -50,78 +158,85 @@ export const WorkspaceUseCaseFlyout = ({
         </EuiTitle>
       </EuiFlyoutHeader>
       <EuiFlyoutBody>
-        {availableUseCases.map(({ id, icon, title, description, features }, index) => (
-          <React.Fragment key={id}>
-            <EuiAccordion
-              id={id}
-              buttonContent={
-                <EuiFlexGroup gutterSize="s" alignItems="center">
-                  {icon && (
-                    <EuiFlexItem grow={false}>
-                      <EuiIcon color="subdued" type={icon} size="l" />
-                    </EuiFlexItem>
-                  )}
-                  <EuiFlexItem>
-                    <EuiText size="s">
-                      <h3>
-                        {title}
-                        {id === ALL_USE_CASE_ID && (
-                          <>
-                            &nbsp;
-                            <EuiText style={{ display: 'inline' }}>
-                              <i>
-                                {i18n.translate('workspace.forms.useCaseFlyout.allUseCaseSuffix', {
-                                  defaultMessage: '(all features)',
-                                })}
-                              </i>
-                            </EuiText>
-                          </>
-                        )}
-                      </h3>
-                    </EuiText>
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              }
-              paddingSize="l"
-              initialIsOpen={id === defaultExpandUseCase}
-            >
-              <EuiText size="s">
-                {description}
-                <br />
-                {features && features.length > 0 && (
-                  <>
-                    {i18n.translate('workspace.forms.useCaseFlyout.featuresIncluded', {
-                      defaultMessage: 'Features included:',
-                    })}
-                    <br />
-                    <ul>
-                      {features.map(({ id: featureId, title: featureTitle, details }) => (
-                        <li key={featureId}>
-                          <b>
-                            {i18n.translate('workspace.forms.useCaseFlyout.featureTitle', {
-                              defaultMessage:
-                                '{featureTitle}{detailsCount, plural, =0 {} other {: }}',
-                              values: { featureTitle, detailsCount: details?.length ?? 0 },
-                            })}
-                          </b>
-                          {details?.join(
-                            i18n.translate(
-                              'workspace.forms.useCaseFlyout.featuresDetails.delimiter',
-                              {
-                                defaultMessage: ', ',
-                              }
-                            )
+        {useCaseWithDisplayedFeatures.map(
+          ({ id, icon, title, description, displayedFeatures }, index) => (
+            <React.Fragment key={id}>
+              <EuiAccordion
+                id={id}
+                buttonContent={
+                  <EuiFlexGroup gutterSize="s" alignItems="center">
+                    {icon && (
+                      <EuiFlexItem grow={false}>
+                        <EuiIcon color="subdued" type={icon} size="l" />
+                      </EuiFlexItem>
+                    )}
+                    <EuiFlexItem>
+                      <EuiText size="s">
+                        <h3>
+                          {title}
+                          {id === ALL_USE_CASE_ID && (
+                            <>
+                              &nbsp;
+                              <EuiText style={{ display: 'inline' }}>
+                                <i>
+                                  {i18n.translate(
+                                    'workspace.forms.useCaseFlyout.allUseCaseSuffix',
+                                    {
+                                      defaultMessage: '(all features)',
+                                    }
+                                  )}
+                                </i>
+                              </EuiText>
+                            </>
                           )}
-                        </li>
-                      ))}
-                    </ul>
-                  </>
-                )}
-              </EuiText>
-            </EuiAccordion>
-            {index < availableUseCases.length - 1 && <EuiSpacer size="m" />}
-          </React.Fragment>
-        ))}
+                        </h3>
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                }
+                paddingSize="l"
+                initialIsOpen={id === defaultExpandUseCase}
+              >
+                <EuiText size="s">
+                  {description}
+                  <br />
+                  {displayedFeatures && displayedFeatures.length > 0 && (
+                    <>
+                      {i18n.translate('workspace.forms.useCaseFlyout.featuresIncluded', {
+                        defaultMessage: 'Features included:',
+                      })}
+                      <br />
+                      <ul>
+                        {displayedFeatures.map(
+                          ({ id: featureId, title: featureTitle, details }) => (
+                            <li key={featureId}>
+                              <b>
+                                {i18n.translate('workspace.forms.useCaseFlyout.featureTitle', {
+                                  defaultMessage:
+                                    '{featureTitle}{detailsCount, plural, =0 {} other {: }}',
+                                  values: { featureTitle, detailsCount: details?.length ?? 0 },
+                                })}
+                              </b>
+                              {details?.join(
+                                i18n.translate(
+                                  'workspace.forms.useCaseFlyout.featuresDetails.delimiter',
+                                  {
+                                    defaultMessage: ', ',
+                                  }
+                                )
+                              )}
+                            </li>
+                          )
+                        )}
+                      </ul>
+                    </>
+                  )}
+                </EuiText>
+              </EuiAccordion>
+              {index < availableUseCases.length - 1 && <EuiSpacer size="m" />}
+            </React.Fragment>
+          )
+        )}
       </EuiFlyoutBody>
     </EuiFlyout>
   );
