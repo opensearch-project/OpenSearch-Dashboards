ec4b9d736e9940fe57d8dd4d12ae8c76d67a062b
Lin Wang <wonglam@amazon.com>
2024-10-03 17:49:15 +0800

    [Workspace]Restrict at least one data source in workspace creation page (#8461)
    * Restrict at least one data sources for workspace creation
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Update empty data sources panel UI
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Changeset file for PR #8461 created/updated
    
    * Fix wrong testing id for data source empty state
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Remove populate workspace name
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    ---------
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/workspace/public/components/workspace_creator/workspace_create_action_panel.test.tsx
+++ src/plugins/workspace/public/components/workspace_creator/workspace_create_action_panel.test.tsx
@@ -9,6 +9,7 @@ import {
   MAX_WORKSPACE_DESCRIPTION_LENGTH,
   MAX_WORKSPACE_NAME_LENGTH,
 } from '../../../common/constants';
+import { DataSourceConnectionType } from '../../../common/types';
 import { WorkspaceCreateActionPanel } from './workspace_create_action_panel';
 
 const mockApplication = applicationServiceMock.createStartContract();
@@ -18,6 +19,14 @@ describe('WorkspaceCreateActionPanel', () => {
   const formData = {
     name: 'Test Workspace',
     description: 'This is a test workspace',
+    selectedDataSourceConnections: [
+      {
+        id: 'data-source-1',
+        name: 'Data Source 1',
+        type: '',
+        connectionType: DataSourceConnectionType.OpenSearchConnection,
+      },
+    ],
   };
 
   it('should disable the "Create Workspace" button when name exceeds the maximum length', () => {
@@ -25,9 +34,13 @@ describe('WorkspaceCreateActionPanel', () => {
     render(
       <WorkspaceCreateActionPanel
         formId={formId}
-        formData={{ name: longName, description: formData.description }}
+        formData={{
+          ...formData,
+          name: longName,
+        }}
         application={mockApplication}
         isSubmitting={false}
+        dataSourceEnabled
       />
     );
     const createButton = screen.getByText('Create workspace');
@@ -39,13 +52,51 @@ describe('WorkspaceCreateActionPanel', () => {
     render(
       <WorkspaceCreateActionPanel
         formId={formId}
-        formData={{ name: formData.name, description: longDescription }}
+        formData={{
+          ...formData,
+          description: longDescription,
+        }}
         application={mockApplication}
         isSubmitting={false}
+        dataSourceEnabled
       />
     );
     const createButton = screen.getByText('Create workspace');
     expect(createButton.closest('button')).toBeDisabled();
+  });
+
+  it('should disable the "Create Workspace" button when data source enabled and no data sources selected', () => {
+    render(
+      <WorkspaceCreateActionPanel
+        formId={formId}
+        formData={{
+          ...formData,
+          selectedDataSourceConnections: [],
+        }}
+        application={mockApplication}
+        isSubmitting={false}
+        dataSourceEnabled
+      />
+    );
+    const createButton = screen.getByText('Create workspace');
+    expect(createButton.closest('button')).toBeDisabled();
+  });
+
+  it('should enable the "Create Workspace" button when no data sources selected but no data source enabled', () => {
+    render(
+      <WorkspaceCreateActionPanel
+        formId={formId}
+        formData={{
+          ...formData,
+          selectedDataSourceConnections: [],
+        }}
+        application={mockApplication}
+        isSubmitting={false}
+        dataSourceEnabled={false}
+      />
+    );
+    const createButton = screen.getByText('Create workspace');
+    expect(createButton.closest('button')).not.toBeDisabled();
   });
 
   it('should enable the "Create Workspace" button when name and description are within the maximum length', () => {
@@ -55,6 +106,7 @@ describe('WorkspaceCreateActionPanel', () => {
         formData={formData}
         application={mockApplication}
         isSubmitting={false}
+        dataSourceEnabled
       />
     );
     const createButton = screen.getByText('Create workspace');
@@ -65,9 +117,10 @@ describe('WorkspaceCreateActionPanel', () => {
     render(
       <WorkspaceCreateActionPanel
         formId={formId}
-        formData={{ name: 'test' }}
+        formData={formData}
         application={mockApplication}
         isSubmitting
+        dataSourceEnabled
       />
     );
     expect(screen.getByText('Create workspace').closest('button')).toBeDisabled();
