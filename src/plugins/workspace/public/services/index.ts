d3ddf9176ac9b83dd23ea2e062bd6ea5147df08c
Lin Wang <wonglam@amazon.com>
2024-10-05 11:13:06 +0800

    [Workspace]Add WorkspaceCollaboratorTypesService and AddCollaboratorsModal (#8486)

---
+++ src/plugins/workspace/public/services/index.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { UseCaseService } from './use_case_service';
+export {
+  WorkspaceCollaboratorType,
+  WorkspaceCollaboratorTypesService,
+} from './workspace_collaborator_types_service';
