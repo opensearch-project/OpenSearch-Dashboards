bae8f14b9b6bbde3a01906d355fa91f1eaf3ff7b
Miki <miki@amazon.com>
2024-10-08 10:28:59 -0700

    Add i18n checks to PR workflows (#8411) (#8517)
    * Ignore missing `formats` while checking locale files
    
    Also:
    * Add help text and description to `i18n-check`
    * Fix malformed translations
    
    * Add i18n checks to PR workflows
    
    * Changeset file for PR #8411 created/updated
    
    ---------
    
    (cherry picked from commit 428a7c2c9165b43c0343e293acd143e0bdac58b4)
    
    Signed-off-by: Miki <miki@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>

--- src/dev/i18n/integrate_locale_files.ts
+++ src/dev/i18n/integrate_locale_files.ts
@@ -56,6 +56,7 @@ export interface IntegrateOptions {
   ignoreIncompatible: boolean;
   ignoreUnused: boolean;
   ignoreMissing: boolean;
+  ignoreMissingFormats?: boolean;
   config: I18nConfig;
   log: ToolingLog;
 }
@@ -211,7 +212,11 @@ export async function integrateLocaleFiles(
 ) {
   const localizedMessages = JSON.parse((await readFileAsync(options.sourceFileName)).toString());
   if (!localizedMessages.formats) {
-    throw createFailError(`Locale file should contain formats object.`);
+    if (options.ignoreMissingFormats) {
+      options.log.warning('Missing formats object ignored');
+    } else {
+      throw createFailError(`Locale file should contain formats object.`);
+    }
   }
 
   const localizedMessagesMap: LocalizedMessageMap = new Map(
