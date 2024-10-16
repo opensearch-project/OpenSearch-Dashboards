bfdbfcd52b57b839222b3d42a4ad52ac406902e7
Eric <menwe@amazon.com>
2024-10-16 21:07:56 +0000

    add mappings
    Signed-off-by: Eric <menwe@amazon.com>

--- src/plugins/home/server/services/sample_data/data_sets/otel/services_field_mappings.ts
+++ src/plugins/home/server/services/sample_data/data_sets/otel/services_field_mappings.ts
@@ -4,44 +4,58 @@
  */
 
 export const servicesFieldMappings = {
-  destination: {
-    properties: {
-      domain: {
-        type: 'keyword',
-        ignore_above: 1024,
-      },
-      resource: {
-        type: 'keyword',
-        ignore_above: 1024,
-      },
-    },
-  },
-  hashId: {
-    type: 'keyword',
-    ignore_above: 1024,
-  },
-  kind: {
-    type: 'keyword',
-    ignore_above: 1024,
-  },
-  serviceName: {
-    type: 'keyword',
-    ignore_above: 1024,
-  },
-  target: {
-    properties: {
-      domain: {
-        type: 'keyword',
-        ignore_above: 1024,
-      },
-      resource: {
-        type: 'keyword',
-        ignore_above: 1024,
+  dynamic_templates: [
+    {
+      strings_as_keyword: {
+        match_mapping_type: 'string',
+        mapping: {
+          ignore_above: 1024,
+          type: 'keyword',
+        },
       },
     },
-  },
-  traceGroupName: {
-    type: 'keyword',
-    ignore_above: 1024,
+  ],
+  date_detection: false,
+  properties: {
+    destination: {
+      properties: {
+        domain: {
+          type: 'keyword',
+          ignore_above: 1024,
+        },
+        resource: {
+          type: 'keyword',
+          ignore_above: 1024,
+        },
+      },
+    },
+    hashId: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+    kind: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+    serviceName: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
+    target: {
+      properties: {
+        domain: {
+          type: 'keyword',
+          ignore_above: 1024,
+        },
+        resource: {
+          type: 'keyword',
+          ignore_above: 1024,
+        },
+      },
+    },
+    traceGroupName: {
+      type: 'keyword',
+      ignore_above: 1024,
+    },
   },
 };
