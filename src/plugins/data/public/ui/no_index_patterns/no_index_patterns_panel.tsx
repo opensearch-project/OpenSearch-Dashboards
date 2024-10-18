f52e2c32bdcb07a3ea98bfdcda3d83c4933bbecd
Miki <miki@amazon.com>
2024-10-18 10:51:17 -0700

    Update design of no data selected
    Signed-off-by: Miki <miki@amazon.com>

--- src/plugins/data/public/ui/no_index_patterns/no_index_patterns_panel.tsx
+++ src/plugins/data/public/ui/no_index_patterns/no_index_patterns_panel.tsx
@@ -10,9 +10,10 @@ import {
   EuiFlexItem,
   EuiIcon,
   EuiText,
-  EuiButton,
-  EuiLink,
+  EuiSmallButton,
   EuiSpacer,
+  EuiTitle,
+  EuiButtonEmpty,
 } from '@elastic/eui';
 
 interface NoIndexPatternsPanelProps {
@@ -22,59 +23,85 @@ interface NoIndexPatternsPanelProps {
 export const NoIndexPatternsPanel: React.FC<NoIndexPatternsPanelProps> = ({
   onOpenDataSelector,
 }) => (
-  <EuiFlexGroup justifyContent="center" alignItems="center" style={{ height: '100%' }}>
-    <EuiFlexItem grow={false} style={{ width: '100%', maxWidth: '500px' }}>
+  <EuiFlexGroup justifyContent="center" alignItems="center" gutterSize="none">
+    <EuiFlexItem grow={false} className="dataUI-centerPanel">
       <EuiPanel paddingSize="l">
-        <EuiFlexGroup direction="column" alignItems="center" gutterSize="s">
+        <EuiFlexGroup direction="column" alignItems="center" gutterSize="m">
           <EuiFlexItem>
-            <EuiIcon type="dataVisualizer" size="xxl" color="subdued" />
+            <EuiIcon type="dataVisualizer" size="xl" color="subdued" />
           </EuiFlexItem>
           <EuiFlexItem>
-            <EuiText textAlign="center">
+            <EuiTitle size="m">
               <h2>Select data</h2>
+            </EuiTitle>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiText textAlign="center" color="subdued" size="xs">
+              Select an available data source and choose a query language to use for running
+              queries. You can use the data dropdown or use the enhanced data selector to select
+              data.
             </EuiText>
           </EuiFlexItem>
           <EuiFlexItem>
-            <EuiText textAlign="center" color="subdued" size="s">
-              <p>
-                Select an available data source and choose a query language to use for running
-                queries. You can use the data dropdown or use the enhanced data selector to select
-                data.
-              </p>
-            </EuiText>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiButton fill onClick={onOpenDataSelector}>
+            <EuiSmallButton fill onClick={onOpenDataSelector}>
               Open data selector
-            </EuiButton>
+            </EuiSmallButton>
           </EuiFlexItem>
           <EuiSpacer size="s" />
           <EuiFlexItem>
-            <EuiText textAlign="center" size="xs">
-              <p>Learn more about query languages</p>
-            </EuiText>
+            <EuiTitle size="xs">
+              <h4>Learn more about query languages</h4>
+            </EuiTitle>
           </EuiFlexItem>
           <EuiFlexItem>
             <EuiFlexGroup justifyContent="center" gutterSize="s" wrap>
               <EuiFlexItem grow={false}>
-                <EuiLink href="#" target="_blank">
-                  PPL documentation
-                </EuiLink>
+                <EuiButtonEmpty
+                  href="#"
+                  target="_blank"
+                  size="xs"
+                  iconType="popout"
+                  iconSide="right"
+                  iconGap="s"
+                >
+                  <EuiText size="xs">PPL documentation</EuiText>
+                </EuiButtonEmpty>
               </EuiFlexItem>
               <EuiFlexItem grow={false}>
-                <EuiLink href="#" target="_blank">
-                  SQL documentation
-                </EuiLink>
+                <EuiButtonEmpty
+                  href="#"
+                  target="_blank"
+                  size="xs"
+                  iconType="popout"
+                  iconSide="right"
+                  iconGap="s"
+                >
+                  <EuiText size="xs">SQL documentation</EuiText>
+                </EuiButtonEmpty>
               </EuiFlexItem>
               <EuiFlexItem grow={false}>
-                <EuiLink href="#" target="_blank">
-                  Lucene documentation
-                </EuiLink>
+                <EuiButtonEmpty
+                  href="#"
+                  target="_blank"
+                  size="xs"
+                  iconType="popout"
+                  iconSide="right"
+                  iconGap="s"
+                >
+                  <EuiText size="xs">Lucene documentation</EuiText>
+                </EuiButtonEmpty>
               </EuiFlexItem>
               <EuiFlexItem grow={false}>
-                <EuiLink href="#" target="_blank">
-                  DQL documentation
-                </EuiLink>
+                <EuiButtonEmpty
+                  href="#"
+                  target="_blank"
+                  size="xs"
+                  iconType="popout"
+                  iconSide="right"
+                  iconGap="s"
+                >
+                  <EuiText size="xs">DQL documentation</EuiText>
+                </EuiButtonEmpty>
               </EuiFlexItem>
             </EuiFlexGroup>
           </EuiFlexItem>
