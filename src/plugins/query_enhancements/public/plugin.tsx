33b1ed29c2f4d828c0dbb3659463e7cb128f04bb
Jialiang Liang <jiallian@amazon.com>
2024-10-27 19:32:15 -0400

    Resolve comments
    Signed-off-by: Jialiang Liang <jiallian@amazon.com>

--- src/plugins/query_enhancements/public/plugin.tsx
+++ src/plugins/query_enhancements/public/plugin.tsx
@@ -2,7 +2,6 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-
 import { i18n } from '@osd/i18n';
 import { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '../../../core/public';
 import { ConfigSchema } from '../common/config';
@@ -43,125 +42,117 @@ export class QueryEnhancementsPlugin
   ): QueryEnhancementsPluginSetup {
     const { queryString } = data.query;
 
-    // Subscribe to updates to track language changes
-    queryString.getUpdates$().subscribe((query) => {
-      const selectedLanguage = query.language;
+    // Define controls once for each language and register language configurations outside of `getUpdates$`
+    const pplControls = [pplLanguageReference('PPL')];
+    const sqlControls = [sqlLanguageReference('SQL')];
 
-      // Re-render the controls with updated `autoShow` flags
-      const pplControls = [pplLanguageReference(selectedLanguage)];
-      const sqlControls = [sqlLanguageReference(selectedLanguage)];
-
-      const enhancedPPLQueryEditor = createEditor(SingleLineInput, null, pplControls, DefaultInput);
-      const enhancedSQLQueryEditor = createEditor(SingleLineInput, null, sqlControls, DefaultInput);
-
-      // Register PPL language configuration
-      const pplLanguageConfig: LanguageConfig = {
-        id: 'PPL',
-        title: 'PPL',
-        search: new PPLSearchInterceptor({
-          toasts: core.notifications.toasts,
-          http: core.http,
-          uiSettings: core.uiSettings,
-          startServices: core.getStartServices(),
-          usageCollector: data.search.usageCollector,
+    // Register PPL language configuration
+    const pplLanguageConfig: LanguageConfig = {
+      id: 'PPL',
+      title: 'PPL',
+      search: new PPLSearchInterceptor({
+        toasts: core.notifications.toasts,
+        http: core.http,
+        uiSettings: core.uiSettings,
+        startServices: core.getStartServices(),
+        usageCollector: data.search.usageCollector,
+      }),
+      getQueryString: (currentQuery: Query) => `source = ${currentQuery.dataset?.title}`,
+      fields: { filterable: false, visualizable: false },
+      docLink: {
+        title: i18n.translate('queryEnhancements.pplLanguage.docLink', {
+          defaultMessage: 'PPL documentation',
         }),
-        getQueryString: (currentQuery: Query) => `source = ${currentQuery.dataset?.title}`,
-        fields: { filterable: false, visualizable: false },
-        docLink: {
-          title: i18n.translate('queryEnhancements.pplLanguage.docLink', {
-            defaultMessage: 'PPL documentation',
-          }),
-          url: 'https://opensearch.org/docs/latest/search-plugins/sql/ppl/syntax/',
-        },
-        showDocLinks: false,
-        editor: enhancedPPLQueryEditor,
-        editorSupportedAppNames: ['discover'],
-        supportedAppNames: ['discover', 'data-explorer'],
-      };
-      queryString.getLanguageService().registerLanguage(pplLanguageConfig);
+        url: 'https://opensearch.org/docs/latest/search-plugins/sql/ppl/syntax/',
+      },
+      showDocLinks: false,
+      editor: createEditor(SingleLineInput, null, pplControls, DefaultInput),
+      editorSupportedAppNames: ['discover'],
+      supportedAppNames: ['discover', 'data-explorer'],
+    };
+    queryString.getLanguageService().registerLanguage(pplLanguageConfig);
 
-      // Register SQL language configuration
-      const sqlLanguageConfig: LanguageConfig = {
-        id: 'SQL',
-        title: 'OpenSearch SQL',
-        search: new SQLSearchInterceptor({
-          toasts: core.notifications.toasts,
-          http: core.http,
-          uiSettings: core.uiSettings,
-          startServices: core.getStartServices(),
-          usageCollector: data.search.usageCollector,
+    // Register SQL language configuration
+    const sqlLanguageConfig: LanguageConfig = {
+      id: 'SQL',
+      title: 'OpenSearch SQL',
+      search: new SQLSearchInterceptor({
+        toasts: core.notifications.toasts,
+        http: core.http,
+        uiSettings: core.uiSettings,
+        startServices: core.getStartServices(),
+        usageCollector: data.search.usageCollector,
+      }),
+      getQueryString: (currentQuery: Query) =>
+        `SELECT * FROM ${currentQuery.dataset?.title} LIMIT 10`,
+      fields: { filterable: false, visualizable: false },
+      docLink: {
+        title: i18n.translate('queryEnhancements.sqlLanguage.docLink', {
+          defaultMessage: 'SQL documentation',
         }),
-        getQueryString: (currentQuery: Query) =>
-          `SELECT * FROM ${currentQuery.dataset?.title} LIMIT 10`,
-        fields: { filterable: false, visualizable: false },
-        docLink: {
-          title: i18n.translate('queryEnhancements.sqlLanguage.docLink', {
-            defaultMessage: 'SQL documentation',
+        url: 'https://opensearch.org/docs/latest/search-plugins/sql/sql/basic/',
+      },
+      showDocLinks: false,
+      editor: createEditor(SingleLineInput, null, sqlControls, DefaultInput),
+      editorSupportedAppNames: ['discover'],
+      supportedAppNames: ['discover', 'data-explorer'],
+      hideDatePicker: true,
+      sampleQueries: [
+        {
+          title: i18n.translate('queryEnhancements.sqlLanguage.sampleQuery.titleContainsWind', {
+            defaultMessage: 'The title field contains the word wind.',
           }),
-          url: 'https://opensearch.org/docs/latest/search-plugins/sql/sql/basic/',
+          query: `SELECT * FROM your_table WHERE title LIKE '%wind%'`,
         },
-        showDocLinks: false,
-        editor: enhancedSQLQueryEditor,
-        editorSupportedAppNames: ['discover'],
-        supportedAppNames: ['discover', 'data-explorer'],
-        hideDatePicker: true,
-        sampleQueries: [
-          {
-            title: i18n.translate('queryEnhancements.sqlLanguage.sampleQuery.titleContainsWind', {
-              defaultMessage: 'The title field contains the word wind.',
-            }),
-            query: `SELECT * FROM your_table WHERE title LIKE '%wind%'`,
-          },
-          {
-            title: i18n.translate(
-              'queryEnhancements.sqlLanguage.sampleQuery.titleContainsWindOrWindy',
-              {
-                defaultMessage: 'The title field contains the word wind or the word windy.',
-              }
-            ),
-            query: `SELECT * FROM your_table WHERE title LIKE '%wind%' OR title LIKE '%windy%';`,
-          },
-          {
-            title: i18n.translate(
-              'queryEnhancements.sqlLanguage.sampleQuery.titleContainsPhraseWindRises',
-              {
-                defaultMessage: 'The title field contains the phrase wind rises.',
-              }
-            ),
-            query: `SELECT * FROM your_table WHERE title LIKE '%wind rises%'`,
-          },
-          {
-            title: i18n.translate(
-              'queryEnhancements.sqlLanguage.sampleQuery.titleExactMatchWindRises',
-              {
-                defaultMessage: 'The title.keyword field exactly matches The wind rises.',
-              }
-            ),
-            query: `SELECT * FROM your_table WHERE title = 'The wind rises'`,
-          },
-          {
-            title: i18n.translate(
-              'queryEnhancements.sqlLanguage.sampleQuery.titleFieldsContainWind',
-              {
-                defaultMessage:
-                  'Any field that starts with title (for example, title and title.keyword) contains the word wind',
-              }
-            ),
-            query: `SELECT * FROM your_table WHERE title LIKE '%wind%' OR title = 'wind'`,
-          },
-          {
-            title: i18n.translate(
-              'queryEnhancements.sqlLanguage.sampleQuery.descriptionFieldExists',
-              {
-                defaultMessage: 'Documents in which the field description exists.',
-              }
-            ),
-            query: `SELECT * FROM your_table WHERE description IS NOT NULL AND description != '';`,
-          },
-        ],
-      };
-      queryString.getLanguageService().registerLanguage(sqlLanguageConfig);
-    });
+        {
+          title: i18n.translate(
+            'queryEnhancements.sqlLanguage.sampleQuery.titleContainsWindOrWindy',
+            {
+              defaultMessage: 'The title field contains the word wind or the word windy.',
+            }
+          ),
+          query: `SELECT * FROM your_table WHERE title LIKE '%wind%' OR title LIKE '%windy%';`,
+        },
+        {
+          title: i18n.translate(
+            'queryEnhancements.sqlLanguage.sampleQuery.titleContainsPhraseWindRises',
+            {
+              defaultMessage: 'The title field contains the phrase wind rises.',
+            }
+          ),
+          query: `SELECT * FROM your_table WHERE title LIKE '%wind rises%'`,
+        },
+        {
+          title: i18n.translate(
+            'queryEnhancements.sqlLanguage.sampleQuery.titleExactMatchWindRises',
+            {
+              defaultMessage: 'The title.keyword field exactly matches The wind rises.',
+            }
+          ),
+          query: `SELECT * FROM your_table WHERE title = 'The wind rises'`,
+        },
+        {
+          title: i18n.translate(
+            'queryEnhancements.sqlLanguage.sampleQuery.titleFieldsContainWind',
+            {
+              defaultMessage:
+                'Any field that starts with title (for example, title and title.keyword) contains the word wind',
+            }
+          ),
+          query: `SELECT * FROM your_table WHERE title LIKE '%wind%' OR title = 'wind'`,
+        },
+        {
+          title: i18n.translate(
+            'queryEnhancements.sqlLanguage.sampleQuery.descriptionFieldExists',
+            {
+              defaultMessage: 'Documents in which the field description exists.',
+            }
+          ),
+          query: `SELECT * FROM your_table WHERE description IS NOT NULL AND description != '';`,
+        },
+      ],
+    };
+    queryString.getLanguageService().registerLanguage(sqlLanguageConfig);
 
     data.__enhance({
       editor: {
