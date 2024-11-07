1765b3331efd033a7ec5698dae3ac9c8f825e72c
abbyhu2000 <abigailhu2000@gmail.com>
2024-11-07 19:08:49 +0000

    remove head 10 for PPL to unblock cypress test failures
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

--- src/plugins/query_enhancements/public/datasets/s3_type.ts
+++ src/plugins/query_enhancements/public/datasets/s3_type.ts
@@ -131,7 +131,7 @@ export const s3TypeConfig: DatasetTypeConfig = {
             title: i18n.translate('queryEnhancements.s3Type.sampleQuery.basicPPLQuery', {
               defaultMessage: 'Sample query for PPL',
             }),
-            query: `source = ${dataset.title} | head 10`,
+            query: `source = ${dataset.title}`,
           },
         ];
       case 'SQL':
