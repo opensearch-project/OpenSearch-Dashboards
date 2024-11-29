50f1fac810395efe3b688fbbee37d362e1a43351
Lin Wang <wonglam@amazon.com>
2024-11-29 15:52:57 +0800

    Update UT for useWorkspaceForm
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- src/plugins/workspace/public/components/workspace_form/use_workspace_form.test.ts
+++ src/plugins/workspace/public/components/workspace_form/use_workspace_form.test.ts
@@ -7,7 +7,13 @@ import { renderHook, act } from '@testing-library/react-hooks';
 
 import { applicationServiceMock } from '../../../../../core/public/mocks';
 import { WorkspacePermissionMode } from '../../../common/constants';
-import { WorkspaceOperationType, WorkspacePermissionItemType } from './constants';
+import { PermissionModeId } from '../../../../../core/public';
+import {
+  optionIdToWorkspacePermissionModesMap,
+  WorkspaceOperationType,
+  WorkspacePermissionItemType,
+  WorkspacePrivacyItemType,
+} from './constants';
 import { WorkspaceFormSubmitData, WorkspaceFormErrorCode } from './types';
 import { useWorkspaceForm } from './use_workspace_form';
 import { waitFor } from '@testing-library/dom';
@@ -193,6 +199,40 @@ describe('useWorkspaceForm', () => {
     });
     await waitFor(() => {
       expect(renderResult.result.current.formData.permissionSettings).toStrictEqual([]);
+    });
+  });
+
+  it('should return permissions settings after setPrivacyType called', async () => {
+    const onSubmitMock = jest.fn().mockResolvedValue({ success: true });
+    const { renderResult } = setup({
+      defaultValues: {
+        name: 'current-workspace-name',
+        features: ['use-case-observability'],
+      },
+      onSubmit: onSubmitMock,
+    });
+    act(() => {
+      renderResult.result.current.setPrivacyType(WorkspacePrivacyItemType.AnyoneCanEdit);
+    });
+    await waitFor(() => {
+      expect(renderResult.result.current.formData.permissionSettings).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            type: 'user',
+            userId: '*',
+            modes: optionIdToWorkspacePermissionModesMap[PermissionModeId.ReadAndWrite],
+          }),
+        ])
+      );
+    });
+
+    const oldPermissionSettings = renderResult.result.current.formData.permissionSettings;
+
+    act(() => {
+      renderResult.result.current.setPrivacyType(WorkspacePrivacyItemType.AnyoneCanEdit);
+    });
+    await waitFor(() => {
+      expect(renderResult.result.current.formData.permissionSettings).toBe(oldPermissionSettings);
     });
   });
 });
