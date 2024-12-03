ea152abf9ba62ef42de3387e5859b15204d88b6f
Argus Li <contactme@chunkeili.com>
2024-12-02 16:19:58 -0800

    Add utils to select a data source.
    Signed-off-by: Federico Silva <federico.silva.-nd@disney.com>

--- cypress.config.js
+++ cypress.config.js
@@ -23,6 +23,8 @@ module.exports = defineConfig({
     DATASOURCE_MANAGEMENT_ENABLED: false,
     ML_COMMONS_DASHBOARDS_ENABLED: true,
     WAIT_FOR_LOADER_BUFFER_MS: 0,
+    INDEX_CLUSTER_NAME: 'cypress-test-os',
+    INDEX_NAME: 'vis-builder',
   },
   e2e: {
     baseUrl: 'http://localhost:5601',
