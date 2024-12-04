af1fd472c184c83884281b0fc0d30946782e2b7e
Argus Li <contactme@chunkeili.com>
2024-12-03 21:58:09 -0800

    Add support for different languages when creating dataset. Filter Out is almost complete, just working on removing the filter.

--- cypress/utils/data_explorer_page/data_explorer_elements.js
+++ cypress/utils/data_explorer_page/data_explorer_elements.js
@@ -5,6 +5,7 @@
 
 export const DATA_EXPLORER_PAGE_ELEMENTS = {
   NEW_SEARCH_BUTTON: '[data-test-subj="discoverNewButton"]',
+  DISCOVER_QUERY_HITS: '[data-test-subj="discoverQueryHits"]',
   DATASET_SELECTOR_BUTTON: '[data-test-subj="datasetSelectorButton"]',
   ALL_DATASETS_BUTTON: '[data-test-subj="datasetSelectorAdvancedButton"]',
   DATASET_EXPLORER_WINDOW: '[data-test-subj="datasetExplorerWindow"]',
@@ -12,4 +13,15 @@ export const DATA_EXPLORER_PAGE_ELEMENTS = {
   DATASET_SELECTOR_LANGUAGE_SELECTOR: '[data-test-subj="advancedSelectorLanguageSelect"]',
   DATASET_SELECTOR_TIME_SELECTOR: '[data-test-subj="advancedSelectorTimeFieldSelect"]',
   DATASET_SELECTOR_SELECT_DATA_BUTTON: '[data-test-subj="advancedSelectorConfirmButton"]',
+  DOC_TABLE: '[data-test-subj="docTable"]',
+  DOC_TABLE_ROW_FIELD: '[data-test-subj="docTableField"]',
+  TABLE_FIELD_FILTER_FOR_BUTTON: '[data-test-subj="filterForValue"]',
+  TABLE_FIELD_FILTER_OUT_BUTTON: '[data-test-subj="filterOutValue"]',
+  SEARCH_DATE_PICKER_BUTTON: '[data-test-subj="superDatePickerShowDatesButton"]',
+  SEARCH_DATE_PICKER_RELATIVE_TAB: '[data-test-subj="superDatePickerRelativeTab"]',
+  SEARCH_DATE_RELATIVE_PICKER_INPUT: '[data-test-subj="superDatePickerRelativeDateInputNumber"]',
+  SEARCH_DATE_RELATIVE_PICKER_UNIT_SELECTOR:
+    '[data-test-subj="superDatePickerRelativeDateInputUnitSelector"]',
+  QUERY_SUBMIT_BUTTON: '[data-test-subj="querySubmitButton"]',
+  GLOBAL_QUERY_EDITOR_FILTER_VALUE: '[data-test-subj="globalFilterLabelValue"]',
 };
