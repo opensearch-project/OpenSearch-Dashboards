3dc82ba7b373c0d0edcbb48269caef28bfd74259
Argus Li <contactme@chunkeili.com>
2024-12-04 12:29:36 -0800

    Reformat to match OSD-functional-tests.
    Reformat for TS.

---
+++ cypress/utils/dashboards/data_explorer/elements.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const DATA_EXPLORER_PAGE_ELEMENTS = {
+  NEW_SEARCH_BUTTON: 'discoverNewButton',
+  DISCOVER_QUERY_HITS: 'discoverQueryHits',
+  DATASET_SELECTOR_BUTTON: 'datasetSelectorButton',
+  ALL_DATASETS_BUTTON: 'datasetSelectorAdvancedButton',
+  DATASET_EXPLORER_WINDOW: 'datasetExplorerWindow',
+  DATASET_SELECTOR_NEXT_BUTTON: 'datasetSelectorNext',
+  DATASET_SELECTOR_LANGUAGE_SELECTOR: 'advancedSelectorLanguageSelect',
+  DATASET_SELECTOR_TIME_SELECTOR: 'advancedSelectorTimeFieldSelect',
+  DATASET_SELECTOR_SELECT_DATA_BUTTON: 'advancedSelectorConfirmButton',
+  DOC_TABLE: 'docTable',
+  DOC_TABLE_ROW_FIELD: 'docTableField',
+  TABLE_FIELD_FILTER_FOR_BUTTON: 'filterForValue',
+  TABLE_FIELD_FILTER_OUT_BUTTON: 'filterOutValue',
+  SEARCH_DATE_PICKER_BUTTON: 'superDatePickerShowDatesButton',
+  SEARCH_DATE_PICKER_RELATIVE_TAB: 'superDatePickerRelativeTab',
+  SEARCH_DATE_RELATIVE_PICKER_INPUT: 'superDatePickerRelativeDateInputNumber',
+  SEARCH_DATE_RELATIVE_PICKER_UNIT_SELECTOR: 'superDatePickerRelativeDateInputUnitSelector',
+  QUERY_SUBMIT_BUTTON: 'querySubmitButton',
+  GLOBAL_QUERY_EDITOR_FILTER_VALUE: 'globalFilterLabelValue',
+};
