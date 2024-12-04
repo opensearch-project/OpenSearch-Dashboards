25bf53ee4bf638694875c1591b3313f1d99588ed
Argus Li <contactme@chunkeili.com>
2024-12-04 12:29:36 -0800

    Reformat to match OSD-functional-tests.
    Reformat for TS.
    
    Signed-off-by: Federico Silva <federico.silva.-nd@disney.com>

--- cypress/integration/core_opensearch_dashboards/filter_for_value_spec.js
+++ cypress/integration/core_opensearch_dashboards/filter_for_value_spec.js
@@ -4,54 +4,52 @@
  */
 
 import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
-import { DataExplorerPage } from '../../utils/data_explorer_page/data_explorer_page';
 
 const miscUtils = new MiscUtils(cy);
-const dataExplorerPage = new DataExplorerPage(cy);
 
 describe('filter for value spec', () => {
   beforeEach(() => {
     cy.localLogin(Cypress.env('username'), Cypress.env('password'));
     miscUtils.visitPage('app/data-explorer/discover');
-    dataExplorerPage.clickNewSearchButton();
+    cy.clickNewSearchButton();
   });
   describe('filter actions in table field', () => {
     describe('index pattern dataset', () => {
       // filter actions should not exist for DQL
-      it.only('DQL', () => {
-        dataExplorerPage.selectIndexPatternDataset('DQL');
-        dataExplorerPage.setSearchDateRange('15', 'Years ago');
-        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(true);
-        dataExplorerPage.checkDocTableFirstFieldFilterForButtonFiltersCorrectField();
+      it('DQL', () => {
+        cy.selectIndexPatternDataset('DQL');
+        cy.setSearchRelativeDateRange('15', 'Years ago');
+        cy.checkDocTableFirstFieldFilterForAndOutButton(true);
+        cy.checkDocTableFirstFieldFilterForButtonFiltersCorrectField();
       });
       // filter actions should not exist for PPL
       it('Lucene', () => {
-        dataExplorerPage.selectIndexPatternDataset('Lucene');
-        dataExplorerPage.setSearchDateRange('15', 'Years ago');
-        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(true);
+        cy.selectIndexPatternDataset('Lucene');
+        cy.setSearchRelativeDateRange('15', 'Years ago');
+        cy.checkDocTableFirstFieldFilterForAndOutButton(true);
       });
       // filter actions should not exist for SQL
       it('SQL', () => {
-        dataExplorerPage.selectIndexPatternDataset('OpenSearch SQL');
-        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+        cy.selectIndexPatternDataset('OpenSearch SQL');
+        cy.checkDocTableFirstFieldFilterForAndOutButton(false);
       });
       // filter actions should not exist for PPL
       it('PPL', () => {
-        dataExplorerPage.selectIndexPatternDataset('PPL');
-        dataExplorerPage.setSearchDateRange('15', 'Years ago');
-        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+        cy.selectIndexPatternDataset('PPL');
+        cy.setSearchRelativeDateRange('15', 'Years ago');
+        cy.checkDocTableFirstFieldFilterForAndOutButton(false);
       });
     });
     describe('index dataset', () => {
       // filter actions should not exist for SQL
       it('SQL', () => {
-        dataExplorerPage.selectIndexDataset('OpenSearch SQL');
-        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+        cy.selectIndexDataset('OpenSearch SQL');
+        cy.checkDocTableFirstFieldFilterForAndOutButton(false);
       });
       // filter actions should not exist for PPL
       it('PPL', () => {
-        dataExplorerPage.selectIndexDataset('PPL');
-        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+        cy.selectIndexDataset('PPL');
+        cy.checkDocTableFirstFieldFilterForAndOutButton(false);
       });
     });
   });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -4,8 +4,11 @@
  */
 
 import '../utils/commands';
+import '../utils/dashboards/data_explorer/commands';
 
-// eslint-disable-next-line no-unused-vars
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
 Cypress.on('uncaught:exception', (_err) => {
   // returning false here prevents Cypress from failing the test
   return false;
