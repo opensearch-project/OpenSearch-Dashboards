3091b664901ff22d763abbab187e3b12467b9a24
tygao <tygao@amazon.com>
2024-12-04 17:46:06 +0800

    fix: fix page hanging when none collaborators for non dashboard admin
    Signed-off-by: tygao <tygao@amazon.com>

--- src/plugins/workspace/public/components/workspace_form/utils.test.ts
+++ src/plugins/workspace/public/components/workspace_form/utils.test.ts
@@ -11,6 +11,7 @@ import {
   getNumberOfErrors,
   isWorkspacePermissionSetting,
   getPermissionModeName,
+  EMPTY_PERMISSIONS,
 } from './utils';
 import { WorkspacePermissionMode } from '../../../common/constants';
 import { WorkspacePermissionItemType, optionIdToWorkspacePermissionModesMap } from './constants';
@@ -19,9 +20,9 @@ import { WorkspaceFormErrorCode } from './types';
 import { PermissionModeId } from '../../../../../core/public';
 
 describe('convertPermissionSettingsToPermissions', () => {
-  it('should return undefined if permission items not provided', () => {
-    expect(convertPermissionSettingsToPermissions(undefined)).toBeUndefined();
-    expect(convertPermissionSettingsToPermissions([])).toBeUndefined();
+  it('should return empty permission object if permission items are not provided', () => {
+    expect(convertPermissionSettingsToPermissions(undefined)).toBe(EMPTY_PERMISSIONS);
+    expect(convertPermissionSettingsToPermissions([])).toBe(EMPTY_PERMISSIONS);
   });
 
   it('should not add duplicate users and groups', () => {
