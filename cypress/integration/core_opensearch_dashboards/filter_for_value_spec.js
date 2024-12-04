c8e3118909b4df47cbaa58c89520d90650aac923
Argus Li <contactme@chunkeili.com>
2024-12-03 21:58:09 -0800

    Add support for different languages when creating dataset. Filter Out is almost complete, just working on removing the filter.

--- cypress/integration/core_opensearch_dashboards/filter_for_value_spec.js
+++ cypress/integration/core_opensearch_dashboards/filter_for_value_spec.js
@@ -10,16 +10,49 @@ const miscUtils = new MiscUtils(cy);
 const dataExplorerPage = new DataExplorerPage(cy);
 
 describe('filter for value spec', () => {
-  before(() => {
+  beforeEach(() => {
     cy.localLogin(Cypress.env('username'), Cypress.env('password'));
     miscUtils.visitPage('app/data-explorer/discover');
-  });
-
-  beforeEach(() => {
     dataExplorerPage.clickNewSearchButton();
   });
-
-  it('filter actions in table field', () => {
-    dataExplorerPage.selectIndexDataset('OpenSearch SQL');
+  describe('filter actions in table field', () => {
+    describe('index pattern dataset', () => {
+      // filter actions should not exist for DQL
+      it.only('DQL', () => {
+        dataExplorerPage.selectIndexPatternDataset('DQL');
+        dataExplorerPage.setSearchDateRange('15', 'Years ago');
+        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(true);
+        dataExplorerPage.checkDocTableFirstFieldFilterForButtonFiltersCorrectField();
+      });
+      // filter actions should not exist for PPL
+      it('Lucene', () => {
+        dataExplorerPage.selectIndexPatternDataset('Lucene');
+        dataExplorerPage.setSearchDateRange('15', 'Years ago');
+        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(true);
+      });
+      // filter actions should not exist for SQL
+      it('SQL', () => {
+        dataExplorerPage.selectIndexPatternDataset('OpenSearch SQL');
+        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+      });
+      // filter actions should not exist for PPL
+      it('PPL', () => {
+        dataExplorerPage.selectIndexPatternDataset('PPL');
+        dataExplorerPage.setSearchDateRange('15', 'Years ago');
+        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+      });
+    });
+    describe('index dataset', () => {
+      // filter actions should not exist for SQL
+      it('SQL', () => {
+        dataExplorerPage.selectIndexDataset('OpenSearch SQL');
+        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+      });
+      // filter actions should not exist for PPL
+      it('PPL', () => {
+        dataExplorerPage.selectIndexDataset('PPL');
+        dataExplorerPage.checkDocTableFirstFieldFilterForAndOutButton(false);
+      });
+    });
   });
 });
