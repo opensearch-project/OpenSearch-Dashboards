ea46542af04e4b8194de2740a3a30d1c6382629f
Argus Li <contactme@chunkeili.com>
2024-12-04 23:36:11 -0800

    Complete test suite filter actions in table field.
    Refactor to match OSD-functional-tests-layout.

--- cypress.config.ts
+++ cypress.config.ts
@@ -35,8 +35,9 @@ module.exports = defineConfig({
   },
   e2e: {
     baseUrl: 'http://localhost:5601',
-    specPattern: 'cypress/integration/**/*.spec.{js,jsx,ts,tsx}',
-    testIsolation: false,
+    supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/integration/**/*_spec.{js,jsx,ts,tsx}',
+    testIsolation: true,
     setupNodeEvents,
   },
 });
