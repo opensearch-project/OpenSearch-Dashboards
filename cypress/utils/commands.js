cd6c3d8cb923e0885e0d0476eff4c1aa1d17e301
Argus Li <contactme@chunkeili.com>
2024-12-04 23:36:11 -0800

    Complete test suite filter actions in table field.
    Refactor to match OSD-functional-tests-layout.

--- cypress/utils/commands.js
+++ cypress/utils/commands.js
@@ -11,37 +11,39 @@ import {
 const miscUtils = new MiscUtils(cy);
 const loginPage = new LoginPage(cy);
 
-// --- Typed commands --
-
+/**
+ * Get DOM element by data-test-subj id.
+ */
 Cypress.Commands.add('getElementByTestId', (testId, options = {}) => {
   return cy.get(`[data-test-subj="${testId}"]`, options);
 });
 
+/**
+ * Get multiple DOM elements by data-test-subj ids.
+ */
 Cypress.Commands.add('getElementsByTestIds', (testIds, options = {}) => {
   const selectors = [testIds].flat(Infinity).map((testId) => `[data-test-subj="${testId}"]`);
   return cy.get(selectors.join(','), options);
 });
 
-Cypress.Commands.add('findElementByTestId', (testId, options = {}) => {
-  return cy.find(`[data-test-subj="${testId}"]`, options);
-});
+/**
+ * Find element from previous chained element by data-test-subj id.
+ */
+Cypress.Commands.add(
+  'findElementByTestId',
+  { prevSubject: true },
+  (subject, testId, options = {}) => {
+    return cy.wrap(subject).find(`[data-test-subj="${testId}"]`, options);
+  }
+);
 
+/**
+ * Go to the local instance of OSD's home page and login.
+ */
 Cypress.Commands.add('localLogin', (username, password) => {
   miscUtils.visitPage('/app/home');
   loginPage.enterUserName(username);
   loginPage.enterPassword(password);
   loginPage.submit();
   cy.url().should('contain', '/app/home');
-});
-
-Cypress.Commands.add('waitForLoader', () => {
-  const opts = { log: false };
-
-  Cypress.log({
-    name: 'waitForPageLoad',
-    displayName: 'wait',
-    message: 'page load',
-  });
-  cy.wait(Cypress.env('WAIT_FOR_LOADER_BUFFER_MS'));
-  cy.getElementByTestId('recentItemsSectionButton', opts); // Update to `homeLoader` once useExpandedHeader is enabled
 });
