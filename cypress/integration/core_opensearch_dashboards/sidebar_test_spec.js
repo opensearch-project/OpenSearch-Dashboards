85fa6abf5ae511cf2a9d6e11d7386b9cea47bea7
Federico Silva <federico.silva.-nd@disney.com>
2024-12-06 15:27:19 -0300

    fix syntax error uncaught by eslint

--- cypress/integration/core_opensearch_dashboards/sidebar_test_spec.js
+++ cypress/integration/core_opensearch_dashboards/sidebar_test_spec.js
@@ -17,46 +17,36 @@ describe('sidebar spec', () => {
   it('add field: DQL', () => {
     DataExplorerPage.setSearchRelativeDateRange('15', 'Years ago');
 
-    it('check default field name', () => {
-      cy.intercept('/internal/search/opensearch-with-long-numerals').as('data');
-      DataExplorerPage.selectIndexPatternDataset('DQL');
-      cy.wait('@data').then(() => {
-        DataExplorerPage.getDocTableHeader(1).should('have.text', '_source');
-      });
+    cy.intercept('/internal/search/opensearch-with-long-numerals').as('data');
+    DataExplorerPage.selectIndexPatternDataset('DQL');
+    cy.wait('@data').then(() => {
+      DataExplorerPage.getDocTableHeader(1).should('have.text', '_source');
     });
 
     const testFields = ['_id', 'age', 'birthdate', 'salary'];
 
-    it('add some fields and check default field is gone', () => {
-      testFields.forEach((field) => {
-        DataExplorerPage.getFieldBtnByName(field).click();
-      });
-
-      DataExplorerPage.getDocTableHeader(1).should('not.have.text', '_source');
+    testFields.forEach((field) => {
+      DataExplorerPage.getFieldBtnByName(field).click();
     });
 
-    it('check headers persistence between DQL and PPL', () => {
-      DataExplorerPage.checkTableHeadersByArray(testFields);
-      DataExplorerPage.setQueryEditorLanguage('PPL');
-      DataExplorerPage.checkTableHeadersByArray(testFields);
-    });
+    DataExplorerPage.getDocTableHeader(1).should('not.have.text', '_source');
 
-    it('remove two fields', () => {
-      const firstTestField = testFields[0];
-      const secondTestField = testFields[1];
-      DataExplorerPage.getFieldBtnByName(firstTestField).click();
-      DataExplorerPage.getFieldBtnByName(secondTestField).click();
-      DataExplorerPage.getDocTableHeader(1).should('not.have.text', firstTestField);
-      DataExplorerPage.getDocTableHeader(2).should('not.have.text', secondTestField);
-    });
+    DataExplorerPage.checkTableHeadersByArray(testFields);
+    DataExplorerPage.setQueryEditorLanguage('PPL');
+    DataExplorerPage.checkTableHeadersByArray(testFields);
 
-    it('remove remaining fields (all)', () => {
-      const thirdTestField = testFields[2];
-      const fourthTestField = testFields[3];
-      DataExplorerPage.getFieldBtnByName(thirdTestField).click();
-      DataExplorerPage.getFieldBtnByName(fourthTestField).click();
-      DataExplorerPage.getDocTableHeader(1).should('have.text', '_source');
-      DataExplorerPage.getDocTableHeader(2).should('not.exist');
-    });
+    const firstTestField = testFields[0];
+    const secondTestField = testFields[1];
+    DataExplorerPage.getFieldBtnByName(firstTestField).click();
+    DataExplorerPage.getFieldBtnByName(secondTestField).click();
+    DataExplorerPage.getDocTableHeader(1).should('not.have.text', firstTestField);
+    DataExplorerPage.getDocTableHeader(2).should('not.have.text', secondTestField);
+
+    const thirdTestField = testFields[2];
+    const fourthTestField = testFields[3];
+    DataExplorerPage.getFieldBtnByName(thirdTestField).click();
+    DataExplorerPage.getFieldBtnByName(fourthTestField).click();
+    DataExplorerPage.getDocTableHeader(1).should('have.text', '_source');
+    DataExplorerPage.getDocTableHeader(2).should('not.exist');
   });
 });
