bd63276e2062887668e90023ee7309eb7ede4e87
Argus Li <contactme@chunkeili.com>
2024-12-10 10:04:34 -0800

    Add support for expanded row filters.
    Getting disabled filter buttons still not working.

--- cypress/utils/dashboards/data_explorer/commands.js
+++ cypress/utils/dashboards/data_explorer/commands.js
@@ -9,52 +9,42 @@ import { DATA_EXPLORER_PAGE_ELEMENTS } from './elements.js';
  * Get the New Search button.
  */
 Cypress.Commands.add('getNewSearchButton', () => {
-  return cy
-    .getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.NEW_SEARCH_BUTTON, { timeout: 10000 })
-    .should('be.visible');
+  return cy.getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.NEW_SEARCH_BUTTON, { timeout: 10000 });
 });
 
 /**
  * Get the Query Submit button.
  */
 Cypress.Commands.add('getQuerySubmitButton', () => {
-  return cy
-    .getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.QUERY_SUBMIT_BUTTON)
-    .should('be.visible');
+  return cy.getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.QUERY_SUBMIT_BUTTON);
 });
 
 /**
  * Get the Search Bar Date Picker button.
  */
 Cypress.Commands.add('getSearchDatePickerButton', () => {
-  return cy
-    .getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_PICKER_BUTTON)
-    .should('be.visible');
+  return cy.getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_PICKER_BUTTON);
 });
 
 /**
  * Get the Relative Date tab in the Search Bar Date Picker.
  */
 Cypress.Commands.add('getDatePickerRelativeTab', () => {
-  return cy
-    .getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_PICKER_RELATIVE_TAB)
-    .should('be.visible');
+  return cy.getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_PICKER_RELATIVE_TAB);
 });
 
 /**
  * Get the Relative Date Input in the Search Bar Date Picker.
  */
 Cypress.Commands.add('getDatePickerRelativeInput', () => {
-  return cy
-    .getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_RELATIVE_PICKER_INPUT)
-    .should('be.visible');
+  return cy.getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_RELATIVE_PICKER_INPUT);
 });
 
 /**
  * Get the Relative Date Unit selector in the Search Bar Date Picker.
  */
 Cypress.Commands.add('getDatePickerRelativeUnitSelector', () => {
-  return cy
-    .getElementByTestId(DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_RELATIVE_PICKER_UNIT_SELECTOR)
-    .should('be.visible');
+  return cy.getElementByTestId(
+    DATA_EXPLORER_PAGE_ELEMENTS.SEARCH_DATE_RELATIVE_PICKER_UNIT_SELECTOR
+  );
 });
