718135d857e100cbbad4e898e96dd2f9af14d13c
abbyhu2000 <abigailhu2000@gmail.com>
2024-12-10 19:31:31 +0000

    spec file
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

--- cypress/utils/base_constants.js
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export const BASE_PATH = Cypress.config('baseUrl');
-export const BACKEND_BASE_PATH = Cypress.env('openSearchUrl');
--- cypress/utils/constants.js
+++ cypress/utils/constants.js
@@ -3,5 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from './base_constants';
-export * from './dashboards/constants';
+export * from './apps/constants';
+
+export const BASE_PATH = Cypress.config('baseUrl');
+export const BACKEND_BASE_PATH = Cypress.env('openSearchUrl');
