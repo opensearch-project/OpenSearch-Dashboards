b18e7f728e6d9a8eeaf27d7128af45b2ce0278ae
Qxisylolo <qianxisy@amazon.com>
2024-12-16 10:12:00 +0800

    fix tests
    Signed-off-by: Qxisylolo <qianxisy@amazon.com>

--- src/plugins/data_source_management/public/components/drop_down_header/drop_down_header.test.tsx
+++ src/plugins/data_source_management/public/components/drop_down_header/drop_down_header.test.tsx
@@ -47,15 +47,16 @@ describe('DataSourceDropDownHeader', () => {
     expect(wrapper.text()).toContain(`${activeDataSourceCount}/${totalDataSourceCount}`);
   });
 
-  it('should call application.navigateToApp when the "Manage" link is clicked', () => {
+  it('should call application.navigateToApp and close the modal mask when the "Manage" link is clicked', () => {
     const totalDataSourceCount = 5;
     const applicationMock = coreMock.createStart().application;
     const navigateToAppMock = applicationMock.navigateToApp;
-
+    const onChangeDevToolsModalVisibleMock = jest.fn();
     const wrapper = mount(
       <DataSourceDropDownHeader
         totalDataSourceCount={totalDataSourceCount}
         application={applicationMock}
+        onChangeDevToolsModalVisible={onChangeDevToolsModalVisibleMock}
       />
     );
 
@@ -63,5 +64,6 @@ describe('DataSourceDropDownHeader', () => {
     expect(navigateToAppMock).toHaveBeenCalledWith('management', {
       path: `opensearch-dashboards/${DSM_APP_ID}`,
     });
+    expect(onChangeDevToolsModalVisibleMock).toHaveBeenCalled();
   });
 });
