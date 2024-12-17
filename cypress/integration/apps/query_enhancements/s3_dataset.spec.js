42843bd7e61d352d26cca001ffba5b43634bdba5
Sean Li <lnse@amazon.com>
2024-12-16 21:23:23 -0800

    adding integ tests for s3 using github secrets
    Signed-off-by: Sean Li <lnse@amazon.com>

---
+++ cypress/integration/apps/query_enhancements/s3_dataset.spec.js
@@ -0,0 +1,124 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
+import { DS_API, DSM_API, S3_CLUSTER } from '../../../utils/apps/query_enhancements/constants';
+
+const miscUtils = new MiscUtils(cy);
+
+let dataSourceId = '';
+
+describe('S3 Dataset', { defaultCommandTimeout: 120000 }, () => {
+  before(() => {
+    cy.request({
+      method: 'POST',
+      url: `${DSM_API}`,
+      headers: {
+        'osd-xsrf': true,
+      },
+      body: {
+        dataSourceAttr: {
+          endpoint: S3_CLUSTER.url,
+          auth: {
+            type: 'username_password',
+            credentials: {
+              username: S3_CLUSTER.username,
+              password: S3_CLUSTER.password,
+            },
+          },
+        },
+      },
+    }).then((metaRes) => {
+      if (metaRes && metaRes.body) {
+        cy.request({
+          method: 'POST',
+          url: `${DS_API.CREATE_DATA_SOURCE}`,
+          headers: {
+            'osd-xsrf': true,
+            'Content-Type': 'application/json',
+          },
+          body: {
+            attributes: {
+              title: S3_CLUSTER.name,
+              description: '',
+              ...metaRes.body,
+              endpoint: S3_CLUSTER.url,
+              auth: {
+                type: 'username_password',
+                credentials: {
+                  username: S3_CLUSTER.username,
+                  password: S3_CLUSTER.password,
+                },
+              },
+            },
+          },
+        }).then((resp) => {
+          if (resp && resp.body && resp.body.id) {
+            dataSourceId = resp.body.id;
+          }
+        });
+      }
+    });
+  });
+
+  after(() => {
+    cy.request({
+      method: 'DELETE',
+      url: `${DS_API.DELETE_DATA_SOURCE}${dataSourceId}`,
+      body: { force: false },
+      headers: {
+        'osd-xsrf': true,
+      },
+    });
+  });
+
+  describe('Run S3 Query', () => {
+    // Navigate to Discover
+    beforeEach(() => {
+      miscUtils.visitPage(`app/data-explorer/discover#/`);
+      cy.waitForLoaderNewHeader();
+    });
+
+    it('with SQL', function () {
+      cy.getElementByTestId(`datasetSelectorButton`).click();
+      cy.getElementByTestId(`datasetSelectorAdvancedButton`).click();
+
+      cy.get(`[title="S3 Connections"]`).click();
+      cy.get(`[title="BasicS3Connection"]`).click();
+      cy.get(`[title="mys3"]`).click();
+      cy.get(`[title="default"]`).click();
+      cy.get(`[title="http_logs"]`).click();
+      cy.getElementByTestId('datasetSelectorNext').click();
+      cy.get(`[class="euiModalHeader__title"]`).should('contain', 'Step 2: Configure data');
+
+      cy.getElementByTestId('advancedSelectorLanguageSelect').select('OpenSearch SQL');
+      cy.getElementByTestId('advancedSelectorConfirmButton').click();
+      cy.waitForLoaderNewHeader();
+
+      cy.getElementByTestId('queryEditorLanguageSelector').should('contain', 'OpenSearch SQL');
+      cy.get(`[data-test-subj="queryResultCompleteMsg"]`).should('be.visible');
+    });
+
+    // Skipping until #8922 is merged in
+    it.skip('with PPL', function () {
+      cy.getElementByTestId(`datasetSelectorButton`).click();
+      cy.getElementByTestId(`datasetSelectorAdvancedButton`).click();
+
+      cy.get(`[title="S3 Connections"]`).click();
+      cy.get(`[title="BasicS3Connection"]`).click();
+      cy.get(`[title="mys3"]`).click();
+      cy.get(`[title="default"]`).click();
+      cy.get(`[title="http_logs"]`).click();
+      cy.getElementByTestId('datasetSelectorNext').click();
+      cy.get(`[class="euiModalHeader__title"]`).should('contain', 'Step 2: Configure data');
+
+      cy.getElementByTestId('advancedSelectorLanguageSelect').select('PPL');
+      cy.getElementByTestId('advancedSelectorConfirmButton').click();
+      cy.waitForLoaderNewHeader();
+
+      cy.getElementByTestId('queryEditorLanguageSelector').should('contain', 'PPL');
+      cy.get(`[data-test-subj="queryResultCompleteMsg"]`).should('be.visible');
+    });
+  });
+});
