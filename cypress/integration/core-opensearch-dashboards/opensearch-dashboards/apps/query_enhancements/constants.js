66331eb0269bb4fce7c49bde674d40c71735bdb7
Anan Zhuang <ananzh@amazon.com>
2024-12-20 15:19:47 -0800

    Re-enable dataset_selector.spec.js and queries.spec.js under workspace (#9082)
    * Re-enable dataset_selector.spec.js and queries.spec.js under workspace
    
    Signed-off-by: Anan <ananzh@amazon.com>
    
    * fix comment
    
    Signed-off-by: Anan <ananzh@amazon.com>
    
    * add verifyHitCount for PPL
    
    Signed-off-by: Anan <ananzh@amazon.com>
    
    * Changeset file for PR #9082 created/updated
    
    ---------
    
    Signed-off-by: Anan <ananzh@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/constants.js
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/constants.js
@@ -5,3 +5,5 @@
 
 export const DATASOURCE_NAME = 'query-cluster';
 export const WORKSPACE_NAME = 'query-workspace';
+export const START_TIME = 'Jan 1, 2020 @ 00:00:00.000';
+export const END_TIME = 'Jan 1, 2024 @ 00:00:00.000';
