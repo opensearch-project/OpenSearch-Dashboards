ab7a186e40cdf49e43fb4839fea67a705f47ae48
Argus Li <contactme@chunkeili.com>
2024-12-22 15:11:24 -0800

    Refactor test framework to address review comment.

--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/constants.js
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/constants.js
@@ -10,3 +10,24 @@ export const DEFAULT_NO_TIME_INDEX_PATTERN_NAME = 'data_logs_small_no_time_1*';
 export const INDEX_PATTERN_NAME = `${DATASOURCE_NAME}::${DEFAULT_TIME_INDEX_PATTERN_NAME}`;
 export const SEARCH_ABSOLUTE_START_DATE = 'Jan 1, 2020 @ 00:00:00.000';
 export const SEARCH_ABSOLUTE_END_DATE = 'Jan 1, 2024 @ 00:00:00.000';
+
+export const DATASET_CONFIGS = {
+  index_pattern: {
+    type: 'index_pattern',
+    name: INDEX_PATTERN_NAME,
+    languages: [
+      { name: 'DQL', isEnabled: true },
+      { name: 'Lucene', isEnabled: true },
+      { name: 'OpenSearch SQL', isEnabled: false },
+      { name: 'PPL', isEnabled: false },
+    ],
+  },
+  index: {
+    type: 'index',
+    name: INDEX_NAME,
+    languages: [
+      { name: 'OpenSearch SQL', isEnabled: false },
+      { name: 'PPL', isEnabled: false },
+    ],
+  },
+};
--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/field_display_filtering.spec.js
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/field_display_filtering.spec.js
@@ -6,6 +6,7 @@
 import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
 import {
   DATASOURCE_NAME,
+  DATASET_CONFIGS,
   INDEX_NAME,
   INDEX_PATTERN_NAME,
   SEARCH_ABSOLUTE_START_DATE,
@@ -15,7 +16,7 @@ import * as dataExplorer from './helpers.js';
 
 const miscUtils = new MiscUtils(cy);
 
-function selectDataSet(datasetType, language) {
+function selectDataset(datasetType, language) {
   switch (datasetType) {
     case 'index':
       dataExplorer.selectIndexDataset(DATASOURCE_NAME, INDEX_NAME, language);
@@ -36,8 +37,8 @@ function setDateRange(datasetType, language) {
   }
 }
 
-function checkTableFieldFilterActions(datasetType, language, shouldExist) {
-  selectDataSet(datasetType, language);
+function verifyTableFieldFilterActions(datasetType, language, shouldExist) {
+  selectDataset(datasetType, language);
   setDateRange(datasetType, language);
 
   cy.getElementByTestId('discoverQueryHits').should('not.exist'); // To ensure it waits until a full table is loaded into the DOM, instead of a bug where table only has 1 hit.
@@ -50,8 +51,8 @@ function checkTableFieldFilterActions(datasetType, language, shouldExist) {
   }
 }
 
-function checkExpandedTableFilterActions(datasetType, language, isEnabled) {
-  selectDataSet(datasetType, language);
+function verifyExpandedTableFilterActions(datasetType, language, isEnabled) {
+  selectDataset(datasetType, language);
   setDateRange(datasetType, language);
 
   cy.getElementByTestId('discoverQueryHits').should('not.exist'); // To ensure it waits until a full table is loaded into the DOM, instead of a bug where table only has 1 hit.
@@ -89,64 +90,22 @@ describe('filter for value spec', () => {
     miscUtils.visitPage('app/data-explorer/discover');
     cy.getNewSearchButton().click();
   });
-  describe('filter actions in table field', () => {
-    describe('index pattern dataset', () => {
-      // filter actions should exist for DQL
-      it('DQL', () => {
-        checkTableFieldFilterActions('index_pattern', 'DQL', true);
-      });
-      // filter actions should exist for Lucene
-      it('Lucene', () => {
-        checkTableFieldFilterActions('index_pattern', 'Lucene', true);
-      });
-      // filter actions should not exist for SQL
-      it('SQL', () => {
-        checkTableFieldFilterActions('index_pattern', 'OpenSearch SQL', false);
-      });
-      // filter actions should not exist for PPL
-      it('PPL', () => {
-        checkTableFieldFilterActions('index_pattern', 'PPL', false);
-      });
-    });
-    describe('index dataset', () => {
-      // filter actions should not exist for SQL
-      it('SQL', () => {
-        checkTableFieldFilterActions('index', 'OpenSearch SQL', false);
-      });
-      // filter actions should not exist for PPL
-      it('PPL', () => {
-        checkTableFieldFilterActions('index', 'PPL', false);
-      });
-    });
-  });
 
-  describe('filter actions in expanded table', () => {
-    describe('index pattern dataset', () => {
-      // filter actions should exist for DQL
-      it('DQL', () => {
-        checkExpandedTableFilterActions('index_pattern', 'DQL', true);
-      });
-      // filter actions should exist for Lucene
-      it('Lucene', () => {
-        checkExpandedTableFilterActions('index_pattern', 'Lucene', true);
-      });
-      // filter actions should not exist for SQL
-      it('SQL', () => {
-        checkExpandedTableFilterActions('index_pattern', 'OpenSearch SQL', false);
-      });
-      // filter actions should not exist for PPL
-      it('PPL', () => {
-        checkExpandedTableFilterActions('index_pattern', 'PPL', false);
-      });
-    });
-    describe('index dataset', () => {
-      // filter actions should not exist for SQL
-      it('SQL', () => {
-        checkExpandedTableFilterActions('index', 'OpenSearch SQL', false);
-      });
-      // filter actions should not exist for PPL
-      it('PPL', () => {
-        checkExpandedTableFilterActions('index', 'OpenSearch SQL', false);
+  const testCases = [
+    { name: 'table field', verifyFn: verifyTableFieldFilterActions },
+    { name: 'expanded table', verifyFn: verifyExpandedTableFilterActions },
+  ];
+
+  testCases.forEach(({ name, verifyFn }) => {
+    describe(`filter actions in ${name}`, () => {
+      Object.entries(DATASET_CONFIGS).forEach(([type, config]) => {
+        describe(`${type} dataset`, () => {
+          config.languages.forEach(({ name: language, isEnabled }) => {
+            it(`${language}`, () => {
+              verifyFn(type, language, isEnabled);
+            });
+          });
+        });
       });
     });
   });
