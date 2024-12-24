0d31c222ac34e23fafa2216c13cb4e299966378b
Argus Li <contactme@chunkeili.com>
2024-12-24 10:36:06 -0800

    Create Spec

---
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/helpers.js
@@ -0,0 +1,342 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Get specific row of DocTable.
+ * @param {number} rowNumber Integer starts from 0 for the first row
+ */
+export function getDocTableRow(rowNumber) {
+  return cy.getElementByTestId('docTable').get('tbody tr').eq(rowNumber);
+}
+
+/**
+ * Get specific field of DocTable.
+ * @param {number} columnNumber Integer starts from 0 for the first column
+ * @param {number} rowNumber Integer starts from 0 for the first row
+ */
+export function getDocTableField(columnNumber, rowNumber) {
+  return getDocTableRow(rowNumber).findElementByTestId('docTableField').eq(columnNumber);
+}
+
+/**
+ * find all Rows in Doc Table Field Expanded Document.
+ * @param expandedDocument cypress representation of the Doc Table Field Expanded Document
+ */
+export function findDocTableExpandedDocRowsIn(expandedDocument) {
+  return expandedDocument.findElementByTestIdLike('tableDocViewRow-');
+}
+
+/**
+ * Get the "expandedDocumentRowNumber"th row from the expanded document from the "docTableRowNumber"th row of the DocTable.
+ * @param {number} docTableRowNumber Integer starts from 0 for the first row
+ * @param {number} expandedDocumentRowNumber Integer starts from 0 for the first row
+ * @example
+ * // returns the first row from the expanded document from the second row of the DocTable.
+ * getExpandedDocRow(1, 0);
+ */
+export function getExpandedDocRow(docTableRowNumber, expandedDocumentRowNumber) {
+  return findDocTableExpandedDocRowsIn(getDocTableRow(docTableRowNumber + 1)).eq(
+    expandedDocumentRowNumber
+  );
+}
+
+/**
+ * Get the value for the "expandedDocumentRowNumber"th row from the expanded document from the "docTableRowNumber"th row of the DocTable.
+ * @param {number} docTableRowNumber Integer starts from 0 for the first row
+ * @param {number} expandedDocumentRowNumber Integer starts from 0 for the first row
+ * @example
+ * // returns the value of the field from the first row from the expanded document from the second row of the DocTable.
+ * getExpandedDocRow(1, 0);
+ */
+export function getExpandedDocRowValue(docTableRowNumber, expandedDocumentRowNumber) {
+  return getExpandedDocRow(docTableRowNumber, expandedDocumentRowNumber)
+    .find(`[data-test-subj*="tableDocViewRow-"]`)
+    .find('span');
+}
+
+/**
+ * Get the field name for the "expandedDocumentRowNumber"th row from the expanded document from the "docTableRowNumber"th row of the DocTable.
+ * @param {number} docTableRowNumber Integer starts from 0 for the first row
+ * @param {number} expandedDocumentRowNumber Integer starts from 0 for the first row
+ * @example
+ * // returns the name of the field from the first row from the expanded document from the second row of the DocTable.
+ * getExpandedDocRow(1, 0);
+ */
+export function getExpandedDocRowFieldName(docTableRowNumber, expandedDocumentRowNumber) {
+  return getExpandedDocRow(docTableRowNumber, expandedDocumentRowNumber)
+    .find('td')
+    .eq(1) // Field name is in the second column.
+    .find('span[class*="textTruncate"]');
+}
+
+/**
+ * Select a language in the Dataset Selector for Index
+ * @param {string} datasetLanguage Index supports "OpenSearch SQL" and "PPL"
+ */
+export function selectIndexDatasetLanguage(datasetLanguage) {
+  cy.getElementByTestId('advancedSelectorLanguageSelect').select(datasetLanguage);
+  cy.getElementByTestId('advancedSelectorTimeFieldSelect').select(
+    "I don't want to use the time filter"
+  );
+  cy.getElementByTestId('advancedSelectorConfirmButton').click();
+}
+
+/**
+ * Select an index dataset.
+ * @param {string} indexClusterName Name of the cluster to be used for the Index.
+ * @param {string} indexName Name of the index dataset to be used.
+ * @param {string} datasetLanguage Index supports "OpenSearch SQL" and "PPL".
+ */
+export function selectIndexDataset(indexClusterName, indexName, datasetLanguage) {
+  cy.getElementByTestId('datasetSelectorButton').click();
+  cy.getElementByTestId('datasetSelectorAdvancedButton').click();
+  cy.getElementByTestId('datasetExplorerWindow').contains('Indexes').click();
+  cy.getElementByTestId('datasetExplorerWindow')
+    .contains(indexClusterName, { timeout: 10000 })
+    .click();
+  cy.getElementByTestId('datasetExplorerWindow').contains(indexName, { timeout: 10000 }).click();
+  cy.getElementByTestId('datasetSelectorNext').click();
+  selectIndexDatasetLanguage(datasetLanguage);
+}
+
+/**
+ * Select a language in the Dataset Selector for Index Pattern
+ * @param {string} datasetLanguage Index Pattern supports "DQL", "Lucene", "OpenSearch SQL" and "PPL"
+ */
+export function selectIndexPatternDatasetLanguage(datasetLanguage) {
+  cy.getElementByTestId('advancedSelectorLanguageSelect').select(datasetLanguage);
+  cy.getElementByTestId('advancedSelectorConfirmButton').click();
+}
+
+/**
+ * Select an index pattern dataset.
+ * @param {string} indexPatternName Name of the index pattern to be used.
+ * @param {string} datasetLanguage Index Pattern supports "DQL", "Lucene", "OpenSearch SQL" and "PPL"
+ */
+export function selectIndexPatternDataset(indexPatternName, datasetLanguage) {
+  cy.getElementByTestId('datasetSelectorButton').click();
+  cy.getElementByTestId('datasetSelectorAdvancedButton').click();
+  cy.getElementByTestId('datasetExplorerWindow').contains('Index Patterns').click();
+  cy.getElementByTestId('datasetExplorerWindow')
+    .contains(indexPatternName, { timeout: 10000 })
+    .click();
+  cy.getElementByTestId('datasetSelectorNext').click();
+  selectIndexPatternDatasetLanguage(datasetLanguage);
+}
+
+/**
+ * Toggle expansion of row rowNumber of Doc Table.
+ * @param {number} rowNumber rowNumber of Doc Table starts at 0 for row 1.
+ */
+export function toggleDocTableRow(rowNumber) {
+  getDocTableRow(rowNumber).within(() => {
+    cy.getElementByTestId('docTableExpandToggleColumn').find('button').click();
+  });
+}
+
+/**
+ * Check the query hit text matches expectedQueryHitText.
+ * @param {string} expectedQueryHitsText expected text for query hits. Commas must be added e.g. 10,000
+ */
+export function verifyQueryHitsText(expectedQueryHitsText) {
+  cy.getElementByTestId('discoverQueryHits').should('have.text', expectedQueryHitsText);
+}
+
+/**
+ * Check if the Doc table's rowNumberth row's Filter For and Filter Out buttons exists.
+ * @param {number} rowNumber Doc table row number to check (First row is row 0)
+ * @param {boolean} shouldExist Should this button exist
+ */
+export function verifyDocTableRowFilterForAndOutButton(rowNumber, shouldExist) {
+  const shouldText = shouldExist ? 'exist' : 'not.exist';
+  getDocTableField(0, rowNumber).within(() => {
+    cy.getElementByTestId('filterForValue').should(shouldText);
+    cy.getElementByTestId('filterOutValue').should(shouldText);
+  });
+}
+
+/**
+ * Check the Doc Table rowNumberth row's Filter buttons filters the correct value.
+ * @param {number} rowNumber Doc table row number to check (First row is row 0)
+ * @param {string} filterElement data-test-sub element for filter.
+ * @param {string} expectedQueryHitsWithoutFilter expected number of hits in string after the filter is removed Note you should add commas when necessary e.g. 9,999
+ * @param {string} expectedQueryHitsAfterFilterApplied expected number of hits in string after the filter is applied. Note you should add commas when necessary e.g. 9,999
+ * @param {boolean} shouldMatch boolean to determine if same rowNumber text should match after filter is applied
+ * @example verifyDocTableFilterAction(0, 'filterForValue', '10,000', '1', true)
+ */
+export function verifyDocTableFilterAction(
+  rowNumber,
+  filterElement,
+  expectedQueryHitsWithoutFilter,
+  expectedQueryHitsAfterFilterApplied,
+  shouldMatch
+) {
+  getDocTableField(0, rowNumber).then(($field) => {
+    const shouldText = shouldMatch ? 'have.text' : 'not.have.text';
+
+    const filterFieldText = $field.find('span span').text();
+    $field.find(`[data-test-subj="${filterElement}"]`).click();
+    // Verify pill text
+    cy.getElementByTestId('globalFilterLabelValue', {
+      timeout: 10000,
+    }).should('have.text', filterFieldText);
+    cy.getElementByTestId('discoverQueryHits').should(
+      'have.text',
+      expectedQueryHitsAfterFilterApplied
+    ); // checkQueryHitText must be in front of checking first line text to give time for DocTable to update.
+    getDocTableField(0, rowNumber).find('span span').should(shouldText, filterFieldText);
+  });
+  cy.getElementByTestId('globalFilterBar').find('[aria-label="Delete"]').click();
+  cy.getElementByTestId('discoverQueryHits').should('have.text', expectedQueryHitsWithoutFilter);
+}
+
+/**
+ * Check if the first expanded Doc Table Field's first row's Filter For, Filter Out and Exists Filter buttons are disabled.
+ * @param {boolean} isEnabled Boolean determining if these buttons are disabled
+ */
+export function verifyDocTableFirstExpandedFieldFirstRowFilterForFilterOutExistsFilterButtons(
+  isEnabled
+) {
+  const shouldText = isEnabled ? 'be.enabled' : 'be.disabled';
+  getExpandedDocRow(0, 0).within(() => {
+    cy.getElementByTestId('addInclusiveFilterButton').should(shouldText);
+    cy.getElementByTestId('removeInclusiveFilterButton').should(shouldText);
+    cy.getElementByTestId('addExistsFilterButton').should(shouldText);
+  });
+}
+
+/**
+ * Check the Filter For button in the expandedDocumentRowNumberth field in the expanded Document filters the correct value.
+ * @param {number} docTableRowNumber Integer starts from 0 for the first row
+ * @param {number} expandedDocumentRowNumber Integer starts from 0 for the first row
+ * @param {string} expectedQueryHitsWithoutFilter expected number of hits in string after the filter is removed Note you should add commas when necessary e.g. 9,999
+ * @param {string} expectedQueryHitsAfterFilterApplied expected number of hits in string after the filter is applied. Note you should add commas when necessary e.g. 9,999
+ */
+export function verifyDocTableFirstExpandedFieldFirstRowFilterForButtonFiltersCorrectField(
+  docTableRowNumber,
+  expandedDocumentRowNumber,
+  expectedQueryHitsWithoutFilter,
+  expectedQueryHitsAfterFilterApplied
+) {
+  getExpandedDocRowValue(docTableRowNumber, expandedDocumentRowNumber).then(
+    ($expandedDocumentRowValue) => {
+      const filterFieldText = $expandedDocumentRowValue.text();
+      getExpandedDocRow(docTableRowNumber, expandedDocumentRowNumber).within(() => {
+        cy.getElementByTestId('addInclusiveFilterButton').click();
+      });
+      // Verify pill text
+      cy.getElementByTestId('globalFilterLabelValue', {
+        timeout: 10000,
+      }).should('have.text', filterFieldText);
+      cy.getElementByTestId('discoverQueryHits').should(
+        'have.text',
+        expectedQueryHitsAfterFilterApplied
+      ); // checkQueryHitText must be in front of checking first line text to give time for DocTable to update.
+      getExpandedDocRowValue(docTableRowNumber, expandedDocumentRowNumber).should(
+        'have.text',
+        filterFieldText
+      );
+    }
+  );
+  cy.getElementByTestId('globalFilterBar').find('[aria-label="Delete"]').click();
+  cy.getElementByTestId('discoverQueryHits').should('have.text', expectedQueryHitsWithoutFilter);
+}
+
+/**
+ * Check the Filter Out button in the expandedDocumentRowNumberth field in the expanded Document filters the correct value.
+ * @param {number} docTableRowNumber Integer starts from 0 for the first row
+ * @param {number} expandedDocumentRowNumber Integer starts from 0 for the first row
+ * @param {string} expectedQueryHitsWithoutFilter expected number of hits in string after the filter is removed Note you should add commas when necessary e.g. 9,999
+ * @param {string} expectedQueryHitsAfterFilterApplied expected number of hits in string after the filter is applied. Note you should add commas when necessary e.g. 9,999
+ */
+export function verifyDocTableFirstExpandedFieldFirstRowFilterOutButtonFiltersCorrectField(
+  docTableRowNumber,
+  expandedDocumentRowNumber,
+  expectedQueryHitsWithoutFilter,
+  expectedQueryHitsAfterFilterApplied
+) {
+  getExpandedDocRowValue(docTableRowNumber, expandedDocumentRowNumber).then(
+    ($expandedDocumentRowValue) => {
+      const filterFieldText = $expandedDocumentRowValue.text();
+      getExpandedDocRow(docTableRowNumber, expandedDocumentRowNumber).within(() => {
+        cy.getElementByTestId('removeInclusiveFilterButton').click();
+      });
+      // Verify pill text
+      cy.getElementByTestId('globalFilterLabelValue', {
+        timeout: 10000,
+      }).should('have.text', filterFieldText);
+      cy.getElementByTestId('discoverQueryHits').should(
+        'have.text',
+        expectedQueryHitsAfterFilterApplied
+      ); // checkQueryHitText must be in front of checking first line text to give time for DocTable to update.
+      toggleDocTableRow(docTableRowNumber);
+      getExpandedDocRowValue(docTableRowNumber, expandedDocumentRowNumber).should(
+        'not.have.text',
+        filterFieldText
+      );
+    }
+  );
+  cy.getElementByTestId('globalFilterBar').find('[aria-label="Delete"]').click();
+  cy.getElementByTestId('discoverQueryHits').should('have.text', expectedQueryHitsWithoutFilter);
+  toggleDocTableRow(docTableRowNumber);
+}
+
+/**
+ * Check the first expanded Doc Table Field's first row's Toggle Column button has intended behavior.
+ */
+export function verifyDocTableFirstExpandedFieldFirstRowToggleColumnButtonHasIntendedBehavior() {
+  getExpandedDocRowFieldName(0, 0).then(($expandedDocumentRowFieldText) => {
+    const fieldText = $expandedDocumentRowFieldText.text();
+    getExpandedDocRow(0, 0).within(() => {
+      cy.getElementByTestId('docTableHeader-' + fieldText).should('not.exist');
+      cy.getElementByTestId('toggleColumnButton').click();
+    });
+    cy.getElementByTestId('fieldList-selected').within(() => {
+      cy.getElementByTestId('field-' + fieldText).should('exist');
+    });
+    cy.getElementByTestId('docTableHeader-' + fieldText).should('exist');
+    cy.getElementByTestId('fieldToggle-' + fieldText).click();
+    cy.getElementByTestId('fieldList-selected').within(() => {
+      cy.getElementByTestId('field-' + fieldText).should('not.exist');
+    });
+    cy.getElementByTestId('docTableHeader-' + fieldText).should('not.exist');
+  });
+}
+
+/**
+ * Check the first expanded Doc Table Field's first row's Exists Filter button filters the correct Field.
+ * @param {number} docTableRowNumber Integer starts from 0 for the first row
+ * @param {number} expandedDocumentRowNumber Integer starts from 0 for the first row
+ * @param {string} expectedQueryHitsWithoutFilter expected number of hits in string after the filter is removed Note you should add commas when necessary e.g. 9,999
+ * @param {string} expectedQueryHitsAfterFilterApplied expected number of hits in string after the filter is applied. Note you should add commas when necessary e.g. 9,999
+ */
+export function verifyDocTableFirstExpandedFieldFirstRowExistsFilterButtonFiltersCorrectField(
+  docTableRowNumber,
+  expandedDocumentRowNumber,
+  expectedQueryHitsWithoutFilter,
+  expectedQueryHitsAfterFilterApplied
+) {
+  getExpandedDocRowFieldName(docTableRowNumber, expandedDocumentRowNumber).then(
+    ($expandedDocumentRowField) => {
+      const filterFieldText = $expandedDocumentRowField.text();
+      getExpandedDocRow(docTableRowNumber, expandedDocumentRowNumber).within(() => {
+        cy.getElementByTestId('addExistsFilterButton').click();
+      });
+      // Verify full pill text
+      // globalFilterLabelValue gives the inner element, but we may want all the text in the filter pill
+      cy.getElementByTestId('globalFilterLabelValue', {
+        timeout: 10000,
+      })
+        .parent()
+        .should('have.text', filterFieldText + ': ' + 'exists');
+      cy.getElementByTestId('discoverQueryHits').should(
+        'have.text',
+        expectedQueryHitsAfterFilterApplied
+      );
+    }
+  );
+  cy.getElementByTestId('globalFilterBar').find('[aria-label="Delete"]').click();
+  cy.getElementByTestId('discoverQueryHits').should('have.text', expectedQueryHitsWithoutFilter);
+}
---
+++ cypress/utils/dashboards/data_explorer/elements.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const DATA_EXPLORER_PAGE_ELEMENTS = {
+  NEW_SEARCH_BUTTON: 'discoverNewButton',
+  DISCOVER_QUERY_HITS: 'discoverQueryHits',
+  DATASET_SELECTOR_BUTTON: 'datasetSelectorButton',
+  ALL_DATASETS_BUTTON: 'datasetSelectorAdvancedButton',
+  DATASET_EXPLORER_WINDOW: 'datasetExplorerWindow',
+  DATASET_SELECTOR_NEXT_BUTTON: 'datasetSelectorNext',
+  DATASET_SELECTOR_LANGUAGE_SELECTOR: 'advancedSelectorLanguageSelect',
+  DATASET_SELECTOR_TIME_SELECTOR: 'advancedSelectorTimeFieldSelect',
+  DATASET_SELECTOR_SELECT_DATA_BUTTON: 'advancedSelectorConfirmButton',
+  DOC_TABLE: 'docTable',
+  DOC_TABLE_ROW_FIELD: 'docTableField',
+  DOC_TABLE_EXPAND_TOGGLE_COLUMN_BUTTON: 'docTableExpandToggleColumn',
+  DOC_TABLE_EXPANDED_DOC_COLUMN_ROW_PREFIX: 'tableDocViewRow-',
+  DOC_TABLE_EXPANDED_DOC_TOGGLE_COLUMN_BUTTON: 'toggleColumnButton',
+  DOC_TABLE_EXPANDED_DOC_COLUMN_ADD_INCLUSIVE_FILTER_BUTTON: 'addInclusiveFilterButton',
+  DOC_TABLE_EXPANDED_DOC_COLUMN_REMOVE_INCLUSIVE_FILTER_BUTTON: 'removeInclusiveFilterButton',
+  DOC_TABLE_EXPANDED_DOC_COLUMN_EXISTS_FILTER_BUTTON: 'addExistsFilterButton',
+  DOC_TABLE_HEADER_FIELD_PREFIX: 'docTableHeader-',
+  TABLE_FIELD_FILTER_FOR_BUTTON: 'filterForValue',
+  TABLE_FIELD_FILTER_OUT_BUTTON: 'filterOutValue',
+  SEARCH_DATE_PICKER_BUTTON: 'superDatePickerShowDatesButton',
+  SEARCH_DATE_PICKER_START_DATE_BUTTON: 'superDatePickerstartDatePopoverButton',
+  SEARCH_DATE_PICKER_END_DATE_BUTTON: 'superDatePickerendDatePopoverButton',
+  SEARCH_DATE_PICKER_RELATIVE_TAB: 'superDatePickerRelativeTab',
+  SEARCH_DATE_RELATIVE_PICKER_INPUT: 'superDatePickerRelativeDateInputNumber',
+  SEARCH_DATE_RELATIVE_PICKER_UNIT_SELECTOR: 'superDatePickerRelativeDateInputUnitSelector',
+  SEARCH_DATE_PICKER_ABSOLUTE_TAB: 'superDatePickerAbsoluteTab',
+  SEARCH_DATE_PICKER_ABSOLUTE_DATE_INPUT: 'superDatePickerAbsoluteDateInput',
+  SIDE_BAR_SELECTED_FIELDS_LIST: 'fieldList-selected',
+  SIDE_BAR_FIELD_PREFIX: 'field-',
+  SIDE_BAR_SELECTED_FIELD_REMOVE_BUTTON_PREFIX: 'fieldToggle-',
+  QUERY_SUBMIT_BUTTON: 'querySubmitButton',
+  GLOBAL_QUERY_EDITOR_FILTER_VALUE: 'globalFilterLabelValue',
+  GLOBAL_FILTER_BAR: 'globalFilterBar',
+};
