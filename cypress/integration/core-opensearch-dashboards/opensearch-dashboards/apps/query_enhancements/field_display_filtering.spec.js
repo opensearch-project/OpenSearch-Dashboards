0b2ef32672995d748ecf2d9e0e34d84bccb99ce5
Argus Li <contactme@chunkeili.com>
2024-12-30 15:55:24 -0800

    Fix constants after merging main.

--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/field_display_filtering.spec.js
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/query_enhancements/field_display_filtering.spec.js
@@ -3,20 +3,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
 import {
+  WORKSPACE_NAME,
   DATASOURCE_NAME,
-  DATASET_CONFIGS,
   INDEX_NAME,
   INDEX_PATTERN_NAME,
   START_TIME,
   END_TIME,
-  WORKSPACE_NAME,
-} from './constants.js';
+  DATASET_CONFIGS,
+} from '../../../../../utils/apps/constants';
 import * as dataExplorer from './helpers.js';
 import { SECONDARY_ENGINE, BASE_PATH } from '../../../../../utils/constants';
 
-const miscUtils = new MiscUtils(cy);
+const randomString = Math.random().toString(36).substring(7);
+const workspace = `${WORKSPACE_NAME}-${randomString}`;
 
 function selectDataset(datasetType, language) {
   switch (datasetType) {
@@ -122,31 +122,31 @@ describe('filter for value spec', () => {
       ['cypress/fixtures/query_enhancements/data-logs-1/data_logs_small_time_1.mapping.json'],
       ['cypress/fixtures/query_enhancements/data-logs-1/data_logs_small_time_1.data.ndjson']
     );
+
     // Add data source
     cy.addDataSource({
       name: `${DATASOURCE_NAME}`,
       url: `${SECONDARY_ENGINE.url}`,
       authType: 'no_auth',
     });
-
     // Create workspace
-    cy.deleteWorkspaceByName(`${WORKSPACE_NAME}`);
-    miscUtils.visitPage('/app/home');
-    cy.createInitialWorkspaceWithDataSource(`${DATASOURCE_NAME}`, `${WORKSPACE_NAME}`);
+    cy.deleteWorkspaceByName(`${workspace}`);
+    cy.visit('/app/home');
+    cy.createInitialWorkspaceWithDataSource(`${DATASOURCE_NAME}`, `${workspace}`);
     cy.wait(2000);
     cy.createWorkspaceIndexPatterns({
       url: `${BASE_PATH}`,
-      workspaceName: `${WORKSPACE_NAME}`,
-      indexPattern: 'data_logs_small_time_1',
+      workspaceName: `${workspace}`,
+      indexPattern: INDEX_NAME,
       timefieldName: 'timestamp',
       indexPatternHasTimefield: true,
       dataSource: DATASOURCE_NAME,
       isEnhancement: true,
     });
-    cy.navigateToWorkSpaceHomePage(`${BASE_PATH}`, `${WORKSPACE_NAME}`);
   });
 
   beforeEach(() => {
+    cy.navigateToWorkSpaceHomePage(`${BASE_PATH}`, `${workspace}`);
     cy.getNewSearchButton().click();
   });
 
@@ -154,7 +154,7 @@ describe('filter for value spec', () => {
     cy.deleteWorkspaceByName(`${WORKSPACE_NAME}`);
     cy.deleteDataSourceByName(`${DATASOURCE_NAME}`);
     // TODO: Modify deleteIndex to handle an array of index and remove hard code
-    cy.deleteIndex('data_logs_small_time_1');
+    cy.deleteIndex(INDEX_PATTERN_NAME);
   });
 
   const testCases = [
--- cypress/utils/apps/query_enhancements/constants.js
+++ cypress/utils/apps/query_enhancements/constants.js
@@ -7,3 +7,28 @@ export const DATASOURCE_NAME = 'data-logs-1';
 export const WORKSPACE_NAME = 'query-workspace';
 export const START_TIME = 'Jan 1, 2020 @ 00:00:00.000';
 export const END_TIME = 'Jan 1, 2024 @ 00:00:00.000';
+export const INDEX_NAME = 'data_logs_small_time_1';
+export const DEFAULT_TIME_INDEX_PATTERN_NAME = 'data_logs_small_time_1*';
+export const DEFAULT_NO_TIME_INDEX_PATTERN_NAME = 'data_logs_small_no_time_1*';
+export const INDEX_PATTERN_NAME = `${DATASOURCE_NAME}::${DEFAULT_TIME_INDEX_PATTERN_NAME}`;
+
+export const DATASET_CONFIGS = {
+  index_pattern: {
+    type: 'index_pattern',
+    name: INDEX_PATTERN_NAME,
+    languages: [
+      { name: 'DQL', isEnabled: true },
+      { name: 'Lucene', isEnabled: true },
+      { name: 'OpenSearch SQL', isEnabled: false },
+      { name: 'PPL', isEnabled: false },
+    ],
+  },
+  index: {
+    type: 'index',
+    name: INDEX_NAME,
+    languages: [
+      { name: 'OpenSearch SQL', isEnabled: false },
+      { name: 'PPL', isEnabled: false },
+    ],
+  },
+};
