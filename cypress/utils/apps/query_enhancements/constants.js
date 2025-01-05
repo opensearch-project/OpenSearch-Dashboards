a7023acbe573f68c44b78268e85b5ca3a2870f7b
Argus Li <43020525+ArgusLi@users.noreply.github.com>
2025-01-05 15:01:08 -0800

    [Discover 2.0 Testing] Create Filtering Test (TestId-69: Filtering) (#9119)
    * Add test for filtering functionality for discover, which enables query enhancement
    * Make test more robust: Add logic to use date picker start and end buttons if set to absolute times. Check the table length directly instead of number of hits to avoid bug where not all results load.
    
    ---------
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>
    Signed-off-by: Federico Silva <federico.silva.dev@gmail.com>

--- cypress/utils/apps/query_enhancements/constants.js
+++ cypress/utils/apps/query_enhancements/constants.js
@@ -7,6 +7,32 @@ export const DATASOURCE_NAME = 'data-logs-1';
 export const WORKSPACE_NAME = 'query-workspace';
 export const START_TIME = 'Jan 1, 2020 @ 00:00:00.000';
 export const END_TIME = 'Jan 1, 2024 @ 00:00:00.000';
+export const INDEX_NAME = 'data_logs_small_time_1';
+export const DEFAULT_TIME_INDEX_PATTERN_NAME = 'data_logs_small_time_1*';
+export const DEFAULT_NO_TIME_INDEX_PATTERN_NAME = 'data_logs_small_no_time_1*';
+export const INDEX_PATTERN_NAME = `${DATASOURCE_NAME}::${DEFAULT_TIME_INDEX_PATTERN_NAME}`;
+
+export const DATASET_CONFIGS = {
+  index_pattern: {
+    type: 'index_pattern',
+    name: INDEX_PATTERN_NAME,
+    languages: [
+      // isFilterButtonsEnabled signifies if the filter buttons are supposed to be enabled.
+      { name: 'DQL', isFilterButtonsEnabled: true },
+      { name: 'Lucene', isFilterButtonsEnabled: true },
+      { name: 'OpenSearch SQL', isFilterButtonsEnabled: false },
+      { name: 'PPL', isFilterButtonsEnabled: false },
+    ],
+  },
+  index: {
+    type: 'index',
+    name: INDEX_NAME,
+    languages: [
+      { name: 'OpenSearch SQL', isFilterButtonsEnabled: false },
+      { name: 'PPL', isFilterButtonsEnabled: false },
+    ],
+  },
+};
 
 export const clusterName = 'test_cluster';
 export const clusterConnection = 'http://localhost:9200';
