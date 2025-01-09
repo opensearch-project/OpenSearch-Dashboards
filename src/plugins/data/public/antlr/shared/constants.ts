161038a96cea04ccd720ab9a5932e64ac6c96b8e
Paul Sebastian <paulstn@amazon.com>
2025-01-09 20:35:12 +0000

    fix merge again
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>

--- src/plugins/data/public/antlr/shared/constants.ts
+++ src/plugins/data/public/antlr/shared/constants.ts
@@ -175,4 +175,7 @@ export const testingIndex = ({
       subType: undefined,
     },
   ],
+  getFieldByName: jest.fn((fieldName: string) => {
+    return testingIndex.fields.find((field) => field.name === fieldName);
+  }),
 } as unknown) as IndexPattern;
