6938be8a8506a7b434f0eb83b78d2b7c3528d0a3
Justin Kim <jungkm@amazon.com>
2025-01-16 20:35:52 -0800

    when loading a saved search and saving it as a new saved search, use a different name
    Signed-off-by: Justin Kim <jungkm@amazon.com>

--- cypress/utils/apps/data_explorer/index.d.ts
+++ cypress/utils/apps/data_explorer/index.d.ts
@@ -7,7 +7,7 @@ declare namespace Cypress {
   interface Chainable<Subject> {
     verifyTimeConfig(start: string, end: string): Chainable<any>;
     saveSearch(name: string, saveAsNew?: boolean): Chainable<any>;
-    loadSaveSearch(name: string, selectDuplicate?: boolean): Chainable<any>;
+    loadSaveSearch(name: string): Chainable<any>;
     verifyHitCount(count: string): Chainable<any>;
     waitForSearch(): Chainable<any>;
     prepareTest(fromTime: string, toTime: string, interval: string): Chainable<any>;
