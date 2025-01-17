ccfb52d2021e65dca202e81ae9de11519dc6b489
Justin Kim <jungkm@amazon.com>
2025-01-17 12:28:03 -0800

    refactor query enhancement tests (#9208)
    Signed-off-by: Justin Kim <jungkm@amazon.com>

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/saved_queries_popover.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/saved_queries_popover.spec.js
@@ -9,23 +9,26 @@ import {
   INDEX_WITH_TIME_2,
   SECONDARY_ENGINE,
 } from '../../../../../utils/constants';
-
 import {
-  workspaceName,
-  datasourceName,
-  setSearchConfigurations,
+  getRandomizedWorkspaceName,
+  getRandomizedDatasourceName,
   setDatePickerDatesAndSearchIfRelevant,
+  generateAllTestConfigurations,
+} from '../../../../../utils/apps/query_enhancements/shared';
+import {
+  generateSavedTestConfiguration,
+  setSearchConfigurations,
   verifyDiscoverPageState,
-} from '../../../../../utils/apps/query_enhancements/saved_search';
+} from '../../../../../utils/apps/query_enhancements/saved';
 
-import { generateAllTestConfigurations } from '../../../../../utils/apps/query_enhancements/saved_queries';
+const workspaceName = getRandomizedWorkspaceName();
+const datasourceName = getRandomizedDatasourceName();
 
 // This spec assumes data.savedQueriesNewUI.enabled is false.
 // These tests will not be run until the older legacy tests are migrated https://github.com/opensearch-project/OpenSearch-Dashboards/pull/9166#discussion_r1913687440
-
 export const runSavedQueriesPopoverUITests = () => {
   describe.skip('saved queries popover UI', () => {
-    before(() => {
+    beforeEach(() => {
       // Load test data
       cy.setupTestData(
         SECONDARY_ENGINE.url,
@@ -58,7 +61,7 @@ export const runSavedQueriesPopoverUITests = () => {
       });
     });
 
-    after(() => {
+    afterEach(() => {
       // No need to explicitly delete all saved queries as deleting the workspace will delete associated saved queries
       cy.deleteWorkspaceByName(workspaceName);
       // // TODO: Modify deleteIndex to handle an array of index and remove hard code
@@ -67,8 +70,7 @@ export const runSavedQueriesPopoverUITests = () => {
       cy.deleteIndex(INDEX_WITH_TIME_2);
     });
 
-    const testConfigurations = generateAllTestConfigurations();
-    testConfigurations.forEach((config) => {
+    generateAllTestConfigurations(generateSavedTestConfiguration).forEach((config) => {
       it(`should successfully create a saved query for ${config.testName}`, () => {
         cy.navigateToWorkSpaceSpecificPage({
           workspaceName,
@@ -83,14 +85,10 @@ export const runSavedQueriesPopoverUITests = () => {
 
         setSearchConfigurations(config);
         verifyDiscoverPageState(config);
-        cy.saveQuery(config.saveName, ' ', false);
-      });
-    });
+        cy.saveQuery(config.saveName);
 
-    it('should see all saved queries', () => {
-      cy.getElementByTestId('saved-query-management-popover-button').click();
-
-      testConfigurations.forEach((config) => {
+        // verify that it has been saved
+        cy.getElementByTestId('saved-query-management-popover-button').click();
         cy.getElementByTestId('saved-query-management-popover')
           .contains(config.saveName)
           .should('exist');
