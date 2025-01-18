91c01c126ca1403979ed003212d6332eb260d1c9
Justin Kim <jungkm@amazon.com>
2025-01-17 18:39:38 -0800

    chore: change casing of data-logs to be compliant with our git hook rules
    Signed-off-by: Justin Kim <jungkm@amazon.com>

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/field_display_filtering.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/field_display_filtering.spec.js
@@ -25,8 +25,8 @@ describe('filter for value spec', () => {
     // Load test data
     cy.setupTestData(
       SECONDARY_ENGINE.url,
-      ['cypress/fixtures/query_enhancements/data-logs-1/data_logs_small_time_1.mapping.json'],
-      ['cypress/fixtures/query_enhancements/data-logs-1/data_logs_small_time_1.data.ndjson']
+      ['cypress/fixtures/query_enhancements/data_logs_1/data_logs_small_time_1.mapping.json'],
+      ['cypress/fixtures/query_enhancements/data_logs_1/data_logs_small_time_1.data.ndjson']
     );
 
     // Add data source
