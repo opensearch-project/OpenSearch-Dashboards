969114f35c7f0ecfe08d384662b9095f8d11adea
Argus Li <43020525+ArgusLi@users.noreply.github.com>
2025-01-28 08:56:36 -0800

    [TESTID-125,126,127,128,129] Saved Query Test Scope (#9229)
    * add spec for old ui
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add comment about cleaning up saved queries
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add data-test-subj for save query new ui
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add data-test-subj for save query button for new ui
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add data-test-subj for open query button in query management popover
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add new ui spec to test list saved queries
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Fix DataSource url
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Changeset file for PR #9166 created/updated
    
    * Address Justin's comments.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Remove final force.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Fix final force.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Move utils to Suchit's suggested location, fix comments
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Rename specs, change cypress workflow to use new save query ui, merge old ui and new ui commands
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * fix import
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Load query
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Complete load query popover and flyout
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Modularize view and load query popover and flyout
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Complete valid Saved Queries shown across Visualize Pages
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Complete Popover delete saved query
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add data-test-subj for flyoutbody and delete button.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Complete flyout delete saved query
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add data-test-subj for saveAsNewQueryCheckbox
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Complete Flyout Update Saved Query
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Complete Flyout Save as New Saved Query
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Complete Popout Save as New Saved Query
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Changeset file for PR #9229 created/updated
    
    * Fix merge
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Move data-test-subj changes to another PR.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Remove savedQueriesNewUI
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Address all comments.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Fix cypress command namespacing.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Rename saved queries spec.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Remove references to flyout
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Refactor tests to work with beforeEach and afterEach
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Add force to click when loading saved query.
    
    This removes delay when waiting for toast to disperse.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    * Fix test issue. Thank you @angle943 for finding a solution.
    
    Issue was caused by Cypress being too fast for the date picker to appear.
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    
    ---------
    
    Signed-off-by: Argus Li <contactme@chunkeili.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>
    Co-authored-by: Anan Zhuang <ananzh@amazon.com>

--- cypress/utils/commands.js
+++ cypress/utils/commands.js
@@ -28,6 +28,10 @@ Cypress.Commands.add('getElementByTestId', (testId, options = {}) => {
   return cy.get(`[data-test-subj="${testId}"]`, options);
 });
 
+Cypress.Commands.add('getElementByTestIdLike', (testId, options = {}) => {
+  return cy.get(`[data-test-subj*="${testId}"]`, options);
+});
+
 Cypress.Commands.add('getElementsByTestIds', (testIds, options = {}) => {
   const selectors = [testIds].flat(Infinity).map((testId) => `[data-test-subj="${testId}"]`);
   return cy.get(selectors.join(','), options);
