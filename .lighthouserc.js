659f644e8ad2b4943c9a247e91a5aa55d924083b
Ubuntu <ubuntu@ip-172-31-47-220.us-west-2.compute.internal>
2025-01-28 23:15:18 +0000

    lighthouse ci added
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-47-220.us-west-2.compute.internal>

---
+++ .lighthouserc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:5601'], // Adjust to OpenSearch Dashboards URL
+      startServerCommand: 'yarn start',
+      numberOfRuns: 3,
+      settings: {
+        chromeFlags: '--no-sandbox --disable-gpu',
+      },
+    },
+    assert: {
+      assertions: {
+        performance: ['warn', { minScore: 0.9 }],
+        'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
+        'speed-index': ['warn', { maxNumericValue: 3000 }],
+        'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
+        'total-blocking-time': ['warn', { maxNumericValue: 300 }],
+        'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage', // Change to a proper CI storage later
+    },
+  },
+};
