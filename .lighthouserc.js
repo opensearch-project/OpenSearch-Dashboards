3d5dddc1d7bb59ebd8e640a133dc09860baffaae
Ubuntu <ubuntu@ip-172-31-47-220.us-west-2.compute.internal>
2025-01-29 22:50:25 +0000

    ci metrics setup

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,25 +1,31 @@
+const baseUrl = 'http://localhost:5601';
+
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:5601'], // Adjust to OpenSearch Dashboards URL
-      startServerCommand: 'yarn start',
+      url: [`${baseUrl}`, `${baseUrl}/app/data-explorer/discover`, `${baseUrl}/app/dashboards`], // Add more URLs as needed
+      startServerCommand: 'yarn start --no-base-path',
       numberOfRuns: 3,
       settings: {
-        chromeFlags: '--no-sandbox --disable-gpu',
+        chromePath: require('puppeteer').executablePath(),
+        chromeFlags: '--no-sandbox --disable-gpu --headless',
       },
     },
     assert: {
+      preset: 'lighthouse:recommended',
       assertions: {
-        performance: ['warn', { minScore: 0.9 }],
-        'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
-        'speed-index': ['warn', { maxNumericValue: 3000 }],
+        performance: ['error', { minScore: 0.9 }],
+        'first-contentful-paint': ['warn', { maxNumericValue: 1800 }],
         'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
+        interactive: ['warn', { maxNumericValue: 5000 }],
         'total-blocking-time': ['warn', { maxNumericValue: 300 }],
-        'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }],
       },
     },
     upload: {
-      target: 'temporary-public-storage', // Change to a proper CI storage later
+      target: 'temporary-public-storage', // Required for GitHub integration
     },
   },
 };
+
+// target: 'filesystem',
+// outputDir: process.env.LHCI_BASELINE ? './lhci-baseline' : './lhci-reports',
