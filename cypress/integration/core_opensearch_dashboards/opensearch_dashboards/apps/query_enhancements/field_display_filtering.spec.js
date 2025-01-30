e9246d5be9f09fb1871808c56d0882047484e34a
Argus Li <contactme@chunkeili.com>
2025-01-30 09:28:57 -0800

    Fix docker URL for datasource.
    Signed-off-by: Argus Li <contactme@chunkeili.com>

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/field_display_filtering.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/field_display_filtering.spec.js
@@ -32,7 +32,7 @@ describe('filter for value spec', () => {
     // Add data source
     cy.addDataSource({
       name: DATASOURCE_NAME,
-      url: 'http://opensearch-node:9200/',
+      url: SECONDARY_ENGINE.url,
       authType: 'no_auth',
     });
     // Create workspace
