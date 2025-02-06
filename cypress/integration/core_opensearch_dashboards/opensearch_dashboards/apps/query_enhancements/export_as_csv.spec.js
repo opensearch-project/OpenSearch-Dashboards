c3604cb84913b4329bf489b4fdf4a44ff85ac385
Justin Kim <jungkm@amazon.com>
2025-02-06 00:03:58 -0800

    add cypress tests for export csv
    Signed-off-by: Justin Kim <jungkm@amazon.com>

---
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/export_as_csv.spec.js
@@ -0,0 +1,167 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import Papa from 'papaparse';
+import {
+  INDEX_WITH_TIME_1,
+  PATHS,
+  DATASOURCE_NAME,
+  INDEX_WITHOUT_TIME_1,
+  DatasetTypes,
+} from '../../../../../utils/constants';
+import {
+  getRandomizedWorkspaceName,
+  setDatePickerDatesAndSearchIfRelevant,
+} from '../../../../../utils/apps/query_enhancements/shared';
+import { prepareTestSuite } from '../../../../../utils/helpers';
+import {
+  generateExportAsCsvIndexPatternTestConfigurations,
+  getFirstRowForDownloadWithFields,
+  getFirstRowTimeForSourceDownload,
+  getHeadersForDownloadWithFields,
+  getHeadersForSourceDownload,
+  getQueriedCountForLanguage,
+} from '../../../../../utils/apps/query_enhancements/export_as_csv';
+import moment from 'moment';
+import path from 'path';
+
+const workspaceName = getRandomizedWorkspaceName();
+
+const runSavedSearchTests = () => {
+  describe('Export as CSV', () => {
+    beforeEach(() => {
+      // cy.clock(now.toDate());
+      // Load test data
+      cy.osd.setupTestData(
+        PATHS.SECONDARY_ENGINE,
+        [
+          `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITH_TIME_1}.mapping.json`,
+          `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITHOUT_TIME_1}.mapping.json`,
+        ],
+        [
+          `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITH_TIME_1}.data.ndjson`,
+          `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITHOUT_TIME_1}.data.ndjson`,
+        ]
+      );
+
+      // Add data source
+      cy.osd.addDataSource({
+        name: DATASOURCE_NAME,
+        url: PATHS.SECONDARY_ENGINE,
+      });
+
+      // Create workspace
+      cy.deleteAllWorkspaces();
+      cy.visit('/app/home');
+      cy.osd.createInitialWorkspaceWithDataSource(DATASOURCE_NAME, workspaceName);
+      cy.createWorkspaceIndexPatterns({
+        workspaceName: workspaceName,
+        indexPattern: INDEX_WITH_TIME_1,
+        timefieldName: 'timestamp',
+        dataSource: DATASOURCE_NAME,
+        isEnhancement: true,
+      });
+      cy.createWorkspaceIndexPatterns({
+        workspaceName: workspaceName,
+        indexPattern: INDEX_WITHOUT_TIME_1,
+        timefieldName: '',
+        dataSource: DATASOURCE_NAME,
+        isEnhancement: true,
+        indexPatternHasTimefield: false,
+      });
+    });
+
+    afterEach(() => {
+      cy.deleteWorkspaceByName(workspaceName);
+      cy.osd.deleteDataSourceByName(DATASOURCE_NAME);
+      cy.osd.deleteIndex(INDEX_WITH_TIME_1);
+      cy.osd.deleteIndex(INDEX_WITHOUT_TIME_1);
+    });
+
+    generateExportAsCsvIndexPatternTestConfigurations().forEach((config) => {
+      it(`should be able to export for ${config.saveName}`, () => {
+        cy.navigateToWorkSpaceSpecificPage({
+          workspaceName,
+          page: 'discover',
+          isEnhancement: true,
+        });
+
+        if (config.datasetType === DatasetTypes.INDEX_PATTERN.name) {
+          cy.setIndexPatternAsDataset(config.dataset, DATASOURCE_NAME);
+        } else {
+          cy.setIndexAsDataset(
+            config.dataset,
+            DATASOURCE_NAME,
+            'PPL',
+            config.hasTime ? 'timestamp' : "I don't want to use the time filter",
+            'submit'
+          );
+        }
+
+        for (let i = 0; i < config.languages.length; i++) {
+          const language = config.languages[i];
+
+          cy.setQueryLanguage(language.name);
+          if (config.hasTime) {
+            setDatePickerDatesAndSearchIfRelevant(language.name);
+          }
+          // Intentionally waiting for query to complete. When changing languages there is no good way to determine that the data has loaded
+          cy.wait(2000);
+
+          cy.getElementByTestId('dscDownloadCsvButton')
+            .should('exist')
+            .contains(`Download ${getQueriedCountForLanguage(language)} documents as CSV`)
+            .click();
+
+          cy.readFile(
+            path.join(
+              Cypress.config('downloadsFolder'),
+              `opensearch_export_${moment().format('YYYY-MM-DD')}.csv`
+            )
+            // eslint-disable-next-line no-loop-func
+          ).then((csvString) => {
+            const { data } = Papa.parse(csvString);
+            cy.wrap(data).should('have.length', getQueriedCountForLanguage(language) + 1);
+            cy.wrap(data[0]).should('deep.equal', getHeadersForSourceDownload(config.hasTime));
+            if (config.hasTime) {
+              cy.wrap(data[1][0]).should('equal', getFirstRowTimeForSourceDownload(language));
+            }
+          });
+
+          // Select some fields
+          cy.getElementByTestId('fieldToggle-bytes_transferred').click();
+          cy.getElementByTestId('fieldToggle-personal.name').click();
+
+          cy.getElementByTestId('dscDownloadCsvButton').click();
+
+          // wait for file to be downloaded
+          cy.wait(4000);
+
+          cy.readFile(
+            path.join(
+              Cypress.config('downloadsFolder'),
+              `opensearch_export_${moment().format('YYYY-MM-DD')}.csv`
+            )
+            // eslint-disable-next-line no-loop-func
+          ).then((csvString) => {
+            const { data } = Papa.parse(csvString);
+            cy.wrap(data).should('have.length', getQueriedCountForLanguage(language) + 1);
+            cy.wrap(data[0]).should('deep.equal', getHeadersForDownloadWithFields(config.hasTime));
+            cy.wrap(data[1]).should(
+              'deep.equal',
+              getFirstRowForDownloadWithFields(language, config.hasTime)
+            );
+          });
+
+          // deselect the selected fields
+          cy.getElementByTestId('fieldToggle-bytes_transferred').click();
+          cy.getElementByTestId('fieldToggle-personal.name').click();
+        }
+      });
+    });
+  });
+};
+
+prepareTestSuite('Export as CSV', runSavedSearchTests);
---
+++ cypress/utils/apps/query_enhancements/export_as_csv.js
@@ -0,0 +1,130 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { DatasetTypes, INDEX_WITH_TIME_1, INDEX_WITHOUT_TIME_1, QueryLanguages } from './constants';
+
+/**
+ * The configurations needed for saved search/queries tests
+ * @typedef {Object} ExportAsCsvTestConfig
+ * @property {string} dataset - the dataset name to use
+ * @property {QueryEnhancementDataset} datasetType - the type of dataset
+ * @property {QueryEnhancementLanguageData[]} languages - the languages that this dataset supports
+ * @property {string} saveName - the phrase to add to the test case's title
+ * @property {boolean} hasTime - whether the dataset has time
+ */
+
+/**
+ * Returns the ExportAsCsvTestConfig
+ * @returns {ExportAsCsvTestConfig[]}
+ */
+export const generateExportAsCsvIndexPatternTestConfigurations = () => {
+  return Object.values(DatasetTypes)
+    .map((dataset) => {
+      const sharedConfig = {
+        datasetType: dataset.name,
+        languages: dataset.supportedLanguages,
+      };
+      return [
+        {
+          ...sharedConfig,
+          dataset: `${INDEX_WITH_TIME_1}${dataset.name === DatasetTypes.INDEXES.name ? '' : '*'}`,
+          hasTime: true,
+          saveName: `${dataset.name}-Time`,
+        },
+        {
+          ...sharedConfig,
+          dataset: `${INDEX_WITHOUT_TIME_1}${
+            dataset.name === DatasetTypes.INDEXES.name ? '' : '*'
+          }`,
+          hasTime: false,
+          saveName: `${dataset.name}-NoTime`,
+        },
+      ];
+    })
+    .flat();
+};
+
+/**
+ * Returns the number of queried results we should have for a given language
+ * @param {QueryEnhancementLanguageData} language - the language data
+ * @returns {number}
+ */
+export const getQueriedCountForLanguage = (language) => {
+  switch (language.name) {
+    case QueryLanguages.DQL.name:
+      return 500;
+    case QueryLanguages.Lucene.name:
+      return 500;
+    case QueryLanguages.SQL.name:
+      return 10;
+    case QueryLanguages.PPL.name:
+      return 10000;
+  }
+};
+
+/**
+ * Returns what the header should be for the downloaded file with source
+ * @property {boolean} hasTime - whether the dataset has time
+ * @returns {string[]}
+ */
+export const getHeadersForSourceDownload = (hasTime) => {
+  if (hasTime) {
+    return ['timestamp', '_source'];
+  } else {
+    return ['_source'];
+  }
+};
+
+/**
+ * Returns what the header should be for the downloaded file with selected fields
+ * @property {boolean} hasTime - whether the dataset has time
+ * @returns {string[]}
+ */
+export const getHeadersForDownloadWithFields = (hasTime) => {
+  if (hasTime) {
+    return ['timestamp', 'bytes_transferred', 'personal.name'];
+  } else {
+    return ['bytes_transferred', 'personal.name'];
+  }
+};
+
+/**
+ * Returns what the first row should be for the downloaded file for file with _source
+ * @param {QueryEnhancementLanguageData} language - the language data
+ * @returns {string}
+ */
+export const getFirstRowTimeForSourceDownload = (language) => {
+  let time = 'Dec 31, 2022 @ 14:14:42.801';
+
+  if ([QueryLanguages.SQL.name, QueryLanguages.PPL.name].includes(language.name)) {
+    time = 'Jan 1, 2021 @ 00:00:00.000';
+  }
+
+  return time;
+};
+
+/**
+ * Returns what the first row should be for the downloaded file for file with selected fields
+ * @param {QueryEnhancementLanguageData} language - the language data
+ * @param {boolean} hasTime - whether the dataset has time
+ * @returns {string[]}
+ */
+export const getFirstRowForDownloadWithFields = (language, hasTime) => {
+  if (hasTime) {
+    let time = 'Dec 31, 2022 @ 14:14:42.801';
+    let bytesTransferred = '9,268';
+    let name = 'Nina Botsford MD';
+
+    if ([QueryLanguages.SQL.name, QueryLanguages.PPL.name].includes(language.name)) {
+      time = 'Jan 1, 2021 @ 00:00:00.000';
+      bytesTransferred = '2,760';
+      name = 'Curtis Funk';
+    }
+
+    return [time, bytesTransferred, name];
+  } else {
+    return ['4,406', 'Allison Bins'];
+  }
+};
