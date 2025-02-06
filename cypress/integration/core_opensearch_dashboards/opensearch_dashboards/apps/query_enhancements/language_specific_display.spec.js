2ff6f19eddf688fa31eb946eeab68d75d4b11dd3
Justin Kim <jungkm@amazon.com>
2025-02-06 11:28:21 -0800

    [TEST] update tests to use only data-logs-1 (#9339)
    * update tests to use only data-logs-1
    
    Signed-off-by: Justin Kim <jungkm@amazon.com>
    
    * make engine and secondary_engine optional
    
    Signed-off-by: Justin Kim <jungkm@amazon.com>
    
    * update a few more things
    
    Signed-off-by: Justin Kim <jungkm@amazon.com>
    
    ---------
    
    Signed-off-by: Justin Kim <jungkm@amazon.com>

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/language_specific_display.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/query_enhancements/language_specific_display.spec.js
@@ -32,11 +32,11 @@ export const runDisplayTests = () => {
         SECONDARY_ENGINE.url,
         [
           `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITH_TIME_1}.mapping.json`,
-          `cypress/fixtures/query_enhancements/data_logs_2/${INDEX_WITH_TIME_2}.mapping.json`,
+          `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITH_TIME_2}.mapping.json`,
         ],
         [
           `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITH_TIME_1}.data.ndjson`,
-          `cypress/fixtures/query_enhancements/data_logs_2/${INDEX_WITH_TIME_2}.data.ndjson`,
+          `cypress/fixtures/query_enhancements/data_logs_1/${INDEX_WITH_TIME_2}.data.ndjson`,
         ]
       );
       // Add data source
