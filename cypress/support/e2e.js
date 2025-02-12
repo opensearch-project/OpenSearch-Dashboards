f6474f8b4a3c6fa0fab9faa92a12d03a70e6a68a
Justin Kim <jungkm@amazon.com>
2025-02-12 11:50:54 -0800

    removed scopedHistory error swallow in cypress
    Signed-off-by: Justin Kim <jungkm@amazon.com>

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -8,12 +8,3 @@ import '../utils/commands.osd';
 import '../utils/apps/commands';
 import '../utils/dashboards/workspace-plugin/commands';
 import '../utils/dashboards/commands';
-
-// TODO: Remove this after https://github.com/opensearch-project/OpenSearch-Dashboards/issues/5476 is resolved
-const scopedHistoryNavigationError = /^[^(ScopedHistory instance has fell out of navigation scope)]/;
-Cypress.on('uncaught:exception', (err) => {
-  /* returning false here prevents Cypress from failing the test */
-  if (scopedHistoryNavigationError.test(err.message)) {
-    return false;
-  }
-});
