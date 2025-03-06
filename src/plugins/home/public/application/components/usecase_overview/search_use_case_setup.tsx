f577dae2498497abde7cc0915096dfd4e1102f2d
saimedhi <saimedhi@amazon.com>
2025-03-06 12:56:33 -0800

    rearranged search overview page cards
    Signed-off-by: saimedhi <saimedhi@amazon.com>

--- src/plugins/home/public/application/components/usecase_overview/search_use_case_setup.tsx
+++ src/plugins/home/public/application/components/usecase_overview/search_use_case_setup.tsx
@@ -34,20 +34,20 @@ export const setupSearchUseCase = (contentManagement: ContentManagementPluginSet
     sections: [
       getStartedSection,
       {
-        id: SECTIONS.DIFFERENT_SEARCH_TYPES,
-        order: 3000,
-        title: i18n.translate('home.searchOverview.differentSearchTypes.title', {
-          defaultMessage: 'Try out different search techniques',
+        id: SECTIONS.CONFIG_EVALUATE_SEARCH,
+        order: 2000,
+        title: i18n.translate('home.searchOverview.configEvaluate.title', {
+          defaultMessage: 'Configure and evaluate search',
         }),
         kind: 'card',
         grid: true,
         columns: 2,
       },
       {
-        id: SECTIONS.CONFIG_EVALUATE_SEARCH,
-        order: 2000,
-        title: i18n.translate('home.searchOverview.configEvaluate.title', {
-          defaultMessage: 'Configure and evaluate search',
+        id: SECTIONS.DIFFERENT_SEARCH_TYPES,
+        order: 3000,
+        title: i18n.translate('home.searchOverview.differentSearchTypes.title', {
+          defaultMessage: 'Try out different search techniques',
         }),
         kind: 'card',
         grid: true,
