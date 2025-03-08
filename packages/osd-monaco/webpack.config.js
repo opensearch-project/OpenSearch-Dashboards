094232760d8e3ecf63f8d7e8577108703d5b6fc5
Anan <ananzh@amazon.com>
2025-03-07 17:36:43 -0800

    fix icon
    Signed-off-by: Anan <ananzh@amazon.com>

--- packages/osd-monaco/webpack.config.js
+++ packages/osd-monaco/webpack.config.js
@@ -61,6 +61,19 @@ const createLangWorkerConfig = (lang) => ({
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      // Handle font files for codicons
+      {
+        test: /\.(woff|woff2|ttf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
 });
