d0e6ca50c94acf241eb464cd3f58b15c6b2f676b
Hailong Cui <ihailong@amazon.com>
2025-03-28 16:44:51 +0800

    make plugin setup/start order stable
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- src/core/server/plugins/plugins_system.ts
+++ src/core/server/plugins/plugins_system.ts
@@ -244,8 +244,11 @@ export class PluginsSystem {
     // topological ordering. If the cloned graph is _not_ empty at the end, we
     // know we were not able to topologically order the graph. We exclude optional
     // dependencies that are not present in the plugins graph.
+    const sortedPluginMap = new Map<PluginName, PluginWrapper>(
+      [...this.plugins].sort(([a], [b]) => a.localeCompare(b))
+    );
     const pluginsDependenciesGraph = new Map(
-      [...this.plugins.entries()].map(([pluginName, plugin]) => {
+      [...sortedPluginMap.entries()].map(([pluginName, plugin]) => {
         return [
           pluginName,
           new Set([
