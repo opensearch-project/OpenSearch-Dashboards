8e2d5ad68049c25c394a3d265b533fdf05568ce4
Joey Liu <jiyili@amazon.com>
2025-04-01 22:16:32 +0000

    add util function and test
    Signed-off-by: Joey Liu <jiyili@amazon.com>

--- src/plugins/query_enhancements/common/utils.test.ts
+++ src/plugins/query_enhancements/common/utils.test.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { throwFacetError } from './utils';
+import { isSearchQuery, throwFacetError } from './utils';
+import { Query } from 'src/plugins/data/common';
 
 describe('handleFacetError', () => {
   const error = new Error('mock-error');
@@ -24,5 +25,48 @@ describe('handleFacetError', () => {
       expect(err.name).toBe('400');
       expect(err.status).toBe('400');
     }
+  });
+});
+
+describe('isSearchQuery', () => {
+  it('should return false if query language is not PPL', () => {
+    const query: Query = {
+      language: 'lucene',
+      query: 'test',
+    };
+
+    expect(isSearchQuery(query)).toBe(false);
+  });
+
+  it('should return false if query is not string', () => {
+    const query: Query = {
+      language: 'PPL',
+      query: {
+        field: 'something',
+      },
+    };
+
+    expect(isSearchQuery(query)).toBe(false);
+  });
+
+  it('should return false if query is not using search command', () => {
+    const query: Query = {
+      language: 'PPL',
+      query: 'test',
+    };
+
+    expect(isSearchQuery(query)).toBe(false);
+  });
+
+  it('should return true if query is using search command', () => {
+    const query: Query = {
+      language: 'PPL',
+      query: 'source = test | stats count',
+    };
+
+    expect(isSearchQuery(query)).toBe(true);
+
+    query.query = 'search source = test | stats count';
+    expect(isSearchQuery(query)).toBe(true);
   });
 });
