7180a5eaed8bb5099f73dcca5f859d1e7194d70a
Zhenxing Shen <zxshen@amazon.com>
2025-04-09 11:32:12 +0800

    fix: close suggestions when user submits query
    Signed-off-by: Zhenxing Shen <zxshen@amazon.com>

--- src/plugins/query_enhancements/public/query_assist/components/query_assist_input.tsx
+++ src/plugins/query_enhancements/public/query_assist/components/query_assist_input.tsx
@@ -85,7 +85,11 @@ export const QueryAssistInput: React.FC<QueryAssistInputProps> = (props) => {
           disabled={props.isDisabled}
           onClick={() => setIsSuggestionsVisible(true)}
           onChange={(e) => setValue(e.target.value)}
-          onKeyDown={() => setIsSuggestionsVisible(true)}
+          onKeyDown={(e) => {
+            if (e.key !== 'Enter') {
+              setIsSuggestionsVisible(true);
+            }
+          }}
           placeholder={
             props.selectedIndex
               ? i18n.translate('queryEnhancements.queryAssist.input.placeholderWithIndex', {
