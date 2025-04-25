de64163b8e720471cf952180025b57e9c47a8b9e
Luna Liu <lunalzm@amazon.com>
2025-04-25 17:05:51 +0800

    update-test
    Signed-off-by: Luna Liu <lunalzm@amazon.com>

--- src/plugins/discover/public/plugin.test.ts
+++ src/plugins/discover/public/plugin.test.ts
@@ -11,14 +11,53 @@ import { opensearchDashboardsLegacyPluginMock } from '../../opensearch_dashboard
 import { urlForwardingPluginMock } from '../../url_forwarding/public/mocks';
 import { uiActionsPluginMock } from '../../ui_actions/public/mocks';
 import { visualizationsPluginMock } from '../../visualizations/public/mocks';
+import { createHashHistory } from 'history';
+
+jest.mock('history', () => ({
+  createHashHistory: jest.fn(() => ({
+    listen: jest.fn(),
+    push: jest.fn(),
+    replace: jest.fn(),
+    location: { pathname: '/', hash: '', search: '' },
+  })),
+}));
 
 describe('DiscoverPlugin', () => {
-  it('setup successfully', () => {
-    const setupMock = coreMock.createSetup();
+  const locationMock = {
+    hash: '#/',
+    pathname: '/',
+    search: '',
+    href: 'http://[IP_ADDRESS]/#/',
+    origin: 'http://[IP_ADDRESS]',
+    protocol: 'http:',
+    host: '[IP_ADDRESS]',
+    hostname: '[IP_ADDRESS]',
+    port: '',
+    assign: jest.fn(),
+    reload: jest.fn(),
+    replace: jest.fn(),
+    toString: jest.fn(),
+  };
+
+  Object.defineProperty(window, 'location', {
+    value: locationMock,
+    writable: true,
+  });
+
+  let plugin: DiscoverPlugin;
+  let setupMock: ReturnType<typeof coreMock.createSetup>;
+  let startMock: ReturnType<typeof coreMock.createStart>;
+
+  beforeEach(() => {
+    setupMock = coreMock.createSetup();
+    startMock = coreMock.createStart();
     const initializerContext = coreMock.createPluginInitializerContext();
-    const pluginInstance = new DiscoverPlugin(initializerContext);
+    plugin = new DiscoverPlugin(initializerContext);
+  });
+
+  it('setup successfully', () => {
     expect(() =>
-      pluginInstance.setup(setupMock, {
+      plugin.setup(setupMock, {
         data: dataPluginMock.createSetupContract(),
         embeddable: embeddablePluginMock.createSetupContract(),
         opensearchDashboardsLegacy: opensearchDashboardsLegacyPluginMock.createSetupContract(),
@@ -31,5 +70,51 @@ describe('DiscoverPlugin', () => {
       })
     ).not.toThrow();
     expect(setupMock.chrome.navGroup.addNavLinksToGroup).toBeCalledTimes(5);
+  });
+
+  it('should handle mount with data-explorer navigation', async () => {
+    const setupContract = plugin.setup(setupMock, {
+      data: dataPluginMock.createSetupContract(),
+      embeddable: embeddablePluginMock.createSetupContract(),
+      opensearchDashboardsLegacy: opensearchDashboardsLegacyPluginMock.createSetupContract(),
+      urlForwarding: urlForwardingPluginMock.createSetupContract(),
+      uiActions: uiActionsPluginMock.createSetupContract(),
+      visualizations: visualizationsPluginMock.createSetupContract(),
+      dataExplorer: {
+        registerView: jest.fn(),
+      },
+    });
+
+    const startContract = plugin.start(startMock, {
+      data: {
+        ...dataPluginMock.createStartContract(),
+        indexPatterns: {
+          ensureDefaultIndexPattern: jest.fn().mockResolvedValue(undefined),
+        },
+      },
+      embeddable: embeddablePluginMock.createStartContract(),
+      opensearchDashboardsLegacy: opensearchDashboardsLegacyPluginMock.createStartContract(),
+      urlForwarding: urlForwardingPluginMock.createStartContract(),
+      uiActions: uiActionsPluginMock.createStartContract(),
+      navigation: {} as any,
+      charts: {} as any,
+      inspector: {} as any,
+      visualizations: visualizationsPluginMock.createStartContract(),
+    });
+
+    const params = {
+      element: document.createElement('div'),
+      appBasePath: '',
+      onAppLeave: () => Promise.resolve(true),
+      setHeaderActionMenu: () => {},
+      history: createHashHistory(),
+    };
+
+    await setupMock.application.register.mock.calls[0][0].mount(params);
+
+    expect(startMock.application.navigateToApp).toHaveBeenCalledWith('data-explorer', {
+      replace: true,
+      path: '/discover/',
+    });
   });
 });
