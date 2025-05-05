e05b57498c9803cb55efe3e1d2918c71c0358e37
Jialiang Liang <jiallian@amazon.com>
2025-05-05 15:03:34 -0700

    production phase 11 - setup scss instead of inline style
    Signed-off-by: Jialiang Liang <jiallian@amazon.com>

--- src/plugins/dashboard/public/application/embeddable/grid/dashboard_direct_query_sync.tsx
+++ src/plugins/dashboard/public/application/embeddable/grid/dashboard_direct_query_sync.tsx
@@ -32,6 +32,7 @@ import React from 'react';
 import { EuiCallOut, EuiLink, EuiLoadingSpinner, EuiText } from '@elastic/eui';
 import { DirectQueryLoadingStatus } from '../../../../framework/types';
 import { EMR_STATES, intervalAsMinutes } from '../../utils/direct_query_sync/direct_query_sync';
+import './_dashboard_direct_query_sync.scss';
 
 export interface DashboardDirectQuerySyncProps {
   loadStatus: DirectQueryLoadingStatus;
@@ -48,26 +49,30 @@ export const DashboardDirectQuerySync: React.FC<DashboardDirectQuerySyncProps> =
 }) => {
   const state = EMR_STATES.get(loadStatus)!;
 
-  return state.terminal ? (
-    <EuiText size="s">
-      Data scheduled to sync every{' '}
-      {refreshInterval ? intervalAsMinutes(1000 * refreshInterval) : '--'}. Last sync:{' '}
-      {lastRefreshTime ? (
-        <>
-          {new Date(lastRefreshTime).toLocaleString()} (
-          {intervalAsMinutes(new Date().getTime() - lastRefreshTime)} ago)
-        </>
+  return (
+    <div className="dshDashboardGrid__syncBar" data-test-subj="dashboardDirectQuerySyncBar">
+      {state.terminal ? (
+        <EuiText size="s">
+          Data scheduled to sync every{' '}
+          {refreshInterval ? intervalAsMinutes(1000 * refreshInterval) : '--'}. Last sync:{' '}
+          {lastRefreshTime ? (
+            <>
+              {new Date(lastRefreshTime).toLocaleString()} (
+              {intervalAsMinutes(new Date().getTime() - lastRefreshTime)} ago)
+            </>
+          ) : (
+            '--'
+          )}
+          . &nbsp;&nbsp;
+          <EuiLink onClick={onSynchronize}>Sync data</EuiLink>
+        </EuiText>
       ) : (
-        '--'
+        <EuiCallOut size="s">
+          <EuiLoadingSpinner size="s" />
+          &nbsp;&nbsp;&nbsp;Data sync is in progress (<b>{state.ord}%</b> complete). The dashboard
+          will reload on completion.
+        </EuiCallOut>
       )}
-      . &nbsp;&nbsp;
-      <EuiLink onClick={onSynchronize}>Sync data</EuiLink>
-    </EuiText>
-  ) : (
-    <EuiCallOut size="s">
-      <EuiLoadingSpinner size="s" />
-      &nbsp;&nbsp;&nbsp;Data sync is in progress (<b>{state.ord}%</b> complete). The dashboard will
-      reload on completion.
-    </EuiCallOut>
+    </div>
   );
 };
