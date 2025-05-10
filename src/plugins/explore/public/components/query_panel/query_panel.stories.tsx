2d318f8a60fd7a84183b5bc9ad792abd3b9e4ffc
Ruchi Sharma <ruchi492@gmail.com>
2025-05-10 06:32:18 +0000

    add component flow
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>

---
+++ src/plugins/explore/public/components/query_panel/query_panel.stories.tsx
@@ -0,0 +1,16 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React from 'react';
+import { QueryPanel } from './index';
+
+export default {
+  component: QueryPanel,
+  title: 'src/plugins/explore/public/components/query_panel/index.tsx',
+};
+
+export function QueryEditor() {
+  return <QueryPanel />;
+}
