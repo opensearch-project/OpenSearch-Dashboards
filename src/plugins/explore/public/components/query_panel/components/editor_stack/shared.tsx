1a9b27bc0f97a22e8bd22d9a35e3407b765094ac
Ruchi Sharma <ruchi492@gmail.com>
2025-05-15 05:34:27 +0000

    add language type
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>

--- src/plugins/explore/public/components/query_panel/components/editor_stack/shared.tsx
+++ src/plugins/explore/public/components/query_panel/components/editor_stack/shared.tsx
@@ -2,3 +2,35 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
+
+export type LanguageType = 'natural-language' | 'key-value' | 'ppl';
+
+export const getEditorConfig = (languageType: LanguageType) => {
+  switch (languageType) {
+    case 'natural-language':
+      return {
+        languageId: 'natural-language',
+        ariaLabel: 'Type your natural language query here',
+        suggest: {
+          showWords: true,
+        },
+      };
+    case 'key-value':
+      return {
+        languageId: 'ppl',
+        ariaLabel: 'Type your key-value query here',
+        suggest: {
+          showWords: false,
+        },
+      };
+    case 'ppl':
+    default:
+      return {
+        languageId: 'ppl',
+        ariaLabel: 'Type your PPL query here',
+        suggest: {
+          showWords: false,
+        },
+      };
+  }
+};
