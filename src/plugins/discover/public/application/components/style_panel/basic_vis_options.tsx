701ff84460f88528112c76c63e110a369ef70650
abbyhu2000 <abigailhu2000@gmail.com>
2025-05-20 23:52:27 +0000

    with legend position working and updating correctly
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

--- src/plugins/discover/public/application/components/style_panel/basic_vis_options.tsx
+++ src/plugins/discover/public/application/components/style_panel/basic_vis_options.tsx
@@ -7,10 +7,11 @@ import { i18n } from '@osd/i18n';
 import React, { useState } from 'react';
 import { SelectOption, SwitchOption } from '../../../../../charts/public';
 import { getConfigCollections } from '../../../../../vis_type_vislib/public';
+import { LineChartStyleControls } from '../visualizations/line/line_vis_type';
+import { LineChartStyleControlsProps } from '../visualizations/line/line_vis_options';
 
-export const BasicVisOptions = (defaultStyle: any, onChange) => {
-  const [styles, setStyles] = useState(defaultStyle);
-  console.log('styles', styles);
+export const BasicVisOptions = ({ defaultStyles, onChange }: LineChartStyleControlsProps) => {
+  const [styles, setStyles] = useState<LineChartStyleControls>(defaultStyles);
   const updateStyle = (key: any, value: any) => {
     const newStyles = { ...styles, [key]: value };
     setStyles(newStyles);
@@ -25,16 +26,16 @@ export const BasicVisOptions = (defaultStyle: any, onChange) => {
         })}
         options={legendPositions}
         paramName="legendPosition"
-        value={styles.defaultStyle.legendPosition}
-        setValue={(_, value) => console.log('value', value)}
+        value={styles.legendPosition}
+        setValue={(_, value) => updateStyle('legendPosition', value)}
       />
       <SwitchOption
         label={i18n.translate('visBuilder.controls.vislibBasicOptions.showTooltipLabel', {
           defaultMessage: 'Show tooltip',
         })}
         paramName="addTooltip"
-        value={styles.defaultStyle.addTooltip}
-        setValue={(_, value) => console.log('value', value)}
+        value={styles.addTooltip}
+        setValue={(_, value) => updateStyle('addTooltip', value)}
       />
     </>
   );
