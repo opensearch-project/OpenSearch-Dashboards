8eb3011cefa3f0fab71ebabc5de62435ef7dcdc2
abbyhu2000 <abigailhu2000@gmail.com>
2025-05-23 23:05:30 +0000

    adding styling options based on vis_lib
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

---
+++ src/plugins/discover/public/application/components/visualizations/style_panel/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { Option } from './option';
+export { BasicVisOptions } from './basic_vis_options';
+export { ThresholdOptions } from './threshold_options';
+export { GridOptions } from './grid_options';
