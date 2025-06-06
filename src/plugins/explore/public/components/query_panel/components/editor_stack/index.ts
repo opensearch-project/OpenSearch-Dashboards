c1b894c5e25ab5b32422d2d84cd4419802f73925
Ruchi Sharma <ruchi492@gmail.com>
2025-06-06 23:37:38 +0000

    fix toolbar naming
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>

--- src/plugins/explore/public/components/query_panel/components/editor_stack/index.ts
+++ src/plugins/explore/public/components/query_panel/components/editor_stack/index.ts
@@ -7,5 +7,5 @@ export { EditorStack, EditorStackProps } from './editor_stack';
 export { PromptEditor, PromptEditorProps } from './prompt_editor';
 export { QueryEditor, QueryEditorProps } from './query_editor';
 export { ReusableEditor, ReusableEditorProps } from './resuable_editor';
-export { EditToobar } from './edit_toolbar';
+export { EditToolbar } from './edit_toolbar';
 export * from './shared';
