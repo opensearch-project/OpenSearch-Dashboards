5865bf17400b9a489ea360326c139f87c992edae
Joey Liu <55552896+Maosaic@users.noreply.github.com>
2025-06-17 15:14:45 -0700

    Consume tab registry service and related state in explore tabs (#9901)
    * Consume tab registry service and related state in explore tabs
    
    Signed-off-by: Joey Liu <jiyili@amazon.com>
    
    * Changeset file for PR #9901 created/updated
    
    * Address comment
    
    Signed-off-by: Joey Liu <jiyili@amazon.com>
    
    ---------
    
    Signed-off-by: Joey Liu <jiyili@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/explore/public/application/app.tsx
+++ src/plugins/explore/public/application/app.tsx
@@ -27,14 +27,11 @@ import { ResultStatus } from './utils/state_management/types';
 import { TopNav } from './legacy/discover/application/view_components/canvas/top_nav';
 import { DiscoverChartContainer } from './legacy/discover/application/view_components/canvas/discover_chart_container';
 import { QueryPanel } from './components/query_panel';
-import { TabBar } from './components/tab_bar';
-import { TabContent } from './components/tab_content';
 import { DiscoverPanel } from './legacy/discover/application/view_components/panel';
 import { HeaderDatasetSelector } from './components/header_dataset_selector';
 import { useInitialQueryExecution } from './utils/hooks/use_initial_query_execution';
 import { useUrlStateSync } from './utils/hooks/use_url_state_sync';
 import { useTimefilterSubscription } from './utils/hooks/use_timefilter_subscription';
-import { ExploreDataTable } from '../components/data_table/explore_data_table';
 import { ExploreTabs } from '../components/tabs/tabs';
 import { useHeaderVariants } from './utils/hooks/use_header_variants';
 import { NewExperienceBanner } from '../components/experience_banners/new_experience_banner';
@@ -66,27 +63,11 @@ export const ExploreApp: React.FC<{ setHeaderActionMenu?: (menuMount: any) => vo
 
     // Try all available cache keys to find one with results (same logic as TabContent)
     for (const cacheKey of executionCacheKeys) {
+      // why return first hit?
       const results = state.results[cacheKey];
       if (results) {
         const hits = results.hits?.hits || [];
         return hits;
-      }
-    }
-
-    return [];
-  });
-
-  const fieldSchema = useSelector((state: RootState) => {
-    const executionCacheKeys = state.ui?.executionCacheKeys || [];
-    if (executionCacheKeys.length === 0) {
-      return [];
-    }
-
-    // Try all available cache keys to find one with field schema
-    for (const cacheKey of executionCacheKeys) {
-      const results = state.results[cacheKey];
-      if (results && results.fieldSchema) {
-        return results.fieldSchema;
       }
     }
 
@@ -129,20 +110,6 @@ export const ExploreApp: React.FC<{ setHeaderActionMenu?: (menuMount: any) => vo
     status === ResultStatus.READY ||
     (status === ResultStatus.LOADING && !!rows?.length) ||
     (status === ResultStatus.ERROR && !!rows?.length);
-
-  const tabs = [
-    // TODO: Translate
-    {
-      id: 'explore_logs_tab',
-      name: 'Logs',
-      content: <ExploreDataTable rows={rows} />,
-    },
-    {
-      id: 'explore_visualization_tab',
-      name: 'Visualization',
-      content: <MemoizedVisualizationContainer rows={rows} fieldSchema={fieldSchema} />,
-    },
-  ];
 
   return (
     <EuiErrorBoundary>
@@ -238,7 +205,7 @@ export const ExploreApp: React.FC<{ setHeaderActionMenu?: (menuMount: any) => vo
                           </div>
                         )}
 
-                        <ExploreTabs tabs={tabs} />
+                        <ExploreTabs />
                       </EuiPanel>
                     </EuiPageBody>
                   </EuiResizablePanel>
@@ -251,5 +218,3 @@ export const ExploreApp: React.FC<{ setHeaderActionMenu?: (menuMount: any) => vo
     </EuiErrorBoundary>
   );
 };
-
-const MemoizedVisualizationContainer = React.memo(VisualizationContainer);
