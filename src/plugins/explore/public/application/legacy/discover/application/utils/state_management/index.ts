e74b176c6df6b7b55cfc272a0c3e44113b6d700d
Anan Zhuang <ananzh@amazon.com>
2025-06-19 15:14:08 -0700

    [Explore] Fix: PPL queries requiring two clicks to work correctly (#9940)
    The executeQueries thunk was called before queryStringManager was synchronized
    with the new query, causing the search service to use stale query data on first execution.
    Implemented Redux middleware (createQuerySyncMiddleware) that automatically syncs
    Redux query state with the global queryStringManager whenever query actions are dispatched.
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/plugins/explore/public/application/legacy/discover/application/utils/state_management/index.ts
+++ src/plugins/explore/public/application/legacy/discover/application/utils/state_management/index.ts
@@ -9,11 +9,6 @@ import {
   useSelector as useReduxSelector,
 } from 'react-redux';
 import { RootState } from '../../../../../utils/state_management/store';
-import { setDataset } from '../../../../../utils/state_management/slices/query_slice';
-
-// Legacy state management has been migrated to main Redux store
-// This file now just re-exports the main store functionality for compatibility
 
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
 export const useDispatch = useReduxDispatch;
-export const updateIndexPattern = setDataset;
