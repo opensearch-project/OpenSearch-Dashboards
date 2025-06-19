4cc619dd015549c09cba00d892f83b7a5b18b88f
Joshua Li <joshuali925@gmail.com>
2025-06-19 15:44:27 -0700

    [Explore] Support logs, traces, metrics flavor for explore plugin (#9902)
    * move app to logs page
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    * add traces flavor
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    * fix side nav
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    * add metrics flavor
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    * update unit tests
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    * Changeset file for PR #9902 created/updated
    
    * address comments
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    * fix tests
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    * fix tests
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    
    ---------
    
    Signed-off-by: Joshua Li <joshuali925@gmail.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/explore/public/application/index.tsx
+++ src/plugins/explore/public/application/index.tsx
@@ -10,9 +10,13 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { Store } from 'redux';
 import { AppMountParameters } from '../../../../core/public';
 import { ExploreServices } from '../types';
-import { ExploreApp } from './app';
+import { LogsPage } from './pages/logs';
 import { OpenSearchDashboardsContextProvider } from '../../../opensearch_dashboards_react/public';
 import { IndexPatternProvider } from './components/index_pattern_context';
+import { ExploreFlavor } from '../../common';
+import { TracesPage } from './pages/traces';
+import { MetricsPage } from './pages/metrics';
+import { NoFlavor } from './components/no_view';
 
 // Route component props interface
 interface ExploreRouteProps {
@@ -20,20 +24,35 @@ interface ExploreRouteProps {
   history: AppMountParameters['history'];
 }
 
-// Route components for different paths
-const ExploreMainRoute = (props: ExploreRouteProps & { setHeaderActionMenu?: any }) => (
-  <ExploreApp setHeaderActionMenu={props.setHeaderActionMenu} />
-);
+type ExploreComponentProps = ExploreRouteProps &
+  Partial<Pick<AppMountParameters, 'setHeaderActionMenu'>>;
+
+const renderExploreFlavor = (flavor: ExploreFlavor, props: ExploreComponentProps) => {
+  switch (flavor) {
+    case ExploreFlavor.Logs:
+      return <LogsPage setHeaderActionMenu={props.setHeaderActionMenu} />;
+    case ExploreFlavor.Traces:
+      return <TracesPage setHeaderActionMenu={props.setHeaderActionMenu} />;
+    case ExploreFlavor.Metrics:
+      return <MetricsPage setHeaderActionMenu={props.setHeaderActionMenu} />;
+    default:
+      // This code should never be reached at runtime, it exists to make the
+      // switch cases exhaustive
+      const invalidId: never = flavor;
+      return `Unexpected explore flavor id: ${invalidId}`;
+  }
+};
 
 // View route for saved searches
-const ViewRoute = (props: ExploreRouteProps & { setHeaderActionMenu?: any }) => (
-  <ExploreApp setHeaderActionMenu={props.setHeaderActionMenu} />
+const ViewRoute = (props: ExploreComponentProps) => (
+  <LogsPage setHeaderActionMenu={props.setHeaderActionMenu} />
 );
 
 export const renderApp = (
   { element, history, setHeaderActionMenu }: AppMountParameters,
   services: ExploreServices,
-  store: Store
+  store: Store,
+  flavor: ExploreFlavor
 ) => {
   // Create main route props
   const mainRouteProps = {
@@ -53,14 +72,8 @@ export const renderApp = (
                   <ViewRoute {...mainRouteProps} />
                 </Route>
 
-                {/* Main route */}
-                <Route path="/" exact>
-                  <ExploreMainRoute {...mainRouteProps} />
-                </Route>
-
-                {/* Default fallback */}
-                <Route>
-                  <ExploreMainRoute {...mainRouteProps} />
+                <Route path={[`/`]} exact={false}>
+                  {renderExploreFlavor(flavor, mainRouteProps)}
                 </Route>
               </Switch>
             </services.core.i18n.Context>
