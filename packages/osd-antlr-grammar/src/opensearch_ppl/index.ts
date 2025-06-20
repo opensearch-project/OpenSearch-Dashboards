b9a74285ee93af5cd6ec9b2eb5df28eb5d14795d
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2025-06-20 14:13:05 -0700

    [Explore] Add Syntax highlighting for PPL  (#9895)
    * Update webPack to include ANTLR as dependency
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Add PPL Worker Implementation
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Refactor ANTLR grammar into a separate Package
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Add test for ppl analyzer
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Fix AutoComplete in Query Panel Not triggerring
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Fix Unit Tests for PPL Analyzer
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    ---------
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>

---
+++ packages/osd-antlr-grammar/src/opensearch_ppl/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export { OpenSearchPPLLexer } from './.generated/OpenSearchPPLLexer';
+export { OpenSearchPPLParser } from './.generated/OpenSearchPPLParser';
+export { OpenSearchPPLParserVisitor } from './.generated/OpenSearchPPLParserVisitor';
