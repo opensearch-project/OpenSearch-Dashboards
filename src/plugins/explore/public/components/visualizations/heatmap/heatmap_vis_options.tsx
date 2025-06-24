e6647fbad141a825fbf2dac470f827cf61f19985
Qingyang(Abby) Hu <abigailhu2000@gmail.com>
2025-06-24 15:22:05 -0700

    [Explore Vis] Chart type switcher with adding other available bar chart type  (#9961)
    * remove section titles
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    * change panel style
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    * add chart type switcher
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    * update all chart types style panel
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    * add available bar charts for time based data
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    * Changeset file for PR #9961 created/updated
    
    * add unit tests
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    ---------
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/explore/public/components/visualizations/heatmap/heatmap_vis_options.tsx
+++ src/plugins/explore/public/components/visualizations/heatmap/heatmap_vis_options.tsx
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useEffect } from 'react';
-import { EuiTabbedContent, EuiTabbedContentTab } from '@elastic/eui';
+import React, { useEffect, useState } from 'react';
+import { EuiSplitPanel, EuiButtonEmpty } from '@elastic/eui';
 import { i18n } from '@osd/i18n';
 import { HeatmapChartStyleControls } from './heatmap_vis_config';
 import { GeneralVisOptions } from '../style_panel/general_vis_options';
@@ -17,6 +17,7 @@ import { inferAxesFromColumns } from './heatmap_chart_utils';
 import { AllAxesOptions } from '../style_panel/standard_axes_options';
 import { swapAxes } from '../utils/utils';
 import { StyleControlsProps } from '../utils/use_visualization_types';
+import { ChartTypeSwitcher } from '../style_panel/chart_type_switcher';
 
 export type HeatmapVisStyleControlsProps = StyleControlsProps<HeatmapChartStyleControls>;
 
@@ -26,7 +27,26 @@ export const HeatmapVisStyleControls: React.FC<HeatmapVisStyleControlsProps> = (
   numericalColumns = [],
   categoricalColumns = [],
   dateColumns = [],
+  availableChartTypes = [],
+  selectedChartType,
+  onChartTypeChange,
 }) => {
+  // State to track expanded/collapsed state of each panel
+  const [expandedPanels, setExpandedPanels] = useState({
+    general: false,
+    basic: false,
+    exclusive: false,
+    label: false,
+    axes: false,
+  });
+
+  const togglePanel = (panelId: string) => {
+    setExpandedPanels({
+      ...expandedPanels,
+      [panelId]: !expandedPanels[panelId as keyof typeof expandedPanels],
+    });
+  };
+
   const shouldShowType = numericalColumns.length === 3;
   const updateStyleOption = <K extends keyof HeatmapChartStyleControls>(
     key: K,
@@ -56,73 +76,122 @@ export const HeatmapVisStyleControls: React.FC<HeatmapVisStyleControlsProps> = (
     updateStyleOption('StandardAxes', updateAxes);
   };
 
-  const tabs: EuiTabbedContentTab[] = [
-    {
-      id: 'basic',
-      name: i18n.translate('explore.vis.heatmapChart.tabs.basic', {
-        defaultMessage: 'Basic',
-      }),
-      content: (
-        <GeneralVisOptions
-          addTooltip={styleOptions.addTooltip}
-          addLegend={styleOptions.addLegend}
-          legendPosition={styleOptions.legendPosition}
-          onAddTooltipChange={(addTooltip) => updateStyleOption('addTooltip', addTooltip)}
-          onAddLegendChange={(addLegend) => updateStyleOption('addLegend', addLegend)}
-          onLegendPositionChange={(legendPosition) =>
-            updateStyleOption('legendPosition', legendPosition)
-          }
-        />
-      ),
-    },
-    {
-      id: 'exclusive',
-      name: i18n.translate('explore.vis.heatmapChart.tabs.exclusive', {
-        defaultMessage: 'Exclusive',
-      }),
-      content: (
-        <HeatmapExclusiveVisOptions
-          styles={styleOptions.exclusive}
-          onChange={(exclusive) => updateStyleOption('exclusive', exclusive)}
-        />
-      ),
-    },
-    {
-      id: 'label',
-      name: i18n.translate('explore.vis.heatmapChart.tabs.label', {
-        defaultMessage: 'Label',
-      }),
-      content: (
-        <HeatmapLabelVisOptions
-          shouldShowType={shouldShowType}
-          styles={styleOptions.label}
-          onChange={(label) => updateStyleOption('label', label)}
-        />
-      ),
-    },
-    {
-      id: 'axes',
-      name: i18n.translate('explore.vis.heatmapChart.tabs.axes', {
-        defaultMessage: 'Axes',
-      }),
-      content: (
-        <AllAxesOptions
-          disableGrid={true}
-          standardAxes={styleOptions.StandardAxes}
-          onChangeSwitchAxes={handleSwitchAxes}
-          onStandardAxesChange={(standardAxes) => updateStyleOption('StandardAxes', standardAxes)}
-        />
-      ),
-    },
-  ];
-
   return (
-    <EuiTabbedContent
-      tabs={tabs}
-      initialSelectedTab={tabs[0]}
-      autoFocus="selected"
-      size="s"
-      expand={false}
-    />
+    <EuiSplitPanel.Outer>
+      <EuiSplitPanel.Inner paddingSize="s">
+        <EuiButtonEmpty
+          iconSide="left"
+          color="text"
+          iconType={expandedPanels.general ? 'arrowDown' : 'arrowRight'}
+          onClick={() => togglePanel('general')}
+          size="xs"
+          data-test-subj="heatmapVisGeneralButton"
+        >
+          {i18n.translate('explore.vis.heatmapChart.tabs.general', {
+            defaultMessage: 'General',
+          })}
+        </EuiButtonEmpty>
+        {expandedPanels.general && (
+          <ChartTypeSwitcher
+            availableChartTypes={availableChartTypes}
+            selectedChartType={selectedChartType}
+            onChartTypeChange={onChartTypeChange}
+          />
+        )}
+      </EuiSplitPanel.Inner>
+
+      <EuiSplitPanel.Inner paddingSize="s">
+        <EuiButtonEmpty
+          iconSide="left"
+          color="text"
+          iconType={expandedPanels.basic ? 'arrowDown' : 'arrowRight'}
+          onClick={() => togglePanel('basic')}
+          size="xs"
+          data-test-subj="heatmapVisBasicButton"
+        >
+          {i18n.translate('explore.vis.heatmapChart.tabs.basic', {
+            defaultMessage: 'Basic',
+          })}
+        </EuiButtonEmpty>
+        {expandedPanels.basic && (
+          <GeneralVisOptions
+            addTooltip={styleOptions.addTooltip}
+            addLegend={styleOptions.addLegend}
+            legendPosition={styleOptions.legendPosition}
+            onAddTooltipChange={(addTooltip) => updateStyleOption('addTooltip', addTooltip)}
+            onAddLegendChange={(addLegend) => updateStyleOption('addLegend', addLegend)}
+            onLegendPositionChange={(legendPosition) =>
+              updateStyleOption('legendPosition', legendPosition)
+            }
+          />
+        )}
+      </EuiSplitPanel.Inner>
+
+      <EuiSplitPanel.Inner paddingSize="s">
+        <EuiButtonEmpty
+          iconSide="left"
+          color="text"
+          iconType={expandedPanels.exclusive ? 'arrowDown' : 'arrowRight'}
+          onClick={() => togglePanel('exclusive')}
+          size="xs"
+          data-test-subj="heatmapVisExclusiveButton"
+        >
+          {i18n.translate('explore.vis.heatmapChart.tabs.exclusive', {
+            defaultMessage: 'Heatmap',
+          })}
+        </EuiButtonEmpty>
+        {expandedPanels.exclusive && (
+          <HeatmapExclusiveVisOptions
+            styles={styleOptions.exclusive}
+            onChange={(exclusive) => updateStyleOption('exclusive', exclusive)}
+          />
+        )}
+      </EuiSplitPanel.Inner>
+
+      <EuiSplitPanel.Inner paddingSize="s">
+        <EuiButtonEmpty
+          iconSide="left"
+          color="text"
+          iconType={expandedPanels.label ? 'arrowDown' : 'arrowRight'}
+          onClick={() => togglePanel('label')}
+          size="xs"
+          data-test-subj="heatmapVisLabelButton"
+        >
+          {i18n.translate('explore.vis.heatmapChart.tabs.label', {
+            defaultMessage: 'Label',
+          })}
+        </EuiButtonEmpty>
+        {expandedPanels.label && (
+          <HeatmapLabelVisOptions
+            shouldShowType={shouldShowType}
+            styles={styleOptions.label}
+            onChange={(label) => updateStyleOption('label', label)}
+          />
+        )}
+      </EuiSplitPanel.Inner>
+
+      <EuiSplitPanel.Inner paddingSize="s">
+        <EuiButtonEmpty
+          iconSide="left"
+          color="text"
+          iconType={expandedPanels.axes ? 'arrowDown' : 'arrowRight'}
+          onClick={() => togglePanel('axes')}
+          size="xs"
+          data-test-subj="heatmapVisAxesButton"
+        >
+          {i18n.translate('explore.vis.heatmapChart.tabs.axes', {
+            defaultMessage: 'Axes',
+          })}
+        </EuiButtonEmpty>
+        {expandedPanels.axes && (
+          <AllAxesOptions
+            disableGrid={true}
+            standardAxes={styleOptions.StandardAxes}
+            onChangeSwitchAxes={handleSwitchAxes}
+            onStandardAxesChange={(standardAxes) => updateStyleOption('StandardAxes', standardAxes)}
+          />
+        )}
+      </EuiSplitPanel.Inner>
+    </EuiSplitPanel.Outer>
   );
 };
