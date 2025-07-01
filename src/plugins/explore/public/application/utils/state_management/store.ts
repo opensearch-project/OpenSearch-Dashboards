d220129cdf6c4d03e85e13bbc9a7f37038573e42
Anan Zhuang <ananzh@amazon.com>
2025-06-30 17:25:43 -0700

    [Explore] Refactor Explore Redux state and improve persistence performance (#10013)

--- src/plugins/explore/public/application/utils/state_management/store.ts
+++ src/plugins/explore/public/application/utils/state_management/store.ts
@@ -4,10 +4,17 @@
  */
 
 import { configureStore, combineReducers, PreloadedState } from '@reduxjs/toolkit';
-import { isEqual } from 'lodash';
-import { queryReducer, uiReducer, resultsReducer, tabReducer, legacyReducer } from './slices';
-import { persistReduxState, loadReduxState } from './utils/redux_persistence';
+import {
+  queryReducer,
+  uiReducer,
+  resultsReducer,
+  tabReducer,
+  legacyReducer,
+  systemReducer,
+} from './slices';
+import { loadReduxState } from './utils/redux_persistence';
 import { createQuerySyncMiddleware } from './middleware/query_sync_middleware';
+import { createPersistenceMiddleware } from './middleware/persistence_middleware';
 import { ExploreServices } from '../../../types';
 
 const rootReducer = combineReducers({
@@ -16,6 +23,7 @@ const rootReducer = combineReducers({
   results: resultsReducer,
   tab: tabReducer,
   legacy: legacyReducer,
+  system: systemReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
@@ -31,7 +39,9 @@ export const configurePreloadedStore = (
     preloadedState,
     middleware: (getDefaultMiddleware) =>
       services
-        ? getDefaultMiddleware().concat(createQuerySyncMiddleware(services))
+        ? getDefaultMiddleware()
+            .concat(createPersistenceMiddleware(services))
+            .concat(createQuerySyncMiddleware(services))
         : getDefaultMiddleware(),
   });
 };
@@ -39,19 +49,5 @@ export const configurePreloadedStore = (
 export const getPreloadedStore = async (services: ExploreServices) => {
   const preloadedState = await loadReduxState(services);
   const store = configurePreloadedStore(preloadedState, services);
-
-  let previousState = store.getState();
-
-  const handleChange = () => {
-    const state = store.getState();
-    persistReduxState(state, services);
-
-    if (isEqual(state, previousState)) return;
-
-    previousState = state;
-  };
-
-  const unsubscribe = store.subscribe(handleChange);
-
-  return { store, unsubscribe };
+  return { store, unsubscribe: () => {} };
 };
