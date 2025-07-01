954a16c1893e1ff9ca61892c04826a200e1f56b0
Qingyang(Abby) Hu <abigailhu2000@gmail.com>
2025-07-01 01:35:26 -0700

    Update bar chart style panel (#10034)
    * refactor bar style panel
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    * adding paddings
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    * fix bugs
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>
    
    ---------
    
    Signed-off-by: abbyhu2000 <abigailhu2000@gmail.com>

--- src/plugins/explore/public/components/visualizations/bar/bar_vis_options.tsx
+++ src/plugins/explore/public/components/visualizations/bar/bar_vis_options.tsx
@@ -3,17 +3,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import React, { useState } from 'react';
-import { i18n } from '@osd/i18n';
-import { EuiSplitPanel, EuiButtonEmpty } from '@elastic/eui';
-import { GeneralVisOptions } from '../style_panel/general_vis_options';
+import React from 'react';
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { BarChartStyleControls } from './bar_vis_config';
-import { BarExclusiveVisOptions } from './bar_exclusive_vis_options';
-import { ThresholdOptions } from '../style_panel/threshold_options';
-import { GridOptionsPanel } from '../style_panel/grid_options';
-import { AxesOptions } from '../style_panel/axes_options';
 import { StyleControlsProps } from '../utils/use_visualization_types';
-import { ChartTypeSwitcher } from '../style_panel/chart_type_switcher';
+import { LegendOptionsPanel } from '../style_panel/legend/legend';
+import { ThresholdOptions } from '../style_panel/threshold/threshold';
+import { BarExclusiveVisOptions } from './bar_exclusive_vis_options';
+import { TooltipOptionsPanel } from '../style_panel/tooltip/tooltip';
+import { AxesOptions } from '../style_panel/axes/axes';
+import { GridOptionsPanel } from '../style_panel/grid/grid';
 
 export type BarVisStyleControlsProps = StyleControlsProps<BarChartStyleControls>;
 
@@ -27,23 +26,6 @@ export const BarVisStyleControls: React.FC<BarVisStyleControlsProps> = ({
   selectedChartType,
   onChartTypeChange,
 }) => {
-  // State to track expanded/collapsed state of each panel
-  const [expandedPanels, setExpandedPanels] = useState({
-    general: false,
-    basic: false,
-    exclusive: false,
-    threshold: false,
-    grid: false,
-    axes: false,
-  });
-
-  const togglePanel = (panelId: string) => {
-    setExpandedPanels({
-      ...expandedPanels,
-      [panelId]: !expandedPanels[panelId as keyof typeof expandedPanels],
-    });
-  };
-
   const updateStyleOption = <K extends keyof BarChartStyleControls>(
     key: K,
     value: BarChartStyleControls[K]
@@ -56,159 +38,80 @@ export const BarVisStyleControls: React.FC<BarVisStyleControlsProps> = ({
     numericalColumns.length === 1 && categoricalColumns.length === 1 && dateColumns.length === 0;
 
   return (
-    <EuiSplitPanel.Outer>
-      <EuiSplitPanel.Inner paddingSize="s">
-        <EuiButtonEmpty
-          iconSide="left"
-          color="text"
-          iconType={expandedPanels.general ? 'arrowDown' : 'arrowRight'}
-          onClick={() => togglePanel('general')}
-          size="xs"
-          data-test-subj="barVisGeneralButton"
-        >
-          {i18n.translate('explore.vis.barChart.tabs.general', {
-            defaultMessage: 'General',
-          })}
-        </EuiButtonEmpty>
-        {expandedPanels.general && (
-          <ChartTypeSwitcher
-            availableChartTypes={availableChartTypes}
-            selectedChartType={selectedChartType}
-            onChartTypeChange={onChartTypeChange}
-          />
-        )}
-      </EuiSplitPanel.Inner>
-
-      <EuiSplitPanel.Inner paddingSize="s">
-        <EuiButtonEmpty
-          iconSide="left"
-          color="text"
-          iconType={expandedPanels.basic ? 'arrowDown' : 'arrowRight'}
-          onClick={() => togglePanel('basic')}
-          size="xs"
-          data-test-subj="barVisBasicButton"
-        >
-          {i18n.translate('explore.vis.barChart.tabs.basic', {
-            defaultMessage: 'Basic',
-          })}
-        </EuiButtonEmpty>
-        {expandedPanels.basic && (
-          <GeneralVisOptions
-            shouldShowLegend={!notShowLegend}
-            addTooltip={styleOptions.addTooltip}
-            addLegend={styleOptions.addLegend}
-            legendPosition={styleOptions.legendPosition}
-            onAddTooltipChange={(addTooltip) => updateStyleOption('addTooltip', addTooltip)}
-            onAddLegendChange={(addLegend) => updateStyleOption('addLegend', addLegend)}
-            onLegendPositionChange={(legendPosition) =>
-              updateStyleOption('legendPosition', legendPosition)
+    <EuiFlexGroup direction="column" gutterSize="none">
+      <EuiFlexItem grow={false}>
+        <LegendOptionsPanel
+          shouldShowLegend={!notShowLegend}
+          legendOptions={{
+            show: styleOptions.addLegend,
+            position: styleOptions.legendPosition,
+          }}
+          onLegendOptionsChange={(legendOptions) => {
+            if (legendOptions.show !== undefined) {
+              updateStyleOption('addLegend', legendOptions.show);
             }
-          />
-        )}
-      </EuiSplitPanel.Inner>
-
-      <EuiSplitPanel.Inner paddingSize="s">
-        <EuiButtonEmpty
-          iconSide="left"
-          color="text"
-          iconType={expandedPanels.exclusive ? 'arrowDown' : 'arrowRight'}
-          onClick={() => togglePanel('exclusive')}
-          size="xs"
-          data-test-subj="barVisExclusiveButton"
-        >
-          {i18n.translate('explore.vis.barChart.tabs.exclusive', {
-            defaultMessage: 'Bar',
-          })}
-        </EuiButtonEmpty>
-        {expandedPanels.exclusive && (
-          <BarExclusiveVisOptions
-            barWidth={styleOptions.barWidth}
-            barPadding={styleOptions.barPadding}
-            showBarBorder={styleOptions.showBarBorder}
-            barBorderWidth={styleOptions.barBorderWidth}
-            barBorderColor={styleOptions.barBorderColor}
-            onBarWidthChange={(barWidth) => updateStyleOption('barWidth', barWidth)}
-            onBarPaddingChange={(barPadding) => updateStyleOption('barPadding', barPadding)}
-            onShowBarBorderChange={(showBarBorder) =>
-              updateStyleOption('showBarBorder', showBarBorder)
+            if (legendOptions.position !== undefined) {
+              updateStyleOption('legendPosition', legendOptions.position);
             }
-            onBarBorderWidthChange={(barBorderWidth) =>
-              updateStyleOption('barBorderWidth', barBorderWidth)
-            }
-            onBarBorderColorChange={(barBorderColor) =>
-              updateStyleOption('barBorderColor', barBorderColor)
-            }
-          />
-        )}
-      </EuiSplitPanel.Inner>
-
-      <EuiSplitPanel.Inner paddingSize="s">
-        <EuiButtonEmpty
-          iconSide="left"
-          color="text"
-          iconType={expandedPanels.threshold ? 'arrowDown' : 'arrowRight'}
-          onClick={() => togglePanel('threshold')}
-          size="xs"
-          data-test-subj="barVisThresholdButton"
-        >
-          {i18n.translate('explore.vis.barChart.tabs.threshold', {
-            defaultMessage: 'Threshold',
-          })}
-        </EuiButtonEmpty>
-        {expandedPanels.threshold && (
-          <ThresholdOptions
-            thresholdLine={styleOptions.thresholdLine}
-            onThresholdChange={(thresholdLine) => updateStyleOption('thresholdLine', thresholdLine)}
-          />
-        )}
-      </EuiSplitPanel.Inner>
-
-      <EuiSplitPanel.Inner paddingSize="s">
-        <EuiButtonEmpty
-          iconSide="left"
-          color="text"
-          iconType={expandedPanels.grid ? 'arrowDown' : 'arrowRight'}
-          onClick={() => togglePanel('grid')}
-          size="xs"
-          data-test-subj="barVisGridButton"
-        >
-          {i18n.translate('explore.vis.barChart.tabs.grid', {
-            defaultMessage: 'Grid',
-          })}
-        </EuiButtonEmpty>
-        {expandedPanels.grid && (
-          <GridOptionsPanel
-            grid={styleOptions.grid}
-            onGridChange={(grid) => updateStyleOption('grid', grid)}
-          />
-        )}
-      </EuiSplitPanel.Inner>
-
-      <EuiSplitPanel.Inner paddingSize="s">
-        <EuiButtonEmpty
-          iconSide="left"
-          color="text"
-          iconType={expandedPanels.axes ? 'arrowDown' : 'arrowRight'}
-          onClick={() => togglePanel('axes')}
-          size="xs"
-          data-test-subj="barVisAxesButton"
-        >
-          {i18n.translate('explore.vis.barChart.tabs.axes', {
-            defaultMessage: 'Axes',
-          })}
-        </EuiButtonEmpty>
-        {expandedPanels.axes && (
-          <AxesOptions
-            categoryAxes={styleOptions.categoryAxes}
-            valueAxes={styleOptions.valueAxes}
-            onCategoryAxesChange={(categoryAxes) => updateStyleOption('categoryAxes', categoryAxes)}
-            onValueAxesChange={(valueAxes) => updateStyleOption('valueAxes', valueAxes)}
-            numericalColumns={numericalColumns}
-            categoricalColumns={categoricalColumns}
-            dateColumns={dateColumns}
-          />
-        )}
-      </EuiSplitPanel.Inner>
-    </EuiSplitPanel.Outer>
+          }}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <ThresholdOptions
+          thresholdLines={styleOptions.thresholdLines}
+          onThresholdLinesChange={(thresholdLines) =>
+            updateStyleOption('thresholdLines', thresholdLines)
+          }
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <TooltipOptionsPanel
+          tooltipOptions={styleOptions.tooltipOptions}
+          onTooltipOptionsChange={(tooltipOptions) =>
+            updateStyleOption('tooltipOptions', {
+              ...styleOptions.tooltipOptions,
+              ...tooltipOptions,
+            })
+          }
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <AxesOptions
+          categoryAxes={styleOptions.categoryAxes}
+          valueAxes={styleOptions.valueAxes}
+          onCategoryAxesChange={(categoryAxes) => updateStyleOption('categoryAxes', categoryAxes)}
+          onValueAxesChange={(valueAxes) => updateStyleOption('valueAxes', valueAxes)}
+          numericalColumns={numericalColumns}
+          categoricalColumns={categoricalColumns}
+          dateColumns={dateColumns}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <GridOptionsPanel
+          grid={styleOptions.grid}
+          onGridChange={(grid) => updateStyleOption('grid', grid)}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <BarExclusiveVisOptions
+          barWidth={styleOptions.barWidth}
+          barPadding={styleOptions.barPadding}
+          showBarBorder={styleOptions.showBarBorder}
+          barBorderWidth={styleOptions.barBorderWidth}
+          barBorderColor={styleOptions.barBorderColor}
+          onBarWidthChange={(barWidth) => updateStyleOption('barWidth', barWidth)}
+          onBarPaddingChange={(barPadding) => updateStyleOption('barPadding', barPadding)}
+          onShowBarBorderChange={(showBarBorder) =>
+            updateStyleOption('showBarBorder', showBarBorder)
+          }
+          onBarBorderWidthChange={(barBorderWidth) =>
+            updateStyleOption('barBorderWidth', barBorderWidth)
+          }
+          onBarBorderColorChange={(barBorderColor) =>
+            updateStyleOption('barBorderColor', barBorderColor)
+          }
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 };
