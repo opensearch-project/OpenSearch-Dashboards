86b98802839ed2204a442e58ab504a12a47662d4
Sean Li <lnse@amazon.com>
2025-07-01 17:44:30 -0700

    [Explore] Add execute dispatch to histogram and quick select (#10024)
    * Add execute dispatch to histogram and quick select
    
    Signed-off-by: Sean Li <lnse@amazon.com>
    
    * Updating quick selection logic
    
    Signed-off-by: Sean Li <lnse@amazon.com>
    
    * Fix props for onClickRecentQuery
    
    Signed-off-by: Sean Li <lnse@amazon.com>
    
    ---------
    
    Signed-off-by: Sean Li <lnse@amazon.com>

--- src/plugins/explore/public/components/query_panel/query_panel.tsx
+++ src/plugins/explore/public/components/query_panel/query_panel.tsx
@@ -7,7 +7,7 @@ import './index.scss';
 import React, { useState, useMemo, useCallback } from 'react';
 import { monaco } from '@osd/monaco';
 import { useDispatch, useSelector } from 'react-redux';
-import { EuiPanel } from '@elastic/eui';
+import { EuiPanel, OnTimeChangeProps } from '@elastic/eui';
 import { QueryPanelLayout } from './layout';
 import { ExploreServices } from '../../types';
 import { IndexPattern } from '../../../../data/common/index_patterns';
@@ -71,15 +71,23 @@ const QueryPanel: React.FC<QueryPanelProps> = ({ services, indexPattern }) => {
 
   // Handle time range changes
   const handleTimeChange = useCallback(
-    ({ start, end }: { start: string; end: string }) => {
+    ({ start, end, isInvalid, isQuickSelection }: OnTimeChangeProps) => {
       const newTimeRange = { from: start, to: end };
 
       // Update timefilter - this will trigger re-render automatically
       if (timefilter) {
         timefilter.setTime(newTimeRange);
       }
+
+      if (isQuickSelection) {
+        // EXPLICIT cache clear - same pattern as other triggers
+        dispatch(clearResults());
+
+        // Execute queries - interval will be picked up from Redux state
+        dispatch(executeQueries({ services }));
+      }
     },
-    [timefilter]
+    [timefilter, dispatch, services]
   );
 
   const handleRefreshChange = useCallback(
@@ -276,7 +284,13 @@ const QueryPanel: React.FC<QueryPanelProps> = ({ services, indexPattern }) => {
     setIsRecentQueryVisible(false);
     handlePromptChange(updatedQuery);
     handleRun(updatedQuery);
-    if (timeRange) handleTimeChange({ start: timeRange.from, end: timeRange.to });
+    if (timeRange)
+      handleTimeChange({
+        start: timeRange.from,
+        end: timeRange.to,
+        isInvalid: false,
+        isQuickSelection: true,
+      });
   };
 
   const handleShowFieldsToggle = (showField: boolean) => {
