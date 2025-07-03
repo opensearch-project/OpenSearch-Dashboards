729e07248c0df2d91f2dc0e365aea10f14987cc4
Anan Zhuang <ananzh@amazon.com>
2025-07-03 15:07:37 -0700

    [Explore] Fix saved queries (#10059)
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/plugins/explore/public/components/query_panel/components/footer/save_query.tsx
+++ src/plugins/explore/public/components/query_panel/components/footer/save_query.tsx
@@ -2,100 +2,67 @@
  * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-import React, { useCallback, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback, useState, useEffect } from 'react';
 import { i18n } from '@osd/i18n';
 import { cloneDeep } from 'lodash';
 import { EuiPopover, EuiButtonEmpty, EuiIcon } from '@elastic/eui';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   SavedQueryManagementComponent,
-  SaveQueryForm,
   SavedQueryMeta,
   SavedQuery,
-  useSavedQuery,
   TimefilterContract,
 } from '../../../../../../data/public';
-import { selectSavedQuery } from '../../../../application/utils/state_management/selectors';
 
 import { ExploreServices } from '../../../../types';
 import { Query } from '../../types';
+import { RootState } from '../../../../application/utils/state_management/store';
+import {
+  setSavedQuery,
+  setQueryState,
+  clearResults,
+} from '../../../../application/utils/state_management/slices';
+import { executeQueries } from '../../../../application/utils/state_management/actions/query_actions';
 
 export const SaveQueryButton: React.FC<{
   services: ExploreServices;
   showDatePicker: boolean;
   timeFilter: TimefilterContract;
   query: Query;
-  onClearQuery: () => void;
-  onSavedQuery: (newSavedQueryId: string | undefined) => void;
-  onLoadSavedQuery: (savedQuery: SavedQuery) => void;
-}> = ({
-  services,
-  showDatePicker,
-  timeFilter,
-  query,
-  onClearQuery,
-  onSavedQuery,
-  onLoadSavedQuery,
-}) => {
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-
-  const savedQueryId = useSelector(selectSavedQuery);
-
+  onQueryExecute: () => void;
+  onQueryEditorUpdate: (query: Query) => void;
+}> = ({ services, showDatePicker, timeFilter, query, onQueryExecute, onQueryEditorUpdate }) => {
   const savedQueryService = services.data.query.savedQueries;
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  // Get current saved query ID from Redux state
+  const currentSavedQueryId = useSelector((state: RootState) => state.legacy.savedQuery);
+
+  // Get the actual saved query object if we have an ID
+  const [currentSavedQuery, setCurrentSavedQuery] = useState<SavedQuery | undefined>();
+
+  // Load saved query when ID changes
+  useEffect(() => {
+    if (currentSavedQueryId) {
+      savedQueryService
+        .getSavedQuery(currentSavedQueryId)
+        .then(setCurrentSavedQuery)
+        .catch(() => {
+          // If saved query doesn't exist, clear the ID
+          dispatch(setSavedQuery(undefined));
+          setCurrentSavedQuery(undefined);
+        });
+    } else {
+      setCurrentSavedQuery(undefined);
+    }
+  }, [currentSavedQueryId, savedQueryService, dispatch]);
 
   const onButtonClick = () => setIsPopoverOpen(!isPopoverOpen);
   const closePopover = () => setIsPopoverOpen(false);
 
-  const [isSaveQueryFormOpen, setIsSaveQueryFormOpen] = React.useState(false);
-  const [saveAsNew, setSaveAsNew] = React.useState(false);
-
-  const { savedQuery: loadedSavedQuery, setSavedQuery, clearSavedQuery } = useSavedQuery({
-    queryService: services.data.query,
-    savedQueryId,
-    notifications: services.notifications,
-  });
-
-  // Handlers for Saved Query UI
-  const handleInitiateSave = () => {
-    setSaveAsNew(false);
-    setIsSaveQueryFormOpen(true);
-  };
-  const handleInitiateSaveAsNew = () => {
-    setSaveAsNew(true);
-    setIsSaveQueryFormOpen(true);
-  };
-
-  const handleLoadSavedQuery = useCallback(
-    (savedQuery) => {
-      setSavedQuery(savedQuery);
-      if (savedQuery?.attributes?.query) {
-        onLoadSavedQuery(savedQuery);
-      }
-      if (savedQuery?.attributes?.timefilter && timeFilter) {
-        timeFilter.setTime({
-          from: savedQuery.attributes.timefilter.from,
-          to: savedQuery.attributes.timefilter.to,
-        });
-        if (typeof timeFilter.setRefreshInterval === 'function') {
-          timeFilter.setRefreshInterval(savedQuery.attributes.timefilter.refreshInterval);
-        }
-      }
-    },
-    [onLoadSavedQuery, setSavedQuery, timeFilter]
-  );
-
-  const handleClearSavedQuery = useCallback(() => {
-    clearSavedQuery();
-    onClearQuery();
-    if (timeFilter) {
-      timeFilter.setTime({ from: 'now-15m', to: 'now' });
-      if (typeof timeFilter.setRefreshInterval === 'function') {
-        timeFilter.setRefreshInterval({ value: 0, pause: true });
-      }
-    }
-  }, [clearSavedQuery, onClearQuery, timeFilter]);
-
-  const handleSaveQuery = async (meta: SavedQueryMeta, saveAsNewParam?: boolean) => {
+  // Handle save query with Redux state management
+  const handleSaveQuery = async (meta: SavedQueryMeta, saveAsNew?: boolean) => {
     try {
       if (!query) return;
       const clonedQuery = cloneDeep(query);
@@ -125,19 +92,22 @@ export const SaveQueryButton: React.FC<{
         }
       }
 
-      // Overwrite if editing, not if saving as new
-      const overwrite = !(saveAsNewParam ?? saveAsNew);
-      const savedQuery = await savedQueryService.saveQuery(attributes, { overwrite });
-      services.notifications.toasts.addSuccess(
-        `Your query "${savedQuery.attributes.title}" was saved`
-      );
+      // Save query with overwrite option based on saveAsNew flag
+      const savedQuery = await savedQueryService.saveQuery(attributes, {
+        overwrite: !saveAsNew && !!currentSavedQueryId,
+      });
 
-      setIsSaveQueryFormOpen(false);
-      setSavedQuery(savedQuery);
-      onSavedQuery(savedQuery.id);
+      // Update Redux state with new saved query ID
+      dispatch(setSavedQuery(savedQuery.id));
+
+      services.notifications.toasts.addSuccess(`Your query "${attributes.title}" was saved`);
+
+      // Auto-close panel and execute current query
+      setIsPopoverOpen(false);
+      onQueryExecute();
     } catch (error) {
       services.notifications.toasts.addDanger(
-        i18n.translate('data.search_bar.save_query.failedToSaveQuery', {
+        i18n.translate('explore.queryPanel.saveQuery.failedToSaveQuery', {
           defaultMessage: 'An error occured while saving your query{errorMessage}',
           values: { errorMessage: error.message ? `: ${error.message}` : '' },
         })
@@ -146,13 +116,38 @@ export const SaveQueryButton: React.FC<{
     }
   };
 
-  /*
-   * This Function is here to show the toggle in saved query form
-   * in case you the date range (from/to)
-   */
-  const shouldRenderTimeFilterInSavedQueryForm = () => {
-    return showDatePicker || (!showDatePicker && timeFilter.getTime() !== undefined);
-  };
+  const handleLoadSavedQuery = useCallback(
+    (savedQuery: SavedQuery) => {
+      // 1. Update Redux state with saved query ID
+      dispatch(setSavedQuery(savedQuery.id));
+      dispatch(setQueryState(savedQuery.attributes.query));
+
+      // 2. Update Monaco editor via callback
+      onQueryEditorUpdate(savedQuery.attributes.query);
+
+      // 3. Update timefilter if present
+      if (savedQuery.attributes.timefilter && timeFilter) {
+        timeFilter.setTime({
+          from: savedQuery.attributes.timefilter.from,
+          to: savedQuery.attributes.timefilter.to,
+        });
+        if (typeof timeFilter.setRefreshInterval === 'function') {
+          timeFilter.setRefreshInterval(savedQuery.attributes.timefilter.refreshInterval);
+        }
+      }
+
+      // 4. Auto-close panel and execute
+      setIsPopoverOpen(false);
+      dispatch(clearResults());
+      dispatch(executeQueries({ services }));
+    },
+    [dispatch, onQueryEditorUpdate, timeFilter, services]
+  );
+
+  const handleClearSavedQuery = useCallback(() => {
+    dispatch(setSavedQuery(undefined));
+    setIsPopoverOpen(false);
+  }, [dispatch]);
 
   return (
     <>
@@ -165,7 +160,7 @@ export const SaveQueryButton: React.FC<{
             data-test-subj="queryPanelFootersaveQueryButton"
           >
             {i18n.translate('explore.queryPanel.saveQueryButton.savedQueries', {
-              defaultMessage: 'Saved Queries',
+              defaultMessage: 'Saved queries',
             })}
             <EuiIcon type="arrowDown" className="queryPanel__footer__saveQueryButtonIcon" />
           </EuiButtonEmpty>
@@ -176,30 +171,17 @@ export const SaveQueryButton: React.FC<{
       >
         <SavedQueryManagementComponent
           savedQueryService={savedQueryService}
-          loadedSavedQuery={loadedSavedQuery}
-          onInitiateSave={handleInitiateSave}
-          onInitiateSaveAsNew={handleInitiateSaveAsNew}
+          loadedSavedQuery={currentSavedQuery}
+          onInitiateSave={() => {}}
+          onInitiateSaveAsNew={() => {}}
           onLoad={handleLoadSavedQuery}
           onClearSavedQuery={handleClearSavedQuery}
-          closeMenuPopover={() => {}}
-          showSaveQuery={true}
+          closeMenuPopover={() => setIsPopoverOpen(false)}
+          showSaveQuery={!!services.capabilities?.explore?.saveQuery}
           saveQuery={handleSaveQuery}
-          useNewSavedQueryUI={false}
+          useNewSavedQueryUI={true}
         />
       </EuiPopover>
-      {isSaveQueryFormOpen && (
-        <SaveQueryForm
-          formUiType="Modal"
-          savedQuery={saveAsNew ? undefined : loadedSavedQuery?.attributes}
-          savedQueryService={savedQueryService}
-          onSave={handleSaveQuery}
-          onClose={() => setIsSaveQueryFormOpen(false)}
-          saveAsNew={saveAsNew}
-          setSaveAsNew={setSaveAsNew}
-          showFilterOption={false}
-          showTimeFilterOption={shouldRenderTimeFilterInSavedQueryForm()}
-        />
-      )}
     </>
   );
 };
