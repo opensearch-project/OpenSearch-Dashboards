5fa09dc603ea2f394303131236a9c9f3fb5ee8b5
Anan Zhuang <ananzh@amazon.com>
2025-07-03 13:37:20 -0700

    [Explore] Allow switch dataset to update recent query (#10064)
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

---
+++ src/plugins/explore/public/application/utils/state_management/__mocks__/services.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ExploreServices } from '../../../../types';
+
+/**
+ * Creates a mock ExploreServices object for testing
+ */
+export const createMockExploreServices = (
+  overrides: Partial<ExploreServices> = {}
+): ExploreServices =>
+  ({
+    data: {
+      query: {
+        queryString: {
+          getQuery: jest.fn().mockReturnValue({ query: '', language: 'sql' }),
+          setQuery: jest.fn(),
+          addToQueryHistory: jest.fn(),
+        },
+        timefilter: {
+          timefilter: {
+            getTime: jest.fn().mockReturnValue({ from: 'now-15m', to: 'now' }),
+          },
+        },
+      },
+      search: {
+        tabifyAggResponse: jest.fn(),
+      },
+    },
+    ...overrides,
+  } as ExploreServices);
