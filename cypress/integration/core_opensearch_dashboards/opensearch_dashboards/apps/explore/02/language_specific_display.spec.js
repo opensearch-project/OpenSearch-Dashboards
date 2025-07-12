041af83469cd702707e3a9fcaf746124b9740238
Sean Li <lnse@amazon.com>
2025-07-11 17:45:53 -0700

    Fixing language specific display test
    Signed-off-by: Sean Li <lnse@amazon.com>

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/02/language_specific_display.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/02/language_specific_display.spec.js
@@ -50,7 +50,7 @@ export const runDisplayTests = () => {
       it(`should correctly display all UI components for ${config.testName}`, () => {
         cy.osd.navigateToWorkSpaceSpecificPage({
           workspaceName,
-          page: 'explore',
+          page: 'explore/logs',
           isEnhancement: true,
         });
 
@@ -60,36 +60,22 @@ export const runDisplayTests = () => {
 
         // testing the query editor
         if (config.multilineQuery) {
-          cy.getElementByTestId('exploreReusableEditor').should('be.visible');
+          cy.getElementByTestId('exploreReusableEditor-top').should('be.visible');
           cy.getElementByTestId('queryResultCompleteMsg').contains(/Completed in [0-9]+/);
-          cy.getElementByTestId('queryEditorFooterToggleRecentQueriesButton').click();
+          cy.getElementByTestId('exploreRecentQueriesButton').click();
           cy.getElementByTestId('recentQueryTable').should('be.visible');
-          cy.getElementByTestId('queryEditorFooterToggleRecentQueriesButton').click();
-
-          if (config.language === QueryLanguages.SQL.name) {
-            cy.getElementByTestId('exploreReusableEditor').contains('SELECT');
-            cy.getElementByTestId('exploreReusableEditor').contains('FROM');
-            cy.getElementByTestId('exploreReusableEditor').contains('LIMIT');
-          } else if (config.language === QueryLanguages.PPL.name) {
-            cy.getElementByTestId('exploreReusableEditor').contains('source');
-          }
-
-          cy.getElementByTestId('osdQueryEditorLanguageToggle').click();
-          cy.getElementByTestId('osdQueryEditor__singleLine').should('be.visible');
-          cy.getElementByTestId('exploreReusableEditor').should('not.exist');
-          cy.getElementByTestId('osdQueryEditorLanguageToggle').click();
-          cy.getElementByTestId('exploreReusableEditor').should('be.visible');
+          cy.getElementByTestId('exploreRecentQueriesButton').click();
         }
 
         // testing the datepicker
         if (config.datepicker) {
-          cy.getElementByTestId('superDatePickerstartDatePopoverButton').should('be.visible');
+          cy.getElementByTestId('superDatePickerToggleQuickMenuButton').should('be.visible');
         }
 
         // testing the hit count and histogram
         if (config.histogram) {
           cy.getElementByTestId('discoverQueryHits').should('be.visible');
-          cy.getElementByTestId('dscTimechart').should('be.visible');
+          cy.getElementByTestId('discoverChart').should('be.visible');
         }
 
         // testing whether sort appears or not
@@ -107,35 +93,6 @@ export const runDisplayTests = () => {
         cy.getElementByTestId('queryPanelFooterSaveQueryButton').click();
         cy.getElementByTestId('saved-query-management-popover').should('be.visible');
         cy.getElementByTestId('queryPanelFooterSaveQueryButton').click();
-
-        // testing the filter
-        if (config.filters) {
-          cy.getElementByTestId('showFilterActions').should('be.visible');
-          cy.submitFilterFromDropDown('category', 'is', 'Application', true);
-          cy.getElementByTestId(
-            'filter filter-enabled filter-key-category filter-value-Application filter-unpinned '
-          ).should('be.visible');
-          cy.getElementByTestId('showFilterActions').click();
-          cy.getElementByTestId('pinAllFilters').click();
-          cy.getElementByTestId(
-            'filter filter-enabled filter-key-category filter-value-Application filter-pinned '
-          ).should('be.visible');
-          cy.getElementByTestId('globalFilterBar').within(() => {
-            cy.get('button[title="Delete"]').click();
-          });
-          cy.getElementByTestId('globalFilterGroupFilterPrefix').should('not.exist');
-        }
-
-        // testing single/surrounding doc
-        if (config.expandedDocument) {
-          cy.getElementByTestId('docTableExpandToggleColumn')
-            .first()
-            .within(() => {
-              cy.getElementByTestId('docTableExpandToggleColumn').click();
-            });
-          cy.get('a').contains('View surrounding documents').should('be.visible');
-          cy.get('a').contains('View single document').should('be.visible');
-        }
       });
     });
   });
