fea0370651f3d60da60bb0c9ab033cc6d053d973
SuZhou-Joe <suzhou@amazon.com>
2025-07-13 17:58:29 +0800

    feat: support shrinkable footer icon
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

---
+++ src/core/public/chrome/ui/header/left_bottom_action_button.tsx
@@ -0,0 +1,56 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  EuiButtonEmpty,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiIconProps,
+  EuiToolTip,
+} from '@elastic/eui';
+import { CoreStart } from 'opensearch-dashboards/public';
+import React from 'react';
+import { useObservable } from 'react-use';
+import './left_bottom_action_button.scss';
+
+interface LeftBottomActionButtonProps {
+  chrome: CoreStart['chrome'];
+  iconType?: EuiIconProps['type'];
+  icon?: React.ReactNode;
+  onClick?: () => void;
+  title: string;
+}
+
+export const LeftBottomActionButton = (props: LeftBottomActionButtonProps) => {
+  const isNavOpen = useObservable(props.chrome.getIsNavDrawerLocked$(), false);
+
+  let finalIcon: React.ReactNode | undefined = props.icon;
+
+  if (props.iconType) {
+    finalIcon = <EuiIcon type={props.iconType} aria-label={props.title} />;
+  }
+
+  return isNavOpen ? (
+    <EuiFlexGroup
+      style={{ cursor: 'pointer' }}
+      gutterSize="s"
+      alignItems="center"
+      onClick={props.onClick}
+    >
+      <EuiFlexItem className="leftBottomActionButtonIconItem" grow={false}>
+        {finalIcon}
+      </EuiFlexItem>
+      <EuiFlexItem className="leftBottomActionButtonTitleItem">{props.title}</EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiIcon type="arrowRight" color="text" />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  ) : (
+    <EuiToolTip content={props.title}>
+      <EuiButtonEmpty onClick={props.onClick}>{finalIcon}</EuiButtonEmpty>
+    </EuiToolTip>
+  );
+};
