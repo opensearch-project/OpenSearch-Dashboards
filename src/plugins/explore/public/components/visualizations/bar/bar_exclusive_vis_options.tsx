2af69b1f24ab2b569de37fd15ef8bd29f14e3e94
Luna Liu <lunalzm@amazon.com>
2025-07-15 13:17:40 +0800

    Add Bar Size Control Switch
    Signed-off-by: Luna Liu <lunalzm@amazon.com>

--- src/plugins/explore/public/components/visualizations/bar/bar_exclusive_vis_options.tsx
+++ src/plugins/explore/public/components/visualizations/bar/bar_exclusive_vis_options.tsx
@@ -18,11 +18,13 @@ import { useDebouncedNumericValue } from '../utils/use_debounced_value';
 import { StyleAccordion } from '../style_panel/style_accordion';
 
 interface BarExclusiveVisOptionsProps {
+  barSizeMode: 'auto' | 'manual';
   barWidth: number;
   barPadding: number;
   showBarBorder: boolean;
   barBorderWidth: number;
   barBorderColor: string;
+  onBarSizeModeChange: (barSizeMode: 'auto' | 'manual') => void;
   onBarWidthChange: (barWidth: number) => void;
   onBarPaddingChange: (barPadding: number) => void;
   onShowBarBorderChange: (showBarBorder: boolean) => void;
@@ -31,11 +33,13 @@ interface BarExclusiveVisOptionsProps {
 }
 
 export const BarExclusiveVisOptions = ({
+  barSizeMode,
   barWidth,
   barPadding,
   showBarBorder,
   barBorderWidth,
   barBorderColor,
+  onBarSizeModeChange,
   onBarWidthChange,
   onBarPaddingChange,
   onShowBarBorderChange,
@@ -61,6 +65,21 @@ export const BarExclusiveVisOptions = ({
     { min: 1, max: 10, defaultValue: 1 }
   );
 
+  const sizeModeOptions = [
+    {
+      id: 'auto',
+      label: i18n.translate('explore.stylePanel.bar.sizeModeAuto', {
+        defaultMessage: 'Auto',
+      }),
+    },
+    {
+      id: 'manual',
+      label: i18n.translate('explore.stylePanel.bar.sizeModeManual', {
+        defaultMessage: 'Manual',
+      }),
+    },
+  ];
+
   const visibilityOptions = [
     {
       id: 'shown',
@@ -84,48 +103,71 @@ export const BarExclusiveVisOptions = ({
       })}
       initialIsOpen={true}
     >
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <EuiFormRow
-            label={i18n.translate('explore.stylePanel.bar.barWidth', {
-              defaultMessage: 'Bar width',
-            })}
-            helpText={i18n.translate('explore.stylePanel.bar.barWidthHelp', {
-              defaultMessage: 'Value between 0.1 and 1',
-            })}
-          >
-            <EuiFieldNumber
-              compressed
-              value={localBarWidth}
-              onChange={(e) => handleBarWidthChange(e.target.value)}
-              min={0.1}
-              max={1}
-              step={0.1}
-              data-test-subj="barWidthInput"
-            />
-          </EuiFormRow>
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiFormRow
-            label={i18n.translate('explore.stylePanel.bar.barPadding', {
-              defaultMessage: 'Bar padding',
-            })}
-            helpText={i18n.translate('explore.stylePanel.bar.barPaddingHelp', {
-              defaultMessage: 'Value between 0 and 0.5',
-            })}
-          >
-            <EuiFieldNumber
-              compressed
-              value={localBarPadding}
-              onChange={(e) => handleBarPaddingChange(e.target.value)}
-              min={0}
-              max={0.5}
-              step={0.05}
-              data-test-subj="barPaddingInput"
-            />
-          </EuiFormRow>
-        </EuiFlexItem>
-      </EuiFlexGroup>
+      <EuiFormRow
+        label={i18n.translate('explore.stylePanel.bar.sizeMode', {
+          defaultMessage: 'Bar size',
+        })}
+      >
+        <EuiButtonGroup
+          legend={i18n.translate('explore.stylePanel.bar.sizeMode', {
+            defaultMessage: 'Bar size',
+          })}
+          options={sizeModeOptions}
+          idSelected={barSizeMode}
+          onChange={(id) => onBarSizeModeChange(id as 'auto' | 'manual')}
+          buttonSize="compressed"
+          isFullWidth
+          data-test-subj="barSizeModeButtonGroup"
+        />
+      </EuiFormRow>
+
+      {barSizeMode === 'manual' && (
+        <>
+          <EuiSpacer size="s" />
+          <EuiFlexGroup>
+            <EuiFlexItem>
+              <EuiFormRow
+                label={i18n.translate('explore.stylePanel.bar.barWidth', {
+                  defaultMessage: 'Bar width',
+                })}
+                helpText={i18n.translate('explore.stylePanel.bar.barWidthHelp', {
+                  defaultMessage: 'Value between 0.1 and 1',
+                })}
+              >
+                <EuiFieldNumber
+                  compressed
+                  value={localBarWidth}
+                  onChange={(e) => handleBarWidthChange(e.target.value)}
+                  min={0.1}
+                  max={1}
+                  step={0.1}
+                  data-test-subj="barWidthInput"
+                />
+              </EuiFormRow>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiFormRow
+                label={i18n.translate('explore.stylePanel.bar.barPadding', {
+                  defaultMessage: 'Bar padding',
+                })}
+                helpText={i18n.translate('explore.stylePanel.bar.barPaddingHelp', {
+                  defaultMessage: 'Value between 0 and 0.5',
+                })}
+              >
+                <EuiFieldNumber
+                  compressed
+                  value={localBarPadding}
+                  onChange={(e) => handleBarPaddingChange(e.target.value)}
+                  min={0}
+                  max={0.5}
+                  step={0.05}
+                  data-test-subj="barPaddingInput"
+                />
+              </EuiFormRow>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </>
+      )}
 
       <EuiSpacer size="s" />
 
