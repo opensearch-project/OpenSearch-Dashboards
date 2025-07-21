45498bbc3b3204f0c17153b81740d714e7f4ba7a
Paul Sebastian <paulstn@amazon.com>
2025-07-21 06:48:55 -0700

    add patterns to tab slice
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>

--- src/plugins/explore/public/components/tabs/action_bar/patterns_settings/patterns_settings_popover_button.tsx
+++ src/plugins/explore/public/components/tabs/action_bar/patterns_settings/patterns_settings_popover_button.tsx
@@ -6,20 +6,43 @@
 import { EuiButtonIcon, EuiPopover, EuiSelect, EuiText } from '@elastic/eui';
 import React, { useState } from 'react';
 import { FormattedMessage } from '@osd/i18n/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPatternsField } from '../../../../application/utils/state_management/slices/tab/tab_slice';
+import { executeQueries } from '../../../../application/utils/state_management/actions/query_actions';
+import { useOpenSearchDashboards } from '../../../../../../opensearch_dashboards_react/public';
+import { ExploreServices } from '../../../../types';
+import { selectPatternsField } from '../../../../application/utils/state_management/selectors';
 
 export const PatternsSettingsPopoverButton = () => {
+  const { services } = useOpenSearchDashboards<ExploreServices>();
+  const dispatch = useDispatch();
+  const patternsField = useSelector(selectPatternsField);
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
   const openPopover = () => setIsPopoverOpen(true);
   const closePopover = () => setIsPopoverOpen(false);
 
   const options = [
-    { value: 'one', text: 'products.product_name' },
-    { value: 'two', text: 'message' },
-    { value: 'three', text: 'Dest' },
+    { value: 'products.product_name', text: 'products.product_name' },
+    { value: 'message', text: 'message' },
+    { value: 'Dest', text: 'Dest' },
   ];
 
-  const [value, setValue] = useState(options[0].value);
+  // Use the value from Redux state if available, otherwise use the first option
+  const [value, setValue] = useState(patternsField || options[0].value);
+
+  const handleFieldChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue = e.target.value;
+    dispatch(setPatternsField(newValue));
+
+    // Trigger query execution to reload the patterns tab
+    if (services) {
+      dispatch(executeQueries({ services }));
+    }
+    setValue(newValue);
+
+    closePopover();
+  };
 
   return (
     <EuiPopover
@@ -49,13 +72,7 @@ export const PatternsSettingsPopoverButton = () => {
           </EuiText>
         </div>
         <div className="dscDownloadCsvPopoverContent__form">
-          <EuiSelect
-            options={options}
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-          />
+          <EuiSelect options={options} value={value} onChange={handleFieldChange} />
         </div>
       </div>
     </EuiPopover>
