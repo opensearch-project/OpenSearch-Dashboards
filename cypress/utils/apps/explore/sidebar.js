2451a01ae8e4f0640c5320fef1a9bfde36f6ee3b
Joey Liu <jiyili@amazon.com>
2025-07-22 21:14:04 +0000

    Fix Cypress test
    Signed-off-by: Joey Liu <jiyili@amazon.com>

--- cypress/utils/apps/explore/sidebar.js
+++ cypress/utils/apps/explore/sidebar.js
@@ -9,9 +9,10 @@
  */
 export const clickSidebarCollapseBtn = (collapse = true) => {
   if (collapse) {
-    cy.getElementByTestId('euiResizableButton').trigger('mouseover').click();
+    cy.getElementByTestId('fieldList-collapse-button').click({ force: true });
+  } else {
+    cy.get('.euiResizableToggleButton').click({ force: true });
   }
-  cy.getElementByTestId('fieldList-collapse-button').click({ force: true });
 };
 
 /**
