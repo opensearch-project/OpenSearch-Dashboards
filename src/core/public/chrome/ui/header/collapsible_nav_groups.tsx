5a2db49831e39e41e1ea4616a9d88647f93c874d
Lin Wang <wonglam@amazon.com>
2025-07-24 15:15:16 +0800

    Trigger context popover menu with click
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- src/core/public/chrome/ui/header/collapsible_nav_groups.tsx
+++ src/core/public/chrome/ui/header/collapsible_nav_groups.tsx
@@ -14,6 +14,8 @@ import {
   EuiContextMenuItem,
   EuiTitle,
   EuiToolTip,
+  EuiFlexGroup,
+  EuiFlexItem,
 } from '@elastic/eui';
 import React, { useState } from 'react';
 import classNames from 'classnames';
@@ -174,15 +176,22 @@ export function NavGroups({
           props.icon
         ) : (
           <SimplePopover
-            anchorPosition="upLeft"
+            anchorPosition="rightUp"
             panelPaddingSize="none"
             button={props.icon || <></>}
             key={navOpen ? undefined : `popover-${appId}`}
+            triggerMode="click"
+            offset={4}
           >
             <EuiPopoverTitle>
-              <EuiTitle size="s">
-                <span>{navLink.link?.title}</span>
-              </EuiTitle>
+              <EuiFlexGroup alignItems="center" gutterSize="s">
+                {props.icon && <EuiFlexItem grow={false}>{props.icon}</EuiFlexItem>}
+                <EuiFlexItem>
+                  <EuiTitle size="s">
+                    <span>{navLink.link?.title}</span>
+                  </EuiTitle>
+                </EuiFlexItem>
+              </EuiFlexGroup>
             </EuiPopoverTitle>
             <EuiContextMenuPanel
               hasFocus={false}
