d03a9052bce2a32320cbd5bbea7da4aca0801d1c
Justin Kim <jungkm@amazon.com>
2025-07-30 09:59:47 -0700

    ai editor mode cypress test
    Signed-off-by: Justin Kim <jungkm@amazon.com>

---
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/01/ai_editor.spec.js
@@ -0,0 +1,119 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { INDEX_WITH_TIME_1, DATASOURCE_NAME } from '../../../../../../utils/constants';
+import {
+  generateAllTestConfigurations,
+  generateBaseConfiguration,
+  getRandomizedWorkspaceName,
+  setDatePickerDatesIfRelevant,
+} from '../../../../../../utils/apps/explore/shared';
+import { prepareTestSuite } from '../../../../../../utils/helpers';
+import {
+  DatasetTypes,
+  INDEX_PATTERN_WITH_TIME,
+} from '../../../../../../utils/apps/explore/constants';
+
+const workspaceName = getRandomizedWorkspaceName();
+
+const runAiEditorTests = () => {
+  const generatedQuery = 'source=data_logs_small_time_* | where bytes_transferred > 9000';
+
+  describe('AI Editor', () => {
+    before(() => {
+      cy.osd.setupWorkspaceAndDataSourceWithIndices(workspaceName, [INDEX_WITH_TIME_1]);
+      cy.createWorkspaceIndexPatterns({
+        workspaceName: workspaceName,
+        indexPattern: INDEX_PATTERN_WITH_TIME.replace('*', ''),
+        timefieldName: 'timestamp',
+        dataSource: DATASOURCE_NAME,
+        isEnhancement: true,
+      });
+    });
+
+    after(() => {
+      cy.osd.cleanupWorkspaceAndDataSourceAndIndices(workspaceName, [INDEX_WITH_TIME_1]);
+    });
+
+    beforeEach(() => {
+      // mock AI mode enablement
+      cy.intercept('GET', '**/enhancements/assist/languages*', {
+        statusCode: 200,
+        body: {
+          configuredLanguages: ['PPL'],
+        },
+      });
+      // mock generated PPL
+      cy.intercept('POST', '**/enhancements/assist/generate', {
+        statusCode: 200,
+        body: {
+          query: generatedQuery,
+        },
+      });
+      cy.osd.navigateToWorkSpaceSpecificPage({
+        workspaceName,
+        page: 'explore/logs',
+        isEnhancement: true,
+      });
+    });
+
+    generateAllTestConfigurations(generateBaseConfiguration).forEach((config) => {
+      it(`should be able to query via AI mode for ${config.testName}`, () => {
+        cy.explore.setDataset(config.dataset, DATASOURCE_NAME, config.datasetType);
+        setDatePickerDatesIfRelevant(config.language.name);
+        cy.explore.setQueryEditor(' give me all errors');
+        cy.getElementByTestId('exploreTabs').should('exist');
+        cy.verifyHitCount('2,054');
+        cy.getElementByTestId('exploreQueryPanelGeneratedQuery').contains(generatedQuery);
+
+        // check to see if the "Edit query" button works
+        cy.getElementByTestId('exploreQueryPanelGeneratedQueryEditButton').click();
+        cy.getElementByTestId('exploreQueryPanelEditor').should('be.visible');
+        cy.getElementByTestId('exploreQueryPanelEditor').should(
+          'contain.text',
+          'source=data_logs_small_time_*'
+        );
+      });
+
+      it(`should be able to toggle between editor modes for ${config.testName}`, () => {
+        cy.explore.setDataset(config.dataset, DATASOURCE_NAME, config.datasetType);
+        setDatePickerDatesIfRelevant(config.language.name);
+
+        cy.getElementByTestId('exploreSelectedLanguage').contains('PPL');
+
+        // Test via keyboard clicks
+        cy.explore.setQueryEditor(' ');
+        cy.getElementByTestId('exploreSelectedLanguage').contains('Ask AI');
+        cy.explore.setQueryEditor('{esc}');
+        cy.getElementByTestId('exploreSelectedLanguage').contains('PPL');
+        cy.getElementByTestId('queryPanelFooterLanguageToggle').click();
+
+        // Test via toggle
+        cy.getElementByTestId('queryPanelFooterLanguageToggle-AI').click();
+        cy.getElementByTestId('exploreSelectedLanguage').contains('Ask AI');
+      });
+
+      // filtering only works for indexed fields
+      if (config.datasetType === DatasetTypes.INDEX_PATTERN.name) {
+        it(`should be able to add a filter in AI mode for ${config.testName}`, () => {
+          cy.explore.setDataset(config.dataset, DATASOURCE_NAME, config.datasetType);
+          setDatePickerDatesIfRelevant(config.language.name);
+          // fire query so that we have data to add filter on
+          cy.explore.updateTopNav({ log: false });
+          cy.getElementByTestId('exploreTabs').should('exist');
+
+          cy.explore.setQueryEditor(' ');
+          cy.getElementByTestId('field-category-showDetails').click({ force: true });
+          cy.getElementByTestId('plus-category-Network').click();
+          cy.getElementByTestId('exploreQueryPanelEditor').should('contain.text', 'category');
+          cy.getElementByTestId('exploreQueryPanelEditor').should('contain.text', 'is');
+          cy.getElementByTestId('exploreQueryPanelEditor').should('contain.text', "'Network'");
+        });
+      }
+    });
+  });
+};
+
+prepareTestSuite('AI Editor', runAiEditorTests);
