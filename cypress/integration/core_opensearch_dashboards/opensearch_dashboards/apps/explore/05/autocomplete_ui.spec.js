03f93f0ae176a233bbcb6ec08eecf3f60a49dba8
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2025-07-29 17:27:31 -0700

    Add integ tests for New Autocomplete experience in Explore (#10288)

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/05/autocomplete_ui.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/05/autocomplete_ui.spec.js
@@ -15,6 +15,10 @@ import {
   showSuggestionAndHint,
   hideWidgets,
   createQuery,
+  createInvalidQuery,
+  validateEditorContainsError,
+  validateImplicitPPLQuery,
+  validateDocumentationPanelIsOpen,
 } from '../../../../../../utils/apps/explore/autocomplete';
 import { prepareTestSuite } from '../../../../../../utils/helpers';
 
@@ -109,6 +113,46 @@ export const runAutocompleteTests = () => {
             cy.osd.waitForLoader(true);
             cy.wait(2000);
             validateQueryResults('unique_category', 'Configuration');
+          });
+
+          it('should validate that error markers are shown for invalide query', () => {
+            cy.explore.setDataset(config.dataset, DATASOURCE_NAME, config.datasetType);
+            setDatePickerDatesAndSearchIfRelevant(config.language);
+            cy.wait(2000);
+            cy.explore.clearQueryEditor();
+
+            createInvalidQuery(config); // use keyboard
+
+            validateEditorContainsError();
+          });
+
+          it('should validate that error markers are shown for invalid query', () => {
+            cy.explore.setDataset(config.dataset, DATASOURCE_NAME, config.datasetType);
+            setDatePickerDatesAndSearchIfRelevant(config.language);
+            cy.wait(2000);
+            cy.explore.clearQueryEditor();
+
+            createInvalidQuery(config);
+
+            validateEditorContainsError();
+          });
+
+          it('should support implicit ppl queries that dont start with source=', () => {
+            cy.explore.setDataset(config.dataset, DATASOURCE_NAME, config.datasetType);
+            setDatePickerDatesAndSearchIfRelevant(config.language);
+            cy.wait(2000);
+            cy.explore.clearQueryEditor();
+
+            validateImplicitPPLQuery(config);
+          });
+
+          it('should show open documentation panel by default', () => {
+            cy.explore.setDataset(config.dataset, DATASOURCE_NAME, config.datasetType);
+            setDatePickerDatesAndSearchIfRelevant(config.language);
+            cy.wait(2000);
+            cy.explore.clearQueryEditor();
+
+            validateDocumentationPanelIsOpen(config);
           });
         });
       }
