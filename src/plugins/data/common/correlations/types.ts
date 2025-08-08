b63a310c1b8d4e94a1928e4527e27684f28b147d
sumukhswamy <sumukhhs@amazon.com>
2025-08-08 15:50:08 -0700

    moved Correlations plugin to Data
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

---
+++ src/plugins/data/common/correlations/types.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { SavedObject, SavedObjectAttributes } from '../../../../core/public';
+
+// @experimental This schema is experimental and might change in future releases.
+export interface CorrelationSavedObjectAttributes extends SavedObjectAttributes {
+  correlationType: string;
+
+  version: string;
+
+  /*
+   * Using any[] for entities to allow flexible correlation structures.
+   * This enables different correlation types and use cases to define their own entity schemas without strict typing constraints.
+   * Each entity can contain different fields like correlatedFields, meta, datasource info.
+   * @experimental - The any[] type is experimental and may be replaced with more specific types in future releases.
+   */
+  entities: any[];
+}
+
+export interface CorrelationSavedObject extends SavedObject<CorrelationSavedObjectAttributes> {
+  type: 'correlations';
+}
--- src/plugins/data/server/saved_objects/correlations.ts
+++ src/plugins/data/server/saved_objects/correlations.ts
@@ -1,18 +1,24 @@
 /*
- * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Any modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
  */
 
 import { SavedObjectsType } from '../../../../core/server';
 
-// @experimental This API is experimental and might change in future releases.
+// @experimental This schema is experimental and might change in future releases.
 export const correlationsSavedObjectType: SavedObjectsType = {
   name: 'correlations',
   hidden: false,
   namespaceType: 'single',
   management: {
     icon: 'link',
-    defaultSearchField: 'title',
+    defaultSearchField: 'correlationType',
     importableAndExportable: true,
     getTitle(obj) {
       return `Correlation ${obj.id}`;
