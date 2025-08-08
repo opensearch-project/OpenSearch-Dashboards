537572b7773f2d6efdf6b44b682bc6882253b005
sumukhswamy <sumukhhs@amazon.com>
2025-08-08 12:37:57 -0700

    updated the License
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- src/plugins/correlations/common/types.ts
+++ src/plugins/correlations/common/types.ts
@@ -1,16 +1,11 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- *
- * Any modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
  */
 
 import { SavedObject, SavedObjectAttributes } from '../../../core/public';
 
+// @experimental This API is experimental and might change in future releases.
 export interface CorrelationSavedObjectAttributes extends SavedObjectAttributes {
   correlationType: string;
 
