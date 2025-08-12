b886e61dac3848a84264b66a8568ded7e10b4349
Justin Kim <jungkm@amazon.com>
2025-08-12 11:57:16 -0700

    disable pattern tabs for now (#10402)
    Signed-off-by: Justin Kim <jungkm@amazon.com>

--- src/plugins/explore/public/application/register_tabs.ts
+++ src/plugins/explore/public/application/register_tabs.ts
@@ -5,21 +5,10 @@
 
 import { LogsTab } from '../components/tabs/logs_tab';
 import { TabRegistryService } from '../services/tab_registry/tab_registry_service';
-import { PatternsTab } from '../components/tabs/patterns_tab';
 import { ExploreServices } from '../types';
 import { EXPLORE_DEFAULT_LANGUAGE } from '../../common';
 import { VisTab } from '../components/tabs/vis_tab';
 import { getQueryWithSource } from './utils/languages';
-import { setUsingRegexPatterns } from './utils/state_management/slices/tab/tab_slice';
-import {
-  brainPatternQuery,
-  findDefaultPatternsField,
-  regexPatternQuery,
-} from '../components/patterns_table/utils/utils';
-import { executeTabQuery } from './utils/state_management/actions/query_actions';
-import { QueryExecutionStatus } from './utils/state_management/types';
-import { BRAIN_QUERY_OLD_ENGINE_ERROR_PREFIX } from '../components/patterns_table/utils/constants';
-import { setIndividualQueryStatus } from './utils/state_management/slices';
 
 /**
  * Registers built-in tabs with the tab registry
@@ -47,84 +36,85 @@ export const registerBuiltInTabs = (tabRegistry: TabRegistryService, services: E
   tabRegistry.registerTab(logsTabDefinition);
 
   // Register Patterns Tab
-  tabRegistry.registerTab({
-    id: 'explore_patterns_tab',
-    label: 'Patterns',
-    flavor: [],
-    order: 15,
-    supportedLanguages: [EXPLORE_DEFAULT_LANGUAGE],
-
-    prepareQuery: (query) => {
-      const state = services.store.getState();
-
-      // Get the selected patterns field from the Redux state
-      let patternsField = state.tab.patterns.patternsField;
-
-      const preparedQuery = getQueryWithSource(query);
-      if (!patternsField) {
-        patternsField = findDefaultPatternsField(services);
-      }
-
-      if (state.tab.patterns.usingRegexPatterns)
-        return regexPatternQuery(preparedQuery.query, patternsField);
-
-      return brainPatternQuery(preparedQuery.query, patternsField);
-    },
-
-    handleQueryError: (error, cacheKey) => {
-      const state = services.store.getState();
-
-      /**
-       * The below conditional is checking for the error returned when attempting to use a BRAIN
-       * query on an older version of the querying engine. If this error appears, an attempt is made
-       * to switch over to a patterns query which works on older versions of the querying engine.
-       * A redux state is set to inform the UI that this older query is being utilized
-       * Finally, the query is retriggered.
-       * The return value being true will prevent the standard error from dispatching, keeping the page clear
-       */
-      if (
-        error &&
-        error.status &&
-        error.status === 400 &&
-        error.error.details &&
-        error.error.details.startsWith(BRAIN_QUERY_OLD_ENGINE_ERROR_PREFIX)
-      ) {
-        // can check further details of err if needed
-        let patternsField = state.tab.patterns.patternsField;
-        if (!patternsField) {
-          patternsField = findDefaultPatternsField(services);
-        }
-        const query = state.query;
-        const preparedQuery = getQueryWithSource(query);
-        services.store.dispatch(setUsingRegexPatterns(true));
-        services.store.dispatch(
-          executeTabQuery({
-            services,
-            cacheKey: regexPatternQuery(preparedQuery.query, patternsField),
-          })
-        );
-
-        // set the old cacheKey to uninitialized to finalize loading, our new tab query has new cacheKey
-        services.store.dispatch(
-          setIndividualQueryStatus({
-            cacheKey,
-            status: {
-              status: QueryExecutionStatus.UNINITIALIZED,
-              startTime: undefined,
-              elapsedMs: undefined,
-              error: undefined,
-            },
-          })
-        );
-
-        return true;
-      }
-
-      return false;
-    },
-
-    component: PatternsTab,
-  });
+  // TODO: Disabled for P0.
+  // tabRegistry.registerTab({
+  //   id: 'explore_patterns_tab',
+  //   label: 'Patterns',
+  //   flavor: [],
+  //   order: 15,
+  //   supportedLanguages: [EXPLORE_DEFAULT_LANGUAGE],
+  //
+  //   prepareQuery: (query) => {
+  //     const state = services.store.getState();
+  //
+  //     // Get the selected patterns field from the Redux state
+  //     let patternsField = state.tab.patterns.patternsField;
+  //
+  //     const preparedQuery = getQueryWithSource(query);
+  //     if (!patternsField) {
+  //       patternsField = findDefaultPatternsField(services);
+  //     }
+  //
+  //     if (state.tab.patterns.usingRegexPatterns)
+  //       return regexPatternQuery(preparedQuery.query, patternsField);
+  //
+  //     return brainPatternQuery(preparedQuery.query, patternsField);
+  //   },
+  //
+  //   handleQueryError: (error, cacheKey) => {
+  //     const state = services.store.getState();
+  //
+  //     /**
+  //      * The below conditional is checking for the error returned when attempting to use a BRAIN
+  //      * query on an older version of the querying engine. If this error appears, an attempt is made
+  //      * to switch over to a patterns query which works on older versions of the querying engine.
+  //      * A redux state is set to inform the UI that this older query is being utilized
+  //      * Finally, the query is retriggered.
+  //      * The return value being true will prevent the standard error from dispatching, keeping the page clear
+  //      */
+  //     if (
+  //       error &&
+  //       error.status &&
+  //       error.status === 400 &&
+  //       error.error.details &&
+  //       error.error.details.startsWith(BRAIN_QUERY_OLD_ENGINE_ERROR_PREFIX)
+  //     ) {
+  //       // can check further details of err if needed
+  //       let patternsField = state.tab.patterns.patternsField;
+  //       if (!patternsField) {
+  //         patternsField = findDefaultPatternsField(services);
+  //       }
+  //       const query = state.query;
+  //       const preparedQuery = getQueryWithSource(query);
+  //       services.store.dispatch(setUsingRegexPatterns(true));
+  //       services.store.dispatch(
+  //         executeTabQuery({
+  //           services,
+  //           cacheKey: regexPatternQuery(preparedQuery.query, patternsField),
+  //         })
+  //       );
+  //
+  //       // set the old cacheKey to uninitialized to finalize loading, our new tab query has new cacheKey
+  //       services.store.dispatch(
+  //         setIndividualQueryStatus({
+  //           cacheKey,
+  //           status: {
+  //             status: QueryExecutionStatus.UNINITIALIZED,
+  //             startTime: undefined,
+  //             elapsedMs: undefined,
+  //             error: undefined,
+  //           },
+  //         })
+  //       );
+  //
+  //       return true;
+  //     }
+  //
+  //     return false;
+  //   },
+  //
+  //   component: PatternsTab,
+  // });
 
   // Register Visualizations Tab
   tabRegistry.registerTab({
