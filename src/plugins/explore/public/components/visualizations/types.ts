026faa320c5436a7c7ea173389cdf39f92c2b9cf
Luna Liu <lunalzm@amazon.com>
2025-08-26 13:11:16 +0800

    Fix axis label alignment and flush behavior
    Signed-off-by: Luna Liu <lunalzm@amazon.com>

--- src/plugins/explore/public/components/visualizations/types.ts
+++ src/plugins/explore/public/components/visualizations/types.ts
@@ -234,6 +234,7 @@ export interface AxisConfig {
   labelAngle?: number;
   labelLimit?: number;
   labelOverlap?: string;
+  labelFlush?: boolean;
   ticks?: boolean;
   domain?: boolean;
   format?: {
