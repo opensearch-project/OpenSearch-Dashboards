39423f294fee8f3c0a2cc1285f3e2792402605b8
Paul Sebastian <paulstn@amazon.com>
2025-08-26 13:52:42 -0700

    add patternsField to memoized cacheKey dependency array
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>

--- src/plugins/explore/public/application/utils/hooks/use_tab_results.ts
+++ src/plugins/explore/public/application/utils/hooks/use_tab_results.ts
@@ -4,10 +4,12 @@
  */
 
 import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
 import { RootState } from '../state_management/store';
 import { useOpenSearchDashboards } from '../../../../../opensearch_dashboards_react/public';
 import { ExploreServices } from '../../../types';
 import { defaultPrepareQueryString } from '../state_management/actions/query_actions';
+import { selectPatternsField } from '../state_management/selectors';
 
 /**
  * Hook for reading tab specific result from result slice
@@ -17,10 +19,15 @@ export const useTabResults = () => {
   const query = useSelector((state: RootState) => state.query);
   const activeTabId = useSelector((state: RootState) => state.ui.activeTabId);
   const results = useSelector((state: RootState) => state.results);
+  const patternsField = useSelector(selectPatternsField); // for use in updating dependency array of cacheKey
 
-  const activeTab = services.tabRegistry.getTab(activeTabId);
-  const prepareQuery = activeTab?.prepareQuery || defaultPrepareQueryString;
-  const cacheKey = prepareQuery(query);
+  const cacheKey = useMemo(() => {
+    const activeTab = services.tabRegistry.getTab(activeTabId);
+    const prepareQuery = activeTab?.prepareQuery || defaultPrepareQueryString;
+    return prepareQuery(query);
+    // TODO: redo logic of using patternsField in dependency array when we have a better method to update cacheKey
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query, activeTabId, services, patternsField]);
 
   return {
     results: cacheKey ? results[cacheKey] : null,
