4c55432b4ba2f2e3d1bde95e8a2b32baff383a4a
Luna Liu <lunalzm@amazon.com>
2025-09-02 18:44:14 +0800

    Enhancement for field calculation
    Signed-off-by: Luna Liu <lunalzm@amazon.com>

--- src/plugins/explore/public/components/visualizations/table/table_vis_config.ts
+++ src/plugins/explore/public/components/visualizations/table/table_vis_config.ts
@@ -11,12 +11,19 @@ export interface TableChartStyleControls {
   pageSize: number;
   globalAlignment?: 'auto' | 'left' | 'center' | 'right';
   showColumnFilter?: boolean;
+  showFooter?: boolean;
+  footerCalculations?: Array<{
+    fields: string[];
+    calculation: 'total' | 'last' | 'average' | 'min' | 'max';
+  }>;
 }
 
 const defaultTableChartStyles: TableChartStyleControls = {
   pageSize: 10,
   globalAlignment: 'auto',
   showColumnFilter: false,
+  showFooter: false,
+  footerCalculations: [],
 };
 
 export const createTableConfig = (): VisualizationType<'table'> => ({
