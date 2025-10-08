ec8a41833e65baece01524d02cc337bc705dd5a8
Justin Kim <jungkm@amazon.com>
2025-10-07 21:17:39 -0700

    be able to update console_polling through the UI (#10595) (#10646)
    * be able to update console_polling through the UI
    
    
    
    * Changeset file for PR #10595 created/updated
    
    ---------
    
    
    
    (cherry picked from commit 81d5f8199220132b2ec4252bb409a80d744cb38e)
    
    Signed-off-by: Justin Kim <jungkm@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/console/public/application/containers/settings.tsx
+++ src/plugins/console/public/application/containers/settings.tsx
@@ -51,20 +51,23 @@ const refreshAutocompleteSettings = (
   selectedSettings: any,
   dataSourceId?: string
 ) => {
-  retrieveAutoCompleteInfo(http, settings, selectedSettings, dataSourceId);
+  if (dataSourceId) {
+    retrieveAutoCompleteInfo(http, settings, selectedSettings, dataSourceId);
+  }
 };
 
 const fetchAutocompleteSettingsIfNeeded = (
   http: HttpSetup,
   settings: SettingsService,
   newSettings: DevToolsSettings,
-  prevSettings: DevToolsSettings
+  prevSettings: DevToolsSettings,
+  dataSourceId?: string
 ) => {
   // We'll only retrieve settings if polling is on. The expectation here is that if the user
   // disables polling it's because they want manual control over the fetch request (possibly
   // because it's a very expensive request given their cluster and bandwidth). In that case,
   // they would be unhappy with any request that's sent automatically.
-  if (newSettings.polling) {
+  if (newSettings.polling && dataSourceId) {
     const autocompleteDiff = getAutocompleteDiff(newSettings, prevSettings);
 
     const isSettingsChanged = autocompleteDiff.length > 0;
@@ -102,7 +105,7 @@ export function Settings({ onClose, dataSourceId }: Props) {
 
   const onSaveSettings = (newSettings: DevToolsSettings) => {
     const prevSettings = settings.toJSON();
-    fetchAutocompleteSettingsIfNeeded(http, settings, newSettings, prevSettings);
+    fetchAutocompleteSettingsIfNeeded(http, settings, newSettings, prevSettings, dataSourceId);
 
     // Update the new settings in localStorage
     settings.updateSettings(newSettings);
