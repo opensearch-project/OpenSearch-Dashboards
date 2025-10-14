dfb3aebdc93555e45dcd27a78211e6cc16ccfdbf
Adam Tackett <105462877+TackAdam@users.noreply.github.com>
2025-10-13 18:05:57 -0600

    [Explore] show query panel even if there are no datasets traces + metrics (#10713)
    * add query bar to empty state for trace + metrics page
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * changelog
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    ---------
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    Co-authored-by: Adam Tackett <tackadam@amazon.com>

--- src/plugins/explore/public/application/pages/traces/traces_page.tsx
+++ src/plugins/explore/public/application/pages/traces/traces_page.tsx
@@ -18,7 +18,6 @@ import { useUrlStateSync } from '../../utils/hooks/use_url_state_sync';
 import { useTimefilterSubscription } from '../../utils/hooks/use_timefilter_subscription';
 import { useHeaderVariants } from '../../utils/hooks/use_header_variants';
 import { NewExperienceBanner } from '../../../components/experience_banners/new_experience_banner';
-import { useDatasetContext } from '../../context';
 import { BottomContainer } from '../../../components/container/bottom_container';
 import { TopNav } from '../../../components/top_nav/top_nav';
 import { useInitPage } from '../../../application/utils/hooks/use_page_initialization';
@@ -34,7 +33,6 @@ export const TracesPage: React.FC<Partial<Pick<AppMountParameters, 'setHeaderAct
   setHeaderActionMenu,
 }) => {
   const { services } = useOpenSearchDashboards<ExploreServices>();
-  const { dataset, isLoading } = useDatasetContext();
   const { savedExplore } = useInitPage();
   const { keyboardShortcut } = services;
   const dispatch = useDispatch();
@@ -80,7 +78,7 @@ export const TracesPage: React.FC<Partial<Pick<AppMountParameters, 'setHeaderAct
               <NewExperienceBanner />
 
               <div className="dscCanvas__queryPanel">
-                {dataset && !isLoading ? <QueryPanel /> : null}
+                <QueryPanel />
               </div>
 
               {/* Main content area with resizable panels under QueryPanel */}
