b77d0ecc6ba2adcb0f0136bd66cb6d43fcc1a9da
Qxisylolo <qianxisy@amazon.com>
2025-10-20 15:08:37 +0800

    fix test
    Signed-off-by: Qxisylolo <qianxisy@amazon.com>

--- src/plugins/explore/public/components/visualizations/visualization_builder_utils.test.ts
+++ src/plugins/explore/public/components/visualizations/visualization_builder_utils.test.ts
@@ -32,7 +32,7 @@ jest.mock('./rule_repository', () => ({
 
 jest.mock('./style_panel/threshold/threshold_utils', () => ({
   Colors: {
-    ['blues']: {
+    ['blues006C']: {
       baseColor: '#9ecae1',
       colors: [
         '#c6dbef',
