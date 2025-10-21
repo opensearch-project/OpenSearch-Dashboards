020ac538be12e3f343472c8fd278d1ba72486c9b
Saishruti Shetty <earthling.shruti@gmail.com>
2025-10-21 12:58:20 -0700

    feat: Read schemaMappings for logs from dataset object (#10768)

--- src/plugins/explore/public/application/pages/traces/trace_details/public/traces/trace_detail_tabs.tsx
+++ src/plugins/explore/public/application/pages/traces/trace_details/public/traces/trace_detail_tabs.tsx
@@ -13,7 +13,7 @@ export interface TraceDetailTabsProps {
   setActiveTab: (tabId: string) => void;
   transformedHits: any[];
   logDatasets?: any[];
-  logsData?: any[];
+  logCount: number;
   isLogsLoading?: boolean;
 }
 
@@ -22,7 +22,7 @@ export const TraceDetailTabs: React.FC<TraceDetailTabsProps> = ({
   setActiveTab,
   transformedHits,
   logDatasets = [],
-  logsData = [],
+  logCount = 0,
   isLogsLoading = false,
 }) => {
   const tabs = [
@@ -57,12 +57,16 @@ export const TraceDetailTabs: React.FC<TraceDetailTabsProps> = ({
   ];
 
   // Add logs tab if we have log datasets and logs data
-  if (logDatasets.length > 0 && logsData.length > 0) {
+  if (logDatasets.length > 0) {
     tabs.push({
       id: TraceDetailTab.LOGS,
       name: (
         <>
-          <EuiBadge color="default">{logsData.length}</EuiBadge>{' '}
+          {!isLogsLoading && (
+            <>
+              <EuiBadge color="default">{logCount}</EuiBadge>{' '}
+            </>
+          )}
           {i18n.translate('explore.traceView.tab.logs', {
             defaultMessage: 'Related logs',
           })}
