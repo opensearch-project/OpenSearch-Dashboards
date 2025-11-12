77d5e60e0f273be7623e318cafbc94388883135b
Qxisylolo <qianxisy@amazon.com>
2025-11-12 13:29:06 +0800

    small fix
    Signed-off-by: Qxisylolo <qianxisy@amazon.com>

--- cypress/utils/commands.explore.js
+++ cypress/utils/commands.explore.js
@@ -524,6 +524,7 @@ cy.explore.add('createVisualizationWithQuery', (query, chartType, datasetName, o
   }
 
   // Ensure chart type is correct
+  cy.getElementByTestId('exploreChartTypeSelector').should('be.visible').click();
   cy.get(`#${chartType}`).should('match', '[role="option"][aria-selected="true"]');
   cy.get('body').click(0, 0);
 });
