4f7a415867d41f507ef5cd09e6d03b09a76affdc
Yulong Ruan <ruanyl@amazon.com>
2025-11-19 14:11:52 +0800

    add unit tests for normalizeResultRows
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- src/plugins/explore/public/components/visualizations/utils/normalize_result_rows.ts
+++ src/plugins/explore/public/components/visualizations/utils/normalize_result_rows.ts
@@ -12,15 +12,20 @@ export const normalizeResultRows = <T = unknown>(
   rows: Array<OpenSearchSearchHit<T>>,
   schema: Array<{ type?: string; name?: string }>
 ) => {
-  const columns: VisColumn[] = schema.map((field, index) => {
-    return {
-      id: index,
-      schema: FIELD_TYPE_MAP[field.type || ''] || VisFieldType.Unknown,
-      name: field.name || '',
-      column: field.name ? normalizeField(field.name) : '',
-      validValuesCount: 0,
-      uniqueValuesCount: 0,
-    };
+  const columns: VisColumn[] = [];
+  schema.forEach((field, index) => {
+    if (field.name) {
+      const type = field.type ? FIELD_TYPE_MAP[field.type] : VisFieldType.Unknown;
+      const column: VisColumn = {
+        id: index,
+        schema: type ?? VisFieldType.Unknown,
+        name: field.name,
+        column: normalizeField(field.name),
+        validValuesCount: 0,
+        uniqueValuesCount: 0,
+      };
+      columns.push(column);
+    }
   });
 
   const transformedData = rows.map((row: OpenSearchSearchHit) => {
