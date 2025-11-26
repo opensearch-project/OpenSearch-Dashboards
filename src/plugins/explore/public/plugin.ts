614cf0c0b727c379a0cc21fdc6c76a971dd173be
Joshua Li <joshuali925@gmail.com>
2025-11-26 17:53:49 +0000

    clear query on app unmount
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- src/plugins/explore/public/plugin.ts
+++ src/plugins/explore/public/plugin.ts
@@ -338,11 +338,6 @@ export class ExplorePlugin
             ...withNotifyOnErrors(coreStart.notifications.toasts),
           });
 
-          // clear URL state and QueryStringManager state before switching flavors
-          services.osdUrlStateStorage.set('_q', null, { replace: true });
-          services.osdUrlStateStorage.set('_a', null, { replace: true });
-          pluginsStart.data.query.queryString.clearQuery();
-
           // Add scopedHistory to services
           services.scopedHistory = this.currentHistory;
 
@@ -375,6 +370,7 @@ export class ExplorePlugin
             unmount();
             unsubscribeStore();
             resetStore();
+            pluginsStart.data.query.queryString.clearQuery();
           };
         },
         ...options,
