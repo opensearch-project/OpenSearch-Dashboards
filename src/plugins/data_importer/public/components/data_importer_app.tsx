2d1b2e2bdc28cf72b2b94652186b97e7fe4ecdca
Ruchi Sharma <ruchi492@gmail.com>
2025-12-05 16:58:35 -0800

    Data importer ui fixes  (#10961)
    * fix inconsistent caplitalization and change name of create new
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * update font size and data source
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * update yml
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Update opensearch_dashboards.yml
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Changeset file for PR #10961 created/updated
    
    * Update index.scss
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * fix test cases
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Update index.scss
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Update index.scss
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * fix i18n
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * update ii8n mesage
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * update snapshpt
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Update opensearch_dashboards.yml
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Update opensearch_dashboards.yml
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * fix ts error for datasource selector
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * code review fixes
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Update opensearch_dashboards.yml
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * Update import_type_selector.test.tsx.snap
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * update snapshot
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * update snaphost import selector
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    * snapshot updated
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    
    ---------
    
    Signed-off-by: Ruchi Sharma <ruchi492@gmail.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

--- src/plugins/data_importer/public/components/data_importer_app.tsx
+++ src/plugins/data_importer/public/components/data_importer_app.tsx
@@ -73,9 +73,6 @@ export const DataImporterPluginApp = ({
   hideLocalCluster,
   dataSourceManagement,
 }: DataImporterPluginAppProps) => {
-  const DataSourceMenuComponent = dataSourceManagement?.ui.getDataSourceMenu<
-    DataSourceSelectableConfig
-  >();
   const [indexName, setIndexName] = useState<string>();
   const [importType, setImportType] = useState<ImportChoices>(IMPORT_CHOICE_FILE);
   const [disableImport, setDisableImport] = useState<boolean>();
@@ -332,23 +329,22 @@ export const DataImporterPluginApp = ({
   }, [inputText, inputFile, config.maxTextCount, config.maxFileSizeBytes, notifications.toasts]);
 
   const renderDataSourceComponent = useMemo(() => {
+    if (!dataSourceEnabled || !dataSourceManagement) return null;
+
+    const DataSourceSelector = dataSourceManagement!.ui.DataSourceSelector as React.ComponentType<
+      any
+    >;
     return (
-      <div>
-        {DataSourceMenuComponent && (
-          <>
-            <DataSourceMenuComponent
-              componentType={'DataSourceSelectable'}
-              componentConfig={{
-                fullWidth: true,
-                savedObjects: savedObjects.client,
-                notifications,
-                onSelectedDataSources: onDataSourceSelect,
-              }}
-              onManageDataSource={() => {}}
-            />
-            <EuiSpacer size="m" />
-          </>
-        )}
+      <div className="devAppDataSourceSelector">
+        <DataSourceSelector
+          savedObjectsClient={savedObjects.client}
+          notifications={notifications.toasts}
+          onSelectedDataSource={onDataSourceSelect}
+          disabled={!dataSourceEnabled}
+          fullWidth={false}
+          compressed={false}
+          hideLocalCluster={hideLocalCluster}
+        />
       </div>
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -392,13 +388,14 @@ export const DataImporterPluginApp = ({
                   </h1>
                 </EuiTitle>
               </EuiPageHeader>
-              <EuiPageContent>
+              <EuiPageContent className="data_importer_content" paddingSize="s">
                 <EuiFlexGroup>
                   <EuiFlexItem grow={1}>
                     <ImportTypeSelector
                       updateSelection={onImportTypeChange}
                       initialSelection={importType}
                     />
+                    <EuiSpacer size="s" />
                     {dataSourceEnabled && (
                       <>
                         <EuiTitle size="xs">
@@ -415,7 +412,7 @@ export const DataImporterPluginApp = ({
                     <EuiTitle size="xs">
                       <span>
                         {i18n.translate('dataImporter.indexName', {
-                          defaultMessage: 'Create/Select Index Name',
+                          defaultMessage: 'Select an existing index or create new',
                         })}
                       </span>
                     </EuiTitle>
@@ -429,27 +426,45 @@ export const DataImporterPluginApp = ({
                         onCreateOption={onCreateIndexName}
                       />
                     </EuiFormRow>
-                    <EuiSpacer size="s" />
+
                     {showDelimiterChoice && (
-                      <DelimiterSelect
-                        onDelimiterChange={onDelimiterChange}
-                        initialDelimiter={delimiter}
-                      />
+                      <>
+                        <EuiSpacer size="s" />
+                        <DelimiterSelect
+                          onDelimiterChange={onDelimiterChange}
+                          initialDelimiter={delimiter}
+                        />
+                      </>
+                    )}
+
+                    {importType === IMPORT_CHOICE_FILE && (
+                      <>
+                        <EuiSpacer size="s" />
+                        <ImportFileContentBody
+                          enabledFileTypes={config.enabledFileTypes}
+                          onFileUpdate={onFileInput}
+                        />
+                      </>
                     )}
                     {importType === IMPORT_CHOICE_FILE && (
-                      <ImportFileContentBody
-                        enabledFileTypes={config.enabledFileTypes}
-                        onFileUpdate={onFileInput}
-                      />
-                    )}
-                    {importType === IMPORT_CHOICE_FILE && (
-                      <EuiButton fullWidth={true} isDisabled={disableImport} onClick={previewData}>
-                        Preview
-                      </EuiButton>
+                      <>
+                        <EuiSpacer size="s" />
+                        <EuiButton
+                          fullWidth={true}
+                          isDisabled={disableImport}
+                          onClick={previewData}
+                        >
+                          {i18n.translate('dataImporter.previewButton', {
+                            defaultMessage: 'Preview',
+                          })}
+                        </EuiButton>
+                      </>
                     )}
                     <EuiSpacer size="s" />
                     <EuiButton fullWidth={true} isDisabled={disableImport} onClick={importData}>
-                      Import
+                      {i18n.translate('dataImporter.importButton', {
+                        defaultMessage: 'Import',
+                      })}
                     </EuiButton>
                   </EuiFlexItem>
                   <EuiFlexItem grow={2}>
