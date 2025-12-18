f6461730ce6163142ece16a09762a9530952a757
Adam Tackett <tackadam@amazon.com>
2025-12-17 17:16:35 -0800

    test
    Signed-off-by: Adam Tackett <tackadam@amazon.com>

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/04/01_traces.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/04/01_traces.spec.js
@@ -119,7 +119,8 @@ const traceTestSuite = () => {
         page: 'explore/traces',
         isEnhancement: true,
       });
-      cy.get('[data-test-subj="globalLoadingIndicator"]').should('not.exist');
+      cy.wait(5000);
+      //cy.get('[data-test-subj="globalLoadingIndicator"]').should('not.exist');
       cy.explore.setTopNavDate('Aug 1, 2025 @ 00:00:00.000', 'Sep 25, 2025 @ 00:00:00.000');
     });
 
