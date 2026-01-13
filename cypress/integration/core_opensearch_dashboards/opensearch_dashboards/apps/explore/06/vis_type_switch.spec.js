a0b55c163ca33abde8ab4f1e33728aa27e31f64b
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2026-01-13 14:56:06 -0800

    Improve the test utils for Explore/Enhancements (#11138)
    * Improve the test utils for Explore
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Fix Enhancements Test
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Address bugs in the test
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Address the Comments
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    
    * Changeset file for PR #11138 created/updated
    
    ---------
    
    Signed-off-by: Suchit Sahoo <suchsah@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/06/vis_type_switch.spec.js
+++ cypress/integration/core_opensearch_dashboards/opensearch_dashboards/apps/explore/06/vis_type_switch.spec.js
@@ -3,14 +3,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { INDEX_WITH_TIME_1, DATASOURCE_NAME } from '../../../../../../utils/apps/explore/constants';
+import { INDEX_WITH_TIME_1, DATASOURCE_NAME } from '../../../../../../utils/apps/constants';
 import {
   getRandomizedWorkspaceName,
   setDatePickerDatesAndSearchIfRelevant,
+  getRandomizedDatasetId,
 } from '../../../../../../utils/apps/explore/shared';
-import { prepareTestSuite } from '../../../../../../utils/helpers';
+import {
+  prepareTestSuite,
+  createWorkspaceAndDatasetUsingEndpoint,
+} from '../../../../../../utils/helpers';
 
 const workspaceName = getRandomizedWorkspaceName();
+const datasetId = getRandomizedDatasetId();
 
 export const runVisTypeSwitchTests = () => {
   describe('switch visualization type tests', () => {
@@ -53,14 +58,19 @@ export const runVisTypeSwitchTests = () => {
       cy.get('body').click(0, 0);
     };
     before(() => {
-      cy.osd.setupWorkspaceAndDataSourceWithIndices(workspaceName, [INDEX_WITH_TIME_1]);
-      cy.explore.createWorkspaceDataSets({
-        workspaceName: workspaceName,
-        indexPattern: INDEX_WITH_TIME_1,
-        timefieldName: 'timestamp',
-        dataSource: DATASOURCE_NAME,
-        isEnhancement: true,
-      });
+      cy.osd.setupEnvAndGetDataSource(DATASOURCE_NAME);
+
+      // Create workspace and dataset using our new helper function
+      createWorkspaceAndDatasetUsingEndpoint(
+        DATASOURCE_NAME,
+        workspaceName,
+        datasetId,
+        `${INDEX_WITH_TIME_1}*`,
+        'timestamp', // timestampField
+        'logs', // signalType
+        ['use-case-observability'] // features
+      );
+
       cy.osd.navigateToWorkSpaceSpecificPage({
         workspaceName: workspaceName,
         page: 'explore/logs',
