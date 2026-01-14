02f3d3123956a082e53ca59d655b5613c3367bbb
Anan Zhuang <ananzh@amazon.com>
2026-01-14 09:35:04 -0800

    core package updates
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/dev/jest/setup/enzyme.js
+++ src/dev/jest/setup/enzyme.js
@@ -29,6 +29,6 @@
  */
 
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
