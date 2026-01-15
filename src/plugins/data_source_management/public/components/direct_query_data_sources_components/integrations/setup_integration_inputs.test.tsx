ee73df903db14facdaa8003afce7d9d379802c6c
Anan Zhuang <ananzh@amazon.com>
2026-01-14 23:53:15 -0800

    fix tests
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- src/plugins/data_source_management/public/components/direct_query_data_sources_components/integrations/setup_integration_inputs.test.tsx
+++ src/plugins/data_source_management/public/components/direct_query_data_sources_components/integrations/setup_integration_inputs.test.tsx
@@ -4,8 +4,7 @@
  */
 
 import { configure, mount, shallow } from 'enzyme';
-// @ts-expect-error TS7016 TODO(ts-error): fixme
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import React from 'react';
 import { waitFor } from '@testing-library/react';
 import {
