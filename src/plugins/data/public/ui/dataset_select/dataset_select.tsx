51ba21b1f03bbe496eed1fa5132c73f9945fd2a5
Adam Tackett <105462877+TackAdam@users.noreply.github.com>
2026-01-21 11:31:06 -0800

    Traces: Auto detect bug (#11191)
    * disable auto detect in logs
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * changelog
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * add empty state, make dataset default box prop pass
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    * fix test
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    
    ---------
    
    Signed-off-by: Adam Tackett <tackadam@amazon.com>
    Co-authored-by: Adam Tackett <tackadam@amazon.com>

--- src/plugins/data/public/ui/dataset_select/dataset_select.tsx
+++ src/plugins/data/public/ui/dataset_select/dataset_select.tsx
@@ -735,6 +735,7 @@ const DatasetSelect: React.FC<DatasetSelectProps> = ({ onSelect, supportedTypes,
                   toMountPoint(
                     <AdvancedSelector
                       useConfiguratorV2
+                      alwaysShowDatasetFields
                       signalType={signalType || undefined}
                       services={services}
                       onSelect={async (query: Partial<Query>, saveDataset) => {
