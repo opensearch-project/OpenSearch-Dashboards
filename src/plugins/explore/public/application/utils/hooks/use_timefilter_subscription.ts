96141e192bddec3e4e6982c54c54a203cce593a7
SuZhou-Joe <suzhou@amazon.com>
2026-02-12 11:48:49 +0800

    feat: subscribe to time range change
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>

--- src/plugins/explore/public/application/utils/hooks/use_timefilter_subscription.ts
+++ src/plugins/explore/public/application/utils/hooks/use_timefilter_subscription.ts
@@ -5,13 +5,15 @@
 
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { merge } from 'rxjs';
+import { AnyAction } from 'redux';
 import { ExploreServices } from '../../../types';
 import { RootState } from '../state_management/store';
 import { executeQueries } from '../state_management/actions/query_actions';
 import { clearQueryStatusMap, clearResults } from '../state_management/slices';
 
 /**
- * Hook to handle auto-refresh subscription only
+ * Hook to handle timefilter subscription for auto-refresh and time range changes
  */
 export const useTimefilterSubscription = (services: ExploreServices) => {
   const dispatch = useDispatch();
@@ -20,15 +22,17 @@ export const useTimefilterSubscription = (services: ExploreServices) => {
   useEffect(() => {
     if (!services?.data?.query?.timefilter?.timefilter) return;
 
-    const subscription = services.data.query.timefilter.timefilter
-      .getAutoRefreshFetch$()
-      .subscribe(() => {
-        if (query.dataset) {
-          dispatch(clearResults());
-          dispatch(clearQueryStatusMap());
-          dispatch(executeQueries({ services }) as unknown);
-        }
-      });
+    const timefilter = services.data.query.timefilter.timefilter;
+    const subscription = merge(
+      timefilter.getTimeUpdate$(),
+      timefilter.getAutoRefreshFetch$()
+    ).subscribe(() => {
+      if (query.dataset) {
+        dispatch(clearResults());
+        dispatch(clearQueryStatusMap());
+        dispatch((executeQueries({ services }) as unknown) as AnyAction);
+      }
+    });
 
     return () => {
       subscription.unsubscribe();
