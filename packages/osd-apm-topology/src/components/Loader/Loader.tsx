9502000234e4e504f8dbb02829f5d599458f1aa8
ps48 <pshenoy36@gmail.com>
2026-02-26 11:03:04 -0800

    remove the pascal files from git index
    Signed-off-by: ps48 <pshenoy36@gmail.com>

--- packages/osd-apm-topology/src/components/Loader/Loader.tsx
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-import React from 'react';
-
-export const Loader = () => (
-  <span className="osd:w-24 osd:h-24 osd:border-8 osd:border-spinner osd:rounded-full osd:animate-spin osd:border-b-transparent" />
-);
