970890f0ebf214b47986d547956c8b361deae6c8
sumukhswamy <sumukhhs@amazon.com>
2026-03-03 21:41:51 -0800

    CVE vega changes
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- src/plugins/vis_type_vega/public/lib/vega.js
+++ src/plugins/vis_type_vega/public/lib/vega.js
@@ -28,9 +28,11 @@
  * under the License.
  */
 
-// Build vega-lite from source for es5 compatibility
-import { compile, version } from 'vega-lite-next/src';
-import * as vega from 'vega/build-es5/vega';
+/* eslint-disable import/namespace */
+
+// vega-lite 6.x no longer exports from /src subpath
+import { compile, version } from 'vega-lite/build/index.js';
+import * as vega from 'vega/build/vega.module.js';
 import { expressionInterpreter as vegaExpressionInterpreter } from 'vega-interpreter/build/vega-interpreter.module';
 
 const vegaLite = { compile, version };
