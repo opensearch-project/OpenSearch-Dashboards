59335a8bd5ead9e6624fcb6a9c91c48c1c6734a3
Yulong Ruan <ruanyl@amazon.com>
2026-03-31 15:11:54 +0800

    fix: vis editor empty state (#11613)
    * fix: visualization editor should not use empty state components from
    legacy discover app
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    Co-authored-by: opensearch-changeset-bot[bot] <154024398+opensearch-changeset-bot[bot]@users.noreply.github.com>

--- src/plugins/explore/public/application/in_context_vis_editor/component/in_context_bottom_left_container.tsx
+++ src/plugins/explore/public/application/in_context_vis_editor/component/in_context_bottom_left_container.tsx
@@ -14,12 +14,10 @@ import {
   EuiIcon,
 } from '@elastic/eui';
 import { TimeRange } from 'src/plugins/data/common';
-import { useOpenSearchDashboards } from '../../../../../opensearch_dashboards_react/public';
-import { ExploreServices } from '../../../types';
 import { QueryExecutionStatus } from '../../utils/state_management/types';
-import { DiscoverNoResults } from '../../../application/legacy/discover/application/components/no_results/no_results';
-import { DiscoverUninitialized } from '../../../application/legacy/discover/application/components/uninitialized/uninitialized';
-import { LoadingSpinner } from '../../../application/legacy/discover/application/components/loading_spinner/loading_spinner';
+import { VisEditorUninitialized } from './vis_editor_uninitialized';
+import { VisEditorNoResults } from './vis_editor_no_results';
+import { VisEditorLoadingState } from './vis_editor_loading_state';
 import { useSearchContext } from '../../../components/query_panel/utils/use_search_context';
 import { QueryPanel } from './in_context_query_panel';
 import { useQueryBuilderState } from '../hooks/use_query_builder_state';
@@ -39,28 +37,15 @@ const typeText = i18n.translate('explore.errorPanel.type', {
 });
 
 export const ResizableQueryPanelAndVisualization = () => {
-  const { services } = useOpenSearchDashboards<ExploreServices>();
-
   const { queryBuilder, queryEditorState } = useQueryBuilderState();
 
   const queryStatus = queryEditorState.queryStatus;
-
-  const dataview = queryBuilder.getDataView();
-
-  const onRefresh = () => {
-    queryBuilder.executeQuery();
-  };
 
   const renderVis = () => {
     if (queryStatus.status === QueryExecutionStatus.NO_RESULTS) {
       return (
         <EditorPanel>
-          <DiscoverNoResults
-            queryString={services?.data?.query?.queryString}
-            query={services?.data?.query?.queryString?.getQuery()}
-            savedQuery={services?.data?.query?.savedQueries}
-            timeFieldName={dataview?.timeFieldName}
-          />
+          <VisEditorNoResults />
         </EditorPanel>
       );
     }
@@ -68,9 +53,7 @@ export const ResizableQueryPanelAndVisualization = () => {
     if (queryStatus.status === QueryExecutionStatus.UNINITIALIZED) {
       return (
         <EditorPanel>
-          <div style={{ height: '100%' }}>
-            <DiscoverUninitialized onRefresh={onRefresh} />
-          </div>
+          <VisEditorUninitialized />
         </EditorPanel>
       );
     }
@@ -78,7 +61,7 @@ export const ResizableQueryPanelAndVisualization = () => {
     if (queryStatus.status === QueryExecutionStatus.LOADING) {
       return (
         <EditorPanel>
-          <LoadingSpinner />
+          <VisEditorLoadingState />
         </EditorPanel>
       );
     }
