441e104d5947f30b1e2c49a4454eed4e5fe89e7b
Anan Zhuang <ananzh@amazon.com>
2026-04-03 16:57:53 -0700

    Remove changelog infrastructure in favor of centralized release notes automation (#11563)
    * Remove changelog infrastructure in favor of centralized release notes automation
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    
    * More cleans
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    
    ---------
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>

--- scripts/generate_release_note.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-require('../src/setup_node_env');
-require('../src/dev/generate_release_note');
-require('../src/dev/generate_release_note_helper');
