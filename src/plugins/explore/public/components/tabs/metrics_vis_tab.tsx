9522c7baf4dd6e6c98baade189b173563b686790
Joshua Li <joshuali925@gmail.com>
2026-04-16 19:26:26 +0000

    avoid remounting in MetricsVisTab
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- src/plugins/explore/public/components/tabs/metrics_vis_tab.tsx
+++ src/plugins/explore/public/components/tabs/metrics_vis_tab.tsx
@@ -5,20 +5,12 @@
 
 import './metrics_vis_tab.scss';
 
-import { useRef, useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useObservable } from 'react-use';
 import { useSelector } from 'react-redux';
 import { createPortal } from 'react-dom';
 import { i18n } from '@osd/i18n';
-import {
-  EuiButtonGroup,
-  EuiButtonIcon,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiResizableContainer,
-  EuiTitle,
-} from '@elastic/eui';
-import { PanelDirection } from '@elastic/eui/src/components/resizable_container/types';
+import { EuiButtonGroup, EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiTitle } from '@elastic/eui';
 
 import { VisualizationContainer } from '../visualizations/visualization_container';
 import { ExploreMetricsRawTable } from '../data_table/explore_metrics_raw_table';
@@ -43,18 +35,12 @@ export const MetricsVisTab = () => {
   const data = useObservable(visualizationBuilder.data$);
   const visConfig = useObservable(visualizationBuilder.visConfig$);
   const query = useSelector((state: RootState) => state.query);
-  const collapseFn = useRef((id: string, direction: PanelDirection) => {});
 
   useEffect(() => {
     setSlot(document.getElementById(EXPLORE_ACTION_BAR_SLOT_ID));
   }, []);
 
   const showSettings = Boolean(data) && !shouldSkipQueryExecution(query);
-
-  const onChange = (panelId: string) => {
-    collapseFn.current(panelId, 'right');
-    setIsSettingsCollapsed((prev) => !prev);
-  };
 
   const onToggleCollapsed = () => {
     setIsSettingsCollapsed((prev) => !prev);
@@ -86,104 +72,68 @@ export const MetricsVisTab = () => {
     </div>
   );
 
+  const settingsPanel = showSettings && !isSettingsCollapsed && (
+    <div className="visStylePanelOuter metricsVisTab__settingsPanel">
+      <div className="visStylePanelInner">
+        <EuiFlexGroup
+          className="visStylePanelTitle"
+          gutterSize="none"
+          justifyContent="spaceBetween"
+          alignItems="center"
+        >
+          <EuiFlexItem>
+            <EuiTitle size="xxs">
+              <p>
+                {i18n.translate('explore.visualization.stylePanel.title', {
+                  defaultMessage: 'Settings',
+                })}
+              </p>
+            </EuiTitle>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButtonIcon
+              color="text"
+              aria-label={i18n.translate('explore.visualization.stylePanel.toggleAriaLabel', {
+                defaultMessage: 'Toggle visualization style panel',
+              })}
+              iconType="menuRight"
+              onClick={onToggleCollapsed}
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        {visualizationBuilder.renderStylePanel({ className: 'visStylePanelBody' })}
+      </div>
+    </div>
+  );
+
   return (
     <div className="metricsVisTab">
       {slot && createPortal(<ActionBar />, slot)}
       <div className="metricsVisTab__chartArea">
-        {showSettings ? (
-          isSettingsCollapsed ? (
-            <div className="metricsVisTab__collapsedLayout">
-              <div className="metricsVisTab__collapsedChart">
-                {chartTypeToggle}
-                <VisualizationContainer />
-              </div>
-              <button
-                className="metricsVisTab__expandButton"
-                onClick={onExpandSettings}
+        <div className="metricsVisTab__collapsedLayout">
+          <div className="metricsVisTab__collapsedChart">
+            {chartTypeToggle}
+            <VisualizationContainer />
+          </div>
+          {showSettings && isSettingsCollapsed && (
+            <button
+              className="metricsVisTab__expandButton"
+              onClick={onExpandSettings}
+              aria-label={i18n.translate('explore.visualization.stylePanel.expandAriaLabel', {
+                defaultMessage: 'Expand visualization settings panel',
+              })}
+            >
+              <EuiButtonIcon
+                color="text"
+                iconType="menuLeft"
                 aria-label={i18n.translate('explore.visualization.stylePanel.expandAriaLabel', {
                   defaultMessage: 'Expand visualization settings panel',
                 })}
-              >
-                <EuiButtonIcon
-                  color="text"
-                  iconType="menuLeft"
-                  aria-label={i18n.translate('explore.visualization.stylePanel.expandAriaLabel', {
-                    defaultMessage: 'Expand visualization settings panel',
-                  })}
-                />
-              </button>
-            </div>
-          ) : (
-            <EuiResizableContainer
-              className="metricsVisTab__resizable"
-              onToggleCollapsed={onToggleCollapsed}
-            >
-              {(EuiResizablePanel, EuiResizableButton, { togglePanel }) => {
-                collapseFn.current = (id, direction: PanelDirection = 'left') =>
-                  togglePanel?.(id, { direction });
-                return (
-                  <>
-                    <EuiResizablePanel
-                      id="metrics_vis_chart"
-                      initialSize={77.5}
-                      paddingSize="none"
-                      style={{ display: 'flex', flexDirection: 'column' }}
-                    >
-                      {chartTypeToggle}
-                      <VisualizationContainer />
-                    </EuiResizablePanel>
-
-                    <EuiResizableButton />
-
-                    <EuiResizablePanel
-                      mode={['custom', { position: 'top' }]}
-                      id="metrics_vis_settings"
-                      className="visStylePanelOuter"
-                      initialSize={22.5}
-                      minSize="280px"
-                      paddingSize="none"
-                    >
-                      <div className="visStylePanelInner">
-                        <EuiFlexGroup
-                          className="visStylePanelTitle"
-                          gutterSize="none"
-                          justifyContent="spaceBetween"
-                          alignItems="center"
-                        >
-                          <EuiFlexItem>
-                            <EuiTitle size="xxs">
-                              <p>
-                                {i18n.translate('explore.visualization.stylePanel.title', {
-                                  defaultMessage: 'Settings',
-                                })}
-                              </p>
-                            </EuiTitle>
-                          </EuiFlexItem>
-                          <EuiFlexItem grow={false}>
-                            <EuiButtonIcon
-                              color="text"
-                              aria-label={i18n.translate(
-                                'explore.visualization.stylePanel.toggleAriaLabel',
-                                {
-                                  defaultMessage: 'Toggle visualization style panel',
-                                }
-                              )}
-                              iconType="menuRight"
-                              onClick={() => onChange('metrics_vis_settings')}
-                            />
-                          </EuiFlexItem>
-                        </EuiFlexGroup>
-                        {visualizationBuilder.renderStylePanel({ className: 'visStylePanelBody' })}
-                      </div>
-                    </EuiResizablePanel>
-                  </>
-                );
-              }}
-            </EuiResizableContainer>
-          )
-        ) : (
-          <VisualizationContainer />
-        )}
+              />
+            </button>
+          )}
+          {settingsPanel}
+        </div>
       </div>
       <div className="metricsVisTab__rawTable">
         <ExploreMetricsRawTable />
