d3f3e9585766a82c535a80f265bad0cbd5345cf8
Qxisylolo <qianxisy@amazon.com>
2026-05-28 15:52:21 +0800

    update panel style
    Signed-off-by: Qxisylolo <qianxisy@amazon.com>

--- src/plugins/explore/public/application/in_context_vis_editor/component/save_vis_button.test.tsx
+++ src/plugins/explore/public/application/in_context_vis_editor/component/save_vis_button.test.tsx
@@ -10,7 +10,7 @@ import { BehaviorSubject } from 'rxjs';
 import { EditorMode } from '../../utils/state_management/types';
 import { useQueryBuilderState } from '../hooks/use_query_builder_state';
 import { useVisualizationBuilder } from '../hooks/use_visualization_builder';
-import { useCurrentExploreId } from '../hooks/use_explore_id';
+import { useCurrentExploreId } from '../../../application/utils/hooks/use_current_explore_id';
 import { useSavedExplore } from '../../utils/hooks/use_saved_explore';
 import { useOpenSearchDashboards } from '../../../../../opensearch_dashboards_react/public';
 
@@ -18,7 +18,9 @@ jest.mock('../hooks/use_query_builder_state', () => ({
   useQueryBuilderState: jest.fn(),
 }));
 jest.mock('../hooks/use_visualization_builder', () => ({ useVisualizationBuilder: jest.fn() }));
-jest.mock('../hooks/use_explore_id', () => ({ useCurrentExploreId: jest.fn() }));
+jest.mock('../../../application/utils/hooks/use_current_explore_id', () => ({
+  useCurrentExploreId: jest.fn(),
+}));
 jest.mock('../../utils/hooks/use_saved_explore', () => ({ useSavedExplore: jest.fn() }));
 jest.mock('../../../components/query_panel/utils/use_search_context', () => ({
   useSearchContext: jest.fn().mockReturnValue({ query: {}, filters: [] }),
