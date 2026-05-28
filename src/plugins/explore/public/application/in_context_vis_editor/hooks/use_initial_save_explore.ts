01950c7e868039c22d6330469baa04ca3b1ffd7b
Qxisylolo <qianxisy@amazon.com>
2026-05-28 17:20:03 +0800

    update data transform panel style (#12101)
    Signed-off-by: Qxisylolo <qianxisy@amazon.com>

--- src/plugins/explore/public/application/in_context_vis_editor/hooks/use_initial_save_explore.ts
+++ src/plugins/explore/public/application/in_context_vis_editor/hooks/use_initial_save_explore.ts
@@ -5,11 +5,11 @@
 
 import { useMemo } from 'react';
 import { useSavedExplore } from '../../../application/utils/hooks/use_saved_explore';
-import { useCurrentExploreId } from '../hooks/use_explore_id';
+import { useCurrentExploreId } from '../../../application/utils/hooks/use_current_explore_id';
 import { QueryState } from '../query_builder/query_builder';
 
 export const useInitialSaveExplore = () => {
-  const exploreId = useCurrentExploreId();
+  const exploreId = useCurrentExploreId('edit');
   const { savedExplore, error, isLoading } = useSavedExplore(exploreId);
 
   // parse savedQueryState and visconfig from saved explore
