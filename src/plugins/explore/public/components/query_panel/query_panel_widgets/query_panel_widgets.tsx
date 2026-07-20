476a187667fd15ad818cbcbad3b102ec8916db46
Krish Gandhi <kjg2352@gmail.com>
2026-07-20 12:41:42 -0700

    Updated gating logic for when button appears
    Signed-off-by: Krish Gandhi <kjg2352@gmail.com>

--- src/plugins/explore/public/components/query_panel/query_panel_widgets/query_panel_widgets.tsx
+++ src/plugins/explore/public/components/query_panel/query_panel_widgets/query_panel_widgets.tsx
@@ -5,13 +5,13 @@
 
 import React from 'react';
 import { EuiButtonEmpty } from '@elastic/eui';
+import { useSelector } from 'react-redux';
 import { DatasetSelectWidget } from './dataset_select';
 import { SaveQueryButton } from './save_query';
 import { RecentQueriesButton } from './recent_queries_button';
 import { LanguageToggle } from './language_toggle';
 import { QueryPanelActions } from './query_panel_actions';
 import { ExploreServices } from '../../../types';
-import { useSelector } from 'react-redux';
 import { useOpenSearchDashboards } from '../../../../../opensearch_dashboards_react/public';
 // @ts-ignore - allow side-effect scss import without type declarations
 import './query_panel_widgets.scss';
